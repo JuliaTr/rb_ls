0ade22e40526fed4921edf53d560a66387d0dd31
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-29 14:05:17 -0500

    Add pseudo_code_sum_of_integers.rb

---
+++ lesson_2/pseudo_code_sum_of_integers.rb
@@ -0,0 +1,10 @@
+### a method that returns the sum of two integers
+
+=begin
+Given two integers.
+
+Within the method:
+  -save the sum of adding the first value to the second value.
+
+Print the returned saved value of the sum by the method call.
+=end
