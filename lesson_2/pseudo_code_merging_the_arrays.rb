d12ade35f65a6b02239ce23acb6ccb95eb34d7a4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-29 15:15:57 -0500

    Add pseudo_code_merging_the_arrays.rb

---
+++ lesson_2/pseudo_code_merging_the_arrays.rb
@@ -0,0 +1,13 @@
+### a method that takes two arrays of numbers and returns the result of merging the arrays
+
+=begin
+Given two collections of integers.
+
+Iterate through the first collection one by one:
+  - save the first value as the starting value;
+  - for each iteration, add each element of the second array to the next element to the first array.
+
+After iterating through the collection, return the result of merging the arrays.
+=end
+
+
