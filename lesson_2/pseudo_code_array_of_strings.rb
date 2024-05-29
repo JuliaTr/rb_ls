be74930c80c2f9fd13d8d04b8887392b781b647c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-29 14:16:17 -0500

    Add pseudo_code_array_of_strings.rb

---
+++ lesson_2/pseudo_code_array_of_strings.rb
@@ -0,0 +1,12 @@
+### a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+=begin
+Given an array of strings.
+
+Iterate through the array of strings one by one:
+  - save the first value as th starting value;
+  - for each iteration, add the next value to the current value;
+  - reassign the saved value as the current value.
+
+After iterating through the collection, return the saved value.
+=end
