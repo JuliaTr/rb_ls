792347d14b01d674f066d282ecdc4d88d7b8f8b0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-30 13:52:54 -0500

    Refactor prompt message

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -2,16 +2,19 @@
 # ask the user for the operation to perform
 # perform the opertion on the two numbers
 # output the result
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-Kernel.puts("Welcome to Calculator!")
+prompt("Welcome to Calculator!")
 
-Kernel.puts("What's the first number?")
+prompt("What's the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number?")
+prompt("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
 if operator == '1' 
@@ -24,4 +27,4 @@ else
   result = number1.to_f() / number2.to_f()
 end
 
-Kernel.puts("The result is #{result}")
+prompt("The result is #{result}")
