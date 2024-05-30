5e23b0dba3240bf350592704e55c0060df0f53fd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-30 14:22:44 -0500

    Add loops and a method to check a validity of numbers typed by a user

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,13 +6,35 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  num.to_i() != 0
+end
+
 prompt("Welcome to Calculator!")
 
-prompt("What's the first number?")
-number1 = Kernel.gets().chomp()
+number1 = ''
+loop do
+  prompt("What's the first number?")
+  number1 = Kernel.gets().chomp()
 
-prompt("What's the second number?")
-number2 = Kernel.gets().chomp()
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+end
+
+number2 = ''
+loop do
+  prompt("What's the second number?")
+  number2 = Kernel.gets().chomp()
+
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+end
 
 prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
