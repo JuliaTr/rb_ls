581d8a1559f92752d43c6d76524d94b00b672439
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-05-31 14:13:41 -0500

    Add improvements to the code with Rubocop inspection

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -65,14 +65,14 @@ loop do # main loop
 
   operator_prompt = <<-MSG
     What operation would you like to perform?
-    1) add 
-    2) subtract 
-    3) multiply 
+    1) add
+    2) subtract
+    3) multiply
     4) divide
   MSG
 
   prompt(operator_prompt)
-  
+
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
@@ -87,15 +87,15 @@ loop do # main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-          when '1'
-            number1.to_i() + number2.to_i()
-          when '2'
-            number1.to_i() - number2.to_i()
-          when '3'
-            number1.to_i() * number2.to_i()
-          when '4'
-            number1.to_f() / number2.to_f()
-  end
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
