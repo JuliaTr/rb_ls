08dd527c02c3ded780b09176b302a2fc0da9eef1
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-06-23 16:37:04 -0500

    Change return value of the result to float

--- lesson_2/calculator/calculator_bonus_features_improved.rb
+++ lesson_2/calculator/calculator_bonus_features_improved.rb
@@ -105,11 +105,11 @@ loop do
 
   result = case operator
            when '1'
-             number1.to_i() + number2.to_i()
+             number1.to_f() + number2.to_f()
            when '2'
-             number1.to_i() - number2.to_i()
+             number1.to_f() - number2.to_f()
            when '3'
-             number1.to_i() * number2.to_i()
+             number1.to_f() * number2.to_f()
            when '4'
              number1.to_f() / number2.to_f()
            end
