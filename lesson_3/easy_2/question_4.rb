d1e3efcdf98db61c177705a1d7ca06483be8703a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-07-31 20:28:17 -0500

    Add question_4.rb

---
+++ lesson_3/easy_2/question_4.rb
@@ -0,0 +1,11 @@
+### Does "dino" appear in the string below?
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+p advice.match?("dino")        # true
+
+
+## Experiments:
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+p advice.include?('dino')      # true
