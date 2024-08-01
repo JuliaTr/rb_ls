f2eb7edcbd3b40e66acc8c847da2cb4f3036a989
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-01 15:19:59 -0500

    Add medium_1/question_1.rb

---
+++ lesson_3/medium_1/question_1.rb
@@ -0,0 +1,3 @@
+# Write a one-line program that creates the same output 10 times with the subsequent line indented 1 space to the right.
+
+10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
