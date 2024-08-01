21300081e341f0219c4dbdc3c8d3d0cf29efa3d5
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-01 13:14:09 -0500

    Improve the answer

--- lesson_3/easy_2/question_7.rb
+++ lesson_3/easy_2/question_7.rb
@@ -1,14 +1,24 @@
 ### Add multiple items to the array (Dino and Hoppy):
 
 ## 'Array#concat'
-flintstones = %w(Fres Barney Wilma Betty BamBam Pebbles)
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 flintstones.concat(['Dino', 'Hoppy'])
 p flintstones
-# ["Fres", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
+# ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.concat(%w(Dino Hoppy))
+p flintstones
+# ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
+
 
 ## 'Array#push'
-flintstones = %w(Fres Barney Wilma Betty BamBam Pebbles)
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 flintstones.push('Dino', 'Hoppy')
 p flintstones
-# ["Fres", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
+# ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
 
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push('Dino').push('Hoppy')
+p flintstones
+# ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles", "Dino", "Hoppy"]
