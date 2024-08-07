d301f9201abb310c0e69fae67e9cfc94f304ad4b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-07 14:46:34 -0500

    Add hard_1/question_1.rb

---
+++ lesson_3/hard_1/question_1.rb
@@ -0,0 +1,18 @@
+# What is the output?
+
+if false
+  greeting = "hello world"
+end
+
+greeting
+# The output is `nil`.
+
+=begin
+irb(main):001:1* if false
+irb(main):002:1*   greeting = "hello world"
+irb(main):003:0> end
+=> nil
+irb(main):004:0> greeting
+=> nil
+=end
+
