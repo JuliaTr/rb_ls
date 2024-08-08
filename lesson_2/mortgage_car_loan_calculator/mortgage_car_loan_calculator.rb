b22eabf99e4bac87f2bbb88828ef1a6b8eaafdc9
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-08 15:17:49 -0500

    Refactor the code

--- lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
@@ -4,9 +4,18 @@
 # puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-24)))   # 7247.0900752686985
 # puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-36)))   # 6043.445712047528
 
+LANGUAGE = 'en'
 
-def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
-  monthly_payment = loan_amount * (monthly_interest_rate.to_i.to_f / (1 - (1 + monthly_interest_rate.to_i.to_f) ** (-loan_duration_in_months.to_i.to_f)))
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_car_loan_calculator_messages.yml')
+
+def messages(message)
+  MESSAGES[LANGUAGE][message]
+end
+
+def prompt(key, *args)
+  message = messages(key) % args
+  Kernel.puts("=> #{message}")
 end
 
 def valid_number?(input)
@@ -21,16 +30,20 @@ def valid_float?(input)
   input.to_f() if input.to_f().to_s() == input
 end
 
+# def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
+#   monthly_payment = loan_amount * (monthly_interest_rate.to_i.to_f / (1 - (1 + monthly_interest_rate.to_i.to_f) ** (-loan_duration_in_months.to_i.to_f)))
+# end
+
 def get_loan
   loan_amount = ''
   converted_loan_amount = nil
   loop do
-    puts "What amount of loan do you need?"
+    prompt('loan_amount')
     loan_amount = Kernel.gets().chomp()
 
     converted_loan_amount = valid_number?(loan_amount)
     break if converted_loan_amount
-    puts "Please provide a valid amount of loan."
+    prompt('invalid_loan_amount')
   end
 
   converted_loan_amount
@@ -40,12 +53,12 @@ def get_interest_rate
   monthly_interest_rate = ''
   converted_monthly_interest_rate = nil
   loop do
-    puts "What monthly interest rate are you looking for?"
+    prompt('monthly_inerest_rate')
     monthly_interest_rate = Kernel.gets().chomp()
 
     converted_monthly_interest_rate = valid_number?(monthly_interest_rate)  
     break if converted_monthly_interest_rate
-    puts "Please provide a valid monthly interest rate."
+    prompt('invalid_monthly_inerest_rate')
   end
 
   converted_monthly_interest_rate
@@ -55,12 +68,12 @@ def get_loan_duration
   loan_duration_in_months = ''
   converted_loan_duration_in_months = nil
   loop do
-    puts "What is loan duration in months?"
+    prompt('loan_duration_in_monhs')
     loan_duration_in_months = Kernel.gets().chomp()
 
     converted_loan_duration_in_months = valid_number?(loan_duration_in_months)
     break if converted_loan_duration_in_months
-    puts "Please provide a valid loan duration in months."
+    prompt('invalid_loan_duration_in_months')
   end
 end
 
@@ -70,22 +83,21 @@ def get_name
     name = Kernel.gets().chomp()
 
     break unless name.empty?()
-    puts "Please enter your valid name."
+    prompt('invalid_name')
   end
 
   name.upcase()
 end
 
 # Greeting
-puts "Welcome in Mortgage / Car Loan Calculator! Enter your name:"
+prompt('welcome')
 name = get_name
-puts "Hi #{name}!"
+prompt('greeting', name)
 
 # Mortgage car / loan calculator starts
 amount_of_loan = get_loan()
 amount_of_monthly_interest_rate = get_interest_rate()
 amount_of_loan_duration_in_months = get_loan_duration()
+monthly_payment = how_much_to_pay(amount_of_loan, amount_of_monthly_interest_rate, amount_of_loan_duration_in_months)
 
-# how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
-
-# puts "Your monthly payment is #{how_much_to_pay}"
+puts "Your monthly payment is #{monthly}"
