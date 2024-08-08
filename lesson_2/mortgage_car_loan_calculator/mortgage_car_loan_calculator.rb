faf8bd0298616ad93a583345c24d4653c1f5e61c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-08 14:26:52 -0500

    Continue to work on the Mortgage car / loan calculator

--- lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
@@ -1,4 +1,5 @@
 # monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
+
 # puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-12)))   # 11282.541002081534
 # puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-24)))   # 7247.0900752686985
 # puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-36)))   # 6043.445712047528
@@ -8,78 +9,83 @@ def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
   monthly_payment = loan_amount * (monthly_interest_rate.to_i.to_f / (1 - (1 + monthly_interest_rate.to_i.to_f) ** (-loan_duration_in_months.to_i.to_f)))
 end
 
-def number?(input)
-  integer?(input) || float?(input)
+def valid_number?(input)
+  valid_integer?(input) || valid_float?(input)
 end
 
-def integer?(input)
-  input.to_i().to_s() == input
+def valid_integer?(input)
+  input.to_i() if input.to_i().to_s() == input
 end
 
-def float?(input)
-  /\d/.match(input) && /^-?\d*\.?\d*$/.match(input)
+def valid_float?(input)
+  input.to_f() if input.to_f().to_s() == input
 end
 
-
-# Name validation
-puts "Welcome in Mortgage / Car Loan Calculator! Enter your name:"
-
-name = ''
-loop do
-  name = Kernel.gets().chomp()
-
-  if name.empty?()
-    puts "Please enter your valid name."
-  else
-    break
-  end
-end
-
-puts "Hi #{name}!"
-
-# Amount of loan validation
-loan_amount = ''
+def get_loan
+  loan_amount = ''
+  converted_loan_amount = nil
   loop do
     puts "What amount of loan do you need?"
     loan_amount = Kernel.gets().chomp()
 
-    if number?(loan_amount)
-      break
-    else
-      puts "Please provide a valid amount of loan."
-    end
+    converted_loan_amount = valid_number?(loan_amount)
+    break if converted_loan_amount
+    puts "Please provide a valid amount of loan."
   end
 
-# Monthly interest rate validation
-monthly_interest_rate = ''
+  converted_loan_amount
+end
+
+def get_interest_rate
+  monthly_interest_rate = ''
+  converted_monthly_interest_rate = nil
   loop do
     puts "What monthly interest rate are you looking for?"
     monthly_interest_rate = Kernel.gets().chomp()
 
-    if number?(monthly_interest_rate)    # TO DO validation if monthly interest rate is 5%, not 0.05?
-      break
-    else
-      puts "Please provide a valid monthly interest rate."
-    end
+    converted_monthly_interest_rate = valid_number?(monthly_interest_rate)  
+    break if converted_monthly_interest_rate
+    puts "Please provide a valid monthly interest rate."
   end
 
-# puts monthly_interest_rate.class.inspect
+  converted_monthly_interest_rate
+end
 
-# puts "What is loan duration in months?"
-# loan_duration_in_months = gets.chomp
-
-loan_duration_in_months = ''
+def get_loan_duration
+  loan_duration_in_months = ''
+  converted_loan_duration_in_months = nil
   loop do
     puts "What is loan duration in months?"
     loan_duration_in_months = Kernel.gets().chomp()
 
-    if number?(loan_duration_in_months)    # TO DO validation if loan duration in months is in years?
-      break
-    else
-      puts "Please provide a valid loan duration in months."
-    end
+    converted_loan_duration_in_months = valid_number?(loan_duration_in_months)
+    break if converted_loan_duration_in_months
+    puts "Please provide a valid loan duration in months."
+  end
+end
+
+def get_name
+  name = ''
+  loop do
+    name = Kernel.gets().chomp()
+
+    break unless name.empty?()
+    puts "Please enter your valid name."
   end
 
-how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
+  name.upcase()
+end
 
-puts "Your monthly payment is #{how_much_to_pay}"
+# Greeting
+puts "Welcome in Mortgage / Car Loan Calculator! Enter your name:"
+name = get_name
+puts "Hi #{name}!"
+
+# Mortgage car / loan calculator starts
+amount_of_loan = get_loan()
+amount_of_monthly_interest_rate = get_interest_rate()
+amount_of_loan_duration_in_months = get_loan_duration()
+
+# how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
+
+# puts "Your monthly payment is #{how_much_to_pay}"
