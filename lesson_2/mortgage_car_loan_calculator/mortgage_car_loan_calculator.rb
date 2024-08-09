51de01c5e578fe57c493c14f0effeca18b8c28f4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-09 12:48:19 -0500

    Continue to work on Mortgage / car loan calculator

--- lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
@@ -1,9 +1,3 @@
-# monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
-
-# puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-12)))   # 11282.541002081534
-# puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-24)))   # 7247.0900752686985
-# puts monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-36)))   # 6043.445712047528
-
 LANGUAGE = 'en'
 
 require 'yaml'
@@ -28,11 +22,11 @@ end
 
 def valid_float?(input)
   input.to_f() if input.to_f().to_s() == input
-end
+end 
 
-# def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
-#   monthly_payment = loan_amount * (monthly_interest_rate.to_i.to_f / (1 - (1 + monthly_interest_rate.to_i.to_f) ** (-loan_duration_in_months.to_i.to_f)))
-# end
+def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
+  monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
+end
 
 def get_loan
   loan_amount = ''
@@ -41,7 +35,7 @@ def get_loan
     prompt('loan_amount')
     loan_amount = Kernel.gets().chomp()
 
-    converted_loan_amount = valid_number?(loan_amount)
+    converted_loan_amount = valid_integer?(loan_amount)
     break if converted_loan_amount
     prompt('invalid_loan_amount')
   end
@@ -53,28 +47,30 @@ def get_interest_rate
   monthly_interest_rate = ''
   converted_monthly_interest_rate = nil
   loop do
-    prompt('monthly_inerest_rate')
+    prompt('monthly_interest_rate')
     monthly_interest_rate = Kernel.gets().chomp()
 
     converted_monthly_interest_rate = valid_number?(monthly_interest_rate)  
     break if converted_monthly_interest_rate
-    prompt('invalid_monthly_inerest_rate')
+    prompt('invalid_monthly_interest_rate')
   end
 
-  converted_monthly_interest_rate
+  converted_monthly_interest_rate_in_percentage = converted_monthly_interest_rate / 100.0
 end
 
 def get_loan_duration
-  loan_duration_in_months = ''
-  converted_loan_duration_in_months = nil
+  loan_duration_in_years = ''
+  converted_loan_duration_in_years = nil
   loop do
-    prompt('loan_duration_in_monhs')
-    loan_duration_in_months = Kernel.gets().chomp()
+    prompt('loan_duration_in_years')
+    loan_duration_in_years = Kernel.gets().chomp()
 
-    converted_loan_duration_in_months = valid_number?(loan_duration_in_months)
-    break if converted_loan_duration_in_months
-    prompt('invalid_loan_duration_in_months')
+    converted_loan_duration_in_years = valid_number?(loan_duration_in_years)
+    break if converted_loan_duration_in_years
+    prompt('invalid_loan_duration_in_years')
   end
+
+  converted_loan_duration_in_months = converted_loan_duration_in_years * 12
 end
 
 def get_name
@@ -96,8 +92,39 @@ prompt('greeting', name)
 
 # Mortgage car / loan calculator starts
 amount_of_loan = get_loan()
-amount_of_monthly_interest_rate = get_interest_rate()
-amount_of_loan_duration_in_months = get_loan_duration()
-monthly_payment = how_much_to_pay(amount_of_loan, amount_of_monthly_interest_rate, amount_of_loan_duration_in_months)
+puts amount_of_loan
 
-puts "Your monthly payment is #{monthly}"
+amount_of_monthly_interest_rate = get_interest_rate()
+puts amount_of_monthly_interest_rate
+
+amount_of_loan_duration_in_months = get_loan_duration()
+puts amount_of_loan_duration_in_months
+
+monthly_payment = how_much_to_pay(amount_of_loan, amount_of_monthly_interest_rate, amount_of_loan_duration_in_months)
+puts monthly_payment
+
+# prompt('your_monthly_payment', monthly_payment}
+
+
+
+# TESTS:
+# loan_amount = 1000000 == true
+# loan_amount = 100 000 == false
+# loan_amount = 100,000 == false
+
+# monthly_interest_rate = 5 == true
+# monthly_interest_rate = 0.5 == true
+# monthly_interest_rate = 0.05 == true
+# monthly_interest_rate = .5 == false
+# monthly_interest_rate = 00000000000005 == false
+
+# loan_duration_in_months = 10 == true
+# loan_duration_in_months = 10.2 == true
+# loan_duration_in_months = 10,2 == false
+
+# monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
+
+# monthly_payment = 100_000 * (0.01 / (1 - (1 + 0.01) ** (-120)))  # 1434.7094840258733
+# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-12)))   # 11282.541002081534
+# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-24)))   # 7247.0900752686985
+# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-36)))   # 6043.445712047528
