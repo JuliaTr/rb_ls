25509bddb08c5cdec3f874fb6270997200892508
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-11 22:24:33 -0500

    Improve  and  methods

--- lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
@@ -21,7 +21,7 @@ def valid_integer?(input)
 end
 
 def valid_float?(input)
-  input.to_f() if input.to_f().to_s() == input
+  input.to_f().to_s() == input
 end 
 
 # def how_much_to_pay(loan_amount, monthly_interest_rate, loan_duration_in_months)
@@ -30,35 +30,33 @@ end
 #                     (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
 # end
 
-def get_loan
-  loan_amount = ''
-  converted_loan_amount = nil
-  loop do
-    prompt('loan_amount')
-    loan_amount = Kernel.gets().chomp().strip()
-
-    break unless loan_amount.empty?() || valid_integer?(loan_amount) == false || loan_amount.to_i() <= 0
-    prompt('invalid_loan_amount')
-  end
-
-  converted_loan_amount = loan_amount.to_i()
-end
-
-# def get_interest_rate
-#   interest_rate = ''
-#   converted_interest_rate = nil
+# def get_loan              # DONE
+#   loan_amount = ''
+#   converted_loan_amount = nil
 #   loop do
-#     prompt('interest_rate', 5, 1.2)
-#     interest_rate = Kernel.gets().chomp()
+#     prompt('loan_amount')
+#     loan_amount = Kernel.gets().chomp().strip()
 
-#     converted_interest_rate = valid_number?(interest_rate)  
-#     break if converted_interest_rate > 0
-#     prompt('invalid_interest_rate')
+#     break unless loan_amount.empty?() || valid_integer?(loan_amount) == false || loan_amount.to_i() <= 0
+#     prompt('invalid_loan_amount')
 #   end
 
-#   monthly_interest_rate = interest_rate / 12
-#   converted_interest_rate_in_percentage = monthly_interest_rate / 100.0
+#   converted_loan_amount = loan_amount.to_i()
 # end
+
+def get_interest_rate
+  interest_rate = ''
+  converted_interest_rate = nil
+  loop do
+    prompt('interest_rate', 5, 1.2)
+    interest_rate = Kernel.gets().chomp().strip()
+
+    break unless interest_rate.empty?() || valid_number?(interest_rate) == false || interest_rate.to_f() <= 0
+    prompt('invalid_interest_rate')
+  end
+
+  converted_interest_rate = interest_rate.to_f()
+end
 
 # def get_loan_duration
 #   loan_duration_in_years = ''
@@ -99,11 +97,18 @@ end
 # prompt('greeting', name)
 
 # Mortgage Car/Loan Calculator starts
-amount_of_loan = get_loan()
-p amount_of_loan
+# amount_of_loan = get_loan()            # DONE
+# p amount_of_loan
 
-# amount_of_monthly_interest_rate = get_interest_rate()
-# puts amount_of_monthly_interest_rate
+annual_interest_rate = get_interest_rate()
+puts annual_interest_rate
+
+converted_interest_rate_in_percentage = annual_interest_rate / 100.0
+puts converted_interest_rate_in_percentage
+
+monthly_interest_rate = converted_interest_rate_in_percentage / 12
+puts monthly_interest_rate
+
 
 # amount_of_loan_duration_in_months = get_loan_duration()
 # puts amount_of_loan_duration_in_months
@@ -124,41 +129,56 @@ p amount_of_loan
 ## INPUT USE CASES:
 # come in strings
 
+
+### LOAN AMOUNT
+
 # loan_amount = "1000000"         == true
-# loan_amount = "100 000"         == false
+# loan_amount = "1"               == true
+# loan_amount = "9    "           == true             # white space will be stripped
+# loan_amount = "    9"           == true             # white space will be stripped
+
+# loan_amount = "100 000"         == false           
 # loan_amount = "100,000"         == false
 # loan_amount = "0"               == false
-# loan_amount = "1"               == true
+# loan_amount = "-1"              == false            # any negative integer
+# loan_amount = "1.2"             == false            # any float
+
+# loan_amount = "$10000"          == true             # TO DO # (???) should be stripped
+# loan_amount = "$ 10000"         == true             # TO DO # (???) should be stripped
+
 # loam_amount = "     "           == false            # white space will be stripped  
 # loan_amount = ""                == false            # if empty
 # loan_amount = "n"               == false            # any letter
-# loan_amount = "9    "           == true             # white space will be stripped
-# loan_amount = "n    "           == false            
-# loan_amount = "    9"           == true             # white space will be stripped
-# loan_amount = "-1"              == false            # any negative integer
-# loan_amount = "1.2"             == false            # any float   
-# loan_amount = "!@#$%^&*())_+""  == false     # any symbol or symbol with integer/float/letter/white space
+# loan_amount = "n    "           == false             
+# loan_amount = "!@#$%^&*())_+""  == false            # any symbol or symbol with integer/float/letter/white space
 
 
 
-# monthly_interest_rate = 5 == true
-# monthly_interest_rate = 0.5 == true
-# monthly_interest_rate = 0.05 == true
-# monthly_interest_rate = .5 == false
-# monthly_interest_rate = 00000000000005 == false
-# monthly_interest_rate = 0 == false
-# monthly_interest_rate = 0.00001 == false
+### INTEREST RATE
+
+# interest_rate = "5"                == true
+# interest_rate = "0.5"              == true
+# interest_rate = "0.05"             == true          # (???)
+# interest_rate = "%5"               == true          # (???) should be stripped
+# interest_rate = "% 5"              == true          # (???) should be stripped
+
+# interest_rate = ".5"               == false
+# interest_rate = "00000000000005"   == false
+# interest_rate = "0"                == false
+# interest_rate = "0.00001"          == false
+
+# interest_rate = "     "            == false            # white space will be stripped
+# interest_rate = ""                 == false            # if empty
+# interest_rate = "n"                == false            # any letter
+# interest_rate = "n    "            == false            
+# interest_rate = "-1"               == false            # any negative integer
+# interest_rate = "!@#$%^&*())_+""   == false            # any symbol or symbol with integer/float/letter/white space
 
 
 # loan_duration_in_months = 10 == true
 # loan_duration_in_months = 10.2 == true
+
 # loan_duration_in_months = 10,2 == false
 
 # monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-loan_duration_in_months)))
 
-
-
-# monthly_payment = 100_000 * (0.01 / (1 - (1 + 0.01) ** (-120)))  # 1434.7094840258733
-# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-12)))   # 11282.541002081534
-# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-24)))   # 7247.0900752686985
-# monthly_payment = 100_000 * (0.05 / (1 - (1 + 0.05) ** (-36)))   # 6043.445712047528
