8fbaaa7b9dc5ca1e38ae807ccf42c29f9d73dde2
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-14 18:17:05 -0500

    Make changes in use cases

--- rb_101/lesson_2/mortgage_car_loan_calculator/mortgage_calculator_use_cases.rb
+++ rb_101/lesson_2/mortgage_car_loan_calculator/mortgage_calculator_use_cases.rb
@@ -49,9 +49,9 @@ loan_amount = "!@#$%^&*())_+""             == false            # any symbol or s
 interest_rate = "5"                         == true
 interest_rate = "0.5"                       == true
 interest_rate = "0.05"                      == true             
+interest_rate = "%5"                        == true          
+interest_rate = "% 5"                       == true          
 
-interest_rate = "%5"                        == false          
-interest_rate = "% 5"                       == false          
 interest_rate = ".5"                        == false
 interest_rate = "00000000000005"            == false
 interest_rate = "0"                         == false
