3fb31f22ed32f97952dd7fc9a889275c4e833d0b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-19 19:20:54 -0500

    Add included_modules.rb

---
+++ rb_101_rb_119_small_problems/documentation_again/included_modules.rb
@@ -0,0 +1,17 @@
+# Change the code to print the two smallest values in the `Array`.
+# version 2.3.0 of the Ruby Array Documentation
+
+a = [5, 9, 3, 11]
+puts a.min
+# Output:
+# 3
+# 5
+
+
+## Solution:
+
+a = [5, 9, 3, 11]
+puts a.min(2)
+# Output:
+# 3
+# 5
