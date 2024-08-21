6cf22001a7c466c4a75126040dc4d94a2e5ca736
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-20 21:07:44 -0500

    Add down_the_rabbit_hole.rb

---
+++ rb_101_rb_119_small_problems/documentation_again/down_the_rabbit_hole.rb
@@ -0,0 +1,6 @@
+# Find the documentation for `Yaml::load_file`.
+
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# https://docs.ruby-lang.org/en/3.2/Psych.html#module-Psych-label-Reading+from+a+file
