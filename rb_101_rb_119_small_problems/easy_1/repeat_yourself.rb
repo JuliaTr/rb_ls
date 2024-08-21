7a5db4d23ec213ba1e037dc7581f2b3b1b9ffc40
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-21 15:21:54 -0500

    Edit indentation in the description of PEDAC process

--- rb_101_rb_119_small_problems/easy_1/repeat_yourself.rb
+++ rb_101_rb_119_small_problems/easy_1/repeat_yourself.rb
@@ -21,9 +21,9 @@ PEDAC
   - Input: string and positive integer objects
   - Output: a string printed as many times as a positive integer
 
-- Rules:
-  - Explicit: print the string as many times as the integer 
-  - Implicit: output the strings on each line separately
+  - Rules:
+    - Explicit: print the string as many times as the integer 
+    - Implicit: output the strings on each line separately
 
 - Examples and Test Cases:
   - The only test case in the problem description
