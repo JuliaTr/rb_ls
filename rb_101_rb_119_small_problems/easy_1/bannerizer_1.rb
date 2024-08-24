9ee97a6136c09977bc54e9e1121759e362f5690e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-24 18:54:33 -0400

    Add bannerizer_1.rb

---
+++ rb_101_rb_119_small_problems/easy_1/bannerizer_1.rb
@@ -0,0 +1,26 @@
+## Further exploration
+
+# Modify this method so it will truncate the message if it will be too wide to fit inside a standard terminal window
+# (80 columns, including the sides of the box). For a real challenge, try word wrapping very long messages so they appear on
+# multiple lines, but still within a box.
+
+def print_in_box(string)
+  horizontal_rule = "+#{'-' * (string[0..75].size + 2)}+"
+  empty_line = "|#{' ' * (string[0..75].size + 2)}|"
+
+  puts horizontal_rule
+  puts empty_line
+  puts "| #{string[0..75]} |"
+  puts "| #{string[76..136]}#{' ' * (string[0..75].size - string[76..136].size)} |"
+  puts empty_line
+  puts horizontal_rule
+end
+
+print_in_box('To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before.')
+
+# +------------------------------------------------------------------------------+
+# |                                                                              |
+# | To boldly go where no one has gone before. To boldly go where no one has gon |
+# | e before. To boldly go where no one has gone before.                         |
+# |                                                                              |
+# +------------------------------------------------------------------------------+
