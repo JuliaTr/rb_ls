4bf1302586feed0389b1ec06ec489c40ee7b1a23
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-08-29 15:03:44 -0400

    Update the file

--- rb_101_rb_119_small_problems/easy_1/get_middle_character.rb
+++ rb_101_rb_119_small_problems/easy_1/get_middle_character.rb
@@ -45,16 +45,14 @@ Algorithm:
   - Define a method called `center_of` that takes one argument 'string'.                          # DONE
   - Convert string to array `array` (should return: array).                                       # DONE
   - Count the length of `array` by `array_length = array.length / 2` (should return: integer).    # DONE
+  - Return an integer by finding the middle of the array by
+    performing the operation `middle_of_string = array_length / 2` (should return: integer).    # DONE
   - If the `array` length has an odd number of characters:                                        # DONE
-    - Return an integer by finding the middle of the array by
-      performing the operation `middle_of_string = array_length / 2` (should return: integer).    # DONE
     - Return value of `middle_of_string`(should return: string):                                  # DONE
       An index will be the same number as `middle_of_string`, because array is 
       an indexed-based collection, which start with 0.
       Use `middle_of_string` as an index to retrieve the value.                                   
   - If `array_length` has an even number of characters:                                           # DONE
-    - Return an integer by finding the middle of the string by
-      performing the operation `middle_of_string = array.length / 2` (should return: integer).    # DONE
     - Return concatinated values of the 2 indexes (should return: string):                        # DONE
       - Use `middle_of_string - 1` as an index to retrieve the last value of the first substring.
       - Use `middle_of_string` as an index to retrieve the first value the last substring.
@@ -63,12 +61,11 @@ Algorithm:
 def center_of(string)
   array = string.chars
   array_length = array.length
+  middle_of_string = array_length / 2
 
-  if array_length.odd?
-    middle_of_string = array_length / 2
+  if array_length.odd?    
     array[middle_of_string]
   elsif array_length.even?
-    middle_of_string = array_length / 2
     array[middle_of_string - 1] + array[middle_of_string]
   end
 end
@@ -77,4 +74,22 @@ p center_of('I love Ruby') == 'e'  # odd; 11 characters; `"e"` is the 6th charac
 p center_of('Launch School') == ' '  # odd; 13 characters; `' '` is the 7th character
 p center_of('Launch') == 'un'
 p center_of('Launchschool') == 'hs'
-p center_of('x') == 'x'           # odd   
+p center_of('x') == 'x'           # odd  
+
+
+## Solution from the exercise:
+def center_of(string)
+  center_index = string.size / 2
+  if string.size.odd?
+    string[center_index]
+  else
+    string[center_index - 1, 2]    # this is equivalent to `string[1, 2]`
+  end
+end
+
+p center_of('I love Ruby') == 'e'  
+p center_of('Launch School') == ' '  
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'           
+
