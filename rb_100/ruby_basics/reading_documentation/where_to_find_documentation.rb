b3ec1d473a3017d77f3e324f331427ac80bf4235
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-09-06 09:50:45 -0500

    Add where_to_find_documentation.rb

---
+++ rb_100/ruby_basics/reading_documentation/where_to_find_documentation.rb
@@ -0,0 +1,11 @@
+=begin
+Resources for official documentation: 
+- Ruby versions can be found here:
+https://docs.ruby-lang.org/en/. It is possible to select a Ruby version
+to consult with.
+- https://www.rubydoc.info/stdlib/core/index
+
+Resources for unofficial documentation:
+- https://rubyapi.org/
+- https://ruby-doc.org/
+=end
