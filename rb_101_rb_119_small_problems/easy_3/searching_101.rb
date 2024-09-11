eb389fda1c15cb10261cb71f775aa7b7526271b2
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-09-11 13:16:42 -0500

    Improved code for options for looping

--- rb_101_rb_119_small_problems/easy_3/searching_101.rb
+++ rb_101_rb_119_small_problems/easy_3/searching_101.rb
@@ -84,29 +84,29 @@ loop do
   break if counter == 6
 end
 
+=begin
+Optional: for looping:
 
-## Optional: for looping
+1. `Integer#times`
+array = []
+array_for_text = ['1st', '2nd', '3rd', '4th', '5th', 'last']
+(array_for_text.length).times do |current_element|
+  puts "==> Enter the #{current_element} number:"
+  number = gets.chomp.to_i
 
-# `Integer#times`
-# array = []
-# array_for_text = ['1st', '2nd', '3rd', '4th', '5th', 'last']
-# 6.times do |iteration_number|
-#   puts "==> Enter the #{array_for_text[iteration_number]} number:"
-#   number = gets.chomp.to_i
+  array << number
+end
 
-#   array << number
-# end
+2. `Array#each`
+array = []
+array_for_text = ['1st', '2nd', '3rd', '4th', '5th', 'last']
+array_for_text.each do |current_element|
+  puts "==> Enter the #{current_element} number:"
+  number = gets.chomp.to_i
 
-# `Array#each`
-# array = []
-# array_for_text = ['1st', '2nd', '3rd', '4th', '5th', 'last']
-# [1, 2, 3, 4, 5, 6].each do |iteration_number|
-#   puts "==> Enter the #{array_for_text[iteration_number]} number:"
-#   number = gets.chomp.to_i
-
-#   array << number
-# end
-
+  array << number
+end
+=end
 
 popped_element = array.pop
 
