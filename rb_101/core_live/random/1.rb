1cf85d23bac49ad27d44ec6e67fe09eac68ec9c4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-02 21:07:52 -0600

    Changed comments

--- rb_101/core_live/random/1.rb
+++ rb_101/core_live/random/1.rb
@@ -1,48 +1,52 @@
-# 1.  What will the following code output? Explain why.
+# What will the following code output? Explain why.
 a = "hello"
 b = a
 a << " world"
 puts b    # hello world
 
-# Improved explanation:
-# The output is `hello world`.
+=begin
+Improved explanation:
 
-# It illustrates the following concepts: variables as pointers, object 
-# mutation, affecting multiple variables pointing to the same object.
+The output is `hello world`.
 
-# On line 1, a local variable `a` is initialized and references a String 
-# object with the value `"hello"`. On line 2, a local variable `b` is 
-# initialized and references the same object as `a`, pointing to the 
-# same object, and having the same `object_id`. On line 3, the mutating 
-# `String#<<` method, which mutates the caller, not creating and not 
-# returning new objects, is invoked on the same object referenced by `a`, 
-# appending `" world"` to it and mutating it. This is because, in Ruby, 
-# strings, arrays, and hashes are mutable, contrasting with integers, 
-# which are immutable. 
+It illustrates the following concepts: variables as pointers, object 
+mutation, affecting multiple variables pointing to the same object.
 
-# On line 4, the object referenced by `b` is passed as an argument to 
-# the `Kernel#puts` method invocation, outputting `hello world` to the 
-# console.
+On line 1, a local variable `a` is initialized and references a String 
+object with the value `"hello"`. On line 2, a local variable `b` is 
+initialized and references the same object as `a`, pointing to the 
+same object, and having the same `object_id`. On line 3, the mutating 
+`String#<<` method, which mutates the caller, not creating and not 
+returning new objects, is invoked on the same object referenced by `a`, 
+appending `" world"` to it and mutating it. This is because, in Ruby, 
+strings, arrays, and hashes are mutable, contrasting with integers, 
+which are immutable. 
 
-# The `#puts` method returns `nil`, but the return value isn't used and 
-# doesn't affect the output and the behavior of the code.
+On line 4, the object referenced by `b` is passed as an argument to 
+the `Kernel#puts` method invocation, outputting `hello world` to the 
+console.
 
+The `#puts` method returns `nil`, but the return value isn't used and 
+doesn't affect the output and the behavior of the code.
+=end
 
+=begin
+Innitial explanation:
 
-# Innitial explanation:
-# The code snippet outputs `hello world`.
+The code snippet outputs `hello world`.
 
-# It illustrates the variables ss pointers concept.
+It illustrates the variables ss pointers concept.
 
-# On line 1, a local variable is initialized and references a String object 
-# with a value `"hello"`. On line 2, a local variable `b` is initialized 
-# and references the same object as `a`. Now, `a` and `b` point to the 
-# same object. On line 3, the mutating `String#<<` method is invoked on 
-# the object referenced by `a`, appending `" world"` to it and mutating it.
+On line 1, a local variable is initialized and references a String object 
+with a value `"hello"`. On line 2, a local variable `b` is initialized 
+and references the same object as `a`. Now, `a` and `b` point to the 
+same object. On line 3, the mutating `String#<<` method is invoked on 
+the object referenced by `a`, appending `" world"` to it and mutating it.
 
-# Since `a` and `b` point to the same object, the `Kernel#puts` method 
-# invocation, and the object referenced by `b` is passed to it as an 
-# argument that will output `hello world` to the console. 
+Since `a` and `b` point to the same object, the `Kernel#puts` method 
+invocation, and the object referenced by `b` is passed to it as an 
+argument that will output `hello world` to the console. 
 
-# The `#puts` method returns `nil`, but the return value isn't used in this 
-# code snippet.
+The `#puts` method returns `nil`, but the return value isn't used in this 
+code snippet.
+=end
