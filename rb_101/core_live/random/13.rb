dbc81d5e54000f2ec5582399758e524c1ee1baa7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-09 11:11:04 -0600

    Add concise answer

--- rb_101/core_live/random/13.rb
+++ rb_101/core_live/random/13.rb
@@ -3,6 +3,31 @@
   num.odd?
 end
 
+# Concise answer:
+=begin
+The return value is `[1, 3]`.
+
+Lines 1-3 contain a `do...end` block with its parameter and local variable `num`, 
+passed as an argument to the non-mutating `select` method invoked on an Array object 
+`[1, 2, 3]`. On line 2, the non-mutating `odd?` method is invoked on the object 
+referenced by `num`. 
+
+`select` always returns a new array without modifying the original based on 
+the method's return value. `select` iterates over each element in the array.
+If the return value evaluates to true (or truthy), then the element is selected for 
+the new array. 
+
+`odd?` returns a new boolean object, either `true` or `false`. Upon each iteration, 
+`num.odd?` is evaluated, checking if an integer is odd. If it is true, then it returns 
+`true`, which is a truthy value, the value referenced by `num` is selected.
+
+Only `1` and `3` are odd integers, so they are selected. That's why the return 
+value is `[1, 3]`.
+=end
+
+
+
+
 # Answer:
 selected =  [1, 2, 3].select do |num|
               num.odd?
