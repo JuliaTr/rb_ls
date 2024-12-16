25833308cabbc4c6a6d116c7c6f06494f9ab2416
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-15 20:18:01 -0600

    Add an exercise

--- rb_100/codecademy/loops_and_iterators/for_loop.rb
+++ rb_100/codecademy/loops_and_iterators/for_loop.rb
@@ -1,3 +1,5 @@
+# `..` include the final number of the range in the count.
+
 for num in 1..15
   puts num
 end
@@ -18,3 +20,32 @@ end
 # 13
 # 14
 # 15
+
+
+
+# `...` exclude the final number of the range in the count.
+
+for num in 1...20
+  puts num
+end
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
