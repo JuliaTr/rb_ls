1b0bce4813c7f2401d01e5765099982a411896d7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-17 15:53:15 -0600

    Add experiments

--- rb_100/codecademy/loops_and_iterators/each_method.rb
+++ rb_100/codecademy/loops_and_iterators/each_method.rb
@@ -1,10 +1,29 @@
-odds = [1, 2, 3, 4, 5]
+# odds = [1, 2, 3, 4, 5]
 
-odds.each { |number| puts number * 2 }
+# odds.each { |number| puts number * 2 }
+
+# # Output:
+# # 2
+# # 4
+# # 6
+# # 8
+# # 10
+
+
+# Experiments:
+arr1 = [1, 3]
+arr2 = [2, 4]
+
+arr1.each do |number|
+  arr2.each do |element|
+    if number.odd?
+      puts element + 2
+    end
+  end
+end
 
 # Output:
-# 2
 # 4
 # 6
-# 8
-# 10
+# 4
+# 6
