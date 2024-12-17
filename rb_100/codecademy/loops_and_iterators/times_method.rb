08663c5de32932cdc1a3391e5b28a557ffe895f3
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-16 18:26:11 -0600

    Add an exercise

--- rb_100/codecademy/loops_and_iterators/times_method.rb
+++ rb_100/codecademy/loops_and_iterators/times_method.rb
@@ -8,3 +8,11 @@
 # Hello
 # Hello
 # Hello
+
+
+
+# Use the `times` iterator to `print` out the string "Ruby!" 30 times.
+# For practice pursose, I'll print 10, as the output is very long.
+10.times { print "Ruby!" }
+# Output:
+# Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!
