b7f3f7c5fbcc7902e141a76b875b3c3bcae9b035
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-18 20:45:24 -0600

    Add an exercise

--- rb_100/codecademy/data_structures/arrays.rb
+++ rb_100/codecademy/data_structures/arrays.rb
@@ -6,7 +6,7 @@ my_2d_array.each { |array_inside| puts "#{array_inside}" }
 # [4, 5, 6]
 
 
-
+# Exercise:
 languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
 languages.each { |language| puts language }
 
@@ -16,3 +16,29 @@ languages.each { |language| puts language }
 # JavaScript
 # Python
 # Ruby
+
+
+
+=begin
+Exercise:
+
+Puts out every element inside the sub-arrays inside `s`. 
+Iterate through `each` element in the `s` array. Call the 
+elements `sub_array`. Then iterate through `each` `sub_array` 
+and `puts` out their items.
+=end
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+s.each do |sub_array|
+  sub_array.each do |item|
+    puts item
+  end
+end
+
+# Output:
+# ham
+# swiss
+# turkey
+# cheddar
+# roast beef
+# gruyere
