e64b1a9d0e03957bb0844bd673e19800d16c0855
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-18 20:30:10 -0600

    Add comments

--- rb_100/codecademy/data_structures/hashes.rb
+++ rb_100/codecademy/data_structures/hashes.rb
@@ -1,11 +1,15 @@
 pets = Hash.new
+p pets
 
+# Add to hash:
 pets["dog"] = "Lada"
 pets["cat"] = "Timon"
 
+# Acess hash values:
 puts pets["dog"]
 puts pets["cat"]
 
 # Output:
+# {}
 # Lada
 # Timon
