e830741c5074d65e34cd38a324bcf28cc94859ac
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-22 21:01:14 -0600

    Add sorting.rb

---
+++ rb_100/codecademy/methods_blocks_sorting/sorting.rb
@@ -0,0 +1,28 @@
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+p my_array
+
+my_array.sort!
+p my_array
+
+# Output:
+# [3, 4, 8, 7, 1, 6, 5, 9, 2]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+# Exercise:
+books = ["Charlie and the Chocolate Factory", "War and Peace", 
+        "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+p books
+
+books.sort!
+puts books
+
+# Output:
+# ["Charlie and the Chocolate Factory", "War and Peace", 
+# "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+# Brief History of Time
+# A Wrinkle in Time
+# Charlie and the Chocolate Factory
+# Utopia
+# War and Peace
