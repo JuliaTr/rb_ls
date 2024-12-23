cea6ecb7f379afbfce88c8cf55857bff0266bfa5
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-22 20:49:23 -0600

    Add blocks.rb

---
+++ rb_100/codecademy/methods_blocks_sorting/blocks.rb
@@ -0,0 +1,21 @@
+# Exercise:
+# Modify the block so that each element would be multiplied
+# by five. 
+[1, 2, 3, 4, 5].each { |i| puts i }
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# Modified:
+[1, 2, 3, 4, 5].each { |i| puts i * 5 }
+
+# Output:
+# 5
+# 10
+# 15
+# 20
+# 25
