704692bf3382133b3265464036d7bdc0a217a224
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-24 12:25:16 -0600

    Add an exercise

--- rb_100/codecademy/hashes_symbols/symbols.rb
+++ rb_100/codecademy/hashes_symbols/symbols.rb
@@ -63,3 +63,25 @@ movies.each { |key, value| puts "'#{key.capitalize}': #{value}" }
 # {:inside_out_2=>"for kids and adults", :paw_patrol=>"for kids"}
 # 'Inside_out_2': for kids and adults
 # 'Paw_patrol': for kids
+
+
+
+# Exercise:
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 5,
+  truman_show: 4,
+  red_down: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+
+good_movies = movie_ratings.select do |_, value|
+  value > 3
+end
+
+puts good_movies
+# {:primer=>3.5, :the_matrix=>5, :truman_show=>4, :skyfall=>4, :lion_king=>3.5}
