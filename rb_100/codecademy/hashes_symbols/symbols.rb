4e198019eae9ef713cc9e46b7157aafcd6c00f81
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-24 11:56:55 -0600

    Add an exercise

--- rb_100/codecademy/hashes_symbols/symbols.rb
+++ rb_100/codecademy/hashes_symbols/symbols.rb
@@ -1,22 +1,35 @@
-my_first_symbol = :name
-puts my_first_symbol    # name
+# my_first_symbol = :name
+# puts my_first_symbol    # name
 
 
 
-# Experiments:
-puts "name".object_id     # 60
-puts "name".object_id     # 80
+# # Experiments:
+# puts "name".object_id     # 60
+# puts "name".object_id     # 80
 
-puts :name.object_id      # 72028
-puts :name.object_id      # 72028
+# puts :name.object_id      # 72028
+# puts :name.object_id      # 72028
+
+
+
+# # Exercise:
+# symbol_hash = {
+#   :one => 1,
+#   :two => 2,
+#   :three => 3
+# }
+
+# puts symbol_hash[:two]   # 2
 
 
 
 # Exercise:
-symbol_hash = {
-  :one => 1,
-  :two => 2,
-  :three => 3
-}
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
 
-puts symbol_hash[:two]   # 2
+# Convert strings into symbols
+symbols = []
+strings.each do |s|
+  symbols.push(s.to_sym)
+end
+
+p symbols  # [:HTML, :CSS, :JavaScript, :Python, :Ruby]
