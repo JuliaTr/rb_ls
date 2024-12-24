b0571318f3edf9f0b052f0329445b36a84af4893
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-24 12:01:12 -0600

    Add an exercise

--- rb_100/codecademy/hashes_symbols/symbols.rb
+++ rb_100/codecademy/hashes_symbols/symbols.rb
@@ -1,35 +1,48 @@
-# my_first_symbol = :name
-# puts my_first_symbol    # name
+my_first_symbol = :name
+puts my_first_symbol    # name
 
 
 
-# # Experiments:
-# puts "name".object_id     # 60
-# puts "name".object_id     # 80
+# Experiments:
+puts "name".object_id     # 60
+puts "name".object_id     # 80
 
-# puts :name.object_id      # 72028
-# puts :name.object_id      # 72028
+puts :name.object_id      # 72028
+puts :name.object_id      # 72028
 
 
 
-# # Exercise:
-# symbol_hash = {
-#   :one => 1,
-#   :two => 2,
-#   :three => 3
-# }
+# Exercise:
+symbol_hash = {
+  :one => 1,
+  :two => 2,
+  :three => 3
+}
 
-# puts symbol_hash[:two]   # 2
+puts symbol_hash[:two]   # 2
 
 
 
 # Exercise:
 strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
 
-# Convert strings into symbols
+# Convert strings into symbols, using the `to_sym`method:
 symbols = []
 strings.each do |s|
   symbols.push(s.to_sym)
 end
 
 p symbols  # [:HTML, :CSS, :JavaScript, :Python, :Ruby]
+
+
+
+# Exercise:
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+# Convert strings into symbols, using the `intern` method:
+symbols = []
+strings.each do |s|
+  symbols.push(s.intern)
+end
+
+p symbols   # [:HTML, :CSS, :JavaScript, :Python, :Ruby]
