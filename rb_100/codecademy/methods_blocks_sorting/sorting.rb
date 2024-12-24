1a6abaa4e228d080c4e841b4dd93c72d61cb9bde
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-23 21:18:13 -0600

    Add an exercise

--- rb_100/codecademy/methods_blocks_sorting/sorting.rb
+++ rb_100/codecademy/methods_blocks_sorting/sorting.rb
@@ -1,42 +1,42 @@
-# # my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
-# # p my_array
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+p my_array
 
-# # my_array.sort!
-# # p my_array
+my_array.sort!
+p my_array
 
-# # # Output:
-# # # [3, 4, 8, 7, 1, 6, 5, 9, 2]
-# # # [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-
-# # # Exercise:
-# # books = ["Charlie and the Chocolate Factory", "War and Peace", 
-# #         "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
-# # p books
-
-# # books.sort!
-# # puts books
-
-# # # Output:
-# # # ["Charlie and the Chocolate Factory", "War and Peace", 
-# # # "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
-# # # Brief History of Time
-# # # A Wrinkle in Time
-# # # Charlie and the Chocolate Factory
-# # # Utopia
-# # # War and Peace
+# Output:
+# [3, 4, 8, 7, 1, 6, 5, 9, 2]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
-# # Exercise (combined comparison operaor `<=>`):
-# book_1 = "A Wrinkle in Time"
-# book_2 = "A Brief History of Time"
+# Exercise:
+books = ["Charlie and the Chocolate Factory", "War and Peace", 
+        "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+p books
 
-# p book_1.length        # 17
-# p book_2.length        # 23
+books.sort!
+puts books
 
-# p book_1 <=> book_2    # 1
+# Output:
+# ["Charlie and the Chocolate Factory", "War and Peace", 
+# "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+# Brief History of Time
+# A Wrinkle in Time
+# Charlie and the Chocolate Factory
+# Utopia
+# War and Peace
+
+
+
+# Exercise (combined comparison operaor `<=>`):
+book_1 = "A Wrinkle in Time"
+book_2 = "A Brief History of Time"
+
+p book_1.length        # 17
+p book_2.length        # 23
+
+p book_1 <=> book_2    # 1
 
 
 
@@ -59,3 +59,22 @@ end
 p books
 # ["War and Peace", "Utopia", "Charlie and the Chocolate Factory", 
 # "A Wrinkle in Time", "A Brief History of Time"]
+
+
+
+# Exercise:
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+
+# Sort in descending order
+fruits.sort! do |first, second|
+  second <=> first
+end
+
+puts fruits
+
+# Output:
+# pear
+# orange
+# grapes
+# banana
+# apple
