dd55cd735b7bc4f8d72f4861aa225de7882ce832
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 12:05:35 -0600

    Add arrays_strings_methods_more.rb

---
+++ rb_100/codecademy/refactoring/arrays_strings_methods_more.rb
@@ -0,0 +1,11 @@
+# Exercise:
+# The concatenation operator (the shovel):
+alphabet = ['a', 'b', 'c']
+alphabet << 'd'
+p alphabet
+# ["a", "b", "c", "d"]
+
+caption = 'A giraffe surrounded by '
+caption << 'weezards!'
+puts caption
+# A giraffe surrounded by weezards!
