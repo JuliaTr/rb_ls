153f1e9da3c88fae96eed769759c533bd362c6ac
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 10:57:10 -0600

    Add an exercise

--- rb_100/codecademy/refactoring/if_statement.rb
+++ rb_100/codecademy/refactoring/if_statement.rb
@@ -10,3 +10,18 @@ puts "It's winter now." unless hot
 # Output:
 # Go for cross country skiing!
 # It's winter now.
+
+
+
+# Exercise:
+# Ternary conditional expression:
+guests_came = true
+wait_for_the_guests = false
+
+def greeting
+  puts "Hi, friends!"
+end
+
+guests_came ? greeting : wait_for_the_guests
+
+# Output: Hi, friends!
