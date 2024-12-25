927165cb9a67fa7c38d36544bda73856c4619687
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 12:12:14 -0600

    Add an exercise

--- rb_100/codecademy/refactoring/arrays_strings_methods_more.rb
+++ rb_100/codecademy/refactoring/arrays_strings_methods_more.rb
@@ -1,11 +1,28 @@
-# Exercise:
-# The concatenation operator (the shovel):
-alphabet = ['a', 'b', 'c']
-alphabet << 'd'
-p alphabet
-# ["a", "b", "c", "d"]
+# # Exercise:
+# # The concatenation operator (the shovel):
+# alphabet = ['a', 'b', 'c']
+# alphabet << 'd'
+# p alphabet
+# # ["a", "b", "c", "d"]
 
-caption = 'A giraffe surrounded by '
-caption << 'weezards!'
-puts caption
-# A giraffe surrounded by weezards!
+# caption = 'A giraffe surrounded by '
+# caption << 'weezards!'
+# puts caption
+# # A giraffe surrounded by weezards!
+
+
+
+# Exercise:
+favorite_things = ['Ruby', 'cappuccino', 'candy']
+
+puts 'A few of my favorite things:'
+
+favorite_things.each do |thing|
+  puts "I love #{thing}!"
+end
+
+# Output:
+# A few of my favorite things:
+# I love Ruby!
+# I love cappuccino!
+# I love candy!
