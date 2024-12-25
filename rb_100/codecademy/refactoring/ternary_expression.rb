702c8ac44e5de34394802d7fd102c65165ec538b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 11:03:54 -0600

    Add ternary_expression.rb

---
+++ rb_100/codecademy/refactoring/ternary_expression.rb
@@ -0,0 +1,12 @@
+# Exercise:
+# Ternary conditional expression:
+guests_came = true
+wait_for_the_guests = false
+
+def greeting
+  puts "Hi, friends!"
+end
+
+guests_came ? greeting : wait_for_the_guests
+
+# Output: Hi, friends!
