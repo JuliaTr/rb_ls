5b055cafd8099461ec6a43c74fc663b7dbd5be0f
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 12:17:25 -0600

    Uncomment the previous exercise

--- rb_100/codecademy/refactoring/one_line_if_unless_statement.rb
+++ rb_100/codecademy/refactoring/one_line_if_unless_statement.rb
@@ -1,15 +1,15 @@
-# snowy = true
-# hot = false
+snowy = true
+hot = false
 
-# # One-line `if` statement (`if` statement modifier):
-# puts "Go for cross country skiing!" if snowy
+# One-line `if` statement (`if` statement modifier):
+puts "Go for cross country skiing!" if snowy
 
-# # One-line `unless` statement (`unless` statement modifier):
-# puts "It's winter now." unless hot
+# One-line `unless` statement (`unless` statement modifier):
+puts "It's winter now." unless hot
 
-# # Output:
-# # Go for cross country skiing!
-# # It's winter now.
+# Output:
+# Go for cross country skiing!
+# It's winter now.
 
 
 
