ab1a911fc5f3ba37b96fb779fc8e407e0cbd1c0e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 15:09:51 -0600

    Add an exercise

--- rb_100/codecademy/refactoring/implicit_explicit_return.rb
+++ rb_100/codecademy/refactoring/implicit_explicit_return.rb
@@ -15,3 +15,17 @@ end
 
 puts multiple_of_three(3)  # True
 puts multiple_of_three(5)  # False
+
+
+
+# Exercise:
+def square(number)
+  puts "#{number} * #{number} is"
+  number * number
+end
+
+puts square(2)
+
+# Output:
+# 2 * 2 is
+# 4
