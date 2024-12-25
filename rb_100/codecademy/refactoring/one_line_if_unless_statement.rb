77ff255ab844705026e12c30ca209cfced715f89
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-25 11:02:51 -0600

    Rename the file and remove the last exercise

---
+++ rb_100/codecademy/refactoring/one_line_if_unless_statement.rb
@@ -0,0 +1,12 @@
+snowy = true
+hot = false
+
+# One-line `if` statement (`if` statement modifier):
+puts "Go for cross country skiing!" if snowy
+
+# One-line `unless` statement (`unless` statement modifier):
+puts "It's winter now." unless hot
+
+# Output:
+# Go for cross country skiing!
+# It's winter now.
