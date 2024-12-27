25619821933d93dfb51d5eb86ea26be968aa1de4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-27 14:41:16 -0600

    Add an exercise

--- rb_100/codecademy/blocks_procs_lambdas/procs.rb
+++ rb_100/codecademy/blocks_procs_lambdas/procs.rb
@@ -96,3 +96,11 @@ hi = Proc.new { puts "Hello" }
 hi.call
 
 # Output: Hello
+
+
+
+# Exercise:
+numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+strings_array = numbers_array.map(&:to_s)
+p strings_array
+# ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
