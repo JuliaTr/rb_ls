24a7a40d658b3d71087e5f2a0746f585f51dfcf2
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-26 20:24:59 -0600

    Add an exercise

--- rb_100/codecademy/blocks_procs_lambdas/yield.rb
+++ rb_100/codecademy/blocks_procs_lambdas/yield.rb
@@ -14,3 +14,21 @@ yield_name("Yuliia") { |n| puts "My name is #{n}." }
 # In between the yields!
 # My name is Yuliia.
 # Block complete! Back in the method.
+
+
+
+# Exercise:
+=begin
+Define your own method, `double`, that accepts a single 
+parameter and yields to a block. Then call it with a block 
+that multiplies the number parameter by 2. You can double 
+any number you like! `puts` the result in order to see your 
+yield in action!
+=end
+def double(number)
+  yield(number)
+end
+
+double(6) { |n| puts n * 2 }
+
+# Output: 12
