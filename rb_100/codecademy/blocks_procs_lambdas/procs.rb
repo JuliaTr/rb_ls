5ff00e0c3e6e7cc8eb46bf2b63646f6f8a94a7a1
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-27 15:19:24 -0600

    Add an exercise

--- rb_100/codecademy/blocks_procs_lambdas/procs.rb
+++ rb_100/codecademy/blocks_procs_lambdas/procs.rb
@@ -112,3 +112,14 @@ odds_n_ends = [:weezard, 42, "Trady Blix", 3, true, 19, 12.345]
 ints = odds_n_ends.select { |x| x.is_a? Integer }
 p ints
 # [42, 3, 19]
+
+
+
+# Exercise:
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+
+under_100 = Proc.new { |item| item < 100 }
+youngsters = ages.select(&under_100)
+
+p youngsters
+# [23, 7, 11, 94, 70, 44]
