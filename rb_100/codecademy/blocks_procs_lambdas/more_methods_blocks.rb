22400d65aac3f95ce478cb12cbfa5ae0d8be2390
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-26 20:11:46 -0600

    Add more_methods_blocks.rb

---
+++ rb_100/codecademy/blocks_procs_lambdas/more_methods_blocks.rb
@@ -0,0 +1,16 @@
+# Exercise:
+fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+doubled_fibs = fibs.collect { |num| num * 2 }
+puts doubled_fibs
+
+# Output:
+# 2
+# 2
+# 4
+# 6
+# 10
+# 16
+# 26
+# 42
+# 68
+# 110
