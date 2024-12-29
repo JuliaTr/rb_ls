6628599ceabab00b8c4f776791accb2b41ff92e0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-12-28 18:42:00 -0600

    Uncomment

--- rb_100/codecademy/loops_and_iterators/each_method.rb
+++ rb_100/codecademy/loops_and_iterators/each_method.rb
@@ -1,13 +1,13 @@
-# odds = [1, 2, 3, 4, 5]
+odds = [1, 2, 3, 4, 5]
 
-# odds.each { |number| puts number * 2 }
+odds.each { |number| puts number * 2 }
 
-# # Output:
-# # 2
-# # 4
-# # 6
-# # 8
-# # 10
+# Output:
+# 2
+# 4
+# 6
+# 8
+# 10
 
 
 # Experiments:
