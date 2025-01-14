8091f53366737d5e88ac1809fe2a86759a977e61
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-14 16:06:26 -0600

    Add description of evaluation order of operators on the same level of precedence

--- rb_100/intro_ruby_book/basics/2.rb
+++ rb_100/intro_ruby_book/basics/2.rb
@@ -21,3 +21,27 @@ puts 1983 % 1000 / 10 % 10   # 8
 
 # 4. Ones place
 puts 1983 % 100 % 10         # 3
+
+
+
+
+### From the solution in the book:
+
+# 3. Tens place:
+puts 1983 % 100 / 10        # 8
+
+# 4. Ones place:
+puts 1983 % 10              # 3
+
+
+=begin
+The mathematic operators `%` (modulo) and `/` (division) have
+the same level of precedence. So, Ruby evaluates the expression
+`1983 % 100 / 10 % 10` from left to right:
+1) `1983 % 1000` is evaluated to `983`; 
+2) `983 / 10` is evaluated to `98`;
+3) `98 % 10` is evaluated to `8`.
+
+Left-to-right evaluation order for operators of the ssame 
+precedence is consistent in Ruby.
+=end
