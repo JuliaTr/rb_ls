ad5607ae2ce5d578cc50a8ea4efd57eca5cab887
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-15 13:56:17 -0600

    Add 5.rb

---
+++ rb_100/intro_ruby_book/basics/5.rb
@@ -0,0 +1,11 @@
+=begin
+Exercise:
+Write a program that outputs the factorial of the numbers 5, 
+6, 7, and 8.
+=end
+
+# Solution:
+puts 5 * 4 * 3 * 2 * 1               # 120
+puts 6 * 5 * 4 * 3 * 2 * 1           # 720
+puts 7 * 6 * 5 * 4 * 2 * 1           # 1680
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1   # 40320
