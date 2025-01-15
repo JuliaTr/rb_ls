e8f2ac89664c629761683642316296cd28b9064c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-14 20:23:55 -0600

    Add description to the code with iteration

--- rb_100/intro_ruby_book/basics/3.rb
+++ rb_100/intro_ruby_book/basics/3.rb
@@ -37,6 +37,14 @@ The `puts` method returns `nil`, but it's not used here.
 
 
 ### Experiments:
+movies = {
+  a: 1975,
+  b: 2004,
+  c: 2013,
+  d: 2001,
+  e: 1981
+}
+
 movies.each { |_, value| puts value }
 
 # Output:
@@ -45,3 +53,26 @@ movies.each { |_, value| puts value }
 # 2013
 # 2001
 # 1981
+
+=begin
+On line 40, a local variable `movies` is initialized and references
+a Hash object with a collection of key-value pairs.
+
+On line 48, a block enclosed in `{}` is passed to the `#each` method
+called on the object referenced by `movies`. 
+
+The `#each` method iterates over the key-value pairs of 
+`movies`. It returns the original hash referenced by `movies`.
+This return value isn't used in the code snippet. 
+
+The `|_, value|` are the block parameters. The 
+underscore (`_`) signifies the absence of a parameter for 
+the key. On each iteration a value from the hash is assigned 
+to `value`. Inside the block, on each iteration an object 
+referenced by `value` is passed as an argument to the `puts` 
+method invocation and output `1975`, `2004`, `2013`, `2001`, 
+and `1981` on each line, separately, to the console.
+
+The use of `{}` for the block suggests a single-line block, 
+which is a common Ruby style for short blocks.
+=end
