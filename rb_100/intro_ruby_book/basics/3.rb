61fe533c7670b931275bd3f9596af5596802d50b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-14 19:44:43 -0600

    Add description to the solution

--- rb_100/intro_ruby_book/basics/3.rb
+++ rb_100/intro_ruby_book/basics/3.rb
@@ -7,7 +7,7 @@ movie to the screen. The output for your program should
 look something like this.
 =end
 
-# # Solution:
+# Solution:
 movies = {
   a: 1975,
   b: 2004,
@@ -16,12 +16,24 @@ movies = {
   e: 1981
 }
 
-# puts movies[:a]   # 1975
-# puts movies[:b]   # 2004
-# puts movies[:c]   # 2013
-# puts movies[:d]   # 2001
-# puts movies[:e]   # 1981
+puts movies[:a]   # 1975
+puts movies[:b]   # 2004
+puts movies[:c]   # 2013
+puts movies[:d]   # 2001
+puts movies[:e]   # 1981
 
+=begin
+On line 11, a local variable `movies` is initialized and references 
+a Hash object with a collection of key-value pairs.
+
+On lines 19 - 23, hash element references (`#[]`) are used to 
+retrieve values from the `movies` hash. They are passed as 
+arguments to the `puts` method invocation, outputting `1975`, 
+`2004`, `2013`, `2001`, and `1981` on each line, separately, 
+to the console. 
+
+The `puts` method returns `nil`, but it's not used here.
+=end
 
 
 ### Experiments:
