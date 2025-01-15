4d7b7a14d53a6c0821f734d15e1658f98c11a71d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-15 14:08:04 -0600

    Add 6.rb

---
+++ rb_100/intro_ruby_book/basics/6.rb
@@ -0,0 +1,14 @@
+=begin
+Exercise:
+Write a program that calculates the squares of 3 float 
+numbers of your choosing and outputs the result to the screen.
+=end
+
+# Solution:
+square1 = 3.2 * 3.2
+square2 = 6.1 * 6.1
+square3 = 9.5 * 9.5
+
+puts square1   # 10.240000000000002
+puts square2   # 37.209999999999994
+puts square3   # 90.25
