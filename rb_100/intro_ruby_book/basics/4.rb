78cfc9874cf5e71fbab4e7a17a3aedf3a03ebe6f
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-15 11:13:44 -0600

    Add description to the solution

--- rb_100/intro_ruby_book/basics/4.rb
+++ rb_100/intro_ruby_book/basics/4.rb
@@ -12,6 +12,27 @@ puts dates[2]    # 2013
 puts dates[3]    # 2001
 puts dates[4]    # 1981
 
+=begin
+On line 7, a local variable `dates` is initialized and references
+an Array object of a collection of Integer objects.
+
+Array is data type with an ordered list of values with 
+indices starting at 0.
+
+On lines 9 - 13, array element references (`#[]`), which is a 
+method in Ruby are used to retrieve values with indices starting 
+at 0 through 4 from the `dates` array. They are passed as 
+arguments to the `puts` method invocation, outputting `1975`, 
+`2004`, `2013`, `2001`, and `1981` on each line, separately, 
+the console.
+
+The `#[]` returns the element at the specified indice. If indice
+is out of range, `#[]` returns `nil`, which isn't happen in this 
+code snippet.
+
+The `puts` method returns `nil`, which isn't used here.
+=end
+
 
 
 ### Experiments:
