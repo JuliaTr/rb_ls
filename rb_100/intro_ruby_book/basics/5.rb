67c29f8fae8e9c5ad5d191b41ea4a2960764af85
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-15 13:57:59 -0600

    Edit solution

--- rb_100/intro_ruby_book/basics/5.rb
+++ rb_100/intro_ruby_book/basics/5.rb
@@ -7,5 +7,5 @@ Write a program that outputs the factorial of the numbers 5,
 # Solution:
 puts 5 * 4 * 3 * 2 * 1               # 120
 puts 6 * 5 * 4 * 3 * 2 * 1           # 720
-puts 7 * 6 * 5 * 4 * 2 * 1           # 1680
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1       # 5040
 puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1   # 40320
