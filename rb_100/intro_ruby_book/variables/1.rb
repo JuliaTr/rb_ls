947d0430953a5f3ffa5510d41e90fc002016907a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-16 20:29:07 -0600

    Add 1.rb

---
+++ rb_100/intro_ruby_book/variables/1.rb
@@ -0,0 +1,19 @@
+=begin
+Exercise:
+Write a program called `name.rb` that asks the user to type 
+in their name and then prints out a greeting message with 
+their name included.
+=end
+
+# Solution:
+puts "What's your name?"
+name = gets.chomp
+
+puts "Hi #{name}!"
+
+=begin
+$ ruby 1.rb
+What's your name?
+Yuliia
+Hi Yuliia!
+=end
