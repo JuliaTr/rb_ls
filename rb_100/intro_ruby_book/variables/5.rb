77a8bd4375b5f69d79a10ee42c7d3c5b25c5bdf2
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-21 09:59:52 -0600

    Improve explanation

--- rb_100/intro_ruby_book/variables/5.rb
+++ rb_100/intro_ruby_book/variables/5.rb
@@ -40,12 +40,22 @@ snippet 2, `x` prints the `NameError` message.
 Do they both give errors? Are the errors different? Why?
 No, only Code snippet 2 gives the NameError message. 
 
-On line 23, `x` is initialized inside the block, constituted 
-by `do...end` keywords, which is passed to the `times` method 
-invocation.
+In the Code snippet 1, a variable `x` is initialized in the
+outer scope and references an Integer object with the value 
+of `0`. Inside the block, constituted by `do...end` keywords, 
+which is passed to the `times` method invocationon, on line 11, 
+the return value of `x + 1` is reassigned to `x` three times. 
+Because of the reassignment inside the block the `puts x` in 
+the outer scope can access `x` inside the block, outputing 
+`3` to the console, as a result of incremention. 
+
+In the Code snippet 2, on line 23, `x` is initialized inside 
+the block. So, `x` can be accessed only inside the block, but 
+not in the outer scope. 
 
 According to the variable scope regarding blocks concept, 
 variables initialized inside the block are not available for 
-access outside of the block. That's why Ruby cannot access `x`
-in the outer scope.
+access outside of the block. That's why, in Code snippet 2, 
+`put x` in the outer scope cannot access `x` on line 23, resulting
+in the NameError message.
 =end
