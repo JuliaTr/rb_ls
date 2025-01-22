9820318455bcf932b22318e24baf0518029d3f7b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-22 16:25:12 -0600

    Add description

--- rb_100/intro_ruby_book/methods/1.rb
+++ rb_100/intro_ruby_book/methods/1.rb
@@ -14,6 +14,26 @@ puts greeting('Yuliia')
 
 # Output: Welcome, Yuliia
 
+=begin
+Description:
+On line 13, a String object with a value of `'Yuliia'` is passed
+as an argument to the `greeting` method invocation. The return 
+value of `greeting('Yuliia')` is passed as an argument to the 
+`puts` method call, outputting `Welcome, Yuliia!` to the console.
+
+On lines 9 - 11, there's a method definition with a parameter 
+`name`, which is the method's local variable. `name` is bound 
+to the argument, `'Yuliia'`, so `name` references `'Yuliia'`.
+
+On line 10, the object referenced by `name` is interpolated
+into a string. As line 10 is the last and the only method's 
+line evaluated and executed, Ruby implicitly returns 
+`"Welcome, Yuliia!"` for further output. 
+
+This demonstrates method definition vs. method invocation 
+concept.
+=end
+
 
 # Experiments:
 def greeting(name='stranger')
