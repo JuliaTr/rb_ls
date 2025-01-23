e859c4ec963461096b6e7159974cbc7ee4ebb4c9
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-23 16:25:17 -0600

    Add 3.rb

---
+++ rb_100/intro_ruby_book/methods/3.rb
@@ -0,0 +1,12 @@
+=begin
+Exercise:
+Write a program that includes a method called `multiply` that 
+takes two arguments and returns the product of the two numbers.
+=end
+
+# Solution:
+def multiply(number1, number2)
+  number1 * number2
+end
+
+p multiply(3, 2)    # 6
