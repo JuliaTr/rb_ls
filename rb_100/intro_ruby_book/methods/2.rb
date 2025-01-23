0e203e4cebeca446c6d063099c9fdec588feeab4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-23 16:14:42 -0600

    Add 2.rb

---
+++ rb_100/intro_ruby_book/methods/2.rb
@@ -0,0 +1,11 @@
+=begin
+Exercise:
+What do the following expressions evaluate to? That is, what
+value does each expression return?
+=end
+
+x = 2                          # => 2
+puts x = 2                     # => nil
+p name = "Joe"                 # => "Joe"
+four = "four"                  # => "four"
+print something = "nothing"    # => nil
