d0ef80449b29829c1e6ee1a6f862233cfa15c458
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-01-23 20:39:12 -0600

    Add code description

--- rb_100/intro_ruby_book/methods/3.rb
+++ rb_100/intro_ruby_book/methods/3.rb
@@ -9,4 +9,24 @@ def multiply(number1, number2)
   number1 * number2
 end
 
-p multiply(3, 2)    # 6
+p multiply(3, 2)     # 6
+
+=begin
+Description:
+On line 12, two Integer objects with values of `3` and `2` are
+passed as arguments to the `multiply` method invocation, 
+returning `6`. `6` is passed an and argument to the `p` method 
+invocation, outputting it to the console.
+
+On lines 8-10, there's a method definition with two parameters
+`number1` and `number2`. `3` and `2` are bound to `number1` and
+`number2`, respectively. On line 9,  is a multiplication operation 
+represented by `number1 * number2`, which returns `6`. Line 9 is 
+the only line evaluated and executed by `multiply`, so `6` is 
+returned implicitly.
+
+The data should be passed as arguments to the method invocation 
+and be recieved by the parameters. It demonstrates the relation
+betweeen arguments (passed during method invocation) and
+parameters (specified in the method definition). 
+=end
