79c4eee2318be571bbe9ee48ac801f1d76d7f608
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-04 19:10:09 -0600

    Add snippet 6

--- rb_101/random/strings_arrays_references_mutation.rb
+++ rb_101/random/strings_arrays_references_mutation.rb
@@ -1,86 +1,86 @@
-# # strings, arrays, references, mutation
+# strings, arrays, references, mutation
 
-# # Snippet 1
-# def modify_string(str)
-#   str[0] = 'J'
-#   # p str                 # "Juby"
-#   str << 'ava'          # "Jubyava"
-# end
+# Snippet 1
+def modify_string(str)
+  str[0] = 'J'
+  # p str                 # "Juby"
+  str << 'ava'          # "Jubyava"
+end
 
-# text = "Ruby"
-# p text.object_id        # 60
+text = "Ruby"
+p text.object_id        # 60
 
-# modify_string(text)     # "Jubyava"
+modify_string(text)     # "Jubyava"
 
-# puts text               # Jubyava
-# puts text.object_id     # 60
+puts text               # Jubyava
+puts text.object_id     # 60
 
-# new_text = text[0, 2]
-# puts new_text           # Ju
-# puts new_text.object_id   # 80
+new_text = text[0, 2]
+puts new_text           # Ju
+puts new_text.object_id   # 80
 
-# text[0].downcase!      # "j"
+text[0].downcase!      # "j"
 
-# puts text              # Jubyava
-# puts new_text          # Ju
+puts text              # Jubyava
+puts new_text          # Ju
 
-# # In `text[0, 2]` `2` is length.
-
-
-
-# # Snippet 2:
-# arr = ['hello', 'world']
-# str = arr[0]   # arr = [str, 'world']
-
-# arr[0][0] = 'H'  # arr = ['Hello', 'world']
-# str[1] = 'A'     # arr = ['HAllo', 'world']
-
-# puts arr.inspect       # ['HAllo', 'world']
-# puts str               # HAllo
-# puts arr[0].object_id  # 60
-# puts str.object_id     # 60; the same as `arr[0]`
-
-# new_str = str[0] + str[-1]
-# puts new_str                 # "Ho"
-# puts new_str.object_id       # 80
+# In `text[0, 2]` `2` is length.
 
 
 
-# # Snippet 3;
-# def manipulate(a, b)
-#   a << b       # ["ab", "cd", "ef"]
-#   b[0] = a[1]  # ["ab", "cd", "cdf"]
-# end
+# Snippet 2:
+arr = ['hello', 'world']
+str = arr[0]   # arr = [str, 'world']
 
-# x = ["ab", "cd"]
-# y = "ef"
-# p y.object_id        # 60
+arr[0][0] = 'H'  # arr = ['Hello', 'world']
+str[1] = 'A'     # arr = ['HAllo', 'world']
 
-# manipulate(x, y)     # ["ab", "cd", "cdf"]
+puts arr.inspect       # ['HAllo', 'world']
+puts str               # HAllo
+puts arr[0].object_id  # 60
+puts str.object_id     # 60; the same as `arr[0]`
 
-# puts x.inspect       # ["ab", "cd", "cdf"]
-# puts y               # cdf
-# puts x[1].object_id  # 80
-# puts y.object_id     # 60
-
+new_str = str[0] + str[-1]
+puts new_str                 # "Ho"
+puts new_str.object_id       # 80
 
 
-# # Snippet 4:
-# def modify_array(arr)
-#   arr.map! do |word|  # arr = ["HELLO", "WORLD"]
-#     word.upcase
-#   end
-#   arr << "NEW"        # arr = ["HELLO", "WORLD", "NEW"]
-# end
 
-# words = ["hello", "world"]
-# p words.object_id    # 60
+# Snippet 3;
+def manipulate(a, b)
+  a << b       # ["ab", "cd", "ef"]
+  b[0] = a[1]  # ["ab", "cd", "cdf"]
+end
 
-# modify_array(words)  # ["HELLO", "WORLD", "NEW"]
+x = ["ab", "cd"]
+y = "ef"
+p y.object_id        # 60
 
-# p words              # ["HELLO", "WORLD", "NEW"]
-# p words.object_id    # 60
-# p words[0]           # "HELLO"
+manipulate(x, y)     # ["ab", "cd", "cdf"]
+
+puts x.inspect       # ["ab", "cd", "cdf"]
+puts y               # cdf
+puts x[1].object_id  # 80
+puts y.object_id     # 60
+
+
+
+# Snippet 4:
+def modify_array(arr)
+  arr.map! do |word|  # arr = ["HELLO", "WORLD"]
+    word.upcase
+  end
+  arr << "NEW"        # arr = ["HELLO", "WORLD", "NEW"]
+end
+
+words = ["hello", "world"]
+p words.object_id    # 60
+
+modify_array(words)  # ["HELLO", "WORLD", "NEW"]
+
+p words              # ["HELLO", "WORLD", "NEW"]
+p words.object_id    # 60
+p words[0]           # "HELLO"
 
 
 
@@ -93,3 +93,19 @@ arr2[2] = 'D'
 
 p arr1     # ['a', 'B', 'D']
 p arr2     # ['a', 'B', 'D']
+
+
+
+# Snippet 6:
+str1 = 'hello'
+str2 = str1
+
+str1 << ' world'  # str1 = 'hello world'
+str2 << '!'       # str2 = 'hello world!'
+str2[3] = 'H'     # str2 = 'helHo world'
+
+p str1            # "helHo world"
+p str2            # "helHo world"
+
+p str1.object_id  # 60
+p str2.object_id  # 60
