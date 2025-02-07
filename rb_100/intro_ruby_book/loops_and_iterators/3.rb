ad2e19b3313e9384672eca4ac376895db938a8a6
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-06 19:50:00 -0600

    Add experiments

--- rb_100/intro_ruby_book/loops_and_iterators/3.rb
+++ rb_100/intro_ruby_book/loops_and_iterators/3.rb
@@ -72,3 +72,45 @@ count_to_zero(20)
 count_to_zero(-3)
 
 # Output: -3
+
+
+
+#### Experiments:
+def countdown(number)
+  puts number
+end
+
+countdown(6)   # 6
+
+
+def countdown(number)
+  puts number
+  countdown(number - 1)
+end
+
+countdown(6)
+
+# Output: stack level too deep (SystemStackError)
+
+
+# We provide a stop-condition to this recursive method. We
+# want to stop counting when we've reached `0`.
+def countdown(number)
+  if number <= 0
+    puts number
+  else
+    puts number
+    countdown(number - 1)
+  end
+end
+
+countdown(6)
+
+# Output:
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+# 0
