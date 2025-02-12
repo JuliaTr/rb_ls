d1b62d8e5a101f562aed3ae1705e6dde7bd278e7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-11 20:15:53 -0600

    Add return value and the value of arr to the answer.

--- rb_100/intro_ruby_book/arrays/2.rb
+++ rb_100/intro_ruby_book/arrays/2.rb
@@ -11,9 +11,15 @@ arr = arr.product(Array(1..3))
 p arr   
 # [["b", 1], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
 
-arr.first.delete(arr.first.last)
-p arr
+p arr.first.delete(arr.first.last)   # 1
+p arr   
 # [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
+
+=begin
+Snippet 1 returns `1`. 
+The value of `arr` is `[["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]`.
+=end
+
 
 
 # Snippet 2:
@@ -23,5 +29,10 @@ arr = arr.product([Array(1..3)])
 p arr
 # [["b",[1, 2, 3]], ["a", [1, 2, 3]]
 
-arr.first.delete(arr.first.last)
+p arr.first.delete(arr.first.last)   # [1, 2, 3]
 p arr   # [["b"], ["a", [1, 2, 3]]
+
+=begin
+Snippet 1 returns `[1, 2, 3]`. 
+The value of `arr` is `[["b"], ["a", [1, 2, 3]]`.
+=end
