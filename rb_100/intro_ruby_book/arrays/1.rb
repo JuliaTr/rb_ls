103509b9c200daf1b73f945ebe01b0ca6ee7a483
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-11 19:54:59 -0600

    Add 1.rb

---
+++ rb_100/intro_ruby_book/arrays/1.rb
@@ -0,0 +1,34 @@
+=begin
+Exercise:
+Below we have given you an array and a number. Write a program 
+that checks to see if the number appears in the array.
+=end
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+# Solution:
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+p arr.include?(number)    # true
+
+
+
+### from the book
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+arr.each do |num|
+  if num == number
+    puts "#{number} is in the array."
+  end
+end
+
+Output: 3 is in the array.
+
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+if arr.include?(number)
+  puts "#{number} is indeed in the array."
+end
+
+# Output: 3 is indeed in the array.
