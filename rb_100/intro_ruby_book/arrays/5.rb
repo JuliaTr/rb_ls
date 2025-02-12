740feaebcc3e9ce882caa294767a046ce1cd2dfe
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-12 10:14:01 -0600

    Add 5.rb

---
+++ rb_100/intro_ruby_book/arrays/5.rb
@@ -0,0 +1,9 @@
+# What is the value of `a`, `b`, and `c` in the following program?
+string = "Welcome to America!"
+a = string[6]
+b = string[11]
+c = string[19]
+
+p a    # "e"
+p b    # "A"
+p c    # nil
