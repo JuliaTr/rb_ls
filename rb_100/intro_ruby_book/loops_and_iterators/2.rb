fe82645a3fc37d178051df37fad510e7634481dd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-02-11 19:39:17 -0600

    Add description about the while loop

--- rb_100/intro_ruby_book/loops_and_iterators/2.rb
+++ rb_100/intro_ruby_book/loops_and_iterators/2.rb
@@ -30,3 +30,14 @@ great
 Want me to ask again? (Type 'Stop' to exit)
 Stop
 =end
+
+=begin
+On line 1, a local variable `x` is initialized and references an
+empty string `''`. On lines 3-8, there's a `while` loop. 
+
+The `while` loop the same as the `for` and the `until` loops are not 
+methods and don't have thier own variable scope. The variable
+initialized, reassigned inside the loops are in the same scope as the
+variable outside of the `while` loop. So, they can be accessible 
+outside of the `while` loop.
+=end
