d37c9f2d262b90e0a15ed5357c2bd8bcf5362cf0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-03-14 14:58:13 -0500

    Add string_reference_basic.rb

---
+++ rb_110/lesson_1/collections_basics/random/string_reference_basic.rb
@@ -0,0 +1,9 @@
+# Question 1: Basic String Element Reference (Basic)
+
+greeting = "Hello, Launch School!"
+
+### What will each of the following return?
+puts greeting[0]    # H
+puts greeting[7]    # L
+puts greeting[-1]   # !
+puts greeting[20]   # !
