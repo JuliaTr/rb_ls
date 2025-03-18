b72d1a8b7c0c24203b4edbc9ab14305405758a4b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-03-18 09:42:52 -0500

    Add exercise

--- rb_110/lesson_1/collections_basics/hash_element_reference.rb
+++ rb_110/lesson_1/collections_basics/hash_element_reference.rb
@@ -4,7 +4,19 @@ p hsh['fruit']      # "apple"
 p hsh['fruit'][0]   # "a"
 
 =begin
-On line 4, the string elemment reference `[0]` is calle on
+On line 4, the string elemment reference `[0]` is called on
 the return value of array element reference `hsh['fruit']`,
-which is `"apple"`.
+which is `"apple"`. The `hsh['fruit'][0]` returns `"a"`.
+=end
+
+
+
+hsh = { 'fruit' => 'apple', 'vegetable' => 'carrot', 'fruit' => 'pear' }
+p hsh
+
+=begin
+Output:
+hash_element_reference.rb:13: warning: key "fruit" is 
+duplicated and overwritten on line 13
+{"vegetable"=>"carrot", "fruit"=>"pear"}
 =end
