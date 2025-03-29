eccdcf0ea1327842bd55e47b694c13bf6af15d76
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-03-29 21:31:21 +0000

    Add exercise

--- rb_110/lesson_1/collections_basics/conversion.rb
+++ rb_110/lesson_1/collections_basics/conversion.rb
@@ -1,3 +1,4 @@
+### String, array
 str = 'Practice'
 
 arr = str.chars
@@ -13,3 +14,14 @@ arr = str.split
 p arr             # ["How", "do", "you", "get", "to", "Carnegie", "Hall?"]
 
 p arr.join(' ')   # "How do you get to Carnegie Hall?"
+
+
+
+
+### Hash, array
+hsh = { sky: "blue", grass: "green" }
+p hsh.to_a      # [[:sky, "blue"], [:grass, "green"]]
+
+
+arr = [[:name, 'Joe'], [:age, 10], [:favorite_color, 'blue']]
+p arr.to_h      # {:name=>"Joe", :age=>10, :favorite_color=>"blue"}
