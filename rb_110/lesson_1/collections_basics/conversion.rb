bb739cac7a1968b2a704ab219352e0413b1defb2
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-03-29 21:21:52 +0000

    Add exercise

--- rb_110/lesson_1/collections_basics/conversion.rb
+++ rb_110/lesson_1/collections_basics/conversion.rb
@@ -4,3 +4,12 @@ arr = str.chars
 p arr             # ["P", "r", "a", "c", "t", "i", "c", "e"]
 
 p arr.join        # "Practice"
+
+
+
+str = 'How do you get to Carnegie Hall?'
+
+arr = str.split
+p arr             # ["How", "do", "you", "get", "to", "Carnegie", "Hall?"]
+
+p arr.join(' ')   # "How do you get to Carnegie Hall?"
