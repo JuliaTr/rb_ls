d4cfd6f7297706172cdcff4fc833eb4b90fcf970
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-07 15:01:34 +0100

    Add exercise

--- rb_110/lesson_1/collections_basics/string_element_assignment.rb
+++ rb_110/lesson_1/collections_basics/string_element_assignment.rb
@@ -6,3 +6,15 @@ p str[0]              # "J"
 
 p str                 # "Joe's favorite color is blue"
 p str.object_id       # 60
+
+
+
+str = "joe's favorite color is blue"
+
+str[0] = 'J'
+str[6] = 'F'
+str[15] = 'C'
+str[21] = 'I'
+str[24] = 'B'
+
+p str           # "Joe's Favorite Color Is Blue"
