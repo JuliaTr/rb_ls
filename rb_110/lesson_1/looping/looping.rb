458feb4890e4f40f9ef0b9a31045665cbb0cd817
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-09 20:41:16 +0100

    Add experiment

--- rb_110/lesson_1/looping/looping.rb
+++ rb_110/lesson_1/looping/looping.rb
@@ -1,79 +1,79 @@
-# arr = [1, 2, 3, 4, 5]
+arr = [1, 2, 3, 4, 5]
 
-# arr[0] += 1
-# arr[1] += 1
-# arr[2] += 1
-# arr[3] += 1
-# arr[4] += 1
+arr[0] += 1
+arr[1] += 1
+arr[2] += 1
+arr[3] += 1
+arr[4] += 1
 
-# p arr         # [2, 3, 4, 5, 6]
-
-
-
-# # Using `loop` method (for operations above):
-# arr = [1, 2, 3, 4, 5]
-# counter = 0
-
-# loop do
-#   arr[counter] += 1
-#   counter += 1
-#   break if counter == arr.size
-# end
-
-# p arr            # [2, 3, 4, 5, 6]
-
-
-# # Experiment:
-# arr = [1, 2, 3, 4, 5]
-# counter = 0
-
-# loop do
-#   p counter
-#   arr[counter] += 1
-#   counter += 1
-#   p counter
-#   break if counter == arr.size
-# end
-
-# p arr
-
-# # Output:
-# # 0
-# # 1
-# # 1
-# # 2
-# # 2
-# # 3
-# # 3
-# # 4
-# # 4
-# # 5
-# # [2, 3, 4, 5, 6]
+p arr         # [2, 3, 4, 5, 6]
 
 
 
-# #### To control a loop:
+# Using `loop` method (for operations above):
+arr = [1, 2, 3, 4, 5]
+counter = 0
 
-# # Infinite loop
-# loop do
-#   puts 'Hello!'
-# end
+loop do
+  arr[counter] += 1
+  counter += 1
+  break if counter == arr.size
+end
+
+p arr            # [2, 3, 4, 5, 6]
 
 
-# # To exit the nearest loop:
-# loop do
-#   puts 'Hello!'
-#   break
-# end
+# Experiment:
+arr = [1, 2, 3, 4, 5]
+counter = 0
 
-# # Output: Hello!
+loop do
+  p counter
+  arr[counter] += 1
+  counter += 1
+  p counter
+  break if counter == arr.size
+end
 
-# =begin
-# There's only 1 iteration:
+p arr
 
-# `'Hello!'` is output
-# `break` is executed which ends the loop
-# =end
+# Output:
+# 0
+# 1
+# 1
+# 2
+# 2
+# 3
+# 3
+# 4
+# 4
+# 5
+# [2, 3, 4, 5, 6]
+
+
+
+#### To control a loop:
+
+# Infinite loop
+loop do
+  puts 'Hello!'
+end
+
+
+# To exit the nearest loop:
+loop do
+  puts 'Hello!'
+  break
+end
+
+# Output: Hello!
+
+=begin
+There's only 1 iteration:
+
+`'Hello!'` is output
+`break` is executed which ends the loop
+=end
 
 
 loop do
@@ -104,3 +104,18 @@ end
 On line 83, when `number == 5` evaluates to true, `Exiting...` is 
 output and the `break` keyword is executed exiting the `loop` block.
 =end
+
+
+# Experiment:
+counter = 0
+
+loop do
+  puts counter
+  counter += 1
+  break if counter == 3
+end
+
+# Output:
+# 0
+# 1
+# 2
