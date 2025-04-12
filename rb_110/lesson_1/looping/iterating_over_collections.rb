999183d508f6fc3456c2c0fe4cfe6bfa63e6013e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-12 15:31:27 +0100

    Add iterating_over_collections.rb

---
+++ rb_110/lesson_1/looping/iterating_over_collections.rb
@@ -0,0 +1,18 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  break if counter == alphabet.size
+  puts alphabet[counter]
+  counter += 1
+end
+
+# Output:
+# a
+# b
+# c
+# d
+# e
+# f
+# ....
+# z
