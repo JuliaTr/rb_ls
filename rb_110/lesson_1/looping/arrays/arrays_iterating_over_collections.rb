f5b8b78ba45ce9207082adbfab66df715d999d65
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-12 18:31:12 +0100

    Ad exercise

--- rb_110/lesson_1/looping/arrays/arrays_iterating_over_collections.rb
+++ rb_110/lesson_1/looping/arrays/arrays_iterating_over_collections.rb
@@ -12,3 +12,20 @@ end
 # I'm the color blue!
 # I'm the color purple!
 # I'm the color orange!
+
+
+
+objects = ['hello', :key, 10, []]
+counter = 0
+
+loop do
+  break if counter == objects.size
+  puts objects[counter].class
+  counter += 1
+end
+
+# Output:
+# String
+# Symbol
+# Integer
+# Array
