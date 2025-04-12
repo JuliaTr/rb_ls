247cdf0f09e63b59edc2bfefac7578d3c9ba146a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-12 16:21:04 +0100

    Add more experiments

--- rb_110/lesson_1/looping/iterating_over_collections.rb
+++ rb_110/lesson_1/looping/iterating_over_collections.rb
@@ -122,3 +122,67 @@ end
 # ....
 # z
 # (blank line)
+
+
+
+# Empty string
+empty_string = ''
+counter = 0
+
+loop do
+  break if counter == empty_string.size
+  puts empty_string[counter]
+  counter += 1
+end
+
+# Output: (breaks on the first iteration)
+
+
+empty_string = ''
+counter = 0
+
+loop do
+  counter += 1
+  break if counter > empty_string.size
+  puts empty_string[counter - 1]
+end
+
+# Output: (breaks on the first iteration)
+
+
+empty_string = ''
+counter = 0
+
+loop do
+  break if counter >= empty_string.size
+  puts empty_string[counter]
+  counter += 1
+end
+
+# Output: (breaks on the first iteration)
+
+
+
+# Starting with a counter that equals the string size
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = alphabet.size
+
+loop do
+  break if counter == alphabet.size
+  puts alphabet[counter]
+  counter += 1
+end
+
+# Output: (breaks on the first iteration)
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = alphabet.size
+
+loop do
+  break if counter >= alphabet.size
+  puts alphabet[counter]
+  counter += 1
+end
+
+# Output: (breaks on the first iteration)
