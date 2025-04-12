e4d63f933a59e62b3ad16b6ca7c3db2e3ca6e575
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-12 15:32:38 +0100

    Add experiments

--- rb_110/lesson_1/looping/iterating_over_collections.rb
+++ rb_110/lesson_1/looping/iterating_over_collections.rb
@@ -16,3 +16,109 @@ end
 # f
 # ....
 # z
+
+
+
+# Experiments:
+
+# Change a place for `break if counter == alphabet.size`
+
+# Will have the same output:
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  puts alphabet[counter]
+  counter += 1
+  break if counter == alphabet.size
+end
+
+# Output:
+# a
+# b
+# c
+# d
+# e
+# f
+# ....
+# z
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  puts alphabet[counter]
+  break if counter == alphabet.size
+  counter += 1
+end
+
+# Output:
+# a
+# b
+# c
+# d
+# e
+# f
+# ....
+# z
+# (blank line)
+
+
+
+# Change a place for `counter += 1`
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  break if counter == alphabet.size
+  counter += 1
+  puts alphabet[counter]
+end
+
+# Output:
+# b
+# c
+# d
+# e
+# f
+# ....
+# z
+# (blank line)
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  counter += 1
+  break if counter == alphabet.size
+  puts alphabet[counter]
+end
+
+# Output:
+# b
+# c
+# d
+# ...
+# z
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  counter += 1
+  puts alphabet[counter]
+  break if counter == alphabet.size
+end
+
+# Output:
+# b
+# c
+# d
+# e
+# f
+# ....
+# z
+# (blank line)
