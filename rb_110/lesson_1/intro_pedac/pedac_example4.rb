9afb3a6abed9fa8d4b1d5937d43bec4d22669a8c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-13 15:16:10 +0100

    Add examples and test cases

--- rb_110/lesson_1/intro_pedac/pedac_example4.rb
+++ rb_110/lesson_1/intro_pedac/pedac_example4.rb
@@ -30,8 +30,13 @@ Problem:
     - Row 1 has 1 element, row 2 has 2 elements ...
 
   - Sequence:
-    - 2
-    - 4, 6
+    - 2 --> 2
+    - 4, 6 --> 10
     - 8, 10, 12
-    - 14, 16, 18, 20
+    - 14, 16, 18, 20 --> 68
+
+Examples and Test Cases:
+  Row number: 1 -> Sum of integers in row: 2
+  Row number: 2 -> Sum of integers in row: 10
+  Row number: 4 -> Sum of integers in row: 68  
 =end
