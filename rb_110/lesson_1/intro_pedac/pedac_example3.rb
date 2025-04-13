03c44ad876fb7613b7c6c9a121f05bc53e0ec1eb
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-13 14:34:55 +0100

    Add pedac_example3.rb

---
+++ rb_110/lesson_1/intro_pedac/pedac_example3.rb
@@ -0,0 +1,27 @@
+=begin
+
+PROBLEM
+
+Given a string, produce a new string with every other word.
+
+______________________________________________________________
+
+PEDAC
+
+- Explicit requirements:
+  - Input: string
+  - Output: new string
+  - Remove every other word from input string
+
+- Questions:
+1. What do we mean by 'every other word?'
+  - How do we define what 'word' means in this context? 
+    - If a string contain a number is it a 'word?'
+    - If a string contain a non-alfanumeric symbol (like `)`, `%`) is it a 
+      'word?' 
+    - If a string contain a hyphenated word (like 'editor-in-chief') is it one
+      or tree words? 
+  - Do we want to delete odd or even words? 
+ 
+
+=end
