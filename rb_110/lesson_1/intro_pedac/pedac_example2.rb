eb61936cf6d9fa62d8dab34514857d429f42550a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-13 12:33:16 +0100

    Improve algorithm

--- rb_110/lesson_1/intro_pedac/pedac_example2.rb
+++ rb_110/lesson_1/intro_pedac/pedac_example2.rb
@@ -1,4 +1,5 @@
 =begin
+Step 1:
 PROBLEM:
 
 Given a string, write a method `palindrome_substrings` which returns
@@ -14,6 +15,7 @@ palindrome_substrings("") == []
 
 ___________________________________________________________________________
 
+Step 2:
 Some questions I may have:
 1. What is a substring?
 2. What is a palindrome?
@@ -22,6 +24,7 @@ Some questions I may have:
 
 __________________________________________________________________________
 
+Step 3:
 PEDAC
 
 PROBLEM
@@ -29,28 +32,78 @@ Input: string
 Output: array of substrings
 Rules:
      Explicit requirements:
-       - return only substrings which are palindromes.
+      - return only substrings which are palindromes.
        (Reminder: a palindrome is a word that reads the same forwards and 
        backward).
-       - palindromes must be 2 or more characters long. 
-       - Palindromes are case sensitive ("abBA" is not a palindrome).
+      - palindromes must be 2 or more characters long. 
+      - Palindromes are case sensitive ("abBA" is not a palindrome).
 
      Implicit requirements:
-       - the returned array of substrings.
-       - if the string is an empty string, the result should be an empty
+      - the returned array of substrings.
+      - if the string is an empty string, the result should be an empty
          array.
-       - if there's no a palindrome substring, return an empty array.
+      - if there's no a palindrome substring, return an empty array.
 Data Structure: array (comes from desired output)
+
+Step 4:
 Algorithm:
- - initialize a `result` variable to an empty array
- - create an array named `substring_arr` that contains all of the
+- initialize a `result` variable to an empty array
+- create an array named `substring_arr` that contains all of the
    substrings of the input string that are at least 2 characters long.
- - loop through the words in the `substring_arr` array.
- - if the word is a palindrome, append it to the result
+- loop through the words in the `substring_arr` array.
+- if the word is a palindrome, append it to the result
    array
- - return the result array
+- return the result array
+
+Step 8:
+Improved algorithm:
+- create an empty array called `result` that will contain all required substrings
+- create a `starting_index` variable (value `0`) for the starting index of a 
+  substring
+- start a loop that iterates over `starting_index` from `0` to the length of 
+  the string minus 2
+  - create a `num_chars` variable (value `2`) for the length of a substring
+  - start an inner loop that iterates over `num_chars` from `2` to 
+  `string.length - starting_index`
+    - extract a substring of length `num_chars` from `string` starting at 
+    `starting_index`
+    - append the extracted substring to the `result` array
+    - increment the `num_chars` variable by `1`
+  - end the inner loop
+  - increment the `starting_index` variable by `1`
+- end the outer loop
+- return the `result` array
+
+Step 11:
+- Inside the `is_palindrome?` method, check whether the string
+  value is equal to its reversed value. You can use the
+  `String#reverse` method.
+
+Step 9:
+Formal Pseudo-Code:
+
+START
+
+  /* Given a string named `string` */
+
+  SET result = []
+  SET starting_index = 0
+
+  WHILE starting_index <= length of string - 2
+    SET num_chars = 2
+    WHILE num_chars <= length of string - starting_index
+      SET substring = num_chars characters from string starting at index starting_index
+      append substring to result array
+      SET num_chars = num_chars + 1
+
+    SET starting_index = starting_index + 1
+
+  RETURN result
+
+END
 =end
 
+# Step 5:
 def palindrome_substrings(str)
   result = []
   substrings_arr = substrings(str)
@@ -60,11 +113,31 @@ def palindrome_substrings(str)
   result
 end
 
+# Step 10:
+def substrings(str)
+  result = []
+  starting_index = 0
 
+  while (starting_index <= str.lemgth - 2)
+    num_chars = 2
+    while = str.slice(starting_inde, num_chars)
+      result << substring
+      num_chars += 1
+    end
+    starting_index += 1
+  end
+  result
+end
+
+# Step 12:
+def is_palindrome?(str)
+  return str == str.reverse
+end
 
 
 # Experiments:
 
+# Step 6:
 # for each starting index from 0 through the next to last index position.
   # for each substring length from 2 until there are no substrings of that length.
     # extract the substring of the indicated length starting at the indicated 
@@ -72,6 +145,7 @@ end
   # end of inner loop.
 # end of outer loop.
 
+# Step 7:
 str = 'halo'
 counter = 0
 
