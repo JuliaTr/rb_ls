f167a604ae3813456398a5ce83d355624d6c53e5
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-19 17:24:16 -0500

    Add part b) with experiment

--- rb_110/lesson_1/collections_basics/random/referencing_values_intermediate2.rb
+++ rb_110/lesson_1/collections_basics/random/referencing_values_intermediate2.rb
@@ -19,4 +19,35 @@ complex_hash = {
 }
 
 # a) Access the value 'high':
-p complex_hash[:settings][:user][:profile][:priority]  # "high"
+# p complex_hash[:settings][:user][:profile][:priority]  # "high"
+
+
+# b) Add a new key-value pair `:mood => 'happy'` to the hash where the 
+#     current value is [[:status, 'busy']]
+p complex_hash[:settings][:user][:preferences] 
+# [[:status, "busy"], [:sound, "off"]]
+
+p complex_hash[:settings][:user][:preferences][2] = {:mood => 'happy'}
+# {:mood=>"happy"}
+p complex_hash[:settings][:user][:preferences][2].to_a
+# [[:mood, "happy"]]
+
+p complex_hash[:settings][:user][:preferences]
+# [[:status, "busy"], [:sound, "off"], {:mood=>"happy"}]
+
+=begin
+The `Hash#to_a` returns a new array, which doesn't mutate the original one.
+
+irb(main):001:0> h = { :name => 'Yuliia' }
+=> {:name=>"Yuliia"}
+irb(main):002:0> h.to_a
+=> [[:name, "Yuliia"]]
+irb(main):003:0> h
+=> {:name=>"Yuliia"}
+=end
+
+
+# Experiments:
+arr = [[1, 2], [3, 4]]
+p arr[2] = [5, 6]  # [5, 6]
+p arr              # [[1, 2], [3, 4], [5, 6]]
