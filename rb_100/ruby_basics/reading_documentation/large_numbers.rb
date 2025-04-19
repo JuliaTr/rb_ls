ad2ad605bb2f82265f71a244dc1f1c5e753c5454
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-19 16:14:30 -0500

    Add large_numbers.rb

---
+++ rb_100/ruby_basics/reading_documentation/large_numbers.rb
@@ -0,0 +1,12 @@
+=begin
+
+Resource: https://docs.ruby-lang.org/en/3.2/syntax/literals_rdoc.html#label-Number+Literals
+
+Large numbers can be written two ways:
+2345 
+2_345
+
+=end
+
+p 2345 == 2_345 # true
+p 1167893 == 1_167_893 # true
