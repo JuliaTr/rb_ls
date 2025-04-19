af3689f60600b04f26b919fd4d8d8d0eb8442787
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-19 16:43:04 -0500

    Add collection

--- rb_110/lesson_1/collections_basics/random/referencing_values_intermediate.rb
+++ rb_110/lesson_1/collections_basics/random/referencing_values_intermediate.rb
@@ -10,9 +10,20 @@ collection_1 = {
 p collection_1[:outer][1][:c][2][:d]   # 42
 
 
+
 collection_2 = [ 
   [{first: [:alpha, :beta]}, {second: [10, 20, 30, 40, 42]}], 
   ["string"]
 ]
 
 p collection_2[0][1][:second][4]  # 42
+
+
+
+collection_3 = {
+  animals: {
+    mammals: {primates: 42, felines: ['lion', 'tiger']}
+  }
+}
+
+p collection_3[:animals][:mammals][:primates] # 42
