708e49c8198e7966f1948a2a32c218c1c865dd10
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-22 13:33:09 -0500

    Add further exploration

--- rb_100/ruby_basics/reading_documentation/large_numbers.rb
+++ rb_100/ruby_basics/reading_documentation/large_numbers.rb
@@ -10,3 +10,15 @@ Large numbers can be written two ways:
 
 p 2345 == 2_345 # true
 p 1167893 == 1_167_893 # true
+
+
+
+=begin
+
+Further exploration:
+
+Question: Is it okay to write the above number as `19_87_65_4321`?
+Answer: yes, because in Ruby can be placed anywhere in the number.
+=end
+
+p 19_87_65_4321 == 1_987_654_321  # true
