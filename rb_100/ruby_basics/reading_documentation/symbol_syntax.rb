9ea92b7cca5f81da2ef9e031ffa6ef05008d090c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-22 13:51:44 -0500

    Add symbol_syntax.rb

---
+++ rb_100/ruby_basics/reading_documentation/symbol_syntax.rb
@@ -0,0 +1,12 @@
+=begin
+
+Exercise: 
+Using the Ruby documentation, determine how you would write a 
+**Symbol** that represents your name. We aren't looking for a String; we 
+want a Symbol, which is one of Ruby's datatypes.
+
+Answer:
+Resource: https://docs.ruby-lang.org/en/3.2/syntax/literals_rdoc.html#label-Symbol+Literals
+We use a colon (`:`) in front of a character like `:name` to reference a 
+symbol.
+=end
