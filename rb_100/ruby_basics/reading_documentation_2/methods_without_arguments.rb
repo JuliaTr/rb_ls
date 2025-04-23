a4ab328fb6c6f40cc3db101324de86a6984a7566
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-23 10:05:28 -0500

    Add more examples to practice

--- rb_100/ruby_basics/reading_documentation_2/methods_without_arguments.rb
+++ rb_100/ruby_basics/reading_documentation_2/methods_without_arguments.rb
@@ -3,4 +3,12 @@ Exercise: How would you use `String#upcase` to create an uppercase
 version of the string `"xyz"`?
 =end
 
-p "xyz".upcase   # "XYZ"
+p 'xyz'.upcase   # "XYZ"
+
+
+### To practice:
+puts "Welcome to #{'xyz'.upcase} Corporation!"
+# Welcome to XYZ Corporation!
+
+z = 'xyz'.upcase + 'ZYX'.downcase
+p z   # "XYZzyx"
