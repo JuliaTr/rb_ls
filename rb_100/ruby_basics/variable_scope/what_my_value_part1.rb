ea7d5c09addc4301cff13806104f5e8230814d8a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-25 17:06:13 -0500

    Add to explanation

--- rb_100/ruby_basics/variable_scope/what_my_value_part1.rb
+++ rb_100/ruby_basics/variable_scope/what_my_value_part1.rb
@@ -15,18 +15,21 @@ On line 2, a local variable `a` is initialized and references
 an Integer object `7`.
 
 On line 8, an object refeenced by `a` is passed as an argument
-to the `my_value` invocation, returning `17`. Arguments are bound with parameters.
+to the `my_value` invocation, returning `17`. Arguments are 
+bound with parameters.
 
 Lines 4-6 contain `my_value` method definition with a parameter 
 `b`, which is method's local variable.
 
 On line 5, the `b += 10` returns `17`.
 
-Methods are self-contained, accessing from outer scope only 
+Methods are self-contained, accessing data from the outer scope only 
 through arguments and parameters. 
 
-The `+=` method isn't mutating, and it doesn't mutate object in the 
-outer scope. This demonstrates Ruby's behavior as if pass-by-value.
+The `+=` method isn't mutating, and it doesn't mutate object (`7` -
+immutable data type) in the outer scope (line 2). 
+The `+=` returns a new object. This demonstrates Ruby's 
+behavior as if pass-by-value.
 
 However, Ruby behaves more as if pass-by-value-of-the-reference, 
 because parameters recieve copy of the reference of the object, 
