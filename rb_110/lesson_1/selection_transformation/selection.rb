74512d2440689eab91ec2b29ff5ecce3ac75390d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-29 13:34:05 -0500

    Add exercise

--- rb_110/lesson_1/selection_transformation/selection.rb
+++ rb_110/lesson_1/selection_transformation/selection.rb
@@ -34,3 +34,34 @@ loop do
 end
 
 p selected_chars   # "g"
+
+
+
+# Exercise: select all of the vowels in a given string.
+
+def select_vowels(str)
+  selected_chars = ''
+  counter = 0
+
+  loop do
+    current_char = str[counter]
+    if 'aeiouAEIOU'.include?(current_char)
+      selected_chars << current_char
+    end
+
+    counter += 1
+    break if counter == str.size
+  end
+
+  selected_chars
+end
+
+p select_vowels('the quick brown fox') == "euioo"  # true
+
+sentence = 'I wandered lonely as a cloud'
+p select_vowels(sentence) == "Iaeeoeaaou"  # true
+
+p sentence  # "I wandered lonely as a cloud"
+
+number_of_vowels = select_vowels('hello world').size
+p number_of_vowels  # 3
