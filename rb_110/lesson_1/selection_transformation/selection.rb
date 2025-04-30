d4114731d2dfd123e467ebae558a6fc0415faf4d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-30 10:53:15 -0500

    Add selection.rb

--- rb_110/lesson_1/selection_transformation/selection.rb
+++ rb_110/lesson_1/selection_transformation/selection.rb
@@ -65,3 +65,51 @@ p sentence  # "I wandered lonely as a cloud"
 
 number_of_vowels = select_vowels('hello world').size
 p number_of_vowels  # 3
+
+
+
+# Exercise: select the key-value pairs where the value is `'Fruit'`.
+
+produce = {
+  'apple' => 'Fruit',
+  'carrot' => 'Vegetable',
+  'pear' => 'Fruit',
+  'broccoli' => 'Vegetable'
+}
+
+def select_fruit(hsh)
+  new_hsh = {}
+  counter = 0
+  fruits = hsh.keys
+
+  p fruits # ["apple", "carrot", "pear", "broccoli"]
+
+  loop do
+    p current_fruit = fruits[counter]
+    p current_item = hsh[current_fruit]
+
+    if current_item == 'Fruit'
+      new_hsh[current_fruit] = current_item
+    end
+
+    counter += 1
+    break if counter >= hsh.size
+  end
+
+  new_hsh
+end
+
+p select_fruit(produce) # {"apple" => "Fruit", "pear" => "Fruit"}
+
+# Output:
+# ["apple", "carrot", "pear", "broccoli"]
+# "apple"
+# "Fruit"
+# "carrot"
+# "Vegetable"
+# "pear"
+# "Fruit"
+# "broccoli"
+# "Vegetable"
+
+# {"apple"=>"Fruit", "pear"=>"Fruit"}
