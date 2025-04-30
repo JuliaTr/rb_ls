eac036b3f6b213b16d19d00a92d76ed3516062ff
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-30 16:13:48 -0500

    Add exercise

--- rb_110/lesson_1/selection_transformation/transformation.rb
+++ rb_110/lesson_1/selection_transformation/transformation.rb
@@ -117,3 +117,32 @@ p double_odd_numbers(my_numbers)   # [2, 4, 6, 14, 2, 6]
 
 # not mutated
 p my_numbers   # [1, 4, 3, 7, 2, 6]
+
+
+
+# Multiply by 2 if the indice is odd
+def double_values_odd_indices(numbers)
+  doubled_numbers = []
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+
+    current_number = numbers[counter]
+    current_number *= 2 if counter.odd?
+    doubled_numbers << current_number
+
+    counter += 1
+  end
+
+  doubled_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+
+p double_values_odd_indices(my_numbers)
+p my_numbers
+
+# Output:
+# [1, 8, 3, 14, 2, 12]
+# [1, 4, 3, 7, 2, 6]
