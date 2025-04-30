3640ae7bdad74c57b6e1feff97638c257c6fbd57
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-04-30 13:29:48 -0500

    Add exercise

--- rb_110/lesson_1/selection_transformation/transformation.rb
+++ rb_110/lesson_1/selection_transformation/transformation.rb
@@ -1,15 +1,41 @@
-# Append an 's' to each string in the array.
-fruits = ['apple', 'banana', 'pear']
-tranformed_elements = []
-counter = 0
+# # Append an 's' to each string in the array.
+# fruits = ['apple', 'banana', 'pear']
+# tranformed_elements = []
+# counter = 0
 
-loop do
-  current_element = fruits[counter]
+# loop do
+#   current_element = fruits[counter]
 
-  tranformed_elements << (current_element + 's')
+#   tranformed_elements << (current_element + 's')
 
-  counter += 1
-  break if counter == fruits.size
+#   counter += 1
+#   break if counter == fruits.size
+# end
+
+# p tranformed_elements   # ["apples", "bananas", "pears"]
+
+
+
+# Multiply each element in the collection by `2`.
+# Non-mutating method
+def double_numbers(numbers)
+  doubled_numbers = []
+  counter = 0
+
+  loop do
+    break if counter == numbers.size
+    current_number = numbers[counter]
+    doubled_numbers << current_number * 2
+
+    counter += 1
+  end
+
+  doubled_numbers
 end
 
-p tranformed_elements   # ["apples", "bananas", "pears"]
+my_numbers = [1, 4, 3, 7, 2, 6]
+
+p double_numbers(my_numbers) 
+# [2, 8, 6, 14, 4, 12]
+p my_numbers
+# [1, 4, 3, 7, 2, 6]
