e1c1541ec49029d22f574d47b54724fcf32cc2da
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-01 11:12:20 -0500

    Add exercise

--- rb_110/lesson_1/selection_transformation/selection.rb
+++ rb_110/lesson_1/selection_transformation/selection.rb
@@ -1,119 +1,119 @@
-# # Exercise: select all 'g' characters out of a string.
-# alphabet = 'abcdefghijklmnopqrstuvwxyz'
-# selected_chars = ''
-# counter = 0
+# Exercise: select all 'g' characters out of a string.
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+selected_chars = ''
+counter = 0
 
-# loop do
-#   current_char = alphabet[counter]
+loop do
+  current_char = alphabet[counter]
   
-#   if current_char == 'g'
-#     selected_chars << current_char
-#   end
+  if current_char == 'g'
+    selected_chars << current_char
+  end
 
-#   counter += 1
-#   break if counter == alphabet.size
-# end
+  counter += 1
+  break if counter == alphabet.size
+end
 
-# p selected_chars  # "g"
-
-
-
-# # Experiments:
-# alphabet = 'abcdefghijklmnopqrstuvwxyz'
-# selected_chars = ''
-# counter = 0
-
-# loop do
-#   if alphabet[counter] == 'g'
-#     selected_chars << alphabet[counter]
-#   end
-
-#   counter += 1
-#   break if counter >= alphabet.size
-# end
-
-# p selected_chars   # "g"
+p selected_chars  # "g"
 
 
 
-# # Exercise: select all of the vowels in a given string.
-# def select_vowels(str)
-#   selected_chars = ''
-#   counter = 0
+# Experiments:
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+selected_chars = ''
+counter = 0
 
-#   loop do
-#     current_char = str[counter]
-#     if 'aeiouAEIOU'.include?(current_char)
-#       selected_chars << current_char
-#     end
+loop do
+  if alphabet[counter] == 'g'
+    selected_chars << alphabet[counter]
+  end
 
-#     counter += 1
-#     break if counter == str.size
-#   end
+  counter += 1
+  break if counter >= alphabet.size
+end
 
-#   selected_chars
-# end
-
-# p select_vowels('the quick brown fox') == "euioo"  # true
-
-# sentence = 'I wandered lonely as a cloud'
-# p select_vowels(sentence) == "Iaeeoeaaou"  # true
-
-# p sentence  # "I wandered lonely as a cloud"
-
-# number_of_vowels = select_vowels('hello world').size
-# p number_of_vowels  # 3
+p selected_chars   # "g"
 
 
 
-# # Exercise: select the key-value pairs where the value is `'Fruit'`.
-# produce = {
-#   'apple' => 'Fruit',
-#   'carrot' => 'Vegetable',
-#   'pear' => 'Fruit',
-#   'broccoli' => 'Vegetable'
-# }
+# Exercise: select all of the vowels in a given string.
+def select_vowels(str)
+  selected_chars = ''
+  counter = 0
 
-# def select_fruit(hsh)
-#   new_hsh = {}
-#   counter = 0
-#   fruits = hsh.keys
+  loop do
+    current_char = str[counter]
+    if 'aeiouAEIOU'.include?(current_char)
+      selected_chars << current_char
+    end
 
-#   p fruits # ["apple", "carrot", "pear", "broccoli"]
+    counter += 1
+    break if counter == str.size
+  end
 
-#   loop do
-#     break if counter >= hsh.size
+  selected_chars
+end
 
-#     p current_fruit = fruits[counter]
-#     p current_item = hsh[current_fruit]
+p select_vowels('the quick brown fox') == "euioo"  # true
 
-#     if current_item == 'Fruit'
-#       new_hsh[current_fruit] = current_item
-#     end
+sentence = 'I wandered lonely as a cloud'
+p select_vowels(sentence) == "Iaeeoeaaou"  # true
 
-#     counter += 1
-#   end
+p sentence  # "I wandered lonely as a cloud"
 
-#   new_hsh
-# end
+number_of_vowels = select_vowels('hello world').size
+p number_of_vowels  # 3
 
-# p select_fruit(produce) # {"apple" => "Fruit", "pear" => "Fruit"}
-# p produce
 
-# # Output:
-# # ["apple", "carrot", "pear", "broccoli"] (to test)
-# # "apple"                                 (to test)
-# # "Fruit"                                 (to test)
-# # "carrot"                                (to test)
-# # "Vegetable"                             (to test)
-# # "pear"                                  (to test)
-# # "Fruit"                                 (to test)
-# # "broccoli"                              (to test)
-# # "Vegetable"                             (to test)
 
-# # {"apple"=>"Fruit", "pear"=>"Fruit"}
+# Exercise: select the key-value pairs where the value is `'Fruit'`.
+produce = {
+  'apple' => 'Fruit',
+  'carrot' => 'Vegetable',
+  'pear' => 'Fruit',
+  'broccoli' => 'Vegetable'
+}
 
-# # {"apple"=>"Fruit", "carrot"=>"Vegetable", "pear"=>"Fruit", "broccoli"=>"Vegetable"}
+def select_fruit(hsh)
+  new_hsh = {}
+  counter = 0
+  fruits = hsh.keys
+
+  p fruits # ["apple", "carrot", "pear", "broccoli"]
+
+  loop do
+    break if counter >= hsh.size
+
+    p current_fruit = fruits[counter]
+    p current_item = hsh[current_fruit]
+
+    if current_item == 'Fruit'
+      new_hsh[current_fruit] = current_item
+    end
+
+    counter += 1
+  end
+
+  new_hsh
+end
+
+p select_fruit(produce) # {"apple" => "Fruit", "pear" => "Fruit"}
+p produce
+
+# Output:
+# ["apple", "carrot", "pear", "broccoli"] (to test)
+# "apple"                                 (to test)
+# "Fruit"                                 (to test)
+# "carrot"                                (to test)
+# "Vegetable"                             (to test)
+# "pear"                                  (to test)
+# "Fruit"                                 (to test)
+# "broccoli"                              (to test)
+# "Vegetable"                             (to test)
+
+# {"apple"=>"Fruit", "pear"=>"Fruit"}
+
+# {"apple"=>"Fruit", "carrot"=>"Vegetable", "pear"=>"Fruit", "broccoli"=>"Vegetable"}
 
 
 
@@ -154,3 +154,37 @@ p general_select(produce, 'Vegetable')
 
 p general_select(produce, 'Meat')
 # => {}
+
+
+
+# Exercise: Write a method called select_letter, that takes a 
+# string and returns a new string containing only the letter 
+# that we specified.
+question = 'How many times does a particular character appear in this sentence?'
+
+def select_leter(sentence, character)
+  selected_chars = ''
+  counter = 0
+
+  loop do
+    break if counter == sentence.size
+
+    current_char = sentence[counter]
+
+    if current_char == character
+      selected_chars << current_char
+    end
+
+    counter += 1
+  end
+
+  selected_chars
+end
+
+p select_leter(question, 'a')  # "aaaaaaaa"
+p select_leter(question, 't')  # "ttttt"
+p select_leter(question, 'z')  # ""
+
+p select_leter(question, 'a').size  # 8
+p select_leter(question, 't').size  # 5
+p select_leter(question, 'z').size  # 0
