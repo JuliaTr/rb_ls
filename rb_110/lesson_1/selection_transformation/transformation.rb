63084a318bae5862335dc0f8146c03c6302c48c8
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-01 10:46:22 -0500

    Add experiment

--- rb_110/lesson_1/selection_transformation/transformation.rb
+++ rb_110/lesson_1/selection_transformation/transformation.rb
@@ -171,3 +171,23 @@ end
 my_numbers = [1, 4, 3, 7, 2, 6]
 p multiply(my_numbers, 3) # [3, 12, 9, 21, 6, 18]
 p my_numbers              # [1, 4, 3, 7, 2, 6]
+
+
+# Experiments:
+def multiply(arr, transformation_criteria)
+  multiplied_numbers = []
+  counter = 0
+
+  loop do
+    break if counter == arr.size
+
+    multiplied_numbers << arr[counter] * transformation_criteria
+    counter += 1
+  end
+
+  multiplied_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+p multiply(my_numbers, 3)   # [3, 12, 9, 21, 6, 18]
+p my_numbers                # [1, 4, 3, 7, 2, 6]
