294bd7e89b269eb1b25922f45d00504a95b36bd3
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-01 14:34:32 -0500

    Add each_method.rb

---
+++ rb_110/lesson_1/each_map_select_methods/each_method.rb
@@ -0,0 +1,14 @@
+# Print each element in the array
+numbers = [1, 2, 3]
+counter = 0
+
+loop do
+  break if counter == numbers.size
+  puts numbers[counter]
+  counter += 1
+end
+
+# Output:
+# 1
+# 2
+# 3
