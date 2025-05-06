2d7632156caf99847409d2c2340472ffff9378df
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-06 12:12:55 -0500

    Add exercise

--- rb_110/lesson_1/each_map_select_methods/select_method.rb
+++ rb_110/lesson_1/each_map_select_methods/select_method.rb
@@ -11,3 +11,18 @@ result = [1, 2, 3].select do |num|
 end
 
 p result   # [1, 2, 3]
+
+
+
+result = [1, 2, 3].select do |num|
+  num + 1
+  puts num # on each iteration `nil` is returned from the block
+end
+
+p result
+
+# Output:
+# 1
+# 2
+# 3
+# []
