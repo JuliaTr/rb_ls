9fba9a73a41fc1c578ec47e71f33acb535d0b29b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-06 12:50:55 -0500

    Add exercise

--- rb_110/lesson_1/each_map_select_methods/random/select_practice.rb
+++ rb_110/lesson_1/each_map_select_methods/random/select_practice.rb
@@ -8,9 +8,20 @@ end
 p result   # [2, 4]
 
 
+
 ## Exercise: intermediate
 result = { a: 1, b: 2, c: 3, d: 4}.select do |key, value|
   value > 2
 end
 
 p result  # {c: 3, d: 4}
+
+
+
+## Exercise: intermediate
+result = ['apple', 'banana', 'cherry', 'date'].select do |fruit|
+  fruit.size > 5
+  true
+end
+
+p result  # ['apple', 'banana', 'cherry', 'date']
