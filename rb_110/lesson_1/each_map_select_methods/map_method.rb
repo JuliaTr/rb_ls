dadaebd16fca3bd2f6ea7578ebab046510f71045
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-06 14:20:53 -0500

    Add exercise

--- rb_110/lesson_1/each_map_select_methods/map_method.rb
+++ rb_110/lesson_1/each_map_select_methods/map_method.rb
@@ -1,5 +1,14 @@
 result = [1, 2, 3].map do |num|
-  num * 2
+  num * 2    # return value of the block
 end
 
 p result  # [2, 4, 6]
+
+
+
+# not tranformation instruction
+result = [1, 2, 3].map do |num|
+  num.odd?   # return value of the block
+end
+
+p result   # [true, false, true]
