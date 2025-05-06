6e7f8b0da1f3f53bba16a3c83a0bd078a2c7c536
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-06 13:47:47 -0500

    Add exercise

--- rb_110/lesson_1/each_map_select_methods/random/each_practice.rb
+++ rb_110/lesson_1/each_map_select_methods/random/each_practice.rb
@@ -8,3 +8,18 @@ end
 
 p array       # [1, 2, 3, 4, 5]
 p new_array   # [2, 4]
+
+
+
+# Exercise (intermediate): What will the following code output? 
+hash = { a: 1, b: 2, c: 3, d: 4 }
+result = {}
+
+hash.each do |key, value|
+  if value > 2
+    result[key] = value * 2
+  end
+end
+
+p hash    # {a=>1, b=>2, c=>3, d=>4}
+p result  # {c=>6, d=>8}
