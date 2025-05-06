d94864ff7fcc51d17d978ea229b75985cf468fe7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-06 12:40:55 -0500

    Add exercise

--- rb_110/lesson_1/each_map_select_methods/random/select_practice.rb
+++ rb_110/lesson_1/each_map_select_methods/random/select_practice.rb
@@ -6,3 +6,11 @@ result = [1, 2, 3, 4, 5].select do |num|
 end
 
 p result   # [2, 4]
+
+
+## Exercise: intermediate
+result = { a: 1, b: 2, c: 3, d: 4}.select do |key, value|
+  value > 2
+end
+
+p result  # {c: 3, d: 4}
