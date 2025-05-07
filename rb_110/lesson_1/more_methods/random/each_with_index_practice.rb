235bf8b4e06e7e7a0b70fd28d711b5f31d5a75a8
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-07 11:45:25 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/random/each_with_index_practice.rb
+++ rb_110/lesson_1/more_methods/random/each_with_index_practice.rb
@@ -7,3 +7,25 @@ end
 # 0: a
 # 1: b
 # 2: c
+
+
+
+# Exercise (intermediate):
+total = 0
+
+[1, 2, 3, 4, 5].each_with_index do |num, idx|
+  total += num * idx  # total = total + (num * idx)
+end
+
+puts total
+
+# notes on iteration:
+# 0 = 0 + (1 * 0)
+# 2 = 0 + (2 * 1)
+# 8 = 2 + (3 * 2)
+# 20 = 8 + (4 * 3)
+# 40 = 20 + (5 * 4)
+
+# Output: 40
+
+
