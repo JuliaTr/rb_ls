6cf501e3cbfeac677459b07d1034d317776dde9a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-07 11:14:29 -0500

    Add each_with_index_method.rb

---
+++ rb_110/lesson_1/more_methods/each_with_index_method.rb
@@ -0,0 +1,9 @@
+# `Enumerable#each_with_index`
+[1, 2, 3].each_with_index do |num, index|
+  puts "The index of #{num} is #{index}."
+end
+
+# Output:
+# The index of 1 is 0.
+# The index of 2 is 1.
+# The index of 3 is 2.
