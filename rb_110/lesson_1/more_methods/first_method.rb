17602700429b554731d25aac57b1e9e1a97071c1
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 17:00:14 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/first_method.rb
+++ rb_110/lesson_1/more_methods/first_method.rb
@@ -2,3 +2,9 @@
 arr = [1, 2, 3]
 p arr.first   # 1
 p arr         # [1, 2, 3]
+
+
+
+h = { a: "ant", b: "bear", c: "cat" }
+p h.first(2)    # [[:a, "ant"], [:b, "bear"]]
+p h             # {:a=>"ant", :b=>"bear", :c=>"cat"}
