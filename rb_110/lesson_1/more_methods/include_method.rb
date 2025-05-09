eb00c345c4d23ee1891d52b51130d0a1f382e72e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 17:39:38 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/include_method.rb
+++ rb_110/lesson_1/more_methods/include_method.rb
@@ -8,3 +8,11 @@ p arr                # [1, 2, 3]
 h = { a: "ant", b: "bear", c: "cat" }
 p h.include?("ant")   # false
 p h.include?(:a)      # true
+
+p h.key?(:b)          # true
+p h.has_key?(:c)      # true
+
+p h.value?("bear")    # true
+p h.value?("cat")     # true
+
+p h.values.include?("ant")   # true
