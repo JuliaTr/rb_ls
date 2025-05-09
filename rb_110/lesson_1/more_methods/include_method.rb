195642318494638d9d3b0bb0bcf4b7916aa7772b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 17:30:05 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/include_method.rb
+++ rb_110/lesson_1/more_methods/include_method.rb
@@ -2,3 +2,9 @@
 arr = [1, 2, 3]
 p arr.include?(1)    # true
 p arr                # [1, 2, 3]
+
+
+
+h = { a: "ant", b: "bear", c: "cat" }
+p h.include?("ant")   # false
+p h.include?(:a)      # true
