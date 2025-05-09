5ec346f64c7494d5de183f9b3c21a2650a6550ba
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 16:50:52 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/random/each_with_object_advanced.rb
+++ rb_110/lesson_1/more_methods/random/each_with_object_advanced.rb
@@ -9,3 +9,21 @@ end
 
 p new_h  
 # {:a=>[2], :b=>[4, 6], :c=>[4, 6], :d=>[8, 10], :e=>[8, 10]}
+
+
+
+# Exercise (advanced): Determine the output of this multi-level 
+# `each_with_object`:
+nested_arr = [[1, 2], [3, 4], [5, 6]]
+
+result = nested_arr.each_with_object({}) do |arr, hash|
+  arr.each_with_object(hash) do |num, h|
+    if num.odd?
+      h[num] = []
+    else
+      h[arr.first] << num if h[arr.first]
+    end
+  end
+end
+
+p result  # {1=>[2], 3=>[4], 5=>[6]}
