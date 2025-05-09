5bf172c3ac2a28ba5ab8d4997da0390fa3a7386e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 16:10:38 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/random/each_with_object_intermediate_advanced.rb
+++ rb_110/lesson_1/more_methods/random/each_with_object_intermediate_advanced.rb
@@ -5,4 +5,16 @@ new_h = hash.each_with_object({}) do |(key, value), result|
   result[value.to_s] = key.upcase if value > 15
 end
 
-p new_h  # { "20"=>"B", "30"=>"C"}
+p new_h   # { "20"=>"B", "30"=>"C"}
+
+
+
+# Exercise (intemediate): Determine the output of this code:
+nested_arr = [[1, 2], [3, 4], [5, 6]]
+
+new_arr = nested_arr.each_with_object([]) do |arr, result|
+  result << arr.first if arr.last.even?
+  result << arr.last if arr.first.odd?
+end
+
+p new_arr   # [1, 2, 3, 4, 5, 6]
