f01e7d8dcec1e1c7c2fb67a831c6fe839aa953dd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-09 16:25:51 -0500

    Add exercise

--- rb_110/lesson_1/more_methods/random/each_with_object_intermediate_advanced.rb
+++ rb_110/lesson_1/more_methods/random/each_with_object_intermediate_advanced.rb
@@ -21,7 +21,7 @@ p new_arr   # [1, 2, 3, 4, 5, 6]
 
 
 
-# Exercise (advanced): What does this nested each_with_object return?
+# Exercise (advanced): What does this nested `each_with_object` return?
 nested_arr = [['a', 1], ['b', 2], ['c', 3]]
 
 result = nested_arr.each_with_object({}) do |(letter, num), hash|
@@ -30,3 +30,20 @@ result = nested_arr.each_with_object({}) do |(letter, num), hash|
 end
 
 p result   # {"a"=>1, 1=>"a", "b"=>2, "c"=>3, 3=>"c"}
+
+
+
+# Exercise (advanced): What is the return value of this complex 
+# `each_with_object` with conditionals?
+arr = %w(apple banana cherry date)
+
+h = arr.each_with_object({}) do |fruit, result|
+  key = fruit[0]
+  if result.key?(key)      # condition eveluated to false on each iteration
+    result[key] << fruit   # never executed
+  else
+    result[key] = [fruit]
+  end
+end
+
+p h  # {"a"=>[apple], "b"=>[banana], "c"=>[cherry], "d"=>[date]}
