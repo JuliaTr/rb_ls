81128a72ae9304d5363a9b96ef1c75a83d42dac0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-10 16:22:13 -0500

    Add explanation

--- rb_110/lesson_1/practice_problems_methods_more_methods/practice_problem4.rb
+++ rb_110/lesson_1/practice_problems_methods_more_methods/practice_problem4.rb
@@ -9,16 +9,22 @@ end
 p result   # {"a"=>"ant", "b"=>"bear", "c"=>"cat"}
 
 =begin
+The `each_with_object` returns mutated initially given object 
+`{"a"=>"ant", "b"=>"bear", "c"=>"cat"}`.
+
 On lines 5-7, a `do...end` block is passed as an argument to the
 `Enumerable#each_with_object` method invoked on an Array object with 
 a collection of strings `['ant', 'bear', 'cat']`.
 
-The `value` and the `hash` are block's paramaeters and local variables.
+The `value` and the `hash` are block's parameters and local variables.
 
 Before iteration begins, the `each_with_object` method's  argument `{}`
-is passed to the method and is received by `hash`.
+is passed to the method and received by `hash`, being available inside
+the block to be mutated.
 
 On each iteration a hash `hash` is being built adding a key-value pair
 `hash[value[0]] = value` from the each element in the original array to 
-the hash. So, inside the block `{}` is mutated.
+the hash. First iteration `hash` references `{"a"=>"ant"}`, 
+second -- `{"a"=>"ant", "b"=>"bear"}`, third -- 
+`{"a"=>"ant", "b"=>"bear", "c"=>"cat"}`
 =end
