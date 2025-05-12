8ea2de24f4c97cfd42f8d808ff5ef3ff4a8992bf
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-12 09:52:06 -0500

    Improve explanation

--- rb_110/lesson_1/practice_problems_methods_more_methods/practice_problem6.rb
+++ rb_110/lesson_1/practice_problems_methods_more_methods/practice_problem6.rb
@@ -4,8 +4,9 @@ p ['ant', 'bear', 'caterpillar'].pop.size   # 11
 =begin
 The return value is `11`. 
 
-The `#size` method is chained and invoked on the return value of 
-`#pop` called on an array. The `#pop` method returns the 
-last element of the array, which is `"caterpillar"`. 
-The size of `"caterpillar"` is 11 characters.
+The non-destructive `#size` method is chained and invoked on 
+the return value of the destructive `#pop` method called on 
+an array. The `#pop` method returns the last element of the 
+array, which is `"caterpillar"`. The size of `"caterpillar"` 
+is 11 characters.
 =end
