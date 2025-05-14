088e49f7e405be3c1cd1f11ece4bea9594c41e89
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 13:05:52 -0500

    Add solution

--- rb_110/lesson_1/quiz_lesson1.rb
+++ rb_110/lesson_1/quiz_lesson1.rb
@@ -73,3 +73,14 @@ loop do
 end
 
 p odd_numners   # ["3", "5", "7", "11", "13", "15"]
+
+
+# Solution 4:
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers = []
+
+numbers.each do |number|
+  odd_numbers << number if number.to_i.odd?
+end
+
+p odd_numbers   # ["3", "5", "7", "11", "13", "15"]
