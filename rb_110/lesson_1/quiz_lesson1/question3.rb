aba83c82006f52a4e9b74a36cc381aa6ea78d77c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 15:32:09 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/question3.rb
+++ rb_110/lesson_1/quiz_lesson1/question3.rb
@@ -56,3 +56,33 @@ end
 # 2
 # 3
 # 4
+
+
+# (NOT CORRECT) Solution 2:
+# The inner loop runs only once for the first number.
+numbers = [7, 3, 5, 2, 1, 8, 4]
+counter = 0
+
+loop do
+  number = numbers[counter]
+  counter = 0
+
+  loop do
+    counter += 1
+    puts counter
+
+    break if counter >= number
+  end
+
+  counter += 1
+  break if counter >= numbers.size
+end
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
