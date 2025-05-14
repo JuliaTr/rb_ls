9d91bdf8794f4ca0a510c47abd2c27043150e85e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 11:51:44 -0500

    Add unless_statement_basic.rb

---
+++ rb_110/lesson_1/random/unless_statement_basic.rb
@@ -0,0 +1,10 @@
+# Exercise (basic): What will the following code output?
+status = "open"
+
+unless status == "closed"         # evaluates as false    
+  puts "Come on in!"
+else
+  puts "Sorry, we're closed."
+end
+
+# Output: Come on in!
