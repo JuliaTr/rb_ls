e13ed01220879099b504c8c55f2a4624c97c9d2d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 11:34:19 -0500

    Add solution

--- rb_110/lesson_1/quiz_lesson1.rb
+++ rb_110/lesson_1/quiz_lesson1.rb
@@ -1,21 +1,21 @@
-# Question 1: What happens if we try to initialize a Hash that 
-# contains duplicate keys?
+# # Question 1: What happens if we try to initialize a Hash that 
+# # contains duplicate keys?
 
-hsh = { a: 100, b: 200, c: 300, a: 400 }
-p hsh
+# hsh = { a: 100, b: 200, c: 300, a: 400 }
+# p hsh
 
-=begin
-Output:
-warning: key :a is duplicated and overwritten on line 4
-{:b=>200, :c=>300, :a=>400}
+# =begin
+# Output:
+# warning: key :a is duplicated and overwritten on line 4
+# {:b=>200, :c=>300, :a=>400}
 
-The value for the second occurrence of the duplicate key 
-overrites the value of the first occurrence.
-=end
+# The value for the second occurrence of the duplicate key 
+# overrites the value of the first occurrence.
+# =end
 
 
 
-# ---------------------------------------------------------
+# # ---------------------------------------------------------
 
 =begin
 Question 2:
@@ -26,16 +26,33 @@ Once iteration is complete the new array should contain all of
 the odd integer strings from the original array.
 =end
 
+# Expected: ["3", "5", "7", "11", "13", "15"]
+
 # Solutions that solve the problem:
-# Solution 1:
+# Solution 1 could be if we fix `break` condition
+# `odd_numners << numbers[number] if number.to_i.odd?` to
+# `odd_numbers << numbers[number] if number[number].to_i.odd?`:
 numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
 odd_numbers = []
 number = 0
 
 until number == numbers.size
-  odd_numbers << numbers[number] if number.to_i.odd?
+  odd_numbers << numbers[number] if numbers[number].to_i.odd?
 
   number += 1
 end
 
-p odd_numbers # ["3", "7", "11", "15"]
+p odd_numbers # ["3", "5", "7", "11", "13", "15"]
+
+
+# Solution 2:
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers = []
+
+for number in numbers
+  if number.to_i.odd?
+    odd_numbers.push(number)
+  end
+end
+
+p odd_numbers  # ["3", "5", "7", "11", "13", "15"]
