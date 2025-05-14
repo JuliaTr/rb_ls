bb0a845788316a641b444c8b235de61d084e58f7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 13:51:05 -0500

    Add exercise

--- rb_110/lesson_1/random/for_loop.rb
+++ rb_110/lesson_1/random/for_loop.rb
@@ -30,3 +30,17 @@ end
 
 # 3-a
 # 3-b
+
+
+
+# Exercise (​intermediate)​: What will the following code output?
+hash = { a: 1, b: 2, c: 3, d: 4 }
+result = []
+
+for key, value in hash
+  if value.even?
+    result << key
+  end
+end
+
+p result  # [:b, :d]
