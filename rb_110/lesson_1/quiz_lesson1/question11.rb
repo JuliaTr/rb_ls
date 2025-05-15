8bb5ba23fc8dfa4c738654f74ce4e8dd256f5d38
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 13:33:20 -0500

    Rename

---
+++ rb_110/lesson_1/quiz_lesson1/question11.rb
@@ -0,0 +1,99 @@
+# Examples of transformation:
+## Example 1:
+def star_method(stars)
+  arr = []
+
+  stars.each do |star|
+    arr.push(star.upcase)
+  end
+
+  arr
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+p stars_2   
+# ["SIRIUS", "RIEGEL", "VEGA","POLARIS", "ALTAIR"]
+
+
+
+## Example 2:
+def star_method(stars)
+  arr = []
+  counter = 0
+
+  until counter == stars.size do
+    arr << stars[counter][0, 3]
+
+    counter += 1
+  end
+
+  arr
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+p stars_2   
+# ["Sir", "Rig", "Veg", "Polar", "Alt"]
+
+
+
+
+# NOT transformation:
+## Example 1 (selection):
+def star_method(stars)
+  arr = []
+  counter = 0
+
+  until counter == stars.size   # while condition evaluates to false
+    star = stars[counter]
+    arr << star if star.size > 4
+
+    counter += 1
+  end
+
+  arr
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+p stars_2  
+# ["Serius", "Rigel", "Polaris", "Altair"]
+
+
+
+## Example 2 (not selection, not transformation):
+def star_method(stars)
+  for star in stars
+    star.downcase
+  end
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+
+p stars_1   # ["Sirius", "Rigel", "Vega", "Polaris", "Altair"]
+p stars_2   # ["Sirius", "Rigel", "Vega", "Polaris", "Altair"]
+
+
+
+### Experiments:
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+
+ret_val = for star in stars_1
+  star.downcase
+end
+
+p stars_1   # ["Sirius", "Rigel", "Vega", "Polaris", "Altair"]
+p ret_val   # ["Sirius", "Rigel", "Vega", "Polaris", "Altair"]
+
+
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+
+ret_val = for star in stars_1
+  star.downcase!
+end
+
+p stars_1   # ["sirius", "rigel", "vega", "polaris", "altair"]
+p ret_val   # ["sirius", "rigel", "vega", "polaris", "altair"]
