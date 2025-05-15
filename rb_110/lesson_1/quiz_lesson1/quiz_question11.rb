7c8b880e6d1bb6cef5e76cbebe01e0248d6bffe6
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 12:48:37 -0500

    Add quiz_question11.rb

---
+++ rb_110/lesson_1/quiz_lesson1/quiz_question11.rb
@@ -0,0 +1,16 @@
+# Examples of transformation:
+## Example 1:
+def star_method(stars)
+  arr = []
+
+  stars.each do |star|
+    arr.push(star.upcase)
+  end
+
+  arr
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+p stars_2   
+# ["SIRIUS", "RIEGEL", "VEGA","POLARIS", "ALTAIR"]
