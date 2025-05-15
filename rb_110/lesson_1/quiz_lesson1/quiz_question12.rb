4d539e5d350a7a54379c9440ce9046f2b52c3145
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 13:28:49 -0500

    Add experiment

---
+++ rb_110/lesson_1/quiz_lesson1/quiz_question12.rb
@@ -0,0 +1,40 @@
+# Question 12:
+result = ['a', 'b', 'c'].select do |item|
+  puts item  # => nil
+end
+
+p result 
+
+# Output:
+# a
+# b
+# c
+# []
+
+
+
+## Experiments:
+result = ['a', 'b', 'c'].each do |item|
+  puts item
+end
+
+p result
+
+# Output:
+# a
+# b
+# c
+# ["a", "b", "c"]
+
+
+['a', 'b', 'c'].map do |item|
+  puts item
+end
+
+p result
+
+# Output:
+# a
+# b
+# c
+# ["a", "b", "c"]
