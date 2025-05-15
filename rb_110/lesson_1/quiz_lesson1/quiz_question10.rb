a181faf63054b45632f2d3891b5d6fce82f2bc3c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 11:27:14 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/quiz_question10.rb
+++ rb_110/lesson_1/quiz_lesson1/quiz_question10.rb
@@ -1,4 +1,5 @@
 # Examples of selection:
+## Example 1:
 hsh = { 
   a: 'apple',
   b: 'pear', 
@@ -16,3 +17,24 @@ end
 p new_hsh
 
 # Output: {:a=>'apple', :c=>'orange', :e=>'banana'}
+
+
+
+## Example 2:
+def some_method(words)
+  arr = []
+  counter = 0
+
+  loop do
+    word = words[counter]
+    arr << word if word.size < 4
+
+    counter += 1
+    break if counter == words.size
+  end
+
+  arr
+end
+
+poem = %w(April is the cruelest month)
+p some_method(poem)  # ["is", "the"]
