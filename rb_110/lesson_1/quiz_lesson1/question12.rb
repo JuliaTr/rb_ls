c4b8e9187828d868302255b7781ad2c64a2979bf
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 13:34:01 -0500

    Rename

---
+++ rb_110/lesson_1/quiz_lesson1/question12.rb
@@ -0,0 +1,40 @@
+# Question 12:
+result = ['a', 'b', 'c'].select do |item|
+  puts item  # => nil
+end
+
+p result 
+
+# Output:
+# a
+# b
+# c
+# []
+
+
+
+## Experiments:
+result = ['a', 'b', 'c'].each do |item|
+  puts item
+end
+
+p result
+
+# Output:
+# a
+# b
+# c
+# ["a", "b", "c"]
+
+
+result = ['a', 'b', 'c'].map do |item|
+  puts item
+end
+
+p result
+
+# Output:
+# a
+# b
+# c
+# [nil, nil, nil]
