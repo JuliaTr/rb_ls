cd11e017d1435927e63024f51bfd4d0c381a0221
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 13:01:38 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/quiz_question11.rb
+++ rb_110/lesson_1/quiz_lesson1/quiz_question11.rb
@@ -1,29 +1,53 @@
-# Examples of transformation:
-## Example 1:
-def star_method(stars)
-  arr = []
+# # Examples of transformation:
+# ## Example 1:
+# def star_method(stars)
+#   arr = []
 
-  stars.each do |star|
-    arr.push(star.upcase)
-  end
+#   stars.each do |star|
+#     arr.push(star.upcase)
+#   end
 
-  arr
-end
+#   arr
+# end
 
-stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
-stars_2 = star_method(stars_1)
-p stars_2   
-# ["SIRIUS", "RIEGEL", "VEGA","POLARIS", "ALTAIR"]
+# stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+# stars_2 = star_method(stars_1)
+# p stars_2   
+# # ["SIRIUS", "RIEGEL", "VEGA","POLARIS", "ALTAIR"]
 
 
 
-## Example 2:
+# ## Example 2:
+# def star_method(stars)
+#   arr = []
+#   counter = 0
+
+#   until counter == stars.size do
+#     arr << stars[counter][0, 3]
+
+#     counter += 1
+#   end
+
+#   arr
+# end
+
+# stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+# stars_2 = star_method(stars_1)
+# p stars_2   
+# # ["Sir", "Rig", "Veg", "Polar", "Alt"]
+
+
+
+
+# NOT transformation:
+## Example 1 (selection):
 def star_method(stars)
   arr = []
   counter = 0
 
-  until counter == stars.size do
-    arr << stars[counter][0, 3]
+  until counter == stars.size   # while condition evaluates to false
+    star = stars[counter]
+    arr << star if star.size > 4
 
     counter += 1
   end
@@ -33,5 +57,5 @@ end
 
 stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
 stars_2 = star_method(stars_1)
-p stars_2   
-# ["Sir", "Rig", "Veg", "Polar", "Alt"]
+p stars_2  
+# ["Serius", "Rigel", "Polaris", "Altair"]
