817f6cdcebff25677bf67ef39dcf21fb4f0a573a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 14:16:37 -0500

    Add experiments

--- rb_110/lesson_1/quiz_lesson1/question14.rb
+++ rb_110/lesson_1/quiz_lesson1/question14.rb
@@ -10,3 +10,34 @@ p result
 # blue
 # red
 # ["GREEN", "BLUE", "RED"]
+
+
+
+## Experiments:
+result = ['green', 'blue', 'red'].map do |word|
+  puts word
+  word.upcase!
+end
+
+p result
+
+# Output:
+# green
+# blue
+# red
+# ["GREEN", "BLUE", "RED"]
+
+
+result = ['green', 'blue', 'red'].map do |word|
+  word.upcase!
+  puts word
+  word
+end
+
+p result
+
+# Output:
+# GREEN
+# BLUE
+# RED
+# ["GREEN", "BLUE", "RED"]
