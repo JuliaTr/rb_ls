ca168bd761e0e1d29e0e76463a62cc915fb286b0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 13:39:57 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/question13.rb
+++ rb_110/lesson_1/quiz_lesson1/question13.rb
@@ -1,5 +1,13 @@
 result = [1, 2, 3, 4, 5].each do |num|
-  num + 1
+  num + 1   # ignores block's return value
+end
+
+p result  # [1, 2, 3, 4, 5]
+
+
+
+result = [1, 2, 3, 4, 5].select do |num|
+  num + 1   # always truthy value
 end
 
 p result  # [1, 2, 3, 4, 5]
