46edff73773c4a482ceacda52651237022f0a561
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-14 21:12:08 -0500

    Output method return value

--- rb_110/lesson_1/each_map_select_methods/each_method.rb
+++ rb_110/lesson_1/each_map_select_methods/each_method.rb
@@ -30,14 +30,17 @@ end
 # Print each key-value pair from the hash
 hash = { a: 1, b: 2, c: 3 }
 
-hash.each do |key, value|
+result = hash.each do |key, value|
   puts "The key is #{key} and the value is #{value}"
 end
+
+p result
 
 # Output:
 # The key is a and the value is 1
 # The key is b and the value is 2
 # The key is c and the value is 3
+# {:a=>1, :b=>2, :c=>3}
 
 
 
