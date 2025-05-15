b548ed4f953b062ad2aa10e5f3809bdae83dc3f7
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 11:42:02 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/quiz_question10.rb
+++ rb_110/lesson_1/quiz_lesson1/quiz_question10.rb
@@ -38,3 +38,52 @@ end
 
 poem = %w(April is the cruelest month)
 p some_method(poem)  # ["is", "the"]
+
+
+
+
+
+# NOT selection:
+## Example 1:
+# transformation (some values are multiplied by `2`)
+arr = [1, 2, 3, 4, 5]
+counter = 0
+
+new_arr = []
+
+loop do
+  num = arr[counter]
+
+  if num >= 3
+    new_arr << 0
+  else
+    new_arr << num * 2
+  end
+
+  counter += 1
+  break if counter == arr.size
+end
+
+p new_arr # [2, 4, 0, 0, 0]
+
+
+
+## Example 2:
+# just iteration without selection or transformation
+num_arr = [2, 4, 7, 8, 11, 15]
+counter = 0
+
+until counter == num_arr.size
+  puts num_arr[counter] + 1
+
+  counter += 1
+end
+
+
+# Output:
+# 3
+# 5
+# 8
+# 9
+# 12
+# 16
