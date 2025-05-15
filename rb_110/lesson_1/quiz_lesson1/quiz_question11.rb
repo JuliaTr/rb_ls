2041779102cb0a3d34c924f3fd14f559227faa12
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 12:52:44 -0500

    Add exercise

--- rb_110/lesson_1/quiz_lesson1/quiz_question11.rb
+++ rb_110/lesson_1/quiz_lesson1/quiz_question11.rb
@@ -14,3 +14,24 @@ stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
 stars_2 = star_method(stars_1)
 p stars_2   
 # ["SIRIUS", "RIEGEL", "VEGA","POLARIS", "ALTAIR"]
+
+
+
+## Example 2:
+def star_method(stars)
+  arr = []
+  counter = 0
+
+  until counter == stars.size do
+    arr << stars[counter][0, 3]
+
+    counter += 1
+  end
+
+  arr
+end
+
+stars_1 = ['Sirius', 'Rigel', 'Vega', 'Polaris', 'Altair']
+stars_2 = star_method(stars_1)
+p stars_2   
+# ["Sir", "Rig", "Veg", "Polar", "Alt"]
