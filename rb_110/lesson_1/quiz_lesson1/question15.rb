0bf23587f8d53fabc27e3eb16e4e8a160375b200
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-16 14:40:08 -0500

    Add question15.rb

---
+++ rb_110/lesson_1/quiz_lesson1/question15.rb
@@ -0,0 +1,16 @@
+# `each`:
+h = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+result = h.each do |_, num|
+  plus_one = num + 1
+  puts "#{num} plus 1 is #{plus_one}"
+end
+
+p result
+
+# Output:
+# 1 plus 1 is 2
+# 2 plus 1 is 3
+# 3 plus 1 is 4
+# 4 plus 1 is 5
+# 5 plus 1 is 6
+# {:a=>1, :b=>2, :c=>3, :d=>4, :e=>5}
