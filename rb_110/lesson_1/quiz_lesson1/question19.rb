9262d374007002c48c3816bf0c6dfe13ab3d305a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-16 16:53:30 -0500

    Add solution and experiment to debug

--- rb_110/lesson_1/quiz_lesson1/question19.rb
+++ rb_110/lesson_1/quiz_lesson1/question19.rb
@@ -43,7 +43,8 @@ p select_countries(countries_and_capitals)
 
 
 
-# Doesn't solve the problem:
+
+# Don't solve the problem:
 # Solution 1:
 result = countries_and_capitals.select do |_, country| 
           country[0] == 'B'
@@ -53,11 +54,40 @@ p result
 # "Brazil"=>"Brazilia"}
 
 
-# Experiment:
+# Experiment to debug:
 result = countries_and_capitals.select do |key, _|
           key[0] == 'B'
         end
 
 p result
+# {"Belgium"=>"Brussels", "Barbados"=>"Bridgetown", 
+# "Bolivia"=>"La Paz", "Brazil"=>"Brazilia"}
+
+
+
+# Solution 2:
+def begins_with_b(string)
+  string[0] == 'B'
+end
+
+result = countries_and_capitals.find_all do |country, capial|
+          begins_with_b(country)
+        end
+
+p result 
+# [["Belgium", "Brussels"], ["Barbados", "Bridgetown"], 
+# ["Bolivia", "La Paz"], ["Brazil", "Brazilia"]]
+
+
+# Experiment to debug:
+def begins_with_b(string)
+  string[0] == 'B'
+end
+
+result = countries_and_capitals.select do |key, _|
+          begins_with_b(key)
+        end
+
+p result 
 # {"Belgium"=>"Brussels", "Barbados"=>"Bridgetown", 
 # "Bolivia"=>"La Paz", "Brazil"=>"Brazilia"}
