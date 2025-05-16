0112e7332f949e6601a9aa18295026d6cd26787d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-16 16:17:43 -0500

    Add experiment

--- rb_110/lesson_1/quiz_lesson1/question18.rb
+++ rb_110/lesson_1/quiz_lesson1/question18.rb
@@ -67,3 +67,14 @@ odd_numbers, even_numbers = numbers.partition do |number|
                             end
 p odd_numbers     # [2, 4, 8, 10, 14]
 p even_numbers    # [1, 5, 7, 13]
+
+
+
+
+# Experiment:
+numbers = [1, 2, 4, 5, 7, 8, 10, 13, 14]
+even_numbers, odd_numbers = numbers.partition do |number|
+                              number.even?
+                            end
+p odd_numbers    # [1, 5, 7, 13]
+p even_numbers   # [2, 4, 8, 10, 14]
