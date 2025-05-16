e6bd2f76f7266c994f727004e26956eb69cd26dd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-16 16:04:32 -0500

    Add question18.rb

---
+++ rb_110/lesson_1/quiz_lesson1/question18.rb
@@ -0,0 +1,20 @@
+=begin
+Exercise: Given an array numbers which contains both odd 
+and even integers; create two new arrays, `odd_numbers` which 
+contains the odd integers from the original array and `even_numbers` 
+which contains the even integers.
+=end
+
+# Solution 1:
+numbers = [1, 2, 4, 5, 7, 8, 10, 13, 14]
+
+odd_numbers = numbers.select do |number|
+                number.odd?
+              end
+
+even_numbers = numbers.select do |number|
+                number.even?
+              end
+
+p odd_numbers    # [1, 5, 7, 13]
+p even_numbers   # [2, 4, 8, 10, 14]
