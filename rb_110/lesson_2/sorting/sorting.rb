89e52112fd4eb11c8813d6a2fd0fdd2cf8e15c54
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-15 19:52:52 -0500

    Add experiment

--- rb_110/lesson_2/sorting/sorting.rb
+++ rb_110/lesson_2/sorting/sorting.rb
@@ -2,3 +2,12 @@ arr1 = [2, 5, 3, 4, 1]
 arr2 = [1, 2, 3, 4, 5]
 
 p arr1.sort   # [1, 2, 3, 4, 5]
+
+
+
+# Experiments:
+arr = ['', 'cc', 'd', 'aaaaa', 'bbbb']
+result = arr.sort_by { |str| str.size }
+
+p result   # ["", "d", "cc", "bbbb", "aaaaa"]
+p arr      # ["", "cc", "d", "aaaaa", "bbbb"]
