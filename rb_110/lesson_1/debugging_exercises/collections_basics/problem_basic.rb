156202f2ce5977d51d41c9d4b100c35f05c9f076
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-18 17:00:35 -0500

    Add an option for refactoring

--- rb_110/lesson_1/debugging_exercises/collections_basics/problem_basic.rb
+++ rb_110/lesson_1/debugging_exercises/collections_basics/problem_basic.rb
@@ -26,3 +26,20 @@ p extract_elements("launch", ["a", "b", "c"], {name: "school", type: "coding"}) 
 # true
 p extract_elements("hello", [], {language: "Ruby"}) == ["l", nil, nil, nil]
 # true
+
+
+
+## Refactor:
+# Option 1:
+def extract_elements(str, arr, h)
+  new_arr = []
+  new_arr.push(str[2], arr.first, arr.last, h[:name])
+  new_arr
+end
+
+p extract_elements("programming", [1, 2, 3, 4, 5], {name: "Ruby", year: 1995}) == ["o", 1, 5, "Ruby"]
+# true
+p extract_elements("launch", ["a", "b", "c"], {name: "school", type: "coding"}) == ["u", "a", "c", "school"]
+# true
+p extract_elements("hello", [], {language: "Ruby"}) == ["l", nil, nil, nil]
+# true
