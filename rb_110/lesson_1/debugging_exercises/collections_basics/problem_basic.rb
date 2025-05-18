94066d0441ea5547bbce7d8406f85b7d41617fbd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-18 16:56:21 -0500

    Add problem_basic.rb

---
+++ rb_110/lesson_1/debugging_exercises/collections_basics/problem_basic.rb
@@ -0,0 +1,28 @@
+=begin
+Exercise: Element Reference Challenge (Basic)
+
+Write a method called `extract_elements` that takes a string, 
+array, and hash as arguments. The method should return a new 
+array containing:
+•   The third character from the string
+•   The first and last elements from the array
+•   The value associated with the key `:name` from the hash
+=end
+
+def extract_elements(str, arr, h)
+  new_arr = []
+
+  new_arr << str[2]
+  new_arr << arr.first
+  new_arr << arr.last
+  new_arr << h[:name]
+
+  new_arr
+end
+
+p extract_elements("programming", [1, 2, 3, 4, 5], {name: "Ruby", year: 1995}) == ["o", 1, 5, "Ruby"]
+# true
+p extract_elements("launch", ["a", "b", "c"], {name: "school", type: "coding"}) == ["u", "a", "c", "school"]
+# true
+p extract_elements("hello", [], {language: "Ruby"}) == ["l", nil, nil, nil]
+# true
