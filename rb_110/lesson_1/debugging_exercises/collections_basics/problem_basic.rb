fe56db4a983f5d7a0ed860355474c27ba2a4b904
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-18 17:06:23 -0500

    Add an option for refactoring

--- rb_110/lesson_1/debugging_exercises/collections_basics/problem_basic.rb
+++ rb_110/lesson_1/debugging_exercises/collections_basics/problem_basic.rb
@@ -30,12 +30,29 @@ p extract_elements("hello", [], {language: "Ruby"}) == ["l", nil, nil, nil]
 
 
 ## Refactor:
-# Option 1:
+# Option 1 (`Array#push` with multiple arguments):
 def extract_elements(str, arr, h)
   new_arr = []
   new_arr.push(str[2], arr.first, arr.last, h[:name])
   new_arr
 end
+
+p extract_elements("programming", [1, 2, 3, 4, 5], {name: "Ruby", year: 1995}) == ["o", 1, 5, "Ruby"]
+# true
+p extract_elements("launch", ["a", "b", "c"], {name: "school", type: "coding"}) == ["u", "a", "c", "school"]
+# true
+p extract_elements("hello", [], {language: "Ruby"}) == ["l", nil, nil, nil]
+# true
+
+
+# Option 2 (concatenation with Array literals):
+def extract_elements(str, arr, h)
+  new_arr = []
+  new_arr += [str[2], arr.first, arr.last, h[:name]]
+  new_arr
+end
+
+# new_arr = new_arr + [str[2], arr.first, arr.last, h[:name]]
 
 p extract_elements("programming", [1, 2, 3, 4, 5], {name: "Ruby", year: 1995}) == ["o", 1, 5, "Ruby"]
 # true
