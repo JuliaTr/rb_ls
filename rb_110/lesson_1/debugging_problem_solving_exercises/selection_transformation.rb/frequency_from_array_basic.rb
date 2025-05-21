8e2130a6547a14788574135d094db63043878acd
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-21 14:25:32 -0500

    Add frequency_from_array_basic.rb

---
+++ rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/frequency_from_array_basic.rb
@@ -0,0 +1,82 @@
+=begin
+Exercise (basic)​: Write a method count_occurrences that takes 
+an array as an argument and returns a hash that contains the 
+count of each element in the array.
+
+Steps:
+- Define a method to build a hash
+- Select unique values to use them as keys
+  Return: array of unique values
+  - Use each array value as a key
+  - Build hash
+- Count how many of each values are there in the array
+  - Count and push to the array of counted values
+- Build a hash
+
+
+p count_occurences(['car', 'car', 'truck', 'car', 'SUV', 'truck'])
+Expected: {"car"=>3, "truck"=>2, "SUV"=>1}
+
+p count_occurences(['ruby', 'python', 'ruby', 'javascript'])
+# Expected: {"ruby"=>2, "python"=>1, "javascript"=>1}
+
+p count_occurences([]) == {}
+=end
+
+def unique_values(arr)
+  unique_values = arr.uniq
+end
+
+def count_unique_values(arr)
+  counter = 0
+  arr_counted_values = []
+
+  loop do
+    break if counter == unique_values(arr).length
+
+    current_unique_value = unique_values(arr)[counter]
+
+    counted_value = arr.count(current_unique_value)
+    arr_counted_values << counted_value
+
+    counter += 1
+  end
+
+  arr_counted_values
+end
+
+def count_occurences(arr)
+  counter = 0
+  new_hash = {}
+
+  loop do
+    break if counter == unique_values(arr).length
+
+    current_value_key = unique_values(arr)[counter]
+    current_occurence = count_unique_values(arr)[counter]
+
+    new_hash[current_value_key] = current_occurence
+
+    counter += 1
+  end
+
+  new_hash
+end
+
+arr1 = ['car', 'car', 'truck', 'car', 'SUV', 'truck']
+p unique_values(arr1)        # ["car", "truck", "SUV"]
+p count_unique_values(arr1)  # [3, 2, 1]
+p count_occurences(arr1)     # {"car"=>3, "truck"=>2, "SUV"=>1}
+# Expected: {"car"=>3, "truck"=>2, "SUV"=>1}
+
+arr2 = ['ruby', 'python', 'ruby', 'javascript']
+p unique_values(arr2)        # ["ruby", "python", "javascript"]
+p count_unique_values(arr2)  # [2, 1, 1]
+p count_occurences(arr2)     # {"ruby"=>2, "python"=>1, "javascript"=>1}
+# Expected: {"ruby"=>2, "python"=>1, "javascript"=>1}
+
+arr3 = []
+p unique_values(arr3)        # []
+p count_unique_values(arr3)  # []
+p count_occurences(arr3)     # {}
+# Expected: {}
