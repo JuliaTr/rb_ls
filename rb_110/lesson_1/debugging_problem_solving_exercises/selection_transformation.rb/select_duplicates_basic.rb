a787e61162e41736b00c117a021282fd6359d02e
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-23 16:33:10 -0500

    Work out other solutions

--- rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/select_duplicates_basic.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/select_duplicates_basic.rb
@@ -14,7 +14,7 @@ Algorithm:
 - Count occurence
   - Create a hash to assign occurences to keys, which are unique 
     array values (Return: hash)
-- Select key-value pairs which values are more than `1` (Rturn: hash)
+- Select key-value pairs which values are more than `1` (Return: hash)
   - Select keys (Return: array)
 =end
 
@@ -46,6 +46,58 @@ def select_duplicates(arr)
   arr.uniq.each_with_object({}) do |element, hash|
     hash[element] = arr.count(element)
   end.select { |_, value| value > 1 }.keys
+end
+
+p select_duplicates([1, 2, 3, 1, 2, 4]) == [1, 2]
+p select_duplicates(['a', 'b', 'c', 'a', 'b', 'd']) == ['a', 'b']
+p select_duplicates([1, 2, 3, 4]) == []
+p select_duplicates([]) == []
+
+# Output:
+# true
+# true
+# true
+# true
+
+
+
+### LSBot additional recommendations:
+
+# Reduce time complexity for each call and simplified logic without
+# building a hash:
+def select_duplicates(arr)
+  result = []
+
+  arr.uniq.each do |element|
+    result << element if arr.count(element) > 1
+  end
+
+  result
+end
+
+p select_duplicates([1, 2, 3, 1, 2, 4]) == [1, 2]
+p select_duplicates(['a', 'b', 'c', 'a', 'b', 'd']) == ['a', 'b']
+p select_duplicates([1, 2, 3, 4]) == []
+p select_duplicates([]) == []
+
+# Output:
+# true
+# true
+# true
+# true
+
+
+# Single-pass solution:
+def select_duplicates(arr)
+  counts = Hash.new(0)
+
+  # Count sccurences in a single pass
+  arr.each do |element|
+    counts[element] += 1  # counts[element] = counts[element] + 1
+  end
+
+  # Select elements with count > 1
+  counts.select { |_, count| count > 1 }.keys
 end
 
 p select_duplicates([1, 2, 3, 1, 2, 4]) == [1, 2]
