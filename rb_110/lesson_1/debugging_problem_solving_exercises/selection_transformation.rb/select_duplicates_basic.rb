6e374777101f30a5185f76baccf10e7af39477a6
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-23 16:08:43 -0500

    Add select_duplicates_basic.rb

---
+++ rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/select_duplicates_basic.rb
@@ -0,0 +1,40 @@
+=begin
+Exercise (​basic)​: Create a method `select_duplicates` that 
+takes an array and returns a new array containing only the 
+elements that appear more than once in the original array.
+
+p select_duplicates([1, 2, 3, 1, 2, 4]) == [1, 2]
+p select_duplicates(['a', 'b', 'c', 'a', 'b', 'd']) == ['a', 'b']
+p select_duplicates([1, 2, 3, 4]) == []
+p select_duplicates([]) == []
+
+Algorithm:
+- Define method `select_duplicates` that takes an array
+- Find unique elements (Return: array)
+- Count occurence
+  - Create a hash to assign occurences to keys, which are unique 
+    array values (Return: hash)
+- Select key-value pairs which values are more than `1` (Rturn: hash)
+  - Select keys (Return: array)
+=end
+
+def select_duplicates(arr)
+  hash = {}
+
+  arr.uniq.each do |element|
+    hash[element] = arr.count(element)
+  end
+
+  hash.select { |_, value| value > 1 }.keys
+end
+
+p select_duplicates([1, 2, 3, 1, 2, 4]) == [1, 2]
+p select_duplicates(['a', 'b', 'c', 'a', 'b', 'd']) == ['a', 'b']
+p select_duplicates([1, 2, 3, 4]) == []
+p select_duplicates([]) == []
+
+# Ouput:
+# true
+# true
+# true
+# true
