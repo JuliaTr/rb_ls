3acbf8f7888150f9d084ed0edcb3b521d6f08f52
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-24 15:28:39 -0500

    Add altenative posible solution

--- rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/most_frequent_number.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/most_frequent_number.rb
@@ -37,3 +37,31 @@ p most_frequent_number([]) == nil
 # true
 # true
 # true
+
+# appropriate data structure
+
+
+
+## Suggestions from LSBot:
+# Error handling for empty array and variable naming
+def most_frequent_number(arr)
+  return nil if arr.empty?
+
+  frequency_count = arr.each_with_object({}) do |number, counts|
+    counts[number] = counts[number].to_i + 1
+  end 
+
+  max_frequency = frequency_count.values.max
+  frequency_count.find { |num, freq| freq == max_frequency }.first
+end
+
+p most_frequent_number([1, 2, 3, 4, 3, 2, 2]) == 2
+p most_frequent_number([1, 1, 2, 2, 3, 3]) == 1
+p most_frequent_number([5]) == 5
+p most_frequent_number([]) == nil
+
+# Output:
+# true
+# true
+# true
+# true
