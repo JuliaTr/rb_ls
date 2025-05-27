9e4c39e44f57e3da86f6e121e104addf37baf316
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-27 18:11:26 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/map_array_hash_return_value.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/map_array_hash_return_value.rb
@@ -1,8 +1,17 @@
 ## Array Examples with `map`:
 result = [1, 2, 3].map do |num|
-  if num.odd?
-    num * 2
-  end
-end
+          if num.odd?
+            num * 2
+          end
+        end
 
 p result # [2, nil, 6]
+
+
+
+result = ['ant', 'bear', 'cat'].map do |animal|
+          animal[0]
+          animal.size
+        end
+
+p result  # [3, 4, 3]
