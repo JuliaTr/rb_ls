2bff4132b4179bd1760807b8fe1f3a22c9ae6ccf
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-26 20:51:40 -0500

    Add alternative solutions

--- rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/selection/select_most_frequent_char_intermediate.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/selection_transformation.rb/selection/select_most_frequent_char_intermediate.rb
@@ -40,6 +40,54 @@ p most_frequent_char('') == nil               # true
 
 
 
+
+## Alternatives:
+# Option 1 (early return `nil` and hash initialization):
+def most_frequent_char(str)
+  return nil if str.empty?
+
+  frequency_count = Hash.new(0)
+  str.each_char { |char| frequency_count[char] += 1 }
+
+  max_value = frequency_count.values.max
+
+  frequency_count.select { |_, values| value == max_value }.keys.first
+end
+
+p most_frequent_char('abcdefgabc') == 'a'     # true
+p most_frequent_char('Launch School') == 'c'  # true
+p most_frequent_char('aaabbc') == 'a'         # true
+p most_frequent_char('') == nil               # true
+
+
+
+# Option 2 (early return `nil`, hash initialization, single pass):
+def most_frequent_char(str)
+  return nil if str.empty?
+
+  frequency_count = Hash.new(0)
+  max_char = nil
+  max_count = 0
+
+  str.each_char do |char|
+    frequency_count[char] += 1
+    if frequency_count[char] > max_count
+      max_count = frequency_count[char]
+      max_char = char
+    end
+  end
+
+  max_char
+end
+
+p most_frequent_char('abcdefgabc') == 'a'     # true
+p most_frequent_char('Launch School') == 'c'  # true
+p most_frequent_char('aaabbc') == 'a'         # true
+p most_frequent_char('') == nil               # true
+
+
+
+
 ## Experiments:
 p "".downcase   # ""
 p "".chars      # []
