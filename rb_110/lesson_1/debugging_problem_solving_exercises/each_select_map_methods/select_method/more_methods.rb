5e68257eca5dda4be42c341fcd45fb48208b8968
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-28 14:00:55 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
@@ -107,9 +107,37 @@ result =  [1, 2, 3].partition do |num|
 p result  # [[1, 3], [2]]
 
 
+
 odd, even = [1, 2, 3].partition do |num|
   num.odd?
 end
 
 p odd   # [1, 3]
 p even  # [2]
+
+
+
+h = { a: "ant", b: "bear", c: "cat" }
+
+long, short = h.partition do |_, value|
+  value.size > 3
+end
+
+p long, short
+# [[:b, "bear"]]
+# [[:a, "ant"], [:c, "cat"]]
+
+p long   # [[:b, "bear"]]
+p short  # [[:a, "ant"], [:c, "cat"]]
+
+=begin
+irb(main):001:0> h = { a: "ant", b: "bear", c: "cat" }
+=> {:a=>"ant", :b=>"bear", :c=>"cat"}
+irb(main):002:1* long, short = h.partition do |_, value|
+irb(main):003:1*   value.size > 3
+irb(main):004:0> end
+=> [[[:b, "bear"]], [[:a, "ant"], [:c, "cat"]]]
+=end
+
+p long.to_h   # {:b=>"bear"}
+p short.to_h  # {:a=>"ant", :c=>"cat"}
