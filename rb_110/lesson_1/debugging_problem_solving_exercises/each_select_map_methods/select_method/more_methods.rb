5c7d44f17de0af33840e3d0d9fccd47382a78781
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-28 10:59:38 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
@@ -1,22 +1,37 @@
 # `Enumerable#any?`:
-result = { a: "ant", b: "bear", c: "cat" }.any? do |_, value|
-  value.size > 4
-end
+result =  { a: "ant", b: "bear", c: "cat" }.any? do |_, value|
+            value.size > 4
+          end
 
 p result # false
 
 
 
 # `Enumerable#all?`:
-result = [1, 2, 3].all? do |num|
-  num > 2
-end
+result =  [1, 2, 3].all? do |num|
+            num > 2
+          end
 
 p result # false
 
 
-result = { a: "ant", b: "bear", c: "cat" }.all? do |_, value|
-  value.length >= 3
-end
+result =  { a: "ant", b: "bear", c: "cat" }.all? do |_, value|
+            value.length >= 3
+          end
 
 p result  # true
+
+
+
+# `Enumerable#each_with_index`:
+result =  [1, 2, 3].each_with_index do |num, index|
+            puts "The index of #{num} is #{index}"
+          end
+
+p result
+
+# Output:
+# The index of 1 is 0
+# The index of 2 is 1
+# The index of 3 is 2
+# [1, 2, 3]
