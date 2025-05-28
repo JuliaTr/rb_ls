a310aa730e539e6a332cdb1162e15211dbcaa2b5
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-28 11:04:30 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/more_methods.rb
@@ -35,3 +35,18 @@ p result
 # The index of 2 is 1
 # The index of 3 is 2
 # [1, 2, 3]
+
+
+h = { a: "ant", b: "bear", c: "cat" }
+
+result = h.each_with_index do |pair, index|
+  puts "The index of #{pair} is #{index}." 
+end
+
+p result 
+
+# Output:
+# The index of [:a, "ant"] is 0
+# The index of [:b, "bear"] is 1
+# The index of [:c, "cat"] is 2
+# {:a=>"ant", :b=>"bear", :c=>"cat"}
