85dfb343cc37a18583c219d51fe0a278e540b513
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-28 15:54:35 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/methods_and_more_methods_practice_problems.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/methods_and_more_methods_practice_problems.rb
@@ -5,3 +5,12 @@ result =  [1, 2, 3].select do |num|
           end  
 
 p result # [1, 2, 3]
+
+
+
+# Practice Problem 2:
+result =  ['ant', 'bat', 'caterpillar'].count do |str|
+            str.length < 4
+          end
+p result  # 2
+
