563d1805d0d609def3b468d48ee88298070eec83
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-29 16:39:12 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/practice_problems_additional_practice.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/practice_problems_additional_practice.rb
@@ -13,3 +13,29 @@ end
 
 p h 
 # {"Fred"=>0, "Barney"=>1, "Wilma"=>2, "Betty"=>3, "Pebbles"=>4, "BamBam"=>5}
+
+
+
+=begin
+Practice Problem 3:
+Remove people with age 100 and greater.
+=end
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+result = ages.keep_if { |_, value| value < 100 }
+p result   # {"Herman"=>32, "Lily"=>30, "Eddie"=>10}
+p ages     # {"Herman"=>32, "Lily"=>30, "Eddie"=>10}
+
+
+
+## Alternatives:
+# Option 1 (keep the same object unchanged):
+result = ages.select { |_, value| value < 100 }
+p result # {"Herman"=>32, "Lily"=>30, "Eddie"=>10}
+p ages   # {"Herman"=>32, "Lily"=>30, "Grandpa"=>402, "Eddie"=>10}
+
+# Option 2:
+result = ages.reject { |_, value| value >= 100 }
+p result     # {"Herman"=>32, "Lily"=>30, "Eddie"=>10}
+p ages       # {"Herman"=>32, "Lily"=>30, "Eddie"=>10}
