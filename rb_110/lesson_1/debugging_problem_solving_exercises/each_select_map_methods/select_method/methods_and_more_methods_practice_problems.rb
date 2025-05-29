a501ec881991d01040f6bb3dd24a12e970d8a8a3
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-29 13:03:31 -0500

    Add experiment

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/methods_and_more_methods_practice_problems.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/methods_and_more_methods_practice_problems.rb
@@ -1,4 +1,4 @@
-# Practice Problem 1:
+#3 Practice Problem 1:
 result =  [1, 2, 3].select do |num|
             num > 5
             'hi'
@@ -8,7 +8,8 @@ p result # [1, 2, 3]
 
 
 
-# Practice Problem 2:
+
+## Practice Problem 2:
 result =  ['ant', 'bat', 'caterpillar'].count do |str|
             str.length < 4
           end
@@ -16,7 +17,8 @@ p result  # 2
 
 
 
-# Practice Problem 3:
+
+## Practice Problem 3:
 result = [1, 2, 3].reject do |num|
   puts num
 end
@@ -31,14 +33,29 @@ p result
 
 
 
-# Practice Problem 4:
+
+## Practice Problem 4:
 arr = ['ant', 'bear', 'cat']
 
-result = arr.each_with_object({}) do |value, hash|
-  hash[value[0]] = value
-end
+result =  arr.each_with_object({}) do |value, hash|
+            hash[value[0]] = value
+          end
 
 p result # {"a"=>"ant", "b"=>"bear", "c"=>"cat"}
 
 p arr.object_id     # 60
 p result.object_id  # 80
+
+
+### Experiment:
+arr = ['ant', 'bear', 'cat']
+hash = {}
+
+p hash.object_id      # 60
+
+result =  arr.each_with_object(hash) do |value, hash|
+            hash[value[0]] = value
+          end
+
+p result             # {"a"=>"ant", "b"=>"bear", "c"=>"cat"}
+p result.object_id   # 60
