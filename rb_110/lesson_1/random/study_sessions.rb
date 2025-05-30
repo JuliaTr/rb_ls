9afb85ff8d78969d960cc58b77c6f3d32c219176
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-30 11:01:23 -0500

    Add exercises

--- rb_110/lesson_1/random/study_sessions.rb
+++ rb_110/lesson_1/random/study_sessions.rb
@@ -6,3 +6,29 @@ result = ['apple', 'banana', 'cherry'].map do |fruit|
 end
 p result
 # [nil, "BANANA", "CHERRY"]
+
+
+
+result = %w(a b c d e).map do |char|
+  if char == 'c'
+    3
+  else
+    char
+  end
+end
+
+p result  # ["a", "b", 3, "d", "e"]
+
+
+
+result = %w(a b c d e).map do |char|
+  if char == 'c'
+    3
+  else
+    char
+  end
+end.select do |element|
+  element.to_i.odd?
+end
+
+p result  # [3]
