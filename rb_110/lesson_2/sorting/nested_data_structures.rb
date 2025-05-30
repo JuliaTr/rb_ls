7096f30120f833de16cb5294f3698b247d1d542a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-30 15:10:26 -0500

    Add exercise

--- rb_110/lesson_2/sorting/nested_data_structures.rb
+++ rb_110/lesson_2/sorting/nested_data_structures.rb
@@ -1,5 +1,13 @@
-## Refernce collection elements
+## Reference collection elements:
 arr = [[1, 3], [2]]
 
 p arr[0]     # [1, 3]
 p arr[0][1]  # 3
+
+
+
+
+## Update collection elements:
+arr = [[1, 3], [2]]
+arr[1] = "hi there"
+p arr  # [[1, 3], "hi there"]
