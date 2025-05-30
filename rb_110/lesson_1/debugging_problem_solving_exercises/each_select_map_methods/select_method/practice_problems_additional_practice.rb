acdd8cdc40095f1c2fc0d1d820f9c3a321ec18ab
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-05-30 10:18:22 -0500

    Add exercise

--- rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/practice_problems_additional_practice.rb
+++ rb_110/lesson_1/debugging_problem_solving_exercises/each_select_map_methods/select_method/practice_problems_additional_practice.rb
@@ -235,3 +235,43 @@ def capitalize_word(str)
 end
 
 p capitalize_word(words)  # "The Flintstones Rock"
+
+
+
+
+=begin
+Practice Problem 10:
+Modify the hash such that each member of the Munster 
+family has an additional "age_group" key that has one of 
+three values describing the age group the family member 
+is in (kid, adult, or senior).
+
+Note: a kid is in the age range 0 - 17, an adult is in
+the range 18 - 64 and a senior is aged 65+.
+=end
+
+munster = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandps" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female" }
+}
+
+munster.each do |name, details|
+  case details["age"]
+  when 0...18
+    details["age_group"] = "kid"
+  when 18...65
+    details["age_group"] = "adult"
+  else
+    details["age_group"] = "senior"
+  end
+end
+
+p munster
+# {"Herman"=>{"age"=>32, "gender"=>"male", "age_group"=>"adult"}, 
+# "Lily"=>{"age"=>30, "gender"=>"female", "age_group"=>"adult"}, 
+# "Grandps"=>{"age"=>402, "gender"=>"male", "age_group"=>"senior"}, 
+# "Eddie"=>{"age"=>10, "gender"=>"male", "age_group"=>"kid"}, 
+# "Marilyn"=>{"age"=>23, "gender"=>"female", "age_group"=>"adult"}}
