de03cf4dba7e921803a31523264e250005310c64
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-01 16:20:56 -0500

    Add exercise

--- rb_110/lesson_2/sorting/shallow_copy_more.rb
+++ rb_110/lesson_2/sorting/shallow_copy_more.rb
@@ -15,3 +15,30 @@ p original
 p copy
 # ["todo", ["buy chocolate milk", "call me"], "new item"]
 
+
+
+
+arr1 = ["a", "b", "c"]
+p arr1.object_id     # 60
+
+arr2 = arr1.dup   # shallow copy
+p arr2.object_id     # 80
+
+# The structure of the outer collection is modified:
+arr2 << "d"       # doesn't affect arr1
+arr2.delete_at(0) # doesn't affect arr1
+
+p arr1              # ["a", "b", "c"]
+p arr2              # ["b", "c", "d"]
+
+# Inner elements (not modified):
+arr2[1].upcase  # not destructive; creates a new object; not assigned to a variable
+
+p arr1              # ["a", "b", "c"]
+p arr2              # ["b", "c", "d"]
+
+# Inner elements (modified):
+arr2[0].upcase!   # affects both arrays
+
+p arr1              # ["a", "B", "c"]
+p arr2              # ["B", "c", "d"]
