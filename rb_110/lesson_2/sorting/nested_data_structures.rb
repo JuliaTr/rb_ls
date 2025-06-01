3bf60f5be11aa262170a975fc12c901eb1abda6c
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-01 15:05:37 -0500

    Add exercise

--- rb_110/lesson_2/sorting/nested_data_structures.rb
+++ rb_110/lesson_2/sorting/nested_data_structures.rb
@@ -111,3 +111,15 @@ end
 
 p arr1  # ["a", "b", "c"]
 p arr2  # ["A", "B", "C"]
+
+
+
+arr1 = ["a", "b", "c"]
+arr2 = arr1.dup
+
+arr2.each do |char|
+  char.upcase!
+end
+
+p arr1   # ["A", "B", "C"]
+p arr2   # ["A", "B", "C"]
