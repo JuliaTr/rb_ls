a6f4fe388ba543060595c5c129c4577753028563
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-01 16:49:56 -0500

    Add exercise

--- rb_110/lesson_2/sorting/working_with_blocks.rb
+++ rb_110/lesson_2/sorting/working_with_blocks.rb
@@ -24,3 +24,19 @@ p result
 # 1
 # 3
 # [nil, nil]
+
+
+
+
+## Example 3:
+result =  [[1, 2], [3, 4]].map do |arr|
+            puts arr.first
+            arr.first
+          end
+
+p result 
+
+# Output:
+# 1
+# 3
+# [1, 3]
