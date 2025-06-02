c1f92e290bccbcd6cb07276e49ff0b797b6d4db0
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-02 15:06:32 -0500

    Add practice_problem2.rb

--- rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem2.rb
+++ rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem2.rb
@@ -12,13 +12,34 @@ books = [
   {title: 'Ulysses', author: 'James Joyce', published: '1922'}
 ]
 
+result = books.sort_by do |inner_hash|
+  inner_hash[:published]
+end
+
+p result
+# [{:title=>"War and Peace", :author=>"Leo Tolstoy", :published=>"1869"}, 
+# {:title=>"Ulysses", :author=>"James Joyce", :published=>"1922"}, 
+# {:title=>"The Great Gatsby", :author=>"F. Scott Fitzgerald", :published=>"1925"}, 
+# {:title=>"One Hundred Years of Solitude", :author=>"Gabriel Garcia Marquez", :published=>"1967"}]
+
+p books
+# [{:title=>"One Hundred Years of Solitude", :author=>"Gabriel Garcia Marquez", :published=>"1967"}, 
+# {:title=>"The Great Gatsby", :author=>"F. Scott Fitzgerald", :published=>"1925"}, 
+# {:title=>"War and Peace", :author=>"Leo Tolstoy", :published=>"1869"}, 
+# {:title=>"Ulysses", :author=>"James Joyce", :published=>"1922"}]
 
 
 
 ### Experiments:
-books.sort_by do |inner_hash|
-  p inner_hash[:published].to_i
+result = books.sort_by do |inner_hash|
+  inner_hash[:published].to_i  # no need to convert into integers
 end
+
+p result
+# [{:title=>"War and Peace", :author=>"Leo Tolstoy", :published=>"1869"}, 
+# {:title=>"Ulysses", :author=>"James Joyce", :published=>"1922"}, 
+# {:title=>"The Great Gatsby", :author=>"F. Scott Fitzgerald", :published=>"1925"}, 
+# {:title=>"One Hundred Years of Solitude", :author=>"Gabriel Garcia Marquez", :published=>"1967"}]
 
 p books
 # [{:title=>"One Hundred Years of Solitude", :author=>"Gabriel Garcia Marquez", :published=>"1967"}, 
