aef7158ba034fef9efbbc470358658fd07b9e60a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-02 11:10:35 -0500

    Add exercise

--- rb_110/lesson_2/sorting/working_with_blocks.rb
+++ rb_110/lesson_2/sorting/working_with_blocks.rb
@@ -221,3 +221,20 @@ end
 p remove_evens!([1, 1, 2, 3, 4, 6, 8, 9])
 # expected return value [1, 1, 3, 9]
 # actual return value [1, 1, 3, 6, 9]
+
+
+# Bug fix:
+def remove_evens!(arr)
+  cloned_arr = arr.dup
+
+  cloned_arr.each do |num|
+    if num % 2 == 0
+      arr.delete(num)
+    end
+  end
+
+  arr
+end
+
+p remove_evens!([1, 1, 2, 3, 4, 6, 8, 9])  # [1, 1, 3, 9]
+# Works as expected
