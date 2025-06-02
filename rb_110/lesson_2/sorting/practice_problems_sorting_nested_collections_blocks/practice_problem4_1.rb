11e7b45295fa798710fc6e45d707b9d3357f9c50
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-02 15:44:26 -0500

    Add alternative way

--- rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem4_1.rb
+++ rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem4_1.rb
@@ -4,9 +4,10 @@ For each of these collection objects where the value `3` occurs,
 demonstrate how you would change this to `4`.
 =end
 
-## Example 1:
+# ## Example 1:
 arr1 = [1, [2, 3], 4]
 
+# Non-destructive:
 result = arr1.map do |element|
   if element.to_s.to_i != element
 
@@ -26,3 +27,10 @@ end
 
 p result   # [1, [2, 4], 4]
 p arr1     # [1, [2, 3], 4]
+
+
+
+## Alternative:
+# Destructive:
+p arr1[1][1] = 4  # 4
+p arr1            # [1, [2, 4], 4]
