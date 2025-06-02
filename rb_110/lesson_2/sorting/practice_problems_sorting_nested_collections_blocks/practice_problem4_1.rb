5d9e87ee5b88ac9db9dc58c78fb470bf4cd19c74
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-02 15:34:01 -0500

    Add practice_problem4_1.rb

---
+++ rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem4_1.rb
@@ -0,0 +1,28 @@
+=begin
+Practice Problem 4
+For each of these collection objects where the value `3` occurs, 
+demonstrate how you would change this to `4`.
+=end
+
+## Example 1:
+arr1 = [1, [2, 3], 4]
+
+result = arr1.map do |element|
+  if element.to_s.to_i != element
+
+    element.map do |sub_element|
+      if sub_element == 3
+        sub_element = 4
+      else
+        sub_element
+      end
+    end
+
+  else
+    element
+  end
+
+end
+
+p result   # [1, [2, 4], 4]
+p arr1     # [1, [2, 3], 4]
