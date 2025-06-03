d0a51706eabacf3113bbd0906733dae674b1c1db
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-03 11:04:02 -0500

    Add practice_problem11.rb

---
+++ rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem11.rb
@@ -0,0 +1,19 @@
+=begin
+Practice Problem 11
+Given the following data structure use a combination of 
+methods, including either the `select` or `reject` method, 
+to return a new array identical in structure to the 
+original but containing only the integers that are 
+multiples of 3.
+=end
+
+arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
+
+result = arr.map do |sub_array|
+  sub_array.select do |number|
+    number % 3 == 0
+  end
+end
+
+p result  # [[], [3, 12], [9], [15]]
+ 
