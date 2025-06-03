812c6f022bb6604843ace83756257af8567c2195
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-02 21:31:42 -0500

    Add practice_problem5.rb

---
+++ rb_110/lesson_2/sorting/practice_problems_sorting_nested_collections_blocks/practice_problem5.rb
@@ -0,0 +1,24 @@
+=begin
+Practice Problem 5
+Determine the total age of just the male members of the family.
+=end
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+array_of_age = []
+
+munsters.each do |sub_hash|
+  if sub_hash[1]["gender"] == 'male'
+    array_of_age << sub_hash[1]["age"]
+  end
+end
+
+p array_of_age             # [32, 402, 10]
+
+p array_of_age.inject(:+)  # 444
