cfeee52190d4935752cf066e90d10ec4dee975de
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-04 16:06:26 -0500

    Add 7.rb

---
+++ rb_100/intro_ruby_book/hashes/7.rb
@@ -0,0 +1,8 @@
+=begin
+Exercise:
+If you see this error, what do you suspect is the most likely problem?
+
+NoMethodError: undefined method `keys' for Array
+=end
+
+# B. There is no method called keys for Array objects.
