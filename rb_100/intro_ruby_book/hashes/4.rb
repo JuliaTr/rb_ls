b26f79ebf9466e76d376977c990398a68a6622cf
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-04 11:10:55 -0500

    Add 4.rb

---
+++ rb_100/intro_ruby_book/hashes/4.rb
@@ -0,0 +1,9 @@
+=begin
+Exercise:
+Given the following expression, how would you access the 
+name of the person?
+=end
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+p person[:name]  # "Bob"
