3e7286b4e3d07f35d1a3518a11bd2419b30413bc
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-04 10:52:34 -0500

    Uncomment

--- rb_100/intro_ruby_book/hashes/2.rb
+++ rb_100/intro_ruby_book/hashes/2.rb
@@ -1,58 +1,58 @@
-# =begin
-# Exercise:
-# Look at Ruby's merge method. Notice that it has two 
-# versions. What is the difference between `merge` and `merge!`? 
-# Write a program that uses both and illustrate the differences.
-# =end
+=begin
+Exercise:
+Look at Ruby's merge method. Notice that it has two 
+versions. What is the difference between `merge` and `merge!`? 
+Write a program that uses both and illustrate the differences.
+=end
 
-# ## `#merge` (non-destructive):
-# hsh = { 'cat' => 'pet ', 
-#         'hummingbird' => 'wild bird', 
-#         'elephant' => 'wild mammal'
-#       }
-# hsh1 = { 'salmon' => 'fish', 'cat' => 'fluffy' }
-# hsh2 = { 'tree' => 'flora' }
-# result = hsh.merge(hsh1, hsh2) do |key, old_value, new_value|
-#   old_value + new_value
-# end
+## `#merge` (non-destructive):
+hsh = { 'cat' => 'pet ', 
+        'hummingbird' => 'wild bird', 
+        'elephant' => 'wild mammal'
+      }
+hsh1 = { 'salmon' => 'fish', 'cat' => 'fluffy' }
+hsh2 = { 'tree' => 'flora' }
+result = hsh.merge(hsh1, hsh2) do |key, old_value, new_value|
+  old_value + new_value
+end
 
-# p result 
-# # {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
-# # "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
+p result 
+# {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
+# "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
 
-# p hsh
-# # {"cat"=>"pet ", "hummingbird"=>"wild bird", "elephant"=>"wild mammal"}
+p hsh
+# {"cat"=>"pet ", "hummingbird"=>"wild bird", "elephant"=>"wild mammal"}
 
-# p hsh1 # {"salmon"=>"fish", "cat"=>"fluffy"}
-# p hsh2 # {"tree"=>"flora"}
+p hsh1 # {"salmon"=>"fish", "cat"=>"fluffy"}
+p hsh2 # {"tree"=>"flora"}
 
 
 
-# ## `#merge!` (destructive):
-# hsh = { 'cat' => 'pet ', 
-#         'hummingbird' => 'wild bird', 
-#         'elephant' => 'wild mammal'
-#       }
+## `#merge!` (destructive):
+hsh = { 'cat' => 'pet ', 
+        'hummingbird' => 'wild bird', 
+        'elephant' => 'wild mammal'
+      }
 
-# hsh1 = { 'salmon' => 'fish', 'cat' => 'fluffy' }
-# hsh2 = { 'tree' => 'flora' }
+hsh1 = { 'salmon' => 'fish', 'cat' => 'fluffy' }
+hsh2 = { 'tree' => 'flora' }
 
-# result = hsh.merge!(hsh1, hsh2) do |key, old_value, new_value|
-#   old_value + new_value
-# end
+result = hsh.merge!(hsh1, hsh2) do |key, old_value, new_value|
+  old_value + new_value
+end
 
-# p result
-# # {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
-# # "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
+p result
+# {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
+# "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
 
-# p hsh 
-# # {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
-# # "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
+p hsh 
+# {"cat"=>"pet fluffy", "hummingbird"=>"wild bird", 
+# "elephant"=>"wild mammal", "salmon"=>"fish", "tree"=>"flora"}
 
-# p hsh1 # {"salmon"=>"fish", "cat"=>"fluffy"}
-# p hsh2 # {"tree"=>"flora"}
+p hsh1 # {"salmon"=>"fish", "cat"=>"fluffy"}
+p hsh2 # {"tree"=>"flora"}
 
-# # The original object the `#merge!` is called on is mutated.
+# The original object the `#merge!` is called on is mutated.
 
 
 
