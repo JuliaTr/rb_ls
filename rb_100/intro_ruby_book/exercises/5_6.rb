faefaacc62e556360faabbcc1bcd1e9fbf2b6c2d
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-05 10:23:20 -0500

    Add exercise

--- rb_100/intro_ruby_book/exercises/5_6.rb
+++ rb_100/intro_ruby_book/exercises/5_6.rb
@@ -1,6 +1,9 @@
 =begin
 Exercise:
 Get rid of 11. And append a 3.
+
+Get rid of duplicates without specifically removing any one 
+value.
 =end
 
 arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
@@ -19,3 +22,13 @@ p arr     # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
 
 # arr.append(3)   # option
 # p arr       # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
+
+
+## Get rid of duplicates:
+# Doesn't modify the calling object
+p arr.uniq   # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p arr        # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
+
+# Modifies the calling object
+arr.uniq!
+p arr        # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
