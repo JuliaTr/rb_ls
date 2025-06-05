aa96b09a2bb6088a581e3618b570476b66f50b28
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-05 11:22:05 -0500

    Add 7.rb

---
+++ rb_100/intro_ruby_book/exercises/7.rb
@@ -0,0 +1,9 @@
+=begin
+Exercise:
+What's the major difference between an Array and a Hash?
+
+Answer:
+Array and Hash are data structures. Array are zero index-based 
+ordered list of objects. Hashes contain key-value pairs for 
+referencing by key.
+=end
