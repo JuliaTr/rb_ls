67d4accb48fad9efdca3bfbfa297692121daad8a
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-05 10:17:45 -0500

    Add 5_6.rb

---
+++ rb_100/intro_ruby_book/exercises/5_6.rb
@@ -0,0 +1,21 @@
+=begin
+Exercise:
+Get rid of 11. And append a 3.
+=end
+
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+## Get rid of 11:
+arr.pop
+p arr    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+## And append a 3.
+arr.push(3)
+p arr     # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
+
+# arr << 3        # option
+# p arr     # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
+
+# arr.append(3)   # option
+# p arr       # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
