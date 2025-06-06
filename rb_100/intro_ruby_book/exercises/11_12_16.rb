5b1768e623f48ae158c1ab2de777583b45d7dc74
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-06 09:33:13 -0500

    Add solutions

--- rb_100/intro_ruby_book/exercises/11_12_16.rb
+++ rb_100/intro_ruby_book/exercises/11_12_16.rb
@@ -69,6 +69,46 @@ p contacts
 # Works as expected
 
 
+
+## Solution from the book:
+
+# with `each` and `shift` method:
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each do |name, hash|
+  fields.each do |field|
+    hash[field] = contact_data.shift
+  end
+end
+
+p contacts 
+# {"Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", 
+# :phone=>"555-123-4567"}}
+
+
+
+# with `each_with_index`, `each`, `shift`:
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each_with_index do |(name, hash), idx|
+  fields.each do |field|
+    hash[field] = contact_data[idx].shift
+  end
+end
+
+p contacts
+# {"Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", 
+# :phone=>"555-123-4567"}, "Sally Johnson"=>{:email=>"sally@email.com", 
+# :address=>"404 Not Found Dr.", :phone=>"123-234-3454"}}
+
+
+
+
 =begin
 Exercise 12:
 Using the hash you created from the previous exercise, 
