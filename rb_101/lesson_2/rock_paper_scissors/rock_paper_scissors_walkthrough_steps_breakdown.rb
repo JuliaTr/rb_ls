0ebd754760aeccfa00bef7e3a5e42cff995c2cdb
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-09 15:48:13 -0500

    Add step 5

--- rb_101/lesson_2/rock_paper_scissors/rock_paper_scissors_walkthrough_steps_breakdown.rb
+++ rb_101/lesson_2/rock_paper_scissors/rock_paper_scissors_walkthrough_steps_breakdown.rb
@@ -1,3 +1,67 @@
+## Step 5:
+# To play again:
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
+
+  if (choice == 'rock' && computer_choice == 'scissor') ||
+      (choice == 'paper' && computer_choice == 'rock') ||
+      (choice == 'scissors' && computer_choice == 'paper')
+    prompt("You won!")
+  elsif (choice == 'rock' && computer_choice == 'paper') ||
+        (choice == 'rock' && computer_choice == 'scissors') ||
+        (choice == 'scissors' && computer_choice == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
+
+=begin
+
+$ ruby rock_paper_scissors_walkthrough_steps_breakdown.rb
+=> Choose one: rock, paper, scissors
+rock
+=> You choose: rock; Computer choose: rock
+=> It's a tie!
+=> Do you want to play again?
+y
+=> Choose one: rock, paper, scissors
+paper
+=> You choose: paper; Computer choose: paper
+=> It's a tie!
+=> Do you want to play again?
+n
+=> Thank you for playing. Good bye!
+
+=end
+
+
 ## Step 4:
 # Calculate who wins:
 VALID_CHOICES = ['rock', 'paper', 'scissors']
