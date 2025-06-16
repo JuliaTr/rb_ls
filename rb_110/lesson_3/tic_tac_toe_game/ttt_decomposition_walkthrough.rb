3d4ec925582dcb10d42616f3c002535bfb82e3dc
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-16 10:06:48 -0500

    Add steps 3 and 4

--- rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
+++ rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
@@ -8,19 +8,195 @@ row, then the game is a tie.
 
 
 DECOMPOSITION
-1. Display the initial empty 3x3 board.
-2. Ask the user to mark a square.
+1. Display the initial empty 3x3 board.  # loop 1 starts
+2. Ask the user to mark a square.   # loop 2 starts
 3. Computer marks a square.
 4. Display the updated board state.
 5. If winner, display winner.
 6. If board is full, display tie.
-7. If neither winner nor board is full, go to #2
+7. If neither winner nor board is full, go to #2  # stopping condition
 8. Play again?
-9. If yes, go to #1
+9. If yes, go to #1   # loop 1 ends
 10. Good bye!
 =end
 
-# Step 2:
+## Step 4:
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts " #{brd[1]}   | #{brd[2]}   | #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   | #{brd[5]}   | #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   | #{brd[8]}   | #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = ' '}
+  new_board
+end
+
+def player_places_piece(brd)
+  # Want the user to only choose a key whare the value is space.
+  square = ''
+  loop do
+    prompt("Choose a square (1-9):")
+    square = gets.chomp.to_i
+
+    # If user did input of one of the available choices
+    if brd.keys.select { |num| brd[num] == ' ' }.include?(square)
+      break
+    else # We'll keep going
+      prompt "Sorry, that's not a valid choice."
+    end
+  end
+
+  # When we have a valid square we set the marker.
+  brd[square] = 'x'
+end
+
+=begin
+{1=>" ", 2=>" ", 3=>"x", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" ", 10=>"x"}
+
+irb(main):005:0> brd.keys
+=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+irb(main):006:0> brd.keys.select {|num| brd[num] == ' '}
+=> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+irb(main):007:0> brd.keys.select {|num| brd[num] == ' '}.include?(3)
+=> true
+=end
+
+# 1. Display the initial empty 3x3 board.
+board = initialize_board  # hash
+display_board(board)      # empty board
+
+# 2. Ask the user to mark a square.
+player_places_piece(board)    # player puts a piece on the board; modifies `board`
+puts board.inspect
+display_board(board)
+
+
+
+## Step 3:
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts " #{brd[1]}   | #{brd[2]}   | #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   | #{brd[5]}   | #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   | #{brd[8]}   | #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = ' '}
+  new_board
+end
+
+def player_places_piece(brd)
+  prompt("Choose a square (1-9):")
+  square = gets.chomp.to_i
+  brd[square] = 'x'      # modifies the existing value for the key in the `board` hash
+  # A new kay-value pairs are created in the hash, if user enters `10`. 
+  # We won't use it when we display the board.
+end
+
+# 1. Display the initial empty 3x3 board.
+board = initialize_board  # hash
+display_board(board)      # empty board
+
+# 2. Ask the user to mark a square.
+player_places_piece(board)    # player puts a piece on the board; modifies `board`
+puts board.inspect
+display_board(board)          # board with a piece; modified board
+
+=begin
+$ ruby ttt_decomposition_walkthrough.rb
+
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+
+=> Choose a square (1-9):
+3
+{1=>" ", 2=>" ", 3=>"x", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
+
+     |     |
+     |     | x
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+
+$ ruby ttt_decomposition_walkthrough.rb
+
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+
+=> Choose a square (1-9):
+10
+{1=>" ", 2=>" ", 3=>" ", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" ", 10=>"x"}
+
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+=end
+
+
+
+## Step 2:
 # 1. Display the initial empty 3x3 board.
 def display_board(brd)
   puts ""
@@ -49,7 +225,7 @@ display_board(board)
 
 
 
-# Step 1:
+## Step 1:
 # 1. Display the initial empty 3x3 board.
 def display_board
   puts ""
