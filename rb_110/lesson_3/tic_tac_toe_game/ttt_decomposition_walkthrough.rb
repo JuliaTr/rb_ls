01cd137d8d7a72ac377c2c24ac078be22dc00536
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-16 16:02:33 -0500

    Add step 6

--- rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
+++ rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
@@ -22,6 +22,101 @@ DECOMPOSITION
 10. Good bye!
 =end
 
+## Step 6:
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts " #{brd[1]}   | #{brd[2]}   | #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   | #{brd[5]}   | #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   | #{brd[8]}   | #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER } 
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
+    square = gets.chomp.to_i
+
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+# 1. Display the initial empty 3x3 board.
+board = initialize_board  # hash
+display_board(board)      # empty board
+
+# 2. Ask the user to mark a square.
+player_places_piece!(board)    # player puts a piece on the board; modifies `board`
+# 3. Computer marks a square.
+computer_places_piece!(board)
+display_board(board)
+
+=begin
+$ ruby ttt_decomposition_walkthrough.rb
+
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+
+=> Choose a square (1, 2, 3, 4, 5, 6, 7, 8, 9):
+1
+
+     |     |
+ X   |     |  
+     |     |
+-----+-----+-----
+     |     |
+     | O   |  
+     |     |
+-----+-----+-----
+     |     |
+     |     |  
+     |     |
+=end
+
+
 ## Practice using `pry`:
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
@@ -116,9 +211,9 @@ def player_places_piece!(brd)
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
-  binding.pry
+  # binding.pry
   brd[square] = PLAYER_MARKER
-  binding.pry
+  # binding.pry
 end
 
 =begin
