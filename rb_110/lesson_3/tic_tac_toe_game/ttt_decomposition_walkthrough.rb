1e5f52955e80162784b3b6a282649e172da7046b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2025-06-16 09:23:20 -0500

    Add step 2

--- rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
+++ rb_110/lesson_3/tic_tac_toe_game/ttt_decomposition_walkthrough.rb
@@ -20,6 +20,34 @@ DECOMPOSITION
 10. Good bye!
 =end
 
+# Step 2:
+# 1. Display the initial empty 3x3 board.
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts " #{brd[1]}   | #{brd[2]}   | #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   | #{brd[5]}   | #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   | #{brd[8]}   | #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = ' ' }  # creates a new key-value pair in a hash
+  new_board
+end
+
+board = initialize_board
+display_board(board)
+
+
 
 # Step 1:
 # 1. Display the initial empty 3x3 board.
