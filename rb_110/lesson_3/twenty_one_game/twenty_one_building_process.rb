0cc0702dcd9bd7d4bcea6fd5397f4f600cc16eae
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-06-26 16:40:51 -0500

    Add dealer turn

--- rb_110/lesson_3/twenty_one_game/twenty_one_building_process.rb
+++ rb_110/lesson_3/twenty_one_game/twenty_one_building_process.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 =begin
 1. Initialize deck
 2. Deal card to player and dealer
@@ -10,7 +12,12 @@
 7. Compare cards and declare winner.
 =end
 
-## Step 4:
+## Step 5:
+J_Q_K_A_VALUES = 11
+J_Q_K_A_VALUES_BUSTED = 10
+PLAYER_TOTAL = 21
+DEALER_TOTAL = 17
+
 def prompt(message)
   puts "=> #{message}"
 end
@@ -60,7 +67,7 @@ def display_message_dealer(dealer_hand)
 end
 
 ## Calculate sum including Aces:
-def total(hand)
+def total(hand, total)
   # hand = [['H', '3'], ['5', 'Q'], ... ]
 
   values = hand.map { |card| card[1] }
@@ -68,9 +75,9 @@ def total(hand)
   sum = 0
   values.each do |value|
     if value == "A"
-      sum += 11
+      sum += J_Q_K_A_VALUES
     elsif value.to_i == 0 # J, Q, K
-      sum += 10
+      sum += J_Q_K_A_VALUES_BUSTED
     else
       sum += value.to_i
     end
@@ -78,88 +85,237 @@ def total(hand)
 
   # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+    sum -= J_Q_K_A_VALUES_BUSTED if sum > total
   end
 
   p sum
 end
 
-def busted?(hand)
-  # should return `true` or `false` if score > 21
-  return true if total(hand) > 21
+# should return `true` or `false` if score > 21
+def busted?(hand, total)
+  return true if total(hand, total) > total
   false
 end
 
-deck = initialize_deck(card_values, suits)
+def player_turn(deck, player_hand)
+  # answer = nil
+  loop do
+    # p player_hand
+    # total(player_hand, PLAYER_TOTAL)
+
+    prompt "Hit or stay?"
+    answer = gets.chomp
+
+    if (answer == 'stay') || busted?(player_hand, PLAYER_TOTAL)
+      break
+    else
+      give_additional_card(deck, player_hand)
+    end
+  end
+
+  # answer
+end
+
+def play_again?
+  prompt "Do you want to play again? (Press Y or 'Enter' key to continue)"
+  answer = gets.chomp
+  answer.downcase.start_with?('y') || answer == ''
+end
+
+
+# Main loop
 player_hand = []
 dealer_hand = []
 
-game_set = game_setup(deck, player_hand)
-display_message_player(player_hand)
-
-game_set = game_setup(deck, dealer_hand)
-display_message_dealer(dealer_hand)
-
-
-## 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-=begin
-1. ask "hit" or "stay"
-2. if "stay", stop asking
-3. otherwise, go to #1
-=end
-
-answer = nil
 loop do
-  prompt "Hit or stay?"
-  answer = gets.chomp
+  deck = initialize_deck(card_values, suits)
+  game_set = game_setup(deck, player_hand)
+  display_message_player(player_hand)
 
-  p busted?(player_hand)
-  break if answer == 'stay' || busted?(player_hand)
-  give_additional_card(deck, player_hand)
-  p player_hand
-end
+  game_set = game_setup(deck, dealer_hand)
+  display_message_dealer(dealer_hand)
 
-if busted?(player_hand)
-  # probably end the game? or ask the user to play again?
-  prompt "Player is busted. The game is over."
-else
+  # 3. Player turn: hit or stay
+  player_turn(deck, player_hand)
+
+  # 4. If palyer bust, dealer wins.
+  if busted?(player_hand, PLAYER_TOTAL)
+    # probably end the game? or ask the user to play again?
+    prompt "Player is busted. The game is over."
+    prompt "Dealer won."
+    break unless play_again?
+  end
   # if player didn't bust, must have stayed to get here
   prompt "You chose to stay!"
+
+
+  # 5. Dealer turn: hit or stay
+  # - repeat until total >= 17
+  display_message_dealer(dealer_hand)
+  until total(dealer_hand, DEALER_TOTAL) >= DEALER_TOTAL
+    # p dealer_hand
+    give_additional_card(deck, dealer_hand)
+    # display_message_player(player_hand)
+    display_message_dealer(dealer_hand)
+    # p total(dealer_hand, DEALER_TOTAL)
+  end
+
+  break
 end
 
-# ... continue on to Dealer turn
 
-=begin
-$ ruby twenty_one_building_process.rb
-=> Player has: [["Hearts", "2"], ["Hearts", "3"]]
-=> Dealer has: [["Hearts", "4"], ["Hearts", "5"]]
-=> Hit or stay?
-hit
-5
-false
-5
-[["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"]]
-=> Hit or stay?
-hit
-11
-false
-11
-[["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"], ["Hearts", "7"]]
-=> Hit or stay?
-hit
-18
-false
-18
-[["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"], ["Hearts", "7"], ["Hearts", "8"]]
-=> Hit or stay?
-hit
-26
-true
-26
-26
-=> Player is busted. The game is over.
-=end
+
+
+# ## Step 4:
+# def prompt(message)
+#   puts "=> #{message}"
+# end
+
+# card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+#                'J', 'Q', 'K', 'A']
+
+# suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+# # Ex. [['H', '2'], ['S', 'J'], ['D', 'A']]
+# def initialize_deck(crd_values, suits)
+#   # [[suit, value], [suit, value] ... ]
+#   suit_value_pairs = []
+
+#   suits.each do |suit|
+#     crd_values.each do |card|
+#       suit_value_pairs << [suit, card]
+#     end
+#   end
+
+#   suit_value_pairs
+# end
+
+# # Deletes the first inner array
+# def deal_card!(deck)
+#   deck.shift
+# end
+
+# def game_setup(deck, hand)
+#   2.times do 
+#     dealt_card = deal_card!(deck)
+#     hand << dealt_card 
+#   end
+# end
+
+# def give_additional_card(deck, hand)
+#   dealt_card = deal_card!(deck)
+#   hand << dealt_card
+# end
+
+# def display_message_player(plyer_hand)
+#   prompt "Player has: #{plyer_hand}"
+# end
+
+# def display_message_dealer(dealer_hand)
+#   prompt "Dealer has: #{dealer_hand}"
+# end
+
+# ## Calculate sum including Aces:
+# def total(hand)
+#   # hand = [['H', '3'], ['5', 'Q'], ... ]
+
+#   values = hand.map { |card| card[1] }
+
+#   sum = 0
+#   values.each do |value|
+#     if value == "A"
+#       sum += 11
+#     elsif value.to_i == 0 # J, Q, K
+#       sum += 10
+#     else
+#       sum += value.to_i
+#     end
+#   end
+
+#   # correct for Aces
+#   values.select { |value| value == "A" }.count.times do
+#     sum -= 10 if sum > 21
+#   end
+
+#   p sum
+# end
+
+# def busted?(hand)
+#   # should return `true` or `false` if score > 21
+#   return true if total(hand) > 21
+#   false
+# end
+
+# deck = initialize_deck(card_values, suits)
+# player_hand = []
+# dealer_hand = []
+
+# game_set = game_setup(deck, player_hand)
+# display_message_player(player_hand)
+
+# game_set = game_setup(deck, dealer_hand)
+# display_message_dealer(dealer_hand)
+
+
+# ## 3. Player turn: hit or stay
+# #   - repeat until bust or "stay"
+# =begin
+# 1. ask "hit" or "stay"
+# 2. if "stay", stop asking
+# 3. otherwise, go to #1
+# =end
+
+# answer = nil
+# loop do
+#   prompt "Hit or stay?"
+#   answer = gets.chomp
+
+#   p busted?(player_hand)
+#   break if answer == 'stay' || busted?(player_hand)
+#   give_additional_card(deck, player_hand)
+#   p player_hand
+# end
+
+# if busted?(player_hand)
+#   # probably end the game? or ask the user to play again?
+#   prompt "Player is busted. The game is over."
+# else
+#   # if player didn't bust, must have stayed to get here
+#   prompt "You chose to stay!"
+# end
+
+# # ... continue on to Dealer turn
+
+# =begin
+# $ ruby twenty_one_building_process.rb
+# => Player has: [["Hearts", "2"], ["Hearts", "3"]]
+# => Dealer has: [["Hearts", "4"], ["Hearts", "5"]]
+# => Hit or stay?
+# hit
+# 5
+# false
+# 5
+# [["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"]]
+# => Hit or stay?
+# hit
+# 11
+# false
+# 11
+# [["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"], ["Hearts", "7"]]
+# => Hit or stay?
+# hit
+# 18
+# false
+# 18
+# [["Hearts", "2"], ["Hearts", "3"], ["Hearts", "6"], ["Hearts", "7"], ["Hearts", "8"]]
+# => Hit or stay?
+# hit
+# 26
+# true
+# 26
+# 26
+# => Player is busted. The game is over.
+# =end
 
 
 
