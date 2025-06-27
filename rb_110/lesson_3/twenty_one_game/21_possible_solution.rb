8392d60626dc3b9ab7f1c1d2fe3815c4bd9a1ff7
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-06-26 21:03:52 -0500

    Add total method

--- rb_110/lesson_3/twenty_one_game/21_possible_solution.rb
+++ rb_110/lesson_3/twenty_one_game/21_possible_solution.rb
@@ -9,4 +9,25 @@ def initialize_deck
   SUITS.product(VALUES).shuffle
 end
 
-p initialize_deck
+def total(cards)
+  # cards = [['H', '3'], ['S', 'Q']]
+  values = cards.map { |card| card[1] }
+
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0  # J, Q, K
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+
+  # correct for Aces
+  values.selec { |value| value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+
+  sum
+end
