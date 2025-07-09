ba204c44a36e4eee220dc0672125b9930582d3c3
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-08 20:15:00 -0500

    Add further exploration

--- rb_100/ruby_basics/loops1/control_the_loop.rb
+++ rb_100/ruby_basics/loops1/control_the_loop.rb
@@ -24,3 +24,23 @@ end
 # Number of iterations = 3
 # Number of iterations = 4
 # Number of iterations = 5
+
+
+
+### Further exploration
+# If the break statement is moved up one line so it runs 
+# before iterations is incremented, what would need to be 
+# changed?
+iterations = 1
+loop do
+  puts "Number of iterations = #{iterations}"
+  break if iterations == 5
+  iterations += 1
+end
+
+# Output:
+# Number of iterations = 1
+# Number of iterations = 2
+# Number of iterations = 3
+# Number of iterations = 4
+# Number of iterations = 5
