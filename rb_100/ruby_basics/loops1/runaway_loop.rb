c11e82527df321e57a79b2ded77fa875a7e94843
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-08 19:52:53 -0500

    Add runaway_loop.rb

---
+++ rb_100/ruby_basics/loops1/runaway_loop.rb
@@ -0,0 +1,13 @@
+# Modify the code so the loop stops after the first iteration.
+loop do
+  puts 'Just keep printing...'
+end
+
+
+## Solution:
+loop do
+  puts 'Just keep printing...'
+  break
+end
+
+# Output: Just keep printing...
