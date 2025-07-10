5a241130f34d5fc434832c874bdd742f837b8ec0
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-10 18:51:34 -0500

    Add even_or_odd.rb

---
+++ rb_100/ruby_basics/loops2/even_or_odd.rb
@@ -0,0 +1,25 @@
+# Write a loop that prints numbers 1-5 and whether the 
+# number is even or odd. Use the code below to get started.
+
+
+## Solution:
+count = 1
+
+loop do
+  if count.odd?
+    puts "#{count} is odd!"
+    count += 1
+  else
+    puts "#{count} is even!"
+    count += 1
+  end
+  
+  break if count > 5
+end
+
+# Output:
+# 1 is odd!
+# 2 is even!
+# 3 is odd!
+# 4 is even!
+# 5 is odd!
