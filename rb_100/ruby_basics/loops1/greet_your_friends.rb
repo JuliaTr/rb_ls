b511384b19ca889282b64c160e42ac5e1ee1b007
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-10 10:23:31 -0500

    Add greet_your_friends.rb

---
+++ rb_100/ruby_basics/loops1/greet_your_friends.rb
@@ -0,0 +1,13 @@
+# Given the following array of names, use a for loop to greet 
+# each friend individually.
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
+
+# Output:
+# Hello, Sarah!
+# Hello, John!
+# Hello, Hannah!
+# Hello, Dave!
