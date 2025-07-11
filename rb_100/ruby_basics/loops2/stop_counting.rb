c6cbae07f37a3686b3f89fb14378c4b7fa1e373e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-11 12:04:48 -0500

    Add stop_counting.rb

---
+++ rb_100/ruby_basics/loops2/stop_counting.rb
@@ -0,0 +1,20 @@
+=begin
+The method below counts from 0 to 4. Modify the block so 
+that it prints the current number and stops iterating when 
+the current number equals `2`.
+=end
+5.times do |index|
+  puts index
+end
+
+
+## Solution:
+5.times do |index|
+  puts index
+  break if index == 2
+end
+
+# Output:
+# 0
+# 1
+# 2
