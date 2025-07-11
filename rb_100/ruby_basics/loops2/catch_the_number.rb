9129abc07a24db77b517542eb92ba8ba19006995
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-10 19:26:48 -0500

    Add refactored solution

--- rb_100/ruby_basics/loops2/catch_the_number.rb
+++ rb_100/ruby_basics/loops2/catch_the_number.rb
@@ -1,9 +1,51 @@
-# # Modify the following code so that the loop stops if number 
-# # is equal to or between 0 and 10.
-# loop do
-#   number = rand(100)
-#   puts number
-# end
+# Modify the following code so that the loop stops if number 
+# is equal to or between 0 and 10.
+loop do
+  number = rand(100)
+  puts number
+end
+
+## Refactored:
+loop do
+  number = rand(100)
+  puts number
+  break if (0..10).include?(number)
+end
+
+=begin
+$ ruby catch_the_number.rb
+83
+42
+73
+82
+98
+35
+66
+90
+77
+59
+24
+95
+63
+15
+71
+83
+42
+32
+80
+22
+15
+4
+$ ruby catch_the_number.rb
+95
+81
+50
+17
+69
+42
+10
+=end
+
 
 
 ## Solution:
