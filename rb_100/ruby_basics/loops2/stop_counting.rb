aff0b58c7f3cd8dac6deec2da9d294708da4fc1e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-11 15:02:17 -0500

    Add stop_counting.rb

--- rb_100/ruby_basics/loops2/stop_counting.rb
+++ rb_100/ruby_basics/loops2/stop_counting.rb
@@ -18,3 +18,29 @@ end
 # 0
 # 1
 # 2
+
+
+
+### Further exploration:
+# How many values will be printed? -> 5 values
+5.times do |index|
+  puts index
+  break if index == 4
+end
+
+# Output
+# 0
+# 1
+# 2
+# 3
+# 4
+
+
+
+# How many values will be printed? -> 1 value
+5.times do |index|
+  puts index
+  break if index < 7   # 0 < 7   => true
+end
+
+# Output: 0
