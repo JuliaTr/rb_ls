697fea267217ef30a094337da553e97269b07488
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-15 18:15:43 -0500

    Uncomment

--- rb_100/ruby_basics/user_input/launch_school_printer_part2.rb
+++ rb_100/ruby_basics/user_input/launch_school_printer_part2.rb
@@ -42,48 +42,48 @@ q
 
 
 
-# ## Possible solution:
-# loop do
-#   input_string = nil
-#   number_of_lines = nil
+## Possible solution:
+loop do
+  input_string = nil
+  number_of_lines = nil
 
-#   loop do
-#     puts '>> How many output lines do you want? ' \
-#          'Enter a number >= 3 (Q to quit):'
-#     input_string = gets.chomp.downcase
-#     break if input_string == 'q'
+  loop do
+    puts '>> How many output lines do you want? ' \
+         'Enter a number >= 3 (Q to quit):'
+    input_string = gets.chomp.downcase
+    break if input_string == 'q'
 
-#     number_of_lines = input_string.to_i
-#     break if number_of_lines >= 3
+    number_of_lines = input_string.to_i
+    break if number_of_lines >= 3
 
-#     puts ">> That's not enough lines."
-#   end
+    puts ">> That's not enough lines."
+  end
 
-#   break if input_string == 'q'
+  break if input_string == 'q'
 
-#   while number_of_lines > 0
-#     puts 'Launch School is the best!'
-#     number_of_lines -= 1
-#   end
-# end
+  while number_of_lines > 0
+    puts 'Launch School is the best!'
+    number_of_lines -= 1
+  end
+end
 
-# =begin
-# $ ruby launch_school_printer_part2.rb
-# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
-# 5
-# Launch School is the best!
-# Launch School is the best!
-# Launch School is the best!
-# Launch School is the best!
-# Launch School is the best!
-# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
-# 2
-# >> That's not enough lines.
-# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
-# 3
-# Launch School is the best!
-# Launch School is the best!
-# Launch School is the best!
-# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
-# q
-# =end
+=begin
+$ ruby launch_school_printer_part2.rb
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+5
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+2
+>> That's not enough lines.
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+3
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+q
+=end
