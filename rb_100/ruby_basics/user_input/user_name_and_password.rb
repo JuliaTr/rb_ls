dfbba4a450649db5aa1b7da2e1a0061656b8d13f
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-14 20:32:29 -0500

    Validate each entry separately

--- rb_100/ruby_basics/user_input/user_name_and_password.rb
+++ rb_100/ruby_basics/user_input/user_name_and_password.rb
@@ -72,3 +72,54 @@ admin
 SecreT
 Welcome!
 =end
+
+
+
+## Validate each entry separately:
+NAME = 'admin'
+PASSWORD = 'SecreT'
+
+def get_user_name
+  puts '>> Please enter user name:'
+  valid_user_name = gets.chomp
+end
+
+def get_password
+  puts '>> Please enter your password:'
+  valid_password = gets.chomp
+end
+
+loop do
+  valid_user_name = get_user_name
+  valid_password = get_password
+
+  break if valid_user_name == NAME && 
+           valid_password == PASSWORD
+  puts '>> Authorization failed!'
+end
+
+puts 'Welcome!'
+
+=begin
+$ ruby user_name_and_password.rb
+>> Please enter user name:
+John
+>> Please enter your password:
+Hello
+>> Authorization failed!
+>> Please enter user name:
+mary
+>> Please enter your password:
+SecreT
+>> Authorization failed!
+>> Please enter user name:
+admin
+>> Please enter your password:
+root
+>> Authorization failed!
+>> Please enter user name:
+admin
+>> Please enter your password:
+SecreT
+Welcome!
+=end
