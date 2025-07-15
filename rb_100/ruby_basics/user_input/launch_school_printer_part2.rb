e2a60efd495f88a111b6f005f46a77267a9001b4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-15 18:14:52 -0500

    Add launch_school_printer_part2.rb

---
+++ rb_100/ruby_basics/user_input/launch_school_printer_part2.rb
@@ -0,0 +1,89 @@
+=begin
+Modify this program so it repeats itself after each 
+input/print iteration, asking for a new number each time 
+through. The program should keep running until the user 
+enters `q` or `Q`.
+=end
+
+loop do
+  puts '>> How many output lines do you want? ' \
+       'Enter a number >= 3 (Q to quit):'
+  user_input = gets.chomp
+
+  break if user_input.downcase == 'q'
+
+  if user_input.to_i < 3
+    puts "That's not enough lines."
+  else
+    user_input.to_i.times { puts 'Launch School is the best!' }
+  end
+end
+
+=begin
+$ ruby launch_school_printer_part2.rb
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+5
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+2
+That's not enough lines.
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+3
+Launch School is the best!
+Launch School is the best!
+Launch School is the best!
+>> How many output lines do you want? Enter a number >= 3 (Q to quit):
+q
+=end
+
+
+
+# ## Possible solution:
+# loop do
+#   input_string = nil
+#   number_of_lines = nil
+
+#   loop do
+#     puts '>> How many output lines do you want? ' \
+#          'Enter a number >= 3 (Q to quit):'
+#     input_string = gets.chomp.downcase
+#     break if input_string == 'q'
+
+#     number_of_lines = input_string.to_i
+#     break if number_of_lines >= 3
+
+#     puts ">> That's not enough lines."
+#   end
+
+#   break if input_string == 'q'
+
+#   while number_of_lines > 0
+#     puts 'Launch School is the best!'
+#     number_of_lines -= 1
+#   end
+# end
+
+# =begin
+# $ ruby launch_school_printer_part2.rb
+# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
+# 5
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
+# 2
+# >> That's not enough lines.
+# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
+# 3
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# >> How many output lines do you want? Enter a number >= 3 (Q to quit):
+# q
+# =end
