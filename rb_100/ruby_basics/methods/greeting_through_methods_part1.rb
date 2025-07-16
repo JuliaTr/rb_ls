0f7b11f9aaa79fd2a3c03455a5989fa5cff7f1c0
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-15 20:32:53 -0500

    Add greeting_through_methods_part1.rb

---
+++ rb_100/ruby_basics/methods/greeting_through_methods_part1.rb
@@ -0,0 +1,16 @@
+=begin
+Write two methods, one that returns the string `"Hello"` and 
+one that returns the string `"World"`. Then print both strings 
+using `#puts`, combining them into one sentence.
+=end
+
+def return_first_word
+  'Hello'
+end
+
+def return_second_word
+  'World'
+end
+
+puts "#{return_first_word} #{return_second_word}"
+# Hello World
