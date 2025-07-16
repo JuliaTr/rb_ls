2e9a37458fcbeda309d625b667f929433d9cec33
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-16 17:55:02 -0500

    Improve explanation

--- rb_100/ruby_basics/return/breakfast_lunch_or_dinner_part1.rb
+++ rb_100/ruby_basics/return/breakfast_lunch_or_dinner_part1.rb
@@ -13,8 +13,8 @@ On line 7, the `meal` method is invoked and its return value is
 passed as an argument to the `puts` method invocation.
 
 Lines 3-5 contain `meal` method definition. Line 4 is the only 
-line executed by the mehod definition. Also, the `return` key
-word returns the value on the same line. So, the value `"Breakfast"`
+line executed by the method definition. Also, the `return` key
+word returns the value on the same line explicitly. So, the value `"Breakfast"`
 is returned by `meal`. The `puts meal` call outputs `Breakfast` 
 to the console.
 =end
