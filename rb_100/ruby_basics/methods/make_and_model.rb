63a9739800bcf62cd63a2eb40bf93eb90195d615
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-16 12:29:51 -0500

    Add further exploration

--- rb_100/ruby_basics/methods/make_and_model.rb
+++ rb_100/ruby_basics/methods/make_and_model.rb
@@ -13,6 +13,31 @@ def car(make, model)
   puts "#{make} #{model}"
 end
 
-car('Toyota', 'Corolla')
+p car('Toyota', 'Corolla')  # nil
 
-# Output: Toyota Corolla
+# Output: 
+# Toyota Corolla
+# nil
+
+
+
+## Furher exploration:
+# Remove the `#puts` call from the car method. Modify your 
+# program so it still prints the result.
+def car(make, model)
+  "#{make} #{model}"
+end
+
+puts car('Toyota', 'Corolla')
+p car('Toyota', 'Corolla')
+
+# Output:
+# Toyota Corolla
+# "Toyota Corolla"
+
+=begin
+The `puts` method (line 13) always returns `nil`. The method definition 
+returns the last and the only line executed (lines 16 ('nil'), 
+32 (`"Toyota Corolla"`)). That's why the return values of `car` 
+method is different.
+=end
