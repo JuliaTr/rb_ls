a2b12f796294aca51556c2087a99f97a0665d07d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-18 10:37:38 -0500

    Add true_false.rb

--- rb_100/ruby_basics/conditionals/true_false.rb
+++ rb_100/ruby_basics/conditionals/true_false.rb
@@ -6,7 +6,6 @@ equals `true` and prints `"I'm false!"` if boolean equals `false`.
 =end
 
 boolean = [true, false].sample
-
 boolean ? puts("I'm true!") : puts("I'm false!")
 
 =begin
@@ -17,3 +16,25 @@ $ ruby true_false.rb
 I'm true!
 =end
 
+
+
+## Experiments:
+boolean = [true, false].sample
+boolean ? puts "I'm true!" : puts "I'm false!"
+
+=begin
+true_false.rb:22: syntax error, unexpected string literal, expecting `do' or '{' or '(' (SyntaxError)
+boolean ? puts "I'm true!" : puts "I'm false!"
+=end
+
+
+boolean = [true, false].sample
+puts (boolean ? "I'm true!" : "I'm false!")
+
+=begin
+$ ruby true_false.rb
+I'm false!
+
+$ ruby true_false.rb
+I'm true!
+=end
