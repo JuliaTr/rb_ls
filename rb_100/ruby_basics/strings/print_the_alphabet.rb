34ffc943aba55ccafef405e107f8d42c5e0cb9a4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-21 13:50:29 -0500

    Add experiments

--- rb_100/ruby_basics/strings/print_the_alphabet.rb
+++ rb_100/ruby_basics/strings/print_the_alphabet.rb
@@ -1,48 +1,50 @@
-# Using the following code, split the value of `alphabet` 
-# by individual characters and print each character.
+# # Using the following code, split the value of `alphabet` 
+# # by individual characters and print each character.
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-
-## Solution:
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet.chars.each { |char| puts char }
-
-# Output:
-# a
-# b
-# c
-# d
-# ...
-# v
-# w
-# x
-# y
-# z
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 
+# ## Solution:
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# alphabet.chars.each { |char| puts char }
 
-## Possible solution:
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-puts alphabet.split('')
+# # Output:
+# # a
+# # b
+# # c
+# # d
+# # ...
+# # v
+# # w
+# # x
+# # y
+# # z
 
-# Output:
-# a
-# b
-# c
-# d
-# ...
-# v
-# w
-# x
-# y
-# z
+
+
+# ## Possible solution:
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# puts alphabet.split('')
+
+# # Output:
+# # a
+# # b
+# # c
+# # d
+# # ...
+# # v
+# # w
+# # x
+# # y
+# # z
 
 
 # Experiments:
 a = 'Wonderful'
 p a.split('')   # ["W", "o", "n", "d", "e", "r", "f", "u", "l"]
+p a.split(' ')  # ["Wonderful"]
 p a.split       # ["Wonderful"]
 
 b = 'Wonderful World'
 p b.split       # ["Wonderful", "World"]
+p b.split(' ')  # ["Wonderful", "World"]
