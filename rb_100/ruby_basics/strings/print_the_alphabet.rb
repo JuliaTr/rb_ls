dc87ae5e51329e4579be0593c53f6f1e80297054
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-21 13:41:21 -0500

    Add print_the_alphabet.rb

---
+++ rb_100/ruby_basics/strings/print_the_alphabet.rb
@@ -0,0 +1,48 @@
+# Using the following code, split the value of `alphabet` 
+# by individual characters and print each character.
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+## Solution:
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet.chars.each { |char| puts char }
+
+# Output:
+# a
+# b
+# c
+# d
+# ...
+# v
+# w
+# x
+# y
+# z
+
+
+
+## Possible solution:
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+puts alphabet.split('')
+
+# Output:
+# a
+# b
+# c
+# d
+# ...
+# v
+# w
+# x
+# y
+# z
+
+
+# Experiments:
+a = 'Wonderful'
+p a.split('')   # ["W", "o", "n", "d", "e", "r", "f", "u", "l"]
+p a.split       # ["Wonderful"]
+
+b = 'Wonderful World'
+p b.split       # ["Wonderful", "World"]
