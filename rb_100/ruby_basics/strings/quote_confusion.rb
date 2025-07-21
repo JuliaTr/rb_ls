33101e76fd07917a16eb33ce692008adc1651ecc
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-21 11:30:22 -0500

    Add quote_confusion.rb

---
+++ rb_100/ruby_basics/strings/quote_confusion.rb
@@ -0,0 +1,14 @@
+# Modify the following code so that double-quotes are used instead of single-quotes.
+puts 'It\'s now 12 o\'clock.'
+# It's now 12 o'clock.
+
+
+## Solution:
+puts "It's now 12 o'clock."
+# It's now 12 o'clock.
+
+
+
+### Experiments:
+p 'Hello\nworld'       # "Hello\\nworld"
+puts 'Hello\nworld'    # Hello\nworld
