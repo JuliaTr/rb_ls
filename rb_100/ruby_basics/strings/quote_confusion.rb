b11df795f34550b804f05d2ae83ea329c865e72e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-21 11:36:45 -0500

    Add further exploration

--- rb_100/ruby_basics/strings/quote_confusion.rb
+++ rb_100/ruby_basics/strings/quote_confusion.rb
@@ -1,14 +1,24 @@
 # Modify the following code so that double-quotes are used instead of single-quotes.
-puts 'It\'s now 12 o\'clock.'
-# It's now 12 o'clock.
+
+puts 'It\'s now 12 o\'clock.'   # It's now 12 o'clock.
 
 
 ## Solution:
-puts "It's now 12 o'clock."
-# It's now 12 o'clock.
+puts "It's now 12 o'clock."     # It's now 12 o'clock.
 
 
 
 ### Experiments:
 p 'Hello\nworld'       # "Hello\\nworld"
 puts 'Hello\nworld'    # Hello\nworld
+
+
+
+### Further exploration:
+# String contain single-quotes:
+puts %Q(It's now 12 o'clock.)   # It's now 12 o'clock.
+puts %q(It's now 12 o'clock.)   # It's now 12 o'clock.
+
+# String contain double-quotes:
+puts %Q("Deep Work" by Cal Newport)  # "Deep Work" by Cal Newport
+puts %q("Deep Work" by Cal Newport)  # "Deep Work" by Cal Newport
