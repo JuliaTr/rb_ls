c1f9a1d6c8c574ce5239a15d21a5f9adabc9509a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-22 10:45:45 -0500

    Add free_lizard.rb

---
+++ rb_100/ruby_basics/arrays/free_lizard.rb
@@ -0,0 +1,13 @@
+=begin
+Remove `'lizard'` from my_pets then print the value of 
+`my_pets`.
+
+Expected output: I have a pet fish!
+=end
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
+puts "I have a pet #{my_pets[0]}!"
+# I have a pet fish!
