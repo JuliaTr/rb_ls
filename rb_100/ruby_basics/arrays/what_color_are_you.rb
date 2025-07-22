710d91d143c8ce0300373af5ef90fca68feea912
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-22 11:01:54 -0500

    Add what_color_are_you.rb

---
+++ rb_100/ruby_basics/arrays/what_color_are_you.rb
@@ -0,0 +1,31 @@
+=begin
+Use `Array#each` to iterate over `colors` and print each 
+element.
+
+Expected output:
+I'm the color red!
+I'm the color yellow!
+I'm the color purple!
+I'm the color green!
+=end
+
+colors = ['red', 'yellow', 'purple', 'green']
+colors.each { |color| puts "I'm the color #{color}!" }
+
+# Output:
+# I'm the color red!
+# I'm the color yellow!
+# I'm the color purple!
+# I'm the color green!
+
+
+
+## Experiment:
+colors = ['red', 'yellow', 'purple', 'green']
+puts colors
+
+# Output:
+# red
+# yellow
+# purple
+# green
