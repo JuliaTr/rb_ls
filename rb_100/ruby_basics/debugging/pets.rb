a516d6acfa23d2eaa2ed8d91a63cf45651c96bb7
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-23 11:17:15 -0500

    Add pets.rb

--- rb_100/ruby_basics/debugging/pets.rb
+++ rb_100/ruby_basics/debugging/pets.rb
@@ -19,3 +19,46 @@ pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
 pets[:dog][2] = 'bowser'
 p pets  
 # {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
+
+
+
+### Experiments:
+
+## Possible solution (`Array#push`):
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog].push('bowser')
+p pets
+# {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
+
+
+# `Array#<<`:
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] << 'bowser'
+p pets
+# {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
+
+
+# `Array#append`:
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog].append('bowser')
+p pets 
+# {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
+
+
+# `+` (don't mutate the original object):
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+p pets[:dog] + ['bowser']  # ["sparky", "fido", "bowser"]
+p pets
+# {:cat=>"fluffy", :dog=>["sparky", "fido"], :fish=>"oscar"}
+
+
+# `+`, `[]=`:
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] = pets[:dog] + ['bowser']  # mutates the original object
+p pets
+# {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
