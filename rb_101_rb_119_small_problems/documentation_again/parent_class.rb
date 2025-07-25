b8d53482d5241f1d3bb5b5875918c797a1751e32
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-25 14:15:36 -0500

    Add comments

--- rb_101_rb_119_small_problems/documentation_again/parent_class.rb
+++ rb_101_rb_119_small_problems/documentation_again/parent_class.rb
@@ -1,9 +1,22 @@
-# Modify the code to print just the public methods that are defined or overridden by the `String` class to exclude all members that are only defined in `Object`, `BasicObject`, and `Kernel`?
+=begin
+Modify the code to print just the public methods that are 
+defined or overridden by the `String` class to exclude all 
+members that are only defined in `Object`, `BasicObject`, and
+`Kernel`?
+=end
 
 s = 'abc'
 puts s.public_methods.inspect
+
+=begin
+All public methods of String are output, including the methods
+inherited from `Object` (which inherits other methods from the 
+`BasicObject` class and `Kernel`)
+=end
 
 
 ## Solution:
 s = 'abc'
 puts s.public_methods(false).inspect
+
+# The output includes only methods of `String` class.
