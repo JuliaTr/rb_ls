eabcd2cd9d858110adaea524e0c632890166010c
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-25 14:03:04 -0500

    Add comments

--- rb_101_rb_119_small_problems/documentation_again/keyword_arguments.rb
+++ rb_101_rb_119_small_problems/documentation_again/keyword_arguments.rb
@@ -1,7 +1,9 @@
 # What will this code print?
 
 5.step(to: 10, by: 3) { |value| puts value }
-# Output:
-# 5
-# 8
 
+# Output:
+# 5        # begins with self; first iteration
+# 8        # interval of "step" value (5 + 3 = 8); second iteration
+
+# On the last iteration the loop stops because `11 > 10`.
