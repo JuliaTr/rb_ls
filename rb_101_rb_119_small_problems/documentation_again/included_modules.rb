bd39483faf6e02285d36c8564d6176bfcd532599
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-28 09:53:19 -0500

    Update

--- rb_101_rb_119_small_problems/documentation_again/included_modules.rb
+++ rb_101_rb_119_small_problems/documentation_again/included_modules.rb
@@ -1,17 +1,15 @@
 # Change the code to print the two smallest values in the `Array`.
-# version 2.3.0 of the Ruby Array Documentation
+# Use version 2.3.0 of the Ruby Array Documentation.
 
 a = [5, 9, 3, 11]
-puts a.min
-# Output:
-# 3
-# 5
+puts a.min        # 3
 
 
 ## Solution:
-
+# https://docs.ruby-lang.org/en/2.3.0/Enumerable.html#method-i-min
 a = [5, 9, 3, 11]
 puts a.min(2)
+
 # Output:
 # 3
 # 5
