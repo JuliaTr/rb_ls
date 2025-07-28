2d7c7571c082f3c9a69b065a00516e87ae0edb26
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-28 10:11:57 -0500

    Add repeat_yourself.rb

---
+++ rb_101_rb_119_small_problems/easy_1_pass2/repeat_yourself.rb
@@ -0,0 +1,23 @@
+=begin
+Write a method that takes two arguments, a string and a 
+positive integer, and prints the string as many times as the 
+integer indicates.
+
+repeat('Hello', 3)
+
+Output:
+Hello
+Hello
+Hello
+=end
+
+def repeat(str, int)
+  int.times { puts str }
+end
+
+repeat('Hello', 3)
+
+# Output:
+# Hello
+# Hello
+# Hello
