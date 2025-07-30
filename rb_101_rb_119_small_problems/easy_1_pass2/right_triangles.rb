a008b243f0167f718ff6e71af7dfbf4f9fae5bcd
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-30 15:58:55 -0500

    Add right_triangles.rb

---
+++ rb_101_rb_119_small_problems/easy_1_pass2/right_triangles.rb
@@ -0,0 +1,110 @@
+=begin
+Write a method that takes a positive integer, `n`, as an 
+argument, and displays a right triangle whose sides each have 
+`n` stars. The hypotenuse of the triangle (the diagonal side 
+in the images below) should have one end at the lower-left of 
+the triangle, and the other end at the upper-right.
+
+triangle(5)
+    *
+   **
+  ***
+ ****
+*****
+
+triangle(9)
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
+
+____________________________________________________________
+
+PEDAC:
+Reqs/rules:
+- `n` is an argument
+- `n` is a number of stars at the bottom of the triangle
+- display right triangle
+- triangle's sides have `n` start
+- 
+
+Algorithm:
+- Define a `triangle` method which takes an integer as an argument
+- Output empty strings number times less 1 than the number of 
+  star and one star
+- Increment number of stars till the number of stars is equal to `n`
+=end
+
+def triangle(n)
+  star = '*'
+  white_space = ' '
+
+  star_number = 1
+  while n >= star_number
+    puts "#{white_space * (n - star_number)}#{star * star_number}"
+    star_number += 1
+  end
+end
+
+triangle(5)
+=begin
+    *
+   **
+  ***
+ ****
+*****
+=end
+
+triangle(9)
+=begin
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
+=end
+
+
+
+## Possible solution:
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
+
+triangle(5)
+=begin
+    *
+   **
+  ***
+ ****
+*****
+=end
+
+triangle(9)
+=begin
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
+=end
