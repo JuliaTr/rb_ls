4bd5f372bb4aa68a936aa10fbf31aafd268c7409
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-07-30 10:40:20 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_1_pass2/bannerizer.rb
+++ rb_101_rb_119_small_problems/easy_1_pass2/bannerizer.rb
@@ -158,3 +158,118 @@ $ ruby bannerizer.rb
 |  |
 +--+
 =end
+
+
+
+### Further exploration:
+=begin
+Modify this method so it will truncate the message if it will 
+be too wide to fit inside a standard terminal window (80 
+columns, including the sides of the box).
+
+__________________________________________________________
+
+PEDAC:
+
+Reqs/rules:
+- If the length of the string is more than 76 characters,
+  the text should be wrapped by the frame.
+- 80 columns include spaces left, right, sides of the box.
+- String must have maximum 76 characters
+
+Examples:
+
++------------------------------------------------------------------------------+
+|                                                                              |
+| To boldly go where no one has gone before. To boldly go where no one has gon |
+| e before. To boldly go where no one has gone before.                         |
+|                                                                              |
++------------------------------------------------------------------------------+
+
+
+Data Structure: string
+
+
+Algorithm for slicing string:
+- If the length of the string is more than 76 characters
+  - Split string into array of strings, with each string of 76 characters
+  - Output each string
+
+
+Notes:
+76 - length of text
+2 - borders
+2 - white space
+
+80 = 76 + 2 + 2
+=end
+
+PLUS_SIGN = '+'
+PIPE = '|'
+DASH = '-'
+EMPTY_SPACE = ' '
+MAX_WIDTH = 76
+
+def print_in_box_text_wrap(str)
+  borders = "#{PLUS_SIGN}#{DASH * (MAX_WIDTH + 2)}#{PLUS_SIGN}"
+  space_around_string = "#{PIPE} #{EMPTY_SPACE * MAX_WIDTH} #{PIPE}"
+
+  puts borders
+  puts space_around_string
+
+  if str.length > MAX_WIDTH
+    until str.length == 0
+      sliced_str = str.slice!(0..75)
+      puts "#{PIPE} #{sliced_str}#{EMPTY_SPACE * (MAX_WIDTH - sliced_str.length)} #{PIPE}"
+    end
+  end
+
+  puts space_around_string
+  puts borders
+end
+
+print_in_box_text_wrap('To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before.')
+
+=begin
+$ ruby bannerizer.rb
++------------------------------------------------------------------------------+
+|                                                                              |
+| To boldly go where no one has gone before. To boldly go where no one has gon |
+| e before. To boldly go where no one has gone before.                         |
+|                                                                              |
++------------------------------------------------------------------------------+
+=end
+
+
+
+# ### Experiments:
+# str = 'To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before.'
+
+# p str.split(' ', 76)
+# =begin
+# ["To", "boldly", "go", "where", "no", "one", "has", "gone", 
+# "before.", "To", "boldly", "go", "where", "no", "one", "has", 
+# "gone", "before.", "To", "boldly", "go", "where", "no", "one", 
+# "has", "gone", "before."]
+# =end
+
+# p str.slice(0..75)
+# p str
+
+# =begin
+# "To boldly go where no one has gone before. To boldly go where 
+# no one has gon"
+
+# "To boldly go where no one has gone before. To boldly go where 
+# no one has gone before. To boldly go where no one has gone 
+# before."
+# =end
+
+# p str.slice!(0..75)
+# p str
+
+# =begin
+# "To boldly go where no one has gone before. To boldly go where 
+# no one has gon"
+# "e before. To boldly go where no one has gone before."
+# =end
