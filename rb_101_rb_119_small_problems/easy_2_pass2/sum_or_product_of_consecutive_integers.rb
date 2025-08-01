c1ce104593445ff49d89ec9096ac65a107f24974
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-01 14:43:23 -0500

    Add improved solution

--- rb_101_rb_119_small_problems/easy_2_pass2/sum_or_product_of_consecutive_integers.rb
+++ rb_101_rb_119_small_problems/easy_2_pass2/sum_or_product_of_consecutive_integers.rb
@@ -18,6 +18,59 @@ p
 The product of the integers between 1 and 6 is 720.
 =end
 
+## Improved solution:
+# Calculation performs only when user selects it.
+puts '>> Please enter an integer greater than 0:'
+number = gets.chomp.to_i
+
+puts ">> Enter 's' to compute the sum, 'p' to compute the product."
+answer_sum_or_product = gets.chomp
+
+array = (1..number).to_a
+
+case answer_sum_or_product
+when 's'
+  sum = array.sum
+  puts "The sum of the integers between 1 and #{number} is #{sum}."
+when 'p'
+  product = 1
+  counter = 0
+  while counter < array.size
+    product = product * array[counter]
+    counter += 1
+  end
+  puts "The product of the integers between 1 and #{number} is #{product}."
+else
+  puts "We don't have this operation. Please select 's' (sum) " \
+       "or 'p' (product) next time. Good bye!"
+end
+
+=begin
+$ ruby sum_or_product_of_consecutive_integers.rb
+>> Please enter an integer greater than 0:
+5
+>> Enter 's' to compute the sum, 'p' to compute the product.
+s
+The sum of the integers between 1 and 5 is 15.
+
+$ ruby sum_or_product_of_consecutive_integers.rb
+>> Please enter an integer greater than 0:
+6
+>> Enter 's' to compute the sum, 'p' to compute the product.
+p
+The product of the integers between 1 and 6 is 720.
+
+$ ruby sum_or_product_of_consecutive_integers.rb
+>> Please enter an integer greater than 0:
+8
+>> Enter 's' to compute the sum, 'p' to compute the product.
+f
+We don't have this operation. Please select 's' (sum) or 'p' 
+(product) next time. Good bye!
+=end
+
+
+
 ## Solution:
 puts '>> Please enter an integer greater than 0:'
 number = gets.chomp.to_i
