0f4104474673c650568eb05c0874f6f34b72fb3f
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-01 11:54:44 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_2_pass2/tip_calculator.rb
+++ rb_101_rb_119_small_problems/easy_2_pass2/tip_calculator.rb
@@ -32,3 +32,22 @@ What is the tip percentage? 15
 The tip is $30.0
 The total is $230.0
 =end
+
+
+
+## Further exploration:
+# Modify your solution so it always prints the results with 2 
+# decimal places. Example: `$30.00` and `$230.00`.
+PERCENTAGE = 100
+
+print 'What is the bill? '
+bill = gets.to_f
+
+print 'What is the tip percentage? '
+tip_percentage = gets.to_f
+
+tip_in_dollars = bill * (tip_percentage / PERCENTAGE)
+total = bill + tip_in_dollars
+
+puts "The tip is $#{format('%.2f', tip_in_dollars)}"
+puts "The total is $#{format('%.2f', total)}"
