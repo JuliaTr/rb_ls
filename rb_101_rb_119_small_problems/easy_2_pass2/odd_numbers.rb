fb218f90d4d7ec2b363ded2a8d2ba7136700f9d1
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-01 13:45:03 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_2_pass2/odd_numbers.rb
+++ rb_101_rb_119_small_problems/easy_2_pass2/odd_numbers.rb
@@ -3,6 +3,7 @@ Print all odd numbers from 1 to 99, inclusive, to the console,
 with each number on a separate line.
 =end
 
+# `Array#select`, `Array#each`, `Integer#odd?`
 odd_numbers = (1..99).select { |num| num.odd? }
 odd_numbers.each { |num| puts num }
 
@@ -31,6 +32,29 @@ while value <= 99
   puts value
   value += 2
 end
+
+=begin
+1
+3
+5
+7
+9
+....
+85
+87
+89
+91
+93
+95
+97
+99
+=end
+
+
+
+## Further exploration:
+# `Integer#upto`:
+1.upto(99) { |number| puts number if number.odd? }
 
 =begin
 1
