df7ccc2bfef40036a85c4c9f9d1684f85a3fb43f
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-01 13:32:17 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_2_pass2/greeting_a_user.rb
+++ rb_101_rb_119_small_problems/easy_2_pass2/greeting_a_user.rb
@@ -3,6 +3,8 @@ Write a program that will ask for user's name. The program
 will then greet the user. If the user writes "name!" then the 
 computer yells back to the user.
 =end
+
+# `String#chomp` (non-destructive):
 print 'What is your name? '
 name = gets.chomp
 
@@ -12,19 +14,24 @@ else
   puts "Hello #{name}."
 end
 
+p name
+
 =begin
 $ ruby greeting_a_user.rb
 What is your name? Bob
 Hello Bob.
+"Bob"
 
 $ ruby greeting_a_user.rb
 What is your name? Bob!
 HELLO BOB. WHY ARE WE SCREAMING?
+"Bob!"
 =end
 
 
 
 ## Possible solution:
+# `String#chop` (non-destructive):
 print 'What is your name? '
 name = gets.chomp
 
@@ -35,12 +42,65 @@ else
   puts "Hello #{name}."
 end
 
+p name
+
 =begin
 $ ruby greeting_a_user.rb
 What is your name? Bob
 Hello Bob.
+"Bob"
 
 $ ruby greeting_a_user.rb
 What is your name? Bob!
 HELLO BOB. WHY ARE WE SCREAMING?
+"Bob"
+=end
+
+
+
+## Further exploration:
+# `String#chop!` (destructive):
+print 'What is your name? '
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chop!
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
+p name
+
+=begin
+Tested only `if` branch:
+
+$ ruby greeting_a_user.rb
+What is your name? Bob!
+HELLO BOB. WHY ARE WE SCREAMING?
+"Bob"
+=end
+
+
+
+# `String#chomp!` (destructive):
+print 'What is your name? '
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chomp!('!')
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
+p name
+
+=begin
+Tested only `if` branch:
+
+$ ruby greeting_a_user.rb
+What is your name? Bob!
+HELLO BOB. WHY ARE WE SCREAMING?
+"Bob"
 =end
