ad3aac73647cef88d2c802890f205baa6261591a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-01 11:03:49 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_2_pass2/how_old_is_teddy.rb
+++ rb_101_rb_119_small_problems/easy_2_pass2/how_old_is_teddy.rb
@@ -15,3 +15,31 @@ Teddy is 80 years old!
 $ ruby how_old_is_teddy.rb
 Teddy is 94 years old!
 =end
+
+
+
+## Further exploration:
+=begin
+Modify this program to ask for a name, and then print the age 
+for that person. For an extra challenge, use "Teddy" as the 
+name if no name is entered.
+=end
+
+puts 'Type a name:'
+name = gets.chomp.capitalize
+
+def display_age(name='Teddy')
+  age = rand(20..200)
+  "#{name} is #{age} years old!"
+end
+
+puts display_age
+puts display_age(name)
+
+=begin
+$ ruby how_old_is_teddy.rb
+Type a name:
+yuliia
+Teddy is 168 years old!
+Yuliia is 61 years old!
+=end
