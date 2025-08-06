d314248d113310f93abc6c84c26ebd8131ba16b0
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-06 10:21:45 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_3_pass2/multiplying_two_numbers.rb
+++ rb_101_rb_119_small_problems/easy_3_pass2/multiplying_two_numbers.rb
@@ -14,8 +14,18 @@ end
 p multiply(5, 3) == 15   # true
 
 
+
 ## Experiments:
 p 5 * 2            # 10
 p "hi" * 2         # "hihi"
 p [1, 2, 3] * 2    # [1, 2, 3, 1, 2, 3]
 
+
+
+## Further exploration:
+def multiply(number1, number2)
+  number1 * number2
+end
+
+p multiply([1, 2, 3], 3)
+# [1, 2, 3, 1, 2, 3, 1, 2, 3]
