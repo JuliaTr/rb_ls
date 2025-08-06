e70c9f806ec1fb6392ff8a2223607f912784a338
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-06 13:54:36 -0500

    Add improved solution with edge cases for further exploration

--- rb_101_rb_119_small_problems/easy_3_pass2/squaring_an_argument.rb
+++ rb_101_rb_119_small_problems/easy_3_pass2/squaring_an_argument.rb
@@ -18,7 +18,7 @@ def square(number)
 end
 
 p square(5) == 25    # true
-p square(-8) == 64   # true
+p square(-8) == 64   # true  (-8 * -8 = 64)
 
 
 
@@ -27,6 +27,33 @@ p square(-8) == 64   # true
 Write a generic method to a "power to the n" type method: cubed, to 
 the 4th power, to the 5th, etc. Use `multiply` method?
 =end
+
+## Improved solution with edge cases:
+def multiply(number1, number2)
+  number1 * number2
+end
+
+def power_to_n(number, n)
+  case n
+  when 0 then 1
+  when 1 then number
+  else
+    power = multiply(number, number)
+
+    while (n - 2) > 0
+      power = multiply(power, number)
+      n -= 1
+    end
+
+    power
+  end
+end
+
+p power_to_n(5, 4) == 625       # true
+p power_to_n(-8, 5) == -32768   # true
+p power_to_n(4, 1) == 4         # true
+p power_to_n(3, 0) == 1         # true
+
 
 
 ## Solution:
@@ -45,8 +72,8 @@ def cubed(number, power_to_n)
   cubed_number
 end
 
-p cubed(5, 4) == 625      # true
-p cubed(-8, 5) == 32768   # true
+p cubed(5, 4) == 625       # true
+p cubed(-8, 5) == -32768   # true
 
 
 
