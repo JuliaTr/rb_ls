fe8c1257c34414cd4b794e247e7d4d1ac00df1dc
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-06 13:08:00 -0500

    Add squaring_an_argument.rb

--- rb_101_rb_119_small_problems/easy_3_pass2/squaring_an_argument.rb
+++ rb_101_rb_119_small_problems/easy_3_pass2/squaring_an_argument.rb
@@ -19,3 +19,45 @@ end
 
 p square(5) == 25    # true
 p square(-8) == 64   # true
+
+
+
+## Further exploration:
+=begin
+Write a generic method to a "power to the n" type method: cubed, to 
+the 4th power, to the 5th, etc. Use `multiply` method?
+=end
+
+
+## Solution:
+def multiply(number1, number2)
+  number1 * number2
+end
+
+def cubed(number, power_to_n)
+  cubed_number = multiply(number, number)
+
+  while (power_to_n - 2) > 0
+    cubed_number = cubed_number * number
+    power_to_n -= 1
+  end
+
+  cubed_number
+end
+
+p cubed(5, 4) == 625      # true
+p cubed(-8, 5) == 32768   # true
+
+
+
+## Experiments:
+cubed_number = 5 * 5
+cubed_number = cubed_number * 5
+cubed_number = cubed_number * 5
+p cubed_number   # 625 (as expected)
+
+cubed_number = 8 * 8
+cubed_number = cubed_number * 8
+cubed_number = cubed_number * 8
+cubed_number = cubed_number * 8
+p cubed_number  # 32768 (as expected)
