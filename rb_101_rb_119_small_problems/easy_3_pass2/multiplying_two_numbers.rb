0b211a4665c3810c6c737bff608f1e762ed684a5
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-06 10:19:06 -0500

    Add experiments

--- rb_101_rb_119_small_problems/easy_3_pass2/multiplying_two_numbers.rb
+++ rb_101_rb_119_small_problems/easy_3_pass2/multiplying_two_numbers.rb
@@ -6,8 +6,16 @@ Example:
 multiply(5, 3) == 15
 =end
 
+## Solution:
 def multiply(number1, number2)
   number1 * number2
 end
 
 p multiply(5, 3) == 15   # true
+
+
+## Experiments:
+p 5 * 2            # 10
+p "hi" * 2         # "hihi"
+p [1, 2, 3] * 2    # [1, 2, 3, 1, 2, 3]
+
