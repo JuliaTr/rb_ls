50f1b2d6c79936029ed5caa9161d26b262b7b294
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-06 10:26:55 -0500

    Add squaring_an_argument.rb

---
+++ rb_101_rb_119_small_problems/easy_3_pass2/squaring_an_argument.rb
@@ -0,0 +1,21 @@
+=begin
+Using the multiply method from the "Multiplying Two Numbers" 
+problem, write a method that computes the square of its 
+argument (the square is the result of multiplying a number by 
+itself).
+
+Example:
+square(5) == 25
+square(-8) == 64
+=end
+
+def multiply(number1, number2)
+  number1 * number2
+end
+
+def square(number)
+  multiply(number, number)
+end
+
+p square(5) == 25    # true
+p square(-8) == 64   # true
