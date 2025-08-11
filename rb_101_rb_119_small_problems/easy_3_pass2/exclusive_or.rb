f637828953d805d7780f56acf1d7211339522f5e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-11 11:05:34 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_3_pass2/exclusive_or.rb
+++ rb_101_rb_119_small_problems/easy_3_pass2/exclusive_or.rb
@@ -16,15 +16,6 @@ def xor?(bool1, bool2)
   end
 end
 
-p xor?(5.even?, 4.even?) == true     # true
-p xor?(5.odd?, 4.odd?) == true       # true
-p xor?(5.odd?, 4.even?) == false     # true
-p xor?(5.even?, 4.odd?) == false     # true
-p xor?('abc', nil) == true           # true
-p xor?(nil, 'abc') == true           # true
-p xor?('abc', 'abc') == false        # true
-p xor?(nil, nil) == false            # true
-
 
 
 ## Possible solution:
@@ -146,3 +137,184 @@ p false ^ false
 
 p 1 ^ 2
 # Output: 3
+
+
+
+### Further exploration:
+
+=begin
+*Does the xor method perform short-circuit evaluation of its 
+operands? Why or why not?*
+
+If expression contains `&&` operator, the first falsey value 
+is a result and the expression is short-circuiting.
+
+If expression contains `||` operator, the first truthy value 
+is a result and the expression is short-circuiting.
+
+When short-circuiting happens, there's no need to evaluate
+the second operand.
+
+The variations of `xor?` method above contain short-circuiting 
+evaluation. For example in the following break down:
+=end
+def xor?(value1, value2)
+  !!((value1 && !value2) || (value2 && !value1))
+end
+
+p xor?(5.even?, 4.even?) == true
+=begin
+!!((false && !true) || (true && !false))
+!!((false && false) || true && true)      
+# `(false && false)` short-circuits, because of the first operand is
+`false`, which is a falsey value.
+
+!!(false || true)
+!!(true)
+!false
+true
+=end
+
+p xor?(5.odd?, 4.odd?) == true
+=begin
+!!((true && !false) || (false && !true))
+!!((true && true) || (false && false))    
+# `(false && false)` isn't evaluated, because `(true && true)`
+evaluated to true.
+
+!!(true || false)                         
+# `(true || false)` short-circuits, because of the first operand is
+`true`, which is a truthy velue. 
+
+!!true
+!false
+true
+=end
+
+p xor?(5.even?, 4.odd?) == false
+=begin
+!!((false && !false) || (false && !false))
+!!(false && true) || (false && true)   
+# `(false && true)`, `(false && true)` short-circuit, because 
+of the first operands are `true`, which are truthy value.
+
+!!(false || false)
+!!false
+!true
+false
+=end
+
+p xor?('abc', nil) == true
+=begin
+!!(('abc' && !nil) || (nil && !'abc'))
+!!(('abc' && true) || (nil && false))   
+# `(nil && false)` isn't evaluated, because `('abc' && true)`
+evaluated to true.
+
+
+!!(true || nil)                         
+# `(true || nil)` short-circuits, because of the first operand is
+`true`, which is a truthy velue.
+
+!!true
+!false
+true
+=end
+
+p xor?(nil, 'abc') == true
+=begin
+!!((nil && !'abc') || ('abc' && !nil))
+!!((nil && false) || ('abc' && true))  
+# `(nil && false)` short-circuits, because of the first operand is
+`nil`, which is a falsey value.
+
+!!(nil || true)
+!!true
+!false
+true
+=end
+
+p xor?(nil, nil) == false
+=begin
+!!((nil && !nil) || (nil && !nil))
+!!((nil && true) || (nil && true))  
+# `(nil && true)`, `(nil && true)` short-circuit, because of 
+the first operand is `nil`, which is a falsey velue.
+
+!!(nil || nil)
+!!nil            
+!true
+false
+=end
+
+=begin
+*Does short-circuit evaluation in `xor` operations even make 
+sense?*
+
+In general short-circuit evaluation makes sense. 
+Logically the `&&` would work if 2 operands are truthy. If 
+one operand is falsey, it makes sense to return falsey value right away.
+The same is for `||` operator. We will be satisfied if one 
+of the conditiion is truthy. Then there's no sense to check
+another condition if the first one is already truthy.
+
+In `xor?` method isn't enough to know if the first operand is 
+truthy, or it it's falsey. We must know the truthiness of
+**both** operands. 
+
+The example below shows that `xor?(5.odd?, 4.even?)`, and 
+`xor?('abc', 'abc')` failed because the `if` condition, 
+inside the method definition` checks truthines only for one
+of the operands.
+
+If `5.odd?` is true, which returns `true`, then it short-circuits
+and `true` is returned from `xor?`. The same is for the second 
+failed test case.
+
+This `if` structure always finds the first truthy value ans short-
+circuits.
+
+So, we must always check truthiness for both operands to make sure
+that they are not both truthy and not falsey (Example 2 below).
+=end
+
+def xor?(bool1, bool2)
+  if bool1
+    true
+  elsif bool2
+    true
+  else
+    false
+  end
+end
+
+p xor?(5.even?, 4.even?) == true     # true  (`elsif bool2`)
+p xor?(5.odd?, 4.odd?) == true       # true  (`if bool1`)
+# p xor?(5.odd?, 4.even?) == false     # failed test case
+p xor?(5.even?, 4.odd?) == false     # true  (`else` brunch)
+p xor?('abc', nil) == true           # true  (`if bool1`)
+p xor?(nil, 'abc') == true           # true  (`elsif bool2`)
+# p xor?('abc', 'abc') == false        # failed test case
+p xor?(nil, nil) == false            # true  (`else` brunch)
+
+# Example 2:
+def xor?(bool1, bool2)
+  if bool1 && bool2
+    false
+  elsif bool1
+    true
+  elsif bool2
+    true
+  else
+    false
+  end
+end
+
+p xor?(5.even?, 4.even?) == true     # true
+p xor?(5.odd?, 4.odd?) == true       # true
+p xor?(5.odd?, 4.even?) == false     # true
+p xor?(5.even?, 4.odd?) == false     # true
+p xor?('abc', nil) == true           # true
+p xor?(nil, 'abc') == true           # true
+p xor?('abc', 'abc') == false        # true
+p xor?(nil, nil) == false            # true
