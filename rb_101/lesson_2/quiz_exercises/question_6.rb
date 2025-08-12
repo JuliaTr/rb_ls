30ee0c09500cbc77d342704db8536bcdba3200ee
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-12 17:19:09 -0500

    Add question_6.rb

---
+++ rb_101/lesson_2/quiz_exercises/question_6.rb
@@ -0,0 +1,38 @@
+## Option 1:
+num = 8
+
+if num > 5 && num < 10
+  puts 'Goodbye'
+end
+
+# Output: Goodbye
+
+
+## Option 2:
+num = 8
+
+if num < 4 || num == 8 && num > 6
+  puts 'Goodbye'
+end
+
+# Output: Goodbye
+
+
+## Option 3:
+num = 8
+
+if num < 7 || num > 7
+  puts 'Goodbye'
+end
+
+# Output: Goodbye
+
+
+## Option 4:
+num = 8
+
+if num >= 8 && num < 6 || num > 4
+  puts 'Goodbye'
+end
+
+# Output: Goodbye
