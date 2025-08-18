b8aafab8b7d578773e456314fc9bc532e9effe6b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-17 19:51:56 -0500

    Add question_5.rb

---
+++ rb_101/lesson_3/easy_2_pass2/question_5.rb
@@ -0,0 +1,8 @@
+# Show an easier way to write this array
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+## Solution:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones
+# ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
