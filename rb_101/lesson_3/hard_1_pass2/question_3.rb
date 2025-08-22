68d4d63ae9a24c63142bb537bf8a3ab786aca049
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-21 19:41:31 -0500

    Add code snippet C

--- rb_101/lesson_3/hard_1_pass2/question_3.rb
+++ rb_101/lesson_3/hard_1_pass2/question_3.rb
@@ -36,3 +36,21 @@ puts "one is: #{one}"      # one is: one
 puts "two is: #{two}"      # two is: two
 puts "three is: #{three}"  # three is: three
 
+
+
+# Code snippet C:
+def mess_with_vars(one, two, three)
+  one.gsub!("one", "two")      # "two"
+  two.gsub!("two", "three")    # "three"
+  three.gsub!("three", "one")  # "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"      # one is: two
+puts "two is: #{two}"      # two is: three
+puts "three is: #{three}"  # three is: one
