9c77673baaa157cbd063e38b6215471588eb59ef
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-08-21 19:35:59 -0500

    Add code snippet B

--- rb_101/lesson_3/hard_1_pass2/question_3.rb
+++ rb_101/lesson_3/hard_1_pass2/question_3.rb
@@ -16,3 +16,23 @@ mess_with_vars(one, two, three)
 puts "one is: #{one}"      # one is: one
 puts "two is: #{two}"      # two is: two
 puts "three is: #{three}"  # three is: three
+
+
+
+# Code snippet B:
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"      # one is: one
+puts "two is: #{two}"      # two is: two
+puts "three is: #{three}"  # three is: three
+
