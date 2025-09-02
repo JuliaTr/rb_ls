3fb629ea6106e09afb55fc755ef411dab6ae4486
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-02 14:13:55 -0500

    Add running_totals.rb

---
+++ rb_101_rb_119_small_problems/easy_4/running_totals.rb
@@ -0,0 +1,58 @@
+=begin
+Write a method that takes an Array of numbers, and returns an 
+Array with the same number of elements, and each element has 
+the running total from the original Array.
+
+_____________________________________________________________
+
+PEDAC
+
+Original array:
+[2, 5, 13]
+Array with running totals:
+[2, 7, 20]
+2 = 2 -there's no any number before it
+7 = 2 + 5
+20 = 2 + 5 + 13
+
+Each current number add to the previous one
+=end
+
+def running_total(array)
+  new_array = []
+
+  array.each_index do |index|
+    if index == 0
+      new_array << array[index]
+    elsif index > 0
+      sum_of_previous_numbers = array[0..index].inject(:+)
+      new_array << sum_of_previous_numbers
+    end
+  end
+
+  new_array
+end
+
+p running_total([2, 5, 13]) == [2, 7, 20]                    # true
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # true
+p running_total([3]) == [3]                                  # true
+p running_total([]) == []                                    # true
+
+
+
+## Experiments:
+p [1, 2, 3][1]      # 2
+p [1, 2, 3][1 - 1]  # 1
+
+
+
+## Possible solution:
+def running_total(array)
+  sum = 0
+  array.map { |value| sum += value }
+end
+
+p running_total([2, 5, 13]) == [2, 7, 20]                    # true
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67] # true
+p running_total([3]) == [3]                                  # true
+p running_total([]) == []                                    # true
