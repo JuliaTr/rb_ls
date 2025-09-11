dbb01a084344837447858688d4e8ff7161d90393
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-11 16:58:26 -0500

    Add clean_up_the_words.rb

---
+++ rb_101_rb_119_small_problems/easy_5/clean_up_the_words.rb
@@ -0,0 +1,66 @@
+# =begin
+# Given a string that consists of some words (all lowercased) 
+# and an assortment of non-alphabetic characters, write a method 
+# that returns that string with all of the non-alphabetic 
+# characters replaced by spaces. If one or more non-alphabetic 
+# characters occur in a row, you should only have one space in 
+# the result (the result should never have consecutive spaces).
+
+# ____________________________________________________________
+
+# PEDAC
+
+# High-level:
+# - delete non-alphabetic characters 
+# - substitute them with 1 whitespace
+
+# - delete white spaces if more than 1 in a row
+# =end
+
+# def clean_str(str)
+#   new_str = ''
+
+#   str.chars.each do |char|
+#     char = char.sub(char, ' ') if char.match(/[-'+*&?]/)
+#     new_str << char 
+#   end
+
+#   new_str
+# end
+
+# def cleanup(str)
+#   ' ' + clean_str(str).split.join(' ') + ' '
+# end
+
+# p cleanup("---what's my +*& line?") == ' what s my line '  # true
+
+
+
+## Possible solution:
+# Option 1:
+ALPHABET = ('a'..'z').to_a
+
+def cleanup(text)
+  clean_chars = []
+
+  text.chars.each do |char|
+    if ALPHABET.include?(char)
+      clean_chars << char
+    else
+      clean_chars << ' ' unless clean_chars.last == ' '
+    end
+  end
+
+  clean_chars.join
+end
+
+p cleanup("---what's my +*& line?") == ' what s my line '  # true
+
+
+
+# Option 2:
+def cleanup(text)
+  text.gsub(/[^a-z]/, ' ').squeeze(' ')
+end
+
+p cleanup("---what's my +*& line?") == ' what s my line '  # true
