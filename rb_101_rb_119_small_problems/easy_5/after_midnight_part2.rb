4e023b2a901666934654dd545c8d57f7dc80baa3
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-11 15:04:46 -0500

    Add further exploration and experiment

--- rb_101_rb_119_small_problems/easy_5/after_midnight_part2.rb
+++ rb_101_rb_119_small_problems/easy_5/after_midnight_part2.rb
@@ -1,4 +1,4 @@
-# require 'time'
+require 'time'
 
 =begin
 Write two methods that each take a time of day in 24 hour 
@@ -111,28 +111,71 @@ p before_midnight('24:00') == 0       # true
 
 
 
-# ## Further exploration (in progress):
-# HOURS_PER_DAY = 24
-# MINUTES_PER_HOUR = 60
-# MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR   # 1440
+## Further exploration (learn from others):
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR   # 1440
 
-# def after_midnight(str)
-#   hour, minutes = str.split(':').map(&:to_i)
-#   p hour
-#   p minutes
+def after_midnight(str)
+  time = Time.parse(str)
+  time.hour * MINUTES_PER_HOUR + time.min
+end
 
-#   time = Time.new(2025, 9, 10, hour, minutes)
+def before_midnight(str)
+  (MINUTES_PER_DAY - after_midnight(str)) % MINUTES_PER_DAY
+end
 
-# end
+p after_midnight('00:00') == 0       # true
+p before_midnight('00:00') == 0      # true
+p after_midnight('12:34') == 754     # true
+p before_midnight('12:34') == 686    # true
+p after_midnight('24:00') == 0       # true
+p before_midnight('24:00') == 0      # true
 
-# def before_midnight(str)
-#   after_midnight(str)
+=begin
+p after_midnight('12:34') #== 754
+time = Time.parse(str)     # 2025-09-11 12:34:00 -0500
 
-# end
+p after_midnight('24:00') == 0       # doesn't fail
+`Time.parse` understands that `'24:00'` is the next day. 
+Converts it automatically to `'00:00'`.
 
-# # p after_midnight('00:00') == 0
-# # p before_midnight('00:00') == 0
-# p after_midnight('12:34') #== 754
-# # p before_midnight('12:34') == 686
-# # p after_midnight('24:00') == 0
-# # p before_midnight('24:00') == 0
+time = Time.parse(str)
+p time    # 2025-09-12 00:00:00 -0500
+time.hour * MINUTES_PER_HOUR + time.min  (no need in % MINUTES_PER_DAY at the end of this expression)
+=end
+
+
+
+## Experiments:
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+def after_midnight(time_str)
+  hours, minutes = time_str.split(':').map(&:to_i)
+  hours * MINUTES_PER_HOUR + minutes
+end
+
+def before_midnight(time_str)
+  delta_minutes = MINUTES_PER_DAY - after_midnight(time_str)
+  delta_minutes = 0 if delta_minutes == MINUTES_PER_DAY
+  delta_minutes
+end
+
+p after_midnight('00:00') == 0       # true
+p before_midnight('00:00') == 0      # true
+p after_midnight('12:34') == 754     # true
+p before_midnight('12:34') == 686    # true
+p after_midnight('24:00') == 0       # false
+p before_midnight('24:00') == 0      # true
+
+=begin
+p after_midnight('24:00') == 0       # false
+
+hours * MINUTES_PER_HOUR + minutes
+24 * 60 + 0 = 1440
+
+(hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY
+(24 * 60 + 0) % 1440 = 0
+=end
