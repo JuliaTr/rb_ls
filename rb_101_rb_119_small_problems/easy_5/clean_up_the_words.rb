ecfd44ac4b818f6068a4ac4ceb975e53d3818677
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-12 13:37:37 -0500

    Uncomment

--- rb_101_rb_119_small_problems/easy_5/clean_up_the_words.rb
+++ rb_101_rb_119_small_problems/easy_5/clean_up_the_words.rb
@@ -1,38 +1,38 @@
-# =begin
-# Given a string that consists of some words (all lowercased) 
-# and an assortment of non-alphabetic characters, write a method 
-# that returns that string with all of the non-alphabetic 
-# characters replaced by spaces. If one or more non-alphabetic 
-# characters occur in a row, you should only have one space in 
-# the result (the result should never have consecutive spaces).
+=begin
+Given a string that consists of some words (all lowercased) 
+and an assortment of non-alphabetic characters, write a method 
+that returns that string with all of the non-alphabetic 
+characters replaced by spaces. If one or more non-alphabetic 
+characters occur in a row, you should only have one space in 
+the result (the result should never have consecutive spaces).
 
-# ____________________________________________________________
+____________________________________________________________
 
-# PEDAC
+PEDAC
 
-# High-level:
-# - delete non-alphabetic characters 
-# - substitute them with 1 whitespace
+High-level:
+- delete non-alphabetic characters 
+- substitute them with 1 whitespace
 
-# - delete white spaces if more than 1 in a row
-# =end
+- delete white spaces if more than 1 in a row
+=end
 
-# def clean_str(str)
-#   new_str = ''
+def clean_str(str)
+  new_str = ''
 
-#   str.chars.each do |char|
-#     char = char.sub(char, ' ') if char.match(/[-'+*&?]/)
-#     new_str << char 
-#   end
+  str.chars.each do |char|
+    char = char.sub(char, ' ') if char.match(/[-'+*&?]/)
+    new_str << char 
+  end
 
-#   new_str
-# end
+  new_str
+end
 
-# def cleanup(str)
-#   ' ' + clean_str(str).split.join(' ') + ' '
-# end
+def cleanup(str)
+  ' ' + clean_str(str).split.join(' ') + ' '
+end
 
-# p cleanup("---what's my +*& line?") == ' what s my line '  # true
+p cleanup("---what's my +*& line?") == ' what s my line '  # true
 
 
 
