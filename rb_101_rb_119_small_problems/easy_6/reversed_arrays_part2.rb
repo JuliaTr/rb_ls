81b85fb5c9573d31b58fe4ef15b15e98f9841f37
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-13 15:56:58 -0500

    Add another further exploration

--- rb_101_rb_119_small_problems/easy_6/reversed_arrays_part2.rb
+++ rb_101_rb_119_small_problems/easy_6/reversed_arrays_part2.rb
@@ -55,9 +55,29 @@ p new_list == [2, 3, 1]                         # => true
 
 
 ## Further exploration:
-# `#reduce`:
+# `Enumerable#reduce`:
 def reverse(arr)
   arr.reduce([],:prepend)
+end
+
+p reverse([1, 2, 3, 4]) == [4, 3, 2, 1]         # => true
+p reverse([1, 2, 3, 4, 1]) == [1, 4, 3, 2, 1]   # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)       # => true
+p reverse(['abc']) == ['abc']                   # => true
+p reverse([]) == []                             # => true
+
+list = [1, 3, 2]                                # => [1, 3, 2]
+new_list = reverse(list)                        # => [2, 3, 1]
+p list.object_id != new_list.object_id          # => true
+p list == [1, 3, 2]                             # => true
+p new_list == [2, 3, 1]                         # => true
+
+
+# `Enumerable#each_with_object`:
+def reverse(arr)
+  arr.each_with_object([]) do |element, new_arr|
+    new_arr.prepend(element)
+  end
 end
 
 p reverse([1, 2, 3, 4]) == [4, 3, 2, 1]         # => true
