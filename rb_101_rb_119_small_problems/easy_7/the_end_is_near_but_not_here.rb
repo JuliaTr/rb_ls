3ac28d5e5f71b9ec7a585443e8a5319f2759281a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-14 15:39:48 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_7/the_end_is_near_but_not_here.rb
+++ rb_101_rb_119_small_problems/easy_7/the_end_is_near_but_not_here.rb
@@ -17,5 +17,20 @@ p penultimate('Launch School is great!') == 'is'  # true
 
 
 
+## Further exploration:
+# Write a method that returns the middle word of a phrase or 
+# sentence. It should handle all of the edge cases you thought of.
 
+def middle_word(str)
+  arr = str.split
 
+  arr_length = arr.length
+  middle_word_index = arr_length / 2
+
+  (arr_length % 2 != 0) ? arr[middle_word_index] : ''
+end
+
+p middle_word('this is the last word') == 'the' # true
+p middle_word('Launch School is great!') == ''  # true
+p middle_word('a') == 'a'                       # true
+p middle_word('') == ''                         # true
