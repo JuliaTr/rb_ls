0692737f8ef4c7bda2063e41510bf7238076ad99
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-14 13:57:17 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_7/capitalize_words.rb
+++ rb_101_rb_119_small_problems/easy_7/capitalize_words.rb
@@ -30,3 +30,84 @@ p word_cap('the javaScript language') == 'The Javascript Language'
 p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
 # All test cases return `true`
 
+
+
+## Further exploration:
+# Without `String#capitalize`, how would you solve this problem? 
+# Try to come up with at least two solutions.
+
+# Option 1:
+ALPHABET = {
+  'a' => 'A', 'b' => 'B', 'c' => 'C', 'd' => 'D', 'e' => 'E', 
+  'f' => 'F', 'g' => 'G', 'h' => 'H', 'i' => 'I', 'j' => 'J', 
+  'k' => 'K', 'l' => 'L', 'm' => 'M', 'n' => 'N', 'o' => 'O', 
+  'p' => 'P', 'q' => 'Q', 'r' => 'R', 's' => 'S', 't' => 'T', 
+  'u' => 'U', 'v' => 'V', 'w' => 'W', 'x' => 'X', 'y' => 'Y', 
+  'z' => 'Z'
+ }
+
+def word_cap(str)
+  arr = str.split
+
+  arr_capitalized_words = arr.map do |word|
+    if ALPHABET[word[0]]
+      word[0] = ALPHABET[word[0]]
+
+      if word[1..-1] != word[1..-1].downcase
+        word[1..-1] = word[1..-1].downcase!
+      end
+        
+      word
+    else
+      word
+    end
+  end
+
+  arr_capitalized_words.join(' ')
+end
+
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+# All test cases return `true`
+
+
+# Option 2:
+ALPHABET = {
+  'a' => 'A', 'b' => 'B', 'c' => 'C', 'd' => 'D', 'e' => 'E', 
+  'f' => 'F', 'g' => 'G', 'h' => 'H', 'i' => 'I', 'j' => 'J', 
+  'k' => 'K', 'l' => 'L', 'm' => 'M', 'n' => 'N', 'o' => 'O', 
+  'p' => 'P', 'q' => 'Q', 'r' => 'R', 's' => 'S', 't' => 'T', 
+  'u' => 'U', 'v' => 'V', 'w' => 'W', 'x' => 'X', 'y' => 'Y', 
+  'z' => 'Z'
+ }
+
+def word_cap(str)
+  arr_capitalized_words = str.split.map do |word|
+    if ALPHABET[word[0]]
+      word.sub!(word[0], ALPHABET[word[0]])
+
+      if word[1..-1] != word[1..-1].downcase
+        word[1..-1] = word[1..-1].downcase!
+      end
+
+      word
+    else
+      word
+    end
+  end
+
+  arr_capitalized_words.join(' ')
+end
+
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+# All test cases return `true`
+
+
+
+## Experiments:
+str = 'hELLO'
+str[1..-1].downcase!
+p str   # "hELLO"
