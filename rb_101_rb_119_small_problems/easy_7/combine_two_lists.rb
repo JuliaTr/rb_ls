d7b4b2b9aeb64a99f5b2a0b641d503365e4467f2
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-15 18:58:31 -0500

    Add combine_two_lists.rb

--- rb_101_rb_119_small_problems/easy_7/combine_two_lists.rb
+++ rb_101_rb_119_small_problems/easy_7/combine_two_lists.rb
@@ -53,3 +53,14 @@ end
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
 # Test case returns `true`.
+
+
+
+## Further exploration:
+# See if you can rewrite interleave to use `Array#zip`.
+def interleave(array1, array2)
+  array1.zip(array2).flatten
+end
+
+p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+# Test case returns `true`.
