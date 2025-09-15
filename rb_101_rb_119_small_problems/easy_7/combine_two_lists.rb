a6d0c4530f564d89a469790eaaf26fa04f92804e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-15 17:28:16 -0500

    Add combine_two_lists.rb

---
+++ rb_101_rb_119_small_problems/easy_7/combine_two_lists.rb
@@ -0,0 +1,55 @@
+=begin
+Write a method that combines two Arrays passed in as 
+arguments, and returns a new Array that contains all 
+elements from both Array arguments, with the elements 
+taken in alternation.
+
+You may assume that both input Arrays are non-empty, and 
+that they have the same number of elements.
+=end
+
+def interleave(arr1, arr2)
+  counter_arr1 = 0
+  pairs = arr2.map do |char2|
+    pair = [arr1[counter_arr1]] + [char2]
+    counter_arr1 += 1
+    pair
+  end
+
+  pairs.flatten
+end
+
+p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+# Test case returns `true`.
+
+
+
+# ## Experiments:
+# p 'a'.to_i
+
+
+def interleave(arr1, arr2)
+  arr2.map do |char2|
+    arr_from_arr1 = arr1.map { |char| char }
+    [arr_from_arr1] + [char2]
+  end
+end
+
+p interleave([1, 2, 3], ['a', 'b', 'c'])
+# [[[1, 2, 3], "a"], [[1, 2, 3], "b"], [[1, 2, 3], "c"]]
+
+
+
+## Possible solution:
+def interleave(array1, array2)
+  result = []
+
+  array1.each_with_index do |element, index|
+    result << element << array2[index]
+  end
+
+  result
+end
+
+p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+# Test case returns `true`.
