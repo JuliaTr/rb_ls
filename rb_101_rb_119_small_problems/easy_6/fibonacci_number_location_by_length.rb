5964dee72ec28e4d045c1decd4bfd4baea2eda3c
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-16 15:21:22 -0500

    Add fibonacci_number_location_by_length.rb

---
+++ rb_101_rb_119_small_problems/easy_6/fibonacci_number_location_by_length.rb
@@ -0,0 +1,80 @@
+=begin
+The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...) 
+such that the first 2 numbers are 1 by definition, and each 
+subsequent number is the sum of the two previous numbers. 
+This series appears throughout the natural world.
+
+Computationally, the Fibonacci series is a very simple series, 
+but the results grow at an incredibly rapid rate. For example, 
+the 100th Fibonacci number is 354,224,848,179,261,915,075 -- 
+that's enormous, especially considering that it takes 6 iterations 
+before it generates the first 2 digit number.
+
+Write a method that calculates and returns the index of the 
+first Fibonacci number that has the number of digits specified 
+as an argument. (The first Fibonacci number has index 1.)
+
+_____________________________________________________________
+
+PEDAC
+
+P: return index of the first Fib number consists of a number of
+  digits specified in the argument.
+
+find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+- 2 is number of digits 13 has
+- 7 is 13's index
+
+High-level:
+- Find index of the first number which consists of the number of
+  digits specified in the argument
+=end
+
+## Possible solution:
+def find_fibonacci_index_by_length(number_digits)
+  first = 1
+  second = 1
+  index = 2
+
+  loop do
+    index += 1
+    fibonacci = first + second
+    break if fibonacci.to_s.size >= number_digits
+
+    first = second
+    second = fibonacci
+  end
+
+  index
+end
+
+p find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+p find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+p find_fibonacci_index_by_length(10) == 45
+p find_fibonacci_index_by_length(100) == 476
+p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
+
+
+
+## Experiments:
+def fibonacci(number)
+  if number < 2
+    number
+  else
+    fibonacci(number - 1) + fibonacci(number - 2)
+  end
+end
+
+p fibonacci(0)    # 0
+p fibonacci(1)    # 1
+p fibonacci(2)    # 1   The summing starts here. (0 + 1 = 1)
+p fibonacci(3)    # 2   (1 + 1 = 2)
+p fibonacci(4)    # 3   (2 + 1 = 3)
+p fibonacci(5)    # 5
+p fibonacci(6)    # 8
+p fibonacci(7)    # 13
+
+=begin
+`0`, `1` .. `6` are fibonacci indexes.
+=end
