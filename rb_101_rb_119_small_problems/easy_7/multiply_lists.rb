489c2ca6d2e26e97a583b2f876198b8126817b85
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-16 11:29:40 -0500

    Add option 2

--- rb_101_rb_119_small_problems/easy_7/multiply_lists.rb
+++ rb_101_rb_119_small_problems/easy_7/multiply_lists.rb
@@ -41,4 +41,17 @@ def multiply_list(list_1, list_2)
   list_1.zip(list_2).map { |sub_arr| sub_arr.inject(:*) }
 end
 
-p multiply_list([3, 5, 7], [9, 10, 11]) #== [27, 50, 77] # true
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77] # true
+
+
+# Option 2:
+def multiply_list(arr1, arr2)
+  new_arr = []
+
+  # `number1` and `number2` are integers in each subarray
+  arr1.zip(arr2) { |number1, number2| new_arr << (number1 * number2) }
+  
+  new_arr
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77] # true
