91352dcde7e415fd871832a59d4e578a6abe5a48
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-17 12:10:26 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_8/palindromic_substrings.rb
+++ rb_101_rb_119_small_problems/easy_8/palindromic_substrings.rb
@@ -106,3 +106,63 @@ p palindromes('knitting cassettes') == [
   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
 ]
 # All test cases return `true`.
+
+
+
+## Further exploration:
+# Can you modify this method (and/or its predecessors) to 
+# ignore non-alphanumeric characters and case?
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+def leading_substrings(str)
+  result = []
+
+  0.upto(str.size - 1) do |index|
+    result << str[0..index]
+  end
+
+  result
+end
+
+def substrings(str)
+  results = []
+
+  (0...str.size).each do |start_index|
+    this_substring = str[start_index..-1]
+    results.concat(leading_substrings(this_substring))
+  end
+
+  results
+end
+
+def alphanumeric_characters(string)
+  string.split.map do |word|
+    word.chars.select do |char|
+      char if ALPHABET.include?(char.downcase)
+    end.join
+  end.join
+end
+
+def palindrome?(str)
+  if str.match?(/[^a-zA-Z0-9]/)
+    str = alphanumeric_characters(str)
+  end
+    
+  str.downcase == str.reverse.downcase && str.size > 1
+end
+
+def palindromes(string)
+  all_substrings = substrings(string)
+  results = []
+
+  all_substrings.each do |substring|
+    results << substring if palindrome?(substring)
+  end
+
+  results
+end
+
+p palindromes('44abcd') == ['44']              # true
+p palindromes('Madam') == ['Madam', 'ada']     # true
+p palindromes('Abc-ba') == ['Abc-ba', 'bc-b']  # true
