41887b442982c5e2232b8b9c334fe899edd627f4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-18 14:42:54 -0500

    Add tri_angles.rb

--- rb_101_rb_119_small_problems/easy_9/tri_angles.rb
+++ rb_101_rb_119_small_problems/easy_9/tri_angles.rb
@@ -22,6 +22,50 @@ worry about floating point errors. You may also assume that
 the arguments are specified in degrees.
 =end
 
+## Refactored solution:
+VALID_TRIANGLE_ANGLES_DEGREES = 180
+RIGHT = 90
+
+def invalid_triangle?(angles, sum_of_angles)
+  has_0_angle = angles.any? { |angle| angle == 0 }
+  (sum_of_angles != VALID_TRIANGLE_ANGLES_DEGREES) ||  has_0_angle
+end
+
+def right_triangle?(angles)
+  angles.any? { |angle| angle == RIGHT }
+end
+
+def acute_triangle?(angles)
+  angles.all? { |angle| angle < RIGHT }
+end
+
+def obtuse_triangle?(angles)
+  angles.any? { |angle| angle > RIGHT }
+end
+
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+  sum_of_angles = angles.sum
+
+  case
+  when invalid_triangle?(angles, sum_of_angles)
+    :invalid
+  when right_triangle?(angles)
+    :right
+  when acute_triangle?(angles)
+    :acute
+  when obtuse_triangle?(angles)
+    :obtuse
+  end
+end
+
+p triangle(60, 70, 50) == :acute     # true
+p triangle(30, 90, 60) == :right     # true
+p triangle(120, 50, 10) == :obtuse   # true
+p triangle(0, 90, 90) == :invalid    # true
+p triangle(50, 50, 50) == :invalid   # true
+
+
 
 ## Solution:
 VALID_TRIANGLE_ANGLES_DEGREES = 180
@@ -44,6 +88,30 @@ def triangle(angle1, angle2, angle3)
   when all_angles_less_90
     :acute
   when angle_has_greater_120
+    :obtuse
+  end
+end
+
+p triangle(60, 70, 50) == :acute     # true
+p triangle(30, 90, 60) == :right     # true
+p triangle(120, 50, 10) == :obtuse   # true
+p triangle(0, 90, 90) == :invalid    # true
+p triangle(50, 50, 50) == :invalid   # true
+
+
+
+## Possible solution:
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+
+  case
+  when angles.reduce(:+) != 180, angles.include?(0)
+    :invalid
+  when angles.include?(90)
+    :right
+  when angles.all? { |angle| angle < 90 }
+    :acute
+  else
     :obtuse
   end
 end
