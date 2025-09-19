901d89bd3e0ac649b385fd38a7982934fd59b86a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-19 10:41:40 -0500

    Add is_it_prime.rb

---
+++ rb_101_rb_119_small_problems/easy_9/is_it_prime.rb
@@ -0,0 +1,126 @@
+=begin
+A prime number is a positive number that is evenly divisible 
+only by itself and 1. Thus, 23 is prime since its only 
+divisors are 1 and 23. However, 24 is not prime since it has 
+divisors of 1, 2, 3, 4, 6, 8, 12, and 24. Note that the number 
+1 is not prime.
+
+Write a method that takes a positive integer as an argument 
+and returns true if the number is prime, false if it is not 
+prime.
+
+You may not use Ruby's Prime class to solve this problem. 
+Your task is to programmatically determine whether a number 
+is prime without relying on any methods that already do that 
+for you.
+=end
+
+def array_of_subsequent_integers(number)
+  arr = []
+
+  while number > 0
+    whole, remainder = number.divmod(1)
+    arr.prepend(whole)
+    number -= 1
+  end
+
+  arr
+end
+
+def is_prime(number)
+  return false if number == 1
+
+  # Create array of all subsequent integers upto `number`.
+  arr = array_of_subsequent_integers(number)
+
+  # Find if `number` has divisors other than 1 and `number`.
+  index = 1
+  current_number = nil
+  loop do
+    current_number = arr[index]
+    break if number % current_number == 0
+    index += 1
+  end
+  current_number == number
+end
+
+puts(is_prime(1) == false)              # true
+puts(is_prime(2) == true)               # true
+puts(is_prime(3) == true)               # true
+puts(is_prime(4) == false)              # true
+puts(is_prime(5) == true)               # true
+puts(is_prime(6) == false)              # true
+puts(is_prime(7) == true)               # true
+puts(is_prime(8) == false)              # true
+puts(is_prime(9) == false)              # true
+puts(is_prime(10) == false)             # true
+puts(is_prime(23) == true)              # true
+puts(is_prime(24) == false)             # true
+puts(is_prime(997) == true)             # true
+puts(is_prime(998) == false)            # true
+puts(is_prime(3_297_061) == true)       # true
+puts(is_prime(23_297_061) == false)     # true
+
+
+
+## Possible solution:
+# Option 1:
+def is_prime(number)
+  return false if number == 1
+
+  (2..(number - 1)).each do |divisor|
+    if number % divisor == 0
+      return false
+    end
+  end
+
+  return true
+end
+
+puts(is_prime(1) == false)              # true
+puts(is_prime(2) == true)               # true
+puts(is_prime(3) == true)               # true
+puts(is_prime(4) == false)              # true
+puts(is_prime(5) == true)               # true
+puts(is_prime(6) == false)              # true
+puts(is_prime(7) == true)               # true
+puts(is_prime(8) == false)              # true
+puts(is_prime(9) == false)              # true
+puts(is_prime(10) == false)             # true
+puts(is_prime(23) == true)              # true
+puts(is_prime(24) == false)             # true
+puts(is_prime(997) == true)             # true
+puts(is_prime(998) == false)            # true
+puts(is_prime(3_297_061) == true)       # true
+puts(is_prime(23_297_061) == false)     # true
+
+
+# Option 2:
+def is_prime(number)
+  return false if number == 1
+
+  (2..Math.sqrt(number)).each do |divisor|
+    if number % divisor == 0
+      return false
+    end
+  end
+
+  return true
+end
+
+puts(is_prime(1) == false)              # true
+puts(is_prime(2) == true)               # true
+puts(is_prime(3) == true)               # true
+puts(is_prime(4) == false)              # true
+puts(is_prime(5) == true)               # true
+puts(is_prime(6) == false)              # true
+puts(is_prime(7) == true)               # true
+puts(is_prime(8) == false)              # true
+puts(is_prime(9) == false)              # true
+puts(is_prime(10) == false)             # true
+puts(is_prime(23) == true)              # true
+puts(is_prime(24) == false)             # true
+puts(is_prime(997) == true)             # true
+puts(is_prime(998) == false)            # true
+puts(is_prime(3_297_061) == true)       # true
+puts(is_prime(23_297_061) == false)     # true
