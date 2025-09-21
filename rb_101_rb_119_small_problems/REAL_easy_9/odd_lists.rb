8b7b0211c53d2f4848335a166b60941320295dc3
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-20 19:39:12 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/REAL_easy_9/odd_lists.rb
+++ rb_101_rb_119_small_problems/REAL_easy_9/odd_lists.rb
@@ -43,3 +43,49 @@ p oddities(['abc', 'def']) == ['abc']         # true
 p oddities([123]) == [123]                    # true
 p oddities([]) == []                          # true
 p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]      # true
+
+
+
+## Further exploration:
+# Write a companion method that returns the 2nd, 4th, 6th, and 
+# so on elements of an array. Try to solve this exercise in at 
+# least 2 additional ways.
+
+# Option 1:
+def evens(arr)
+  even_elements = []
+  index = 1
+
+  while index < arr.size
+    even_elements << arr[index]
+    index += 2
+  end
+
+  even_elements
+end
+
+p evens([2, 3, 4, 5, 6]) == [3, 5]         # true
+p evens([1, 2, 3, 4, 5, 6]) == [2, 4, 6]   # true
+p evens(['abc', 'def']) == ['def']         # true
+p evens([123]) == []                       # true
+p evens([]) == []                          # true
+p evens([1, 2, 3, 4, 1]) == [2, 4]         # true
+
+
+# Option 2:
+def evens(arr)
+  index = 0
+  arr.each_with_object([]) do |element, new_arr|
+    if index == 1 ||  index== 3 || index == 5
+      new_arr << element
+    end
+    index += 1
+  end
+end
+
+p evens([2, 3, 4, 5, 6]) == [3, 5]         # true
+p evens([1, 2, 3, 4, 5, 6]) == [2, 4, 6]   # true
+p evens(['abc', 'def']) == ['def']         # true
+p evens([123]) == []                       # true
+p evens([]) == []                          # true
+p evens([1, 2, 3, 4, 1]) == [2, 4]         # true
