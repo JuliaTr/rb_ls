5ad0aea9dcce31ebcd0b083c76424e7f7c825282
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-22 13:46:41 -0500

    Add mutation.rb

---
+++ rb_101_rb_119_small_problems/easy_6/mutation.rb
@@ -0,0 +1,51 @@
+# What will the following code print, and why?
+array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
+array2 = []
+array1.each { |value| array2 << value }  # now array2 has the same values as array1
+array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
+puts array2 
+# ["Moe", "Larry", "CURLY", "SHEMP", "Harpo", "CHICO", "Groucho", "Zeppo"]
+
+=begin
+The `#<<` copies references to the values of the original array.
+If we modify the reference in the original array, the values in the
+`array2` are also modified.
+=end
+
+## Experiments:
+arr1 = ['a', 'b', 'c']
+arr2 = []
+
+p arr1.object_id   # 60
+p arr2.object_id   # 80
+
+
+
+arr1 = ['a', 'b', 'c']
+arr2 = ['a', 'b', 'c']
+
+p arr1.object_id     # 60
+p arr2.object_id     # 80
+
+
+
+arr1 = ['a', 'b', 'c']
+arr2 = []
+
+arr1.each { |value| arr2 << value }
+
+p arr1  # ["a", "b", "c"]
+p arr2  # ["a", "b", "c"]
+
+p arr1.object_id  # 60
+p arr2.object_id  # 80
+
+
+
+arr1 = ['a', 'b', 'c']
+arr2 = []
+arr1.each { |value| arr2 << value }
+arr1.each { |value| value.upcase! if value.start_with?('a') }
+
+p arr1  # ["A", "b", "c"]
+p arr2  # ["A", "b", "c"]
