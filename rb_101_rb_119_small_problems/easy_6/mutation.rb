0d1c25e8dfd124539bd160be16a51cc93b99f6f1
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-23 12:15:18 -0500

    Add further exploration

--- rb_101_rb_119_small_problems/easy_6/mutation.rb
+++ rb_101_rb_119_small_problems/easy_6/mutation.rb
@@ -12,6 +12,8 @@ If we modify the reference in the original array, the values in the
 `array2` are also modified.
 =end
 
+
+
 ## Experiments:
 arr1 = ['a', 'b', 'c']
 arr2 = []
@@ -49,3 +51,59 @@ arr1.each { |value| value.upcase! if value.start_with?('a') }
 
 p arr1  # ["A", "b", "c"]
 p arr2  # ["A", "b", "c"]
+
+
+
+## Further exploration:
+greeting = 'Hello'
+p greeting               # "Hello"
+
+p greeting.object_id     # 60
+
+p 5.object_id            # 11
+p true.object_id         # 20
+p nil.object_id          # 4
+p "abc".object_id        # 60
+
+whazzup = greeting
+p whazzup       # "Hello"
+
+p greeting.object_id       # 60
+p whazzup.object_id        # 60
+
+p greeting.upcase!         # "HELLO"
+
+p greeting                 # "HELLO"
+p whazzup                  # "HELLO"
+
+p whazzup.concat('!')      # "HELLO!"
+
+p greeting                 # "HELLO!"
+p whazzup                  # "HELLO!"
+
+p greeting.object_id       # 60
+p whazzup.object_id        # 60
+
+greeting = 'Dude!'
+p greeting                 # "Dude!"
+
+puts greeting              # Dude!
+puts whazzup               # "Dude!"
+
+p greeting.object_id       # 80
+p whazzup.object_id        # 60
+
+
+a = [1, 2, 3, 4, 5]
+a[3] = 0
+p a                          # [1, 2, 3, 0, 5]
+
+a = %w(a b c)
+
+p a                          # ["a", "b", "c"]
+p a.object_id                # 60
+
+p a[1] = '-'                 # "-"
+
+p a                          # ["a", "-", "c"]
+p a.object_id                # 60
