3ed3bcf7cb1af502da68a53ba93225f88ba08575
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-09-25 15:29:44 -0500

    Add experiments with scan and reject methods

--- spot_wiki/problems_rb_110_119/10.rb
+++ spot_wiki/problems_rb_110_119/10.rb
@@ -142,3 +142,46 @@ p top_3_words(" ' ") == []
 p top_3_words(" ''' ") == []
 p top_3_words("""In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.""") == ["a", "of", "on"]
 # All test cases return `true`.
+
+
+
+## Experiments:
+# `String#scan`:
+def return_occurrences(arr)
+  arr.each_with_object(Hash.new(0)) do |word, h|
+    h[word] += 1
+  end
+end
+
+def top_3_words(str)
+  # Return words with alphabetic characters and apostroph inside.
+  words = str.downcase.scan(/[a-z']+/)
+
+  # Substitutes apostrophs as words into empty string. If the
+  # string is empty, returns `true`, which means that these words
+  # were not selected.
+  words = words.reject { |word| word.gsub("'", "").empty? }
+
+  h = return_occurrences(words)
+
+  h.max_by(3) { |_, v| v }.map { |subarr| subarr[0] }
+end
+
+p top_3_words("In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.")
+# => ["a", "of", "on"]   # works
+
+p top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
+# => ["e", "ddd", "aa"]  # works
+
+p top_3_words(" //wont won't won't")
+# => ["won't", "wont"]   # works
+
+p top_3_words("a a a b c c d d d d e e e e e") == ["e", "d", "a"]
+p top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") == ["e", "ddd", "aa"]
+p top_3_words(" //wont won't won't ") == ["won't", "wont"]  #
+p top_3_words(" , e .. ") == ["e"]
+p top_3_words(" ... ") == []
+p top_3_words(" ' ") == []
+p top_3_words(" ''' ") == []
+p top_3_words("""In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.""") == ["a", "of", "on"]
+# All test cases return `true`.
