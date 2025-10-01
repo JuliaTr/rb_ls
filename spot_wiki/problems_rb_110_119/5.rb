1116c5c3c66059f2aaa05374dd50bcdca96d2562
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-10-01 13:15:00 -0500

    Add 5.rb

---
+++ spot_wiki/problems_rb_110_119/5.rb
@@ -0,0 +1,100 @@
+=begin
+The vowel substrings in the word codewarriors are o,e,a,io. The 
+longest of these has a length of 2. Given a lowercase string 
+that has alphabetic characters only and no spaces, return the 
+length of the longest vowel substring. Vowels are any of aeiou.
+
+_______________________________________________________________
+
+PEDAC
+
+P
+Return an integer which represents the longest substring of vowels 
+in a string.
+
+Rules/reqs:
+- all characters in a string are downcased
+- vowels are `aeiou`
+- substring should have only vowels
+
+D
+Input: string
+Output: integer
+Intermediate:
+- input string to iterate over it
+- range - to find substrings
+- array - to store substrings
+- temporary string
+
+High-level:
+- Find all substrings with vowels: ["o", "e", "a", "io"], ["uoi", "ea"]
+- Find the longest substring
+
+Intermediate:
+- Create array of substrings
+- Create temporary empty substring
+- Iterate over a range of characters
+- Count maximum length of substrings
+=end
+
+## Solution:
+def return_all_substrings_with_vowels_only(str)
+  arr_substrings = []
+  substring = ''
+
+  str.each_char.with_index do |char, index|
+    if 'aeiou'.include?(char)
+      if substring.empty? && (str[index + 1].nil? || !str[index + 1].match?(/[aeiou]/))
+        substring << char
+        arr_substrings << substring
+        substring = ''
+
+      elsif substring.empty? && (str[index + 1].nil? || str[index + 1].match?(/[aeiou]/))
+        substring << char
+
+      elsif !substring.empty? && str[index + 1].nil?
+        substring.concat(char)
+        arr_substrings << substring
+
+      elsif !substring.empty? && (str[index + 1].nil? || str[index + 1].match?(/[aeiou]/))
+        substring.concat(char)
+
+      elsif !substring.empty? && (str[index + 1].nil? || !str[index + 1].match?(/[aeiou]/))
+        substring.concat(char)
+        arr_substrings << substring
+        substring = ''
+      end
+    end
+  end
+
+  arr_substrings
+end
+
+def solve(str)
+  # Find all substrings with vowels only:
+  arr_substrings = return_all_substrings_with_vowels_only(str)
+
+  # Find length of each substring:
+  substrings_length = arr_substrings.map(&:length)
+
+  # Find max length:
+  substrings_length.max
+end
+
+
+p solve("codewarriors") == 2
+p solve("suoidea") == 3
+p solve("iuuvgheaae") == 4
+p solve("ultrarevolutionariees") == 3
+p solve("strengthlessnesses") == 1
+p solve("cuboideonavicuare") == 2
+p solve("chrononhotonthuooaos") == 5
+p solve("iiihoovaeaaaoougjyaw") == 8
+# All test cases return `true`.
+
+
+
+## Experiments:
+a = 's'
+a.concat('o')
+p a  # "so"
