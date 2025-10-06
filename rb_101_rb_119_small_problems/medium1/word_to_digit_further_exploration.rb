2c0b3cee550aa8291077c38a9d91a1f26d660d39
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-10-06 14:10:55 -0500

    Add a solution

--- rb_101_rb_119_small_problems/medium1/word_to_digit_further_exploration.rb
+++ rb_101_rb_119_small_problems/medium1/word_to_digit_further_exploration.rb
@@ -78,39 +78,39 @@
 
 # =end
 
-## Improved solution:
-DIGIT_HASH = {
-  'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
-  'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
-  'eight' => '8', 'nine' => '9'
-}.freeze
+# ## Improved solution:
+# DIGIT_HASH = {
+#   'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
+#   'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
+#   'eight' => '8', 'nine' => '9'
+# }.freeze
 
-def word_to_digit(words)
-  DIGIT_HASH.keys.each do |word|
-    words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
-  end
-  words
-end
+# def word_to_digit(words)
+#   DIGIT_HASH.keys.each do |word|
+#     words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
+#   end
+#   words
+# end
 
-def modify_words(words)
-  words = word_to_digit(words)  # string
-  new_str = ''
+# def modify_words(words)
+#   words = word_to_digit(words)  # string
+#   new_str = ''
 
-  # Iterate over a string directly:
-  # Simplified condition check
-  words.each_char.with_index do |char, index|
-    unless char == ' ' &&
-            words[index - 1].to_i.to_s == words[index - 1] &&
-            words[index + 1].to_i.to_s == words[index + 1]
-      new_str << char
-    end
-  end
+#   # Iterate over a string directly:
+#   # Simplified condition check
+#   words.each_char.with_index do |char, index|
+#     unless char == ' ' &&
+#             words[index - 1].to_i.to_s == words[index - 1] &&
+#             words[index + 1].to_i.to_s == words[index + 1]
+#       new_str << char
+#     end
+#   end
 
-  new_str
-end
+#   new_str
+# end
 
-p modify_words('Please call me at five five five one two three four. Thanks.') == "Please call me at 5551234. Thanks."
-# true
+# p modify_words('Please call me at five five five one two three four. Thanks.') == "Please call me at 5551234. Thanks."
+# # true
 
 
 
@@ -149,65 +149,69 @@ p modify_words('Please call me at five five five one two three four. Thanks.') =
 
 
 
-# =begin
-# Assume that any 10 digit number is a phone number, and that the 
-# proper format should be `"(123) 456-7890"`.
-# =end
+=begin
+Assume that any 10 digit number is a phone number, and that the 
+proper format should be `"(123) 456-7890"`.
+=end
 
-# DIGIT_HASH = {
-#   'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
-#   'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
-#   'eight' => '8', 'nine' => '9'
-# }.freeze
+DIGIT_HASH = {
+  'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
+  'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
+  'eight' => '8', 'nine' => '9'
+}.freeze
 
-# def word_to_digit!(words)
-#   DIGIT_HASH.keys.each do |word|
-#     words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
-#   end
+def word_to_digit!(words)
+  DIGIT_HASH.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
+  end
 
-#   words
-# end
+  words
+end
 
-# # (See how to rewrite to build a new string with less characters 
-# # (in order not to modify the number characters)
+def delete_spaces_between_integers!(words)
+  new_str = ''
 
-# def delete_spaces_between_integers!(words)
-#   arr_chars = words.chars
+  words.each_char.with_index do |char, index|
+    unless char == ' ' &&
+            words[index - 1].to_i.to_s == words[index - 1] &&
+            words[index + 1].to_i.to_s == words[index + 1]
+      new_str << char
+    end
+  end
 
-#   arr_chars.each_with_index do |char, index|
-#     if char.to_i.to_s == char &&           # digit?
-#         arr_chars[index + 1] == ' ' &&
-#         arr_chars[index + 2].to_i.to_s == arr_chars[index + 2]  # digit?
-#       arr_chars.slice!(index + 1)
-#     end
-#   end
+  new_str
+end
 
-#   arr_chars
-# end
+def modify_words(str)
+  words = word_to_digit!(str)  # string
+  str_without_spaces = delete_spaces_between_integers!(words) # string
 
-# def modify_words(str)
-#   words = word_to_digit!(str)  # string
-#   arr_chars = delete_spaces_between_integers!(words) # array
+  # Build a new string with '(', ')', '-'
+  new_str = ''
+  str_without_spaces.each_char.with_index do |char, index|
+    if char == ' ' &&
+        str_without_spaces[index + 1].to_i.to_s == str_without_spaces[index + 1]
+      new_str << char + '('
+    elsif str_without_spaces[index - 5..index].to_i.to_s == str_without_spaces[index - 5..index] &&
+          str_without_spaces[index..index + 4].to_i.to_s == str_without_spaces[index..index + 4]
+      new_str << char + '-'
+    elsif new_str.include?(')')
+      new_str << char
+    elsif str_without_spaces[index - 1].to_i.to_s == str_without_spaces[index - 1] &&
+          str_without_spaces[index - 2].to_i.to_s == str_without_spaces[index - 2]
+      new_str << char + ')' + ' '
+    else
+      new_str << char
+    end
+  end
 
-#   # p arr_chars.join
-
-#   new_str = ''
-#   arr_chars.each_with_index do |char, index|
-#     if char == ' ' &&
-#         arr_chars[index + 1].to_i.to_s == arr_chars[index + 1] &&
-#       arr_chars.su
-#     end
-#   end
+  new_str
 
 
+end
 
-#   # Build a new string with '(', ')', '-'
-
-
-# end
-
-# p modify_words('Please call me at five five five one two three four seven eight. Thanks.') #== "Please call me at (555) 123-478. Thanks."
-# #
+p modify_words('Please call me at five five five one two three four seven eight one. Thanks.') #== "Please call me at (555) 123-4781. Thanks."
+# true
 
 
 
