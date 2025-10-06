c7aed3d856162ba30ff9a4a8ee06f38ed8a01a2b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-10-06 15:17:48 -0500

    Add a solution

--- rb_101_rb_119_small_problems/medium1/word_to_digit_further_exploration2.rb
+++ rb_101_rb_119_small_problems/medium1/word_to_digit_further_exploration2.rb
@@ -110,8 +110,49 @@ p modify_words('Please call me at five five five one two three four. Thanks.') =
 
 
 =begin
-Suppose the string already 
-contains two or more space separated numbers (not words); 
-can you leave those spaces alone, while removing any spaces 
-between numbers that you create?
+Suppose the string already contains two or more space separated 
+numbers (not words); can you leave those spaces alone, while 
+removing any spaces between numbers that you create?
 =end
+
+DIGIT_HASH = {
+  'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
+  'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
+  'eight' => '8', 'nine' => '9'
+}.freeze
+
+def word_to_digit!(words)
+  DIGIT_HASH.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, '$' + DIGIT_HASH[word])
+  end
+  words
+end
+
+def delete_spaces_in_tagged_substrings(words)
+  new_str = ''
+
+  words.each_char.with_index do |char, index|
+    unless char == ' ' && words[index + 1] == '$' &&
+            words[index - 1].to_i.to_s == words[index - 1] &&
+            words[index - 2] == '$'
+      new_str << char
+    end
+  end
+
+  new_str
+end
+
+def modify_words!(words)
+  # String with tags `&`:
+  words = word_to_digit!(words)
+  p words
+
+  # String without spaces between tagged numbers:
+  new_str = delete_spaces_in_tagged_substrings(words)
+
+  # String without tags
+  new_str.delete('$')
+end
+
+p modify_words!('I saw 2 3 birds and five six.') == "I saw 2 3 birds and 56."
+# true
