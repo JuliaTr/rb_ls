0a21fae7ccdffb6dc63baab8952e126de1063880
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-12 13:21:40 -0600

    Add delete_characters.rb

---
+++ study_sessions/delete_characters.rb
@@ -0,0 +1,28 @@
+=begin
+Modify the word_sizes method from the previous exercise to 
+exclude non-letters when determining word size. For instance, 
+the length of "it's" is 3, not 4.
+=end
+
+# Option 1 (learn from others):
+def word_sizes(string)
+  counts = Hash.new(0)
+  string.split.each do |word|
+    length = word.delete('^A-Za-z').length
+    counts[length] += 1
+  end
+  counts
+end
+
+# Option 2:
+def word_sizes(string)
+  string.split.each_with_object(Hash.new(0)) do |word, counts|
+    length = word.delete('^A-Za-z').length
+    counts[length] += 1
+  end
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
