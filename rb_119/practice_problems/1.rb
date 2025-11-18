0d7c93f2f2b2cc86b76f1367c260befe177228ac
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-18 12:22:18 -0600

    Add refactored solution

--- rb_119/practice_problems/1.rb
+++ rb_119/practice_problems/1.rb
@@ -51,12 +51,52 @@ Algo:
 
 [8, 1, 2, 2, 3]
  ^
-[8, 1, 2, 2, 3]
-          ^
+  [8, 1, 2, 2, 3]
+            ^
 
-[3, ]
+[3, ...]
+
+
+[8, 1, 2, 2, 3]
+       ^
+  [8, 1, 2, 3]
+            ^
+
+[3, ...]
+
+
+[1, 4, 6, 8, 13, 2, 4, 5, 4]
+    ^
+  [1, 4, 6, 8, 13, 2, 5]
+                      ^
+
+[0, 2 ...]
 =end
 
+## Refactored:
+def smaller_numbers_than_current(arr)
+  arr.map do |outer_value|
+    count = 0
+    arr.uniq.each do |inner_value|
+      count += 1 if inner_value < outer_value
+    end
+    count
+  end
+end
+
+p smaller_numbers_than_current([8, 1, 2, 2, 3]) == [3, 0, 1, 1, 2]
+p smaller_numbers_than_current([7, 7, 7, 7]) == [0, 0, 0, 0]
+p smaller_numbers_than_current([6, 5, 4, 8]) == [2, 1, 0, 3]
+p smaller_numbers_than_current([1]) == [0]  #
+
+my_array = [1, 4, 6, 8, 13, 2, 4, 5, 4]
+result   = [0, 2, 4, 5, 6, 1, 2, 3, 2]
+p smaller_numbers_than_current(my_array) == result
+# All test cases return `true`.
+
+
+
+## Solution:
 def count_numbers(hash, arr)
   result = []
 
