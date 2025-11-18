246aa079c8743298b160aa75232d283217c1abae
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-18 16:28:40 -0600

    Add more idiomatic solution

--- rb_119/practice_problems/3.rb
+++ rb_119/practice_problems/3.rb
@@ -43,6 +43,127 @@ Algo:
 
 =end
 
+## More idiomatic:
+def upcase_characters(word)
+  word.chars.map.with_index do |char, index|
+    index.odd? ? char.upcase : char
+  end.join
+end
+
+def to_weird_case(str)
+  str.split.map.with_index do |word, index|
+    # p '-------'
+    # p word
+    # p index
+    # p index + 1
+    if ((index + 1) % 3).zero?
+      upcase_characters(word)
+    else
+      word
+    end
+  end.join(' ')
+end
+
+original = 'Lorem Ipsum is simply dummy text of the printing world'
+expected = 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG world'
+p to_weird_case(original) == expected
+
+=begin
+$ ruby 3.rb
+"-------"
+0
+1
+"-------"
+1
+2
+"-------"
+2
+3
+"-------"
+3
+4
+"-------"
+4
+5
+"-------"
+5
+6
+"-------"
+6
+7
+"-------"
+7
+8
+"-------"
+8
+9
+"-------"
+9
+10
+true
+$ ruby 3.rb
+"-------"
+"Lorem"
+0
+1
+"-------"
+"Ipsum"
+1
+2
+"-------"
+"is"
+2
+3
+"-------"
+"simply"
+3
+4
+"-------"
+"dummy"
+4
+5
+"-------"
+"text"
+5
+6
+"-------"
+"of"
+6
+7
+"the"
+8
+"printing"
+9
+"world"
+9
+10
+true
+
+$ irb
+irb(main):001:0> 2 % 3 == 0
+=> false
+irb(main):002:0> 3 % 3 == 0
+=> true
+irb(main):003:0> 4 % 3 == 0
+=> false
+irb(main):004:0> 5 % 3 == 0
+=> false
+irb(main):005:0> 6 % 3 == 0
+=end
+
+original = 'It is a long established fact that a reader will be distracted'
+expected = 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
+p to_weird_case(original) == expected
+
+p to_weird_case('aaA bB c') == 'aaA bB c'
+
+original = "Mary Poppins' favorite word is supercalifragilisticexpialidocious"
+expected = "Mary Poppins' fAvOrItE word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS"
+p to_weird_case(original) == expected
+# All test cases return `true`.
+
+
+
 ## Refactored:
 def upcase_characters(str)
   index2 = 0
