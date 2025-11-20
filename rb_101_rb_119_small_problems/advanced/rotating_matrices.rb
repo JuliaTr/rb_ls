e3af4427ab10d3ec76d7278fa3e06edebdf86b2d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-20 16:59:53 -0600

    Add solution

--- rb_101_rb_119_small_problems/advanced/rotating_matrices.rb
+++ rb_101_rb_119_small_problems/advanced/rotating_matrices.rb
@@ -84,6 +84,7 @@ p new_matrix3 == matrix2
 
 
 ## Possible solution:
+# Option 1:
 def rotate90(matrix)
   result = []
   number_of_rows = matrix.size
@@ -117,6 +118,42 @@ new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
 p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
 p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
 p new_matrix3 == matrix2
+# All test cases print `true`.
+
+
+# Option 2:
+# Won't work, because it's only for 3x3 matrices:
+def rotate90(matrix)
+  result = []
+
+  (0..2).each do |column_index|
+    new_row = (0..2).map do |row_index|
+      matrix[row_index][column_index]
+    end
+    result << new_row.reverse
+  end
+
+  result
+end
+
+matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8]
+]
+
+new_matrix1 = rotate90(matrix1)
+# new_matrix2 = rotate90(matrix2)
+# new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+
+p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+# p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+# p new_matrix3 == matrix2
 # All test cases print `true`.
 
 
