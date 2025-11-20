cb7670ee5eb3d58feced383d5a017449f973a388
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-20 16:42:06 -0600

    Learn from others

--- rb_101_rb_119_small_problems/advanced/rotating_matrices.rb
+++ rb_101_rb_119_small_problems/advanced/rotating_matrices.rb
@@ -118,3 +118,92 @@ p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
 p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
 p new_matrix3 == matrix2
 # All test cases print `true`.
+
+
+
+### Learn from others:
+def rotate90(matrix)
+  rotated = []
+  new_matrix_size = matrix.first.size
+  index = 0
+
+  # p matrix
+  # p new_matrix_size
+  # p matrix.reverse
+
+  new_matrix_size.times do
+    row = []
+    matrix.reverse.each do |arr|
+      # p "-----------------------"
+      # p arr
+      # p arr[index]
+      row << arr[index]
+    end
+    rotated << row
+    index += 1
+  end
+
+  rotated
+end
+
+matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8]
+]
+
+new_matrix1 = rotate90(matrix1)
+new_matrix2 = rotate90(matrix2)
+new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+
+p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+=begin
+$ ruby rotating_matrices.rb
+[[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+3
+[[3, 9, 6], [4, 7, 2], [1, 5, 8]]
+"-----------------------"
+[3, 9, 6]
+3
+"-----------------------"
+[4, 7, 2]
+4
+"-----------------------"
+[1, 5, 8]
+1
+"-----------------------"
+
+[3, 9, 6]
+9
+"-----------------------"
+[4, 7, 2]
+7
+"-----------------------"
+[1, 5, 8]
+5
+"-----------------------"
+
+[3, 9, 6]
+6
+"-----------------------"
+[4, 7, 2]
+2
+"-----------------------"
+[1, 5, 8]
+8
+=end
+
+p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+p new_matrix3 == matrix2
+# All test cases print `true`.
+
+
+
+## Further exploration:
+# Can you modify your solution to perform 90, 180, 270, and 360 
+# degree rotations based on an argument?
