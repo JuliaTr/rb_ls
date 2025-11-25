d83473d8e4ffdf223d387d6478013f21495095df
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-25 13:09:44 -0600

    Add experiment with combination method to see difference

--- rb_119/basic_nested_iteration_problems/13_permutation_manually.rb
+++ rb_119/basic_nested_iteration_problems/13_permutation_manually.rb
@@ -17,90 +17,116 @@ p sub_permutations(sample) == [[1], [1, 2], [1, 2, 3], [1, 3], [1, 3, 2],
 # true
 
 
-# ### Experiments (in progress):
-# =begin
-# PEDAC
 
-# P 
-# Find all possible subarrays consecutive and not.
+## Experiment with `combination` method:
+def sub_combinations(arr)
+  result = []
+  (1..arr.size).each do |number|
+    arr.combination(number) { |combination| result << combination }
+  end
+  result.sort
+end
 
-# Rules/reqs:
-# - subarray order doesn't matter
+sample = [1, 2, 3]
+p sub_combinations(sample)  
+# [[1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
 
-# D
-# Input: array
-# Output: nested array
-# Intemediate:
-# - array: subarray
-# - integer: iterate over indexes to find consecutive subarrays
-
-# High-level:
-# - Find consecutive subarrays without repeating itself
-#   eg [1], [1, 2], [1, 2, 3]
-# - Find first and last elements
-#   eg [1, 3]
-# - Build subarray with first-last-second element
-#   eg [3, 2, 1]
-# - Build subarray with skipping itself
-#   eg [2, 1, 3], [3, 1, 2], [3, 2, 1]
-# -
-#   eg [2, 3, 1], [3, 2, 1]
+sample1 = [1, 2, 3, 4, 5]
+p sub_combinations(sample1)
+# [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5], 
+# [1, 2, 3, 5], [1, 2, 4], [1, 2, 4, 5], [1, 2, 5], [1, 3], 
+# [1, 3, 4], [1, 3, 4, 5], [1, 3, 5], [1, 4], [1, 4, 5], [1, 5], 
+# [2], [2, 3], [2, 3, 4], [2, 3, 4, 5], [2, 3, 5], [2, 4], 
+# [2, 4, 5], [2, 5], 
+# [3], [3, 4], [3, 4, 5], [3, 5], 
+# [4], [4, 5], [5]]
 
 
-# Find consecutive subarrays without repeating itself:
-# Algo:
-# - Iterate over 0 index upto array size - 1 (first_index)
-#   - Iterate over `first_index` upto array size - 1 (second_index)
-#     - Add `[first_index..second_index]` to `subarrays`
 
-# eg 
-# [1, 2, 3]  first_index
-#     ^
-# [1, 2, 3]  second_index
-#     ^
+### Experiments (in progress):
+=begin
+PEDAC
 
-# [first_index..last_index]
-# [1], [1, 2], [1, 2, 3]
-# [2], [2, 3]
+P 
+Find all possible subarrays consecutive and not.
 
-# Main method:
-# - Create empty array `subarrays` 
-# - Find consecutive subarrays without repeating itself
-# - Find first and last elements
-# - Build subarray with first-last-second element
-# - Build subarray with skipping itself
-# - Return `subarrays`
+Rules/reqs:
+- subarray order doesn't matter
 
-# =end
+D
+Input: array
+Output: nested array
+Intemediate:
+- array: subarray
+- integer: iterate over indexes to find consecutive subarrays
 
-# def sub_permutations(arr)
-#   subarrays = []
-
-#   # Find consecutive subarrays without repeating itself
-#   0.upto(arr.size - 1) do |first_index|
-#     first_index.upto(arr.size - 1) do |second_index|
-#       subarray = arr[first_index..second_index]
-#       subarrays << subarray
-
-#       # Build subarray with first-last-second element
-#       subarrays << subarray.reverse if subarray.size > 1
+High-level:
+- Find consecutive subarrays without repeating itself
+  eg [1], [1, 2], [1, 2, 3]
+- Find first and last elements
+  eg [1, 3]
+- Build subarray with first-last-second element
+  eg [3, 2, 1]
+- Build subarray with skipping itself
+  eg [2, 1, 3], [3, 1, 2], [3, 2, 1]
+-
+  eg [2, 3, 1], [3, 2, 1]
 
 
-#     end
-#   end
+Find consecutive subarrays without repeating itself:
+Algo:
+- Iterate over 0 index upto array size - 1 (first_index)
+  - Iterate over `first_index` upto array size - 1 (second_index)
+    - Add `[first_index..second_index]` to `subarrays`
 
-#   # Find first and last elements
-#   subarray_first_last = [arr.first, arr.last]
-#   subarrays << subarray_first_last
+eg 
+[1, 2, 3]  first_index
+    ^
+[1, 2, 3]  second_index
+    ^
 
-#   # Build subarray with first-last-second element
-#   subarrays << subarray_first_last.reverse
+[first_index..last_index]
+[1], [1, 2], [1, 2, 3]
+[2], [2, 3]
+
+Main method:
+- Create empty array `subarrays` 
+- Find consecutive subarrays without repeating itself
+- Find first and last elements
+- Build subarray with first-last-second element
+- Build subarray with skipping itself
+- Return `subarrays`
+
+=end
+
+def sub_permutations(arr)
+  subarrays = []
+
+  # Find consecutive subarrays without repeating itself
+  0.upto(arr.size - 1) do |first_index|
+    first_index.upto(arr.size - 1) do |second_index|
+      subarray = arr[first_index..second_index]
+      subarrays << subarray
+
+      # Build subarray with first-last-second element
+      subarrays << subarray.reverse if subarray.size > 1
+
+
+    end
+  end
+
+  # Find first and last elements
+  subarray_first_last = [arr.first, arr.last]
+  subarrays << subarray_first_last
+
+  # Build subarray with first-last-second element
+  subarrays << subarray_first_last.reverse
 
   
-#   subarrays.sort
-# end
+  subarrays.sort
+end
 
-# sample = [1, 2, 3]
-# p sub_permutations(sample) #== [[1], [1, 2], [1, 2, 3], [1, 3], [1, 3, 2],
-#                               #  [2], [2, 1], [2, 1, 3], [2, 3], [2, 3, 1],
-#                               #  [3], [3, 1], [3, 1, 2], [3, 2], [3, 2, 1]]
+sample = [1, 2, 3]
+p sub_permutations(sample) #== [[1], [1, 2], [1, 2, 3], [1, 3], [1, 3, 2],
+                              #  [2], [2, 1], [2, 1, 3], [2, 3], [2, 3, 1],
+                              #  [3], [3, 1], [3, 1, 2], [3, 2], [3, 2, 1]]
