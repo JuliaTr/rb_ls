c3c7eaf6119190c81d9be9385545d0e8d090e187
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-26 13:50:04 -0600

    Add examples

--- rb_119/basic_nested_iteration_problems/looping.rb
+++ rb_119/basic_nested_iteration_problems/looping.rb
@@ -1,91 +1,133 @@
-# ## Looping
-# index = 0
+## Looping
+index = 0
 
-# loop do
-#   puts "Ruby"
-#   break if index == 3
-#   index += 1
-# end
+loop do
+  puts "Ruby"
+  break if index == 3
+  index += 1
+end
 
-# # Output:
-# # Ruby
-# # Ruby
-# # Ruby
-# # Ruby
-
-
-# times = 1
-
-# loop do
-#   puts "Ruby"
-#   break if times == 3
-#   times += 1
-# end
-
-# # Output:
-# # Ruby
-# # Ruby
-# # Ruby
+# Output:
+# Ruby
+# Ruby
+# Ruby
+# Ruby
 
 
+times = 1
 
+loop do
+  puts "Ruby"
+  break if times == 3
+  times += 1
+end
 
-# ## Iterating
-# arr = [1, 2, 3]
-# index = 0
-
-# loop do
-#   p arr[index]
-#   break if index == arr.size
-#   index += 1
-# end
-
-# # Output:
-# # 1
-# # 2
-# # 3
-# # nil
-
-
-# arr = [1, 2, 3]
-# index = 0
-
-# loop do
-#   p arr[index]
-#   break if index == arr.size - 1
-#   index += 1  
-# end
-
-# # Output:
-# # 1
-# # 2
-# # 3
-
-
-# arr = [1, 2, 3]
-# index = 0
-
-# loop do
-#   break if index == arr.size
-#   p arr[index]
-#   index += 1
-# end
-
-# # Output
-# # 1
-# # 2
-# # 3
-
-
-# [1, 2, 3].each { |val| puts val }
-
-# # Output:
-# # 1
-# # 2
-# # 3
+# Output:
+# Ruby
+# Ruby
+# Ruby
 
 
 
+
+## Iterating
+arr = [1, 2, 3]
+index = 0
+
+loop do
+  p arr[index]
+  break if index == arr.size
+  index += 1
+end
+
+# Output:
+# 1
+# 2
+# 3
+# nil
+
+
+arr = [1, 2, 3]
+index = 0
+
+loop do
+  p arr[index]
+  break if index == arr.size - 1
+  index += 1  
+end
+
+# Output:
+# 1
+# 2
+# 3
+
+
+arr = [1, 2, 3]
+index = 0
+
+loop do
+  break if index == arr.size
+  p arr[index]
+  index += 1
+end
+
+# Output
+# 1
+# 2
+# 3
+
+
+# Iteration using `#each`:
+[1, 2, 3].each { |val| puts val }
+
+# Output:
+# 1
+# 2
+# 3
+
+
+# Nested iteration:
+arr = [[1, 2], [3, 4]]
+index_outer = 0
+
+loop do
+  break if index_outer == arr.size
+  index_inner = 0
+
+  loop do
+    nested_arr = arr[index_outer]
+
+    break if index_inner == nested_arr.size
+    puts nested_arr[index_inner]
+    index_inner += 1
+  end
+
+  index_outer += 1
+end
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+
+
+# Nested iteration using `#each`:
+[[1, 2], [3, 4]].each do |nested_arr|
+  nested_arr.each do |val|
+    puts val
+  end
+end
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+
+
+
+## Transformation:
 arr = [[1, 2], [3, 4]]
 index_outer = 0
 index_inner = 0
@@ -109,10 +151,10 @@ p arr   # [[2, 3], [4, 5]]
 
 
 
-# arr = [[1, 2], [3, 4]]
-# arr.map! do |nested_arr|
-#   nested_arr.map! do |val|
-#     val += 1
-#   end
-# end
-# p arr  # [[2, 3], [4, 5]]
+arr = [[1, 2], [3, 4]]
+arr.map! do |nested_arr|
+  nested_arr.map! do |val|
+    val += 1
+  end
+end
+p arr  # [[2, 3], [4, 5]]
