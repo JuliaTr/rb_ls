292aee4e810a53dbe4d709dfb9f8a706e1fbb78d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-26 13:32:53 -0600

    Debug example

--- rb_119/basic_nested_iteration_problems/looping.rb
+++ rb_119/basic_nested_iteration_problems/looping.rb
@@ -1,88 +1,88 @@
-## Looping
-index = 0
+# ## Looping
+# index = 0
 
-loop do
-  puts "Ruby"
-  break if index == 3
-  index += 1
-end
+# loop do
+#   puts "Ruby"
+#   break if index == 3
+#   index += 1
+# end
 
-# Output:
-# Ruby
-# Ruby
-# Ruby
-# Ruby
-
-
-times = 1
-
-loop do
-  puts "Ruby"
-  break if times == 3
-  times += 1
-end
-
-# Output:
-# Ruby
-# Ruby
-# Ruby
+# # Output:
+# # Ruby
+# # Ruby
+# # Ruby
+# # Ruby
 
 
+# times = 1
+
+# loop do
+#   puts "Ruby"
+#   break if times == 3
+#   times += 1
+# end
+
+# # Output:
+# # Ruby
+# # Ruby
+# # Ruby
 
 
-## Iterating
-arr = [1, 2, 3]
-index = 0
-
-loop do
-  p arr[index]
-  break if index == arr.size
-  index += 1
-end
-
-# Output:
-# 1
-# 2
-# 3
-# nil
 
 
-arr = [1, 2, 3]
-index = 0
+# ## Iterating
+# arr = [1, 2, 3]
+# index = 0
 
-loop do
-  p arr[index]
-  break if index == arr.size - 1
-  index += 1  
-end
+# loop do
+#   p arr[index]
+#   break if index == arr.size
+#   index += 1
+# end
 
-# Output:
-# 1
-# 2
-# 3
-
-
-arr = [1, 2, 3]
-index = 0
-
-loop do
-  break if index == arr.size
-  p arr[index]
-  index += 1
-end
-
-# Output
-# 1
-# 2
-# 3
+# # Output:
+# # 1
+# # 2
+# # 3
+# # nil
 
 
-[1, 2, 3].each { |val| puts val }
+# arr = [1, 2, 3]
+# index = 0
 
-# Output:
-# 1
-# 2
-# 3
+# loop do
+#   p arr[index]
+#   break if index == arr.size - 1
+#   index += 1  
+# end
+
+# # Output:
+# # 1
+# # 2
+# # 3
+
+
+# arr = [1, 2, 3]
+# index = 0
+
+# loop do
+#   break if index == arr.size
+#   p arr[index]
+#   index += 1
+# end
+
+# # Output
+# # 1
+# # 2
+# # 3
+
+
+# [1, 2, 3].each { |val| puts val }
+
+# # Output:
+# # 1
+# # 2
+# # 3
 
 
 
@@ -93,6 +93,7 @@ index_inner = 0
 loop do
   break if index_outer == arr.size
 
+  index_inner = 0
   loop do
     nested_arr = arr[index_outer]
 
@@ -104,14 +105,14 @@ loop do
   index_outer += 1
 end
 
-p arr   # [[2, 3], [3, 4]]
+p arr   # [[2, 3], [4, 5]]
 
 
 
-arr = [[1, 2], [3, 4]]
-arr.map! do |nested_arr|
-  nested_arr.map! do |val|
-    val += 1
-  end
-end
-p arr  # [[2, 3], [4, 5]]
+# arr = [[1, 2], [3, 4]]
+# arr.map! do |nested_arr|
+#   nested_arr.map! do |val|
+#     val += 1
+#   end
+# end
+# p arr  # [[2, 3], [4, 5]]
