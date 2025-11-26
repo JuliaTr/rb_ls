52790b19136c80523b3ee1f7ccfb1c009591867e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-11-26 12:40:56 -0600

    Add example

--- rb_119/basic_nested_iteration_problems/looping.rb
+++ rb_119/basic_nested_iteration_problems/looping.rb
@@ -14,11 +14,17 @@ end
 # nil
 
 
-# arr = [1, 2, 3]
-# index = 0
 
-# loop do
-#   p arr[index]
-#   break if index == arr.size - 1
-#   index += 1  
-# end
+arr = [1, 2, 3]
+index = 0
+
+loop do
+  p arr[index]
+  break if index == arr.size - 1
+  index += 1  
+end
+
+# Output:
+# 1
+# 2
+# 3
