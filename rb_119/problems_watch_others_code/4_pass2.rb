bfdd0af51081617fa77b62e63833abb8e4c41726
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-08 15:21:55 -0600

    Add solution from learning from others

--- rb_119/problems_watch_others_code/4_pass2.rb
+++ rb_119/problems_watch_others_code/4_pass2.rb
@@ -15,11 +15,7 @@ is made up of only negative numbers, return 0 instead.
 Empty array is considered to have zero greatest sum. Note that
 the empty array is also a valid subarray.
 
-
-[-2, 1, -3, 4, -1, 2, 1, -5, 4]
-           [4, -1, 2, 1]
-
-
+________________________________________
 
 P
 Return maximim sum of a subarray.
@@ -81,6 +77,31 @@ def max_sequence(arr)
   substrings = substrings(arr)
   hash = return_hash_with_sum(substrings)
   hash.sort_by { |_, val| val }.last[1]
+end
+
+p max_sequence([]) == 0
+p max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+p max_sequence([11]) == 11
+p max_sequence([-32]) == 0
+p max_sequence([-2, 1, -7, 4, -10, 2, 1, 5, 4]) == 12
+# All test cases return `true`.
+
+
+
+## Learn from others:
+def max_sequence(arr)
+  return 0 if arr.all? { |val| val < 0 }   # return 0 for `[]`
+
+  result = []
+  (0..arr.size - 1).each do |first_idx|
+    (first_idx..arr.size - 1).each do |second_idx|
+      result << arr[first_idx..second_idx]
+    end
+  end
+
+  # Returns subarray with maximum sum.
+  # Calculate sum of this subarray.
+  result.max_by { |subarray| subarray.sum }.sum
 end
 
 p max_sequence([]) == 0
