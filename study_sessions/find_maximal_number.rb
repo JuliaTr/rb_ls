f7f1e8d8dfa9a804287b18cee365b718b34137a7
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-16 13:07:25 -0600

    Add more idiomatic solution

--- study_sessions/find_maximal_number.rb
+++ study_sessions/find_maximal_number.rb
@@ -1,77 +1,98 @@
-# # (recorded) review
+# (recorded) review
 
-# =begin
-# PROBLEM
-# Given an integer `n`, find the maximal number you can obtain by 
-# deleting exactly one digit of the given number.
+=begin
+PROBLEM
+Given an integer `n`, find the maximal number you can obtain by 
+deleting exactly one digit of the given number.
 
-# _________________________________________________________
+_________________________________________________________
 
-# - Find all combinations from input number with only one digit 
-#   removed
-# - Store combinations in array
-# - Sort combinations by descending order
-# =end
+- Find all combinations from input number with only one digit 
+  removed
+- Store combinations in array
+- Sort combinations by descending order
+=end
 
+## More idiomatic version:
+# Chaining on `Enumerator`
+def combinations(num)
+  arr = num.to_s.chars
 
-# ## Alternative:
-# # Refactored, `#max` method:
-# def combinations(num)
-#   combinations = []
-#   arr = num.to_s.chars
+  # `map` is used to not create `combinations = []`
+  arr.combination(arr.size - 1).map do |combination|
+  end
+end
 
-#   arr.combination(arr.size - 1) do |combination| 
-#     combinations << combination.join.to_i
-#   end
+def delete_digit(num)
+  combinations(num).max
+end
 
-#   combinations
-# end
-
-# def delete_digit(num)
-#   combinations(num).max
-# end
-
-# # TEST CASES
-# puts delete_digit(791983) == 91983
-# puts delete_digit(152) == 52
-# puts delete_digit(1001) == 101
-# puts delete_digit(10) == 1
-# # All test cases return `true`
+puts delete_digit(791983) == 91983
+puts delete_digit(152) == 52
+puts delete_digit(1001) == 101
+puts delete_digit(10) == 1
+# All test cases return `true`
 
 
 
-# ## Solutions:
-# def combinations(num)
-#   combinations = []
+## Alternative:
+# Refactored, `#max` method:
+def combinations(num)
+  combinations = []
+  arr = num.to_s.chars
 
-#   num_arr = num.digits.reverse
-#   num_arr.combination(num_arr.size - 1) { |combination| combinations << combination }
+  arr.combination(arr.size - 1) do |combination| 
+    combinations << combination.join.to_i
+  end
 
-#   combinations
-# end
+  combinations
+end
 
-# def convert_to_array_of_numbers(arr)
-#   arr.map do |subarray|
-#     subarray.each do |number|
-#       number.to_s
-#     end
+def delete_digit(num)
+  combinations(num).max
+end
 
-#     subarray.join.to_i
-#   end
-# end
+# TEST CASES
+puts delete_digit(791983) == 91983
+puts delete_digit(152) == 52
+puts delete_digit(1001) == 101
+puts delete_digit(10) == 1
+# All test cases return `true`
 
-# def delete_digit(num)
-#   combinations = combinations(num)
-#   array_numbers = convert_to_array_of_numbers(combinations)
-#   array_numbers.sort_by { |number| -number }[0]
-# end
 
-# # TEST CASES
-# puts delete_digit(791983) == 91983
-# puts delete_digit(152) == 52
-# puts delete_digit(1001) == 101
-# puts delete_digit(10) == 1
-# # All test cases return `true`
+
+## Solutions:
+def combinations(num)
+  combinations = []
+
+  num_arr = num.digits.reverse
+  num_arr.combination(num_arr.size - 1) { |combination| combinations << combination }
+
+  combinations
+end
+
+def convert_to_array_of_numbers(arr)
+  arr.map do |subarray|
+    subarray.each do |number|
+      number.to_s
+    end
+
+    subarray.join.to_i
+  end
+end
+
+def delete_digit(num)
+  combinations = combinations(num)
+  array_numbers = convert_to_array_of_numbers(combinations)
+  array_numbers.sort_by { |number| -number }[0]
+end
+
+# TEST CASES
+puts delete_digit(791983) == 91983
+puts delete_digit(152) == 52
+puts delete_digit(1001) == 101
+puts delete_digit(10) == 1
+# All test cases return `true`
 
 
 
