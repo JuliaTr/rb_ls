811492ae8a8e55cd8510e51d5a3fce30b1b431f1
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-16 12:21:01 -0600

    Add solution without combination method

--- study_sessions/find_maximal_number.rb
+++ study_sessions/find_maximal_number.rb
@@ -1,81 +1,101 @@
-# (recorded) review
+# # (recorded) review
 
-=begin
-PROBLEM
-Given an integer `n`, find the maximal number you can obtain by 
-deleting exactly one digit of the given number.
+# =begin
+# PROBLEM
+# Given an integer `n`, find the maximal number you can obtain by 
+# deleting exactly one digit of the given number.
 
-_________________________________________________________
+# _________________________________________________________
 
-- Find all combinations from input number with only one digit 
-  removed
-- Store combinations in array
-- Sort combinations by descending order
-=end
-
-
-## Alternative:
-# Refactored, `#max` method:
-def combinations(num)
-  combinations = []
-  arr = num.to_s.chars
-
-  arr.combination(arr.size - 1) do |combination| 
-    combinations << combination.join.to_i
-  end
-
-  combinations
-end
-
-def delete_digit(num)
-  combinations(num).max
-end
-
-# TEST CASES
-puts delete_digit(791983) == 91983
-puts delete_digit(152) == 52
-puts delete_digit(1001) == 101
-puts delete_digit(10) == 1
-# All test cases return `true`
+# - Find all combinations from input number with only one digit 
+#   removed
+# - Store combinations in array
+# - Sort combinations by descending order
+# =end
 
 
+# ## Alternative:
+# # Refactored, `#max` method:
+# def combinations(num)
+#   combinations = []
+#   arr = num.to_s.chars
 
-## Solutions:
-def combinations(num)
-  combinations = []
+#   arr.combination(arr.size - 1) do |combination| 
+#     combinations << combination.join.to_i
+#   end
 
-  num_arr = num.digits.reverse
-  num_arr.combination(num_arr.size - 1) { |combination| combinations << combination }
+#   combinations
+# end
 
-  combinations
-end
+# def delete_digit(num)
+#   combinations(num).max
+# end
 
-def convert_to_array_of_numbers(arr)
-  arr.map do |subarray|
-    subarray.each do |number|
-      number.to_s
-    end
+# # TEST CASES
+# puts delete_digit(791983) == 91983
+# puts delete_digit(152) == 52
+# puts delete_digit(1001) == 101
+# puts delete_digit(10) == 1
+# # All test cases return `true`
 
-    subarray.join.to_i
-  end
-end
 
-def delete_digit(num)
-  combinations = combinations(num)
-  array_numbers = convert_to_array_of_numbers(combinations)
-  array_numbers.sort_by { |number| -number }[0]
-end
 
-# TEST CASES
-puts delete_digit(791983) == 91983
-puts delete_digit(152) == 52
-puts delete_digit(1001) == 101
-puts delete_digit(10) == 1
-# All test cases return `true`
+# ## Solutions:
+# def combinations(num)
+#   combinations = []
+
+#   num_arr = num.digits.reverse
+#   num_arr.combination(num_arr.size - 1) { |combination| combinations << combination }
+
+#   combinations
+# end
+
+# def convert_to_array_of_numbers(arr)
+#   arr.map do |subarray|
+#     subarray.each do |number|
+#       number.to_s
+#     end
+
+#     subarray.join.to_i
+#   end
+# end
+
+# def delete_digit(num)
+#   combinations = combinations(num)
+#   array_numbers = convert_to_array_of_numbers(combinations)
+#   array_numbers.sort_by { |number| -number }[0]
+# end
+
+# # TEST CASES
+# puts delete_digit(791983) == 91983
+# puts delete_digit(152) == 52
+# puts delete_digit(1001) == 101
+# puts delete_digit(10) == 1
+# # All test cases return `true`
 
 
 
 # Without `#combination` method:
+def delete_digit(num)
+  arr = num.to_s
+
+  combinations = (0...arr.length).map do |idx|
+    (arr[0...idx] + arr[idx + 1..-1]).to_i
+  end
+
+  combinations.max
+end
+
+# TEST CASES
+p delete_digit(791983) == 91983
+puts delete_digit(152) == 52
+puts delete_digit(1001) == 101
+puts delete_digit(10) == 1
+# All test cases return `true`
+
+
+
+# Without `#combination` break down:
 def delete_digit(num)
   arr = num.to_s
 
@@ -90,7 +110,6 @@ def delete_digit(num)
 
     combination = before + after
     combination.to_i
-    # (arr[0...idx] + arr[idx + 1..-1]).to_i  (short path)
   end
 
   combinations.max
@@ -102,7 +121,7 @@ puts delete_digit(791983) == 91983
 Iteration 1:
 idx = 0
 arr[idx] = 7 (skipped)
-""
+""          # arr[0...idx]
 "91983"
 
 Iteration 2:
@@ -121,6 +140,6 @@ arr[idx] = 9 (skipped)
 "3"
 
 "79198"
-""
+""        # arr[idx + 1..-1]
 true
 =end
