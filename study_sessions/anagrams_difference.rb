c91b8a4b86cf7403f362b739631163e736df0f69
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-17 14:50:43 -0600

    Add improved solution

--- study_sessions/anagrams_difference.rb
+++ study_sessions/anagrams_difference.rb
@@ -10,6 +10,9 @@ _____________________________
 How many letters do you have to remove from them 
 to make them anagrams?
 
+P
+What is the total difference in character counts between the two strings?
+
 Rules:
 - order of characters doesn't matter
 - characters can be rearranged in an anagram
@@ -62,16 +65,53 @@ that are not in str1.
 'ab', 'cd'
 
 
-Idea2: ---
-Create tally for each string.
-'ab', 'a'
-{'a'=>1, 'b'=>1}
+Idea2: (improved)  V
+Create tally for str1 and str2.
+eg: 'aab', 'a'
+'aab'
+{'a'=>2, 'b'=>1}
+'a'
 {'a'=> 1}
 
-'ab', 'cd'
+eg: 'codewars', 'hackerrank'
+'codewars'
+{'c'=>1, 'o'=>1, 'd'=>1, 'e'=>1, 'w'=>1, 'a'=>1, 'r'=>1, 's'=>1}
+sorted in alphabetical order:  
+{'a'=>1, 'c'=>1, 'd'=>1, 'e'=>1, o'=>1, 'r'=>1, 's'=>1, 'w'=>1}
 
-Loop over tally1 and reject pairs which have the same
-key and value
+'hackerrank'
+{'h'=>1, 'a'=>2, 'c'=>1, 'k'=>2, 'e'=>1, 'r'=>2, 'n'=>1}
+sorted in alphabetical order:
+{'a'=>2, 'c'=>1, 'e'=>1, 'h'=>1, 'k'=>2, 'n'=>1, 'r'=>2}
+
+
+Find the difference for each character
+{'a'=>1, 'c'=>1, 'd'=>1, 'e'=>1, o'=>1, 'r'=>1, 's'=>1, 'w'=>1}
+deleted: 'd', 'o', 's', 'w'
+remained: 'a', 'c', 'e', 'r'
+
+{'a'=>2, 'c'=>1, 'e'=>1, 'h'=>1, 'k'=>2, 'n'=>1, 'r'=>2}
+deleted: 'a', 'h', 'k', 'n', 'r'
+remained: 'a', 'c', 'e', 'r'
+
+Algo:
+- Define a shortest string
+- Find unique characters in shortest string
+- Create `count` and assign to `0`
+- Loop over each character in unique shortest string (`char`)
+  - If hash1[char] value SAME as hash2[char] value
+    - next
+  - else
+    - Increment `count` by (hash1[char] - hash2[char]).abs
+- Select pairs from hash2, which keys are not included in shortest string
+  with unique values.
+- Extract values
+- Sum up values
+
+Sum the difference
+Algo:
+- Sum up `count` and rest values
+
 
 
 Idea3: ---
@@ -90,8 +130,47 @@ included in str2.
 str1 size - selected characters size
 +
 str2 size - selected characters size
+
 =end
 
+## Improved logic (Idea2):
+def anagram_difference(str1, str2)
+  hash1 = str1.chars.tally
+  hash2 = str2.chars.tally
+
+  shortest_str = str1.size < str2.size ? str1 : str2
+
+  count = 0
+  shortest_str.chars.uniq.each do |char|
+    if hash1[char] == hash2[char]
+      next
+    elsif hash1[char].nil? || hash2[char].nil?
+      count += 1
+    else
+      count += (hash1[char] - hash2[char]).abs
+    end
+  end
+
+  longest_hash = shortest_str == str1 ? hash2 : hash1
+
+  rest_pairs = longest_hash.reject { |key, val| shortest_str.include?(key) }
+  count + rest_pairs.values.sum
+end
+
+p anagram_difference('banana', 'bandana') == 1
+
+p anagram_difference('', '') == 0
+p anagram_difference('a', '') == 1
+p anagram_difference('', 'a') == 1
+p anagram_difference('ab', 'a') == 1
+p anagram_difference('ab', 'ba') == 0
+p anagram_difference('ab', 'cd') == 4
+p anagram_difference('aab', 'a') == 2
+p anagram_difference('a', 'aab') == 2
+p anagram_difference('codewars', 'hackerrank') == 10
+# All test cases return `true`.
+
+  
 
 ## Solution (Idea4):
 def anagram_difference(str1, str2)
@@ -102,6 +181,8 @@ def anagram_difference(str1, str2)
 
   (str1.size - selected.size) + (str2.size - selected.size)
 end
+
+p anagram_difference('banana', 'bandana') == 1  # false
 
 puts(anagram_difference('', '') == 0)
 puts(anagram_difference('a', '') == 1)
