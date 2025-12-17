76fd17dd89f812a305f022f901875a03f0545422
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-17 13:57:09 -0600

    Add alternative solution

--- rb_119/problems_watch_others_code/3_pass3.rb
+++ rb_119/problems_watch_others_code/3_pass3.rb
@@ -1,5 +1,3 @@
-# need to review
-
 =begin
 PROBLEM
 Create a method that takes a positive integer number and
@@ -141,6 +139,109 @@ end
 p next_bigger_num(9) == -1 
 p next_bigger_num(12) == 21
 p next_bigger_num(513) == 531
+p next_bigger_num(2017) == 2071
+p next_bigger_num(111) == -1
+p next_bigger_num(531) == -1
+p next_bigger_num(123456789) == 123456798
+# All test cases return `true`
+
+
+
+## Alternative:
+=begin
+Thoughts:
+- digits in input and output are the same
+- if they are sorted, they are in arrays in the same order
+- so to find the next bigger number (consists of the same 
+  digits) should start from the next number and till the maximum
+  (sorted and reversed)
+- input sorted number and number sorted in the iteration should 
+  be the same
+=end
+
+def sorted_digits(int)
+  int.to_s.chars.sort
+end
+
+def next_bigger_num(input)
+  max_number = sorted_digits(input).reverse.join.to_i
+
+  # p input
+  (input + 1..max_number).each do |num|
+    # p num
+    # p sorted_digits(num)
+    # p sorted_digits(input)
+    return num if sorted_digits(num) == sorted_digits(input)
+  end
+
+  -1
+end
+
+p next_bigger_num(132) == 213
+
+p next_bigger_num(9) == -1 
+p next_bigger_num(12) == 21
+p next_bigger_num(513) == 531
+=begin
+$ ruby 3_pass3.rb
+513
+514
+["1", "4", "5"]
+["1", "3", "5"]
+515
+["1", "5", "5"]
+["1", "3", "5"]
+516
+["1", "5", "6"]
+["1", "3", "5"]
+517
+["1", "5", "7"]
+["1", "3", "5"]
+518
+["1", "5", "8"]
+["1", "3", "5"]
+519
+["1", "5", "9"]
+["1", "3", "5"]
+520
+["0", "2", "5"]
+["1", "3", "5"]
+521
+["1", "2", "5"]
+["1", "3", "5"]
+522
+["2", "2", "5"]
+["1", "3", "5"]
+523
+["2", "3", "5"]
+["1", "3", "5"]
+524
+["2", "4", "5"]
+["1", "3", "5"]
+525
+["2", "5", "5"]
+["1", "3", "5"]
+526
+["2", "5", "6"]
+["1", "3", "5"]
+527
+["2", "5", "7"]
+["1", "3", "5"]
+528
+["2", "5", "8"]
+["1", "3", "5"]
+529
+["2", "5", "9"]
+["1", "3", "5"]
+530
+["0", "3", "5"]
+["1", "3", "5"]
+531
+["1", "3", "5"]
+["1", "3", "5"]
+531
+=end
+
 p next_bigger_num(2017) == 2071
 p next_bigger_num(111) == -1
 p next_bigger_num(531) == -1
