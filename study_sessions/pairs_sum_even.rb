953c31012b16cc982be12db20dceb1765f18a2b3
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-19 14:27:35 -0600

    Add pairs_sum_even.rb

--- study_sessions/pairs_sum_even.rb
+++ study_sessions/pairs_sum_even.rb
@@ -1,6 +1,13 @@
-# 6. Create a method that takes an array of integers and returns an array containing all pairs of numbers in the original array that sum to an even number. Each pair should be sorted in ascending order, and the entire array of pairs should be sorted by the first element of each pair.
-
 =begin
+6. Create a method that takes an array of integers 
+and returns an array containing all pairs of numbers 
+in the original array that sum to an even number. Each 
+pair should be sorted in ascending order, and the entire 
+array of pairs should be sorted by the first element of 
+each pair.
+
+________________________________
+
 even_sum_pairs([1, 2, 3, 4, 5]) == [[1, 1], [1, 3], [1, 5], [2, 2], [2, 4], [3, 3], [3, 5], [4, 4], [5, 5]]
 
 [1, 1] -> 2
@@ -45,6 +52,34 @@ Output: nested array with pairs
 Sort
 =end
 
+## Alternative with sorting:
+def pairs(arr)
+  result = []
+
+  0.upto(arr.size - 1) do |first|
+    first.upto(arr.size - 1) do |second|
+      pair = [arr[first], arr[second]]
+      result << pair if pair.sum.even?
+    end
+  end
+
+  result
+end
+
+def even_sum_pairs(arr)
+  return arr if arr.empty?
+  pairs = pairs(arr)
+  pairs.sort.map { |subarray| subarray.sort }
+end
+
+p even_sum_pairs([1, 2, 3, 4, 5]) == [[1, 1], [1, 3], [1, 5], [2, 2], [2, 4], [3, 3], [3, 5], [4, 4], [5, 5]]
+p even_sum_pairs([4, 3, 6, 7]) == [[3, 3], [3, 7], [4, 4], [4, 6], [6, 6], [7, 7]]
+p even_sum_pairs([]) == []
+# All test cases return `true`.
+
+
+
+## Solution:
 def pairs(arr)
   result = []
 
