f2cdae64465404fd1603585b56c7359e8b82b347
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-25 17:49:52 -0600

    Add 1.rb

---
+++ rb_120/oop_book/object_model/exercises/1.rb
@@ -0,0 +1,26 @@
+=begin
+Exercise:
+How do we create an object in Ruby? Give an example of 
+the creation of an object.
+=end
+
+class Car
+end
+
+kia = Car.new
+
+=begin
+On line 4, an instamce of a class `Car` is created and stored
+in a variable `kia`. The `kia` object is returned by calling
+the class method `new`. Now `kia` is an object of class `Dog`
+On lines 1-2, a class `Car` is cdefined. 
+=end
+
+=begin
+In other words:
+We've instantiated an object `kia` from the class `Car`.
+
+Or:
+An object is created by defining a class and instantiating 
+it by using the `.new` method to create an instance object
+=end
