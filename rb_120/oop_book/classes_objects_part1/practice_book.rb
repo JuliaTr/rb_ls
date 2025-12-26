57833c7dd628a91d6b0a131a2527e32cd48f67a4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-26 15:14:26 -0600

    Add examples with instance variables

--- rb_120/oop_book/classes_objects_part1/practice_book.rb
+++ rb_120/oop_book/classes_objects_part1/practice_book.rb
@@ -1,4 +1,4 @@
-# Initialize objects:
+## Initialize objects:
 class GoodDog
   def initialize
     puts "This object was initialized!"
@@ -6,3 +6,51 @@ class GoodDog
 end
 
 sparky = GoodDog.new   # This object was initialized!
+
+
+
+## Instance variables
+class GoodDog
+  def initialize(name)
+    @name = name
+  end
+end
+
+sparky = GoodDog.new("Sparky")  # (no output)
+
+
+# Composition
+class Engine
+  def start
+    puts "Engine starting..."
+  end
+end
+
+class Car
+  def initialize
+    @engine = Engine.new  # Engine instance is created when Car is created 
+  end
+
+  def start
+    @engine.start
+  end
+end
+
+my_car = Car.new
+my_car.start      # Engine starting...
+
+
+# Aggregation
+class Passenger
+end
+
+class Car
+  def initialize(passengers)
+    @passengers = passengers
+  end
+end
+
+passengers = [Passenger.new, Passenger.new]
+my_car = Car.new(passengers)    # (no output)
+
+
