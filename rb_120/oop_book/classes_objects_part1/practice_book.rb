4779f4f82caede396d512224e2c091fa16b53b73
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-26 18:41:25 -0600

    Add exercises with accessor methods

--- rb_120/oop_book/classes_objects_part1/practice_book.rb
+++ rb_120/oop_book/classes_objects_part1/practice_book.rb
@@ -1,89 +1,129 @@
-## Initialize Objects:
-class GoodDog
-  def initialize
-    puts "This object was initialized!"
-  end
-end
+# # ## Initialize Objects:
+# # class GoodDog
+# #   def initialize
+# #     puts "This object was initialized!"
+# #   end
+# # end
 
-sparky = GoodDog.new   # This object was initialized!
+# # sparky = GoodDog.new   # This object was initialized!
 
 
 
-## Instance Variables
+# # ## Instance Variables
+# # class GoodDog
+# #   def initialize(name)
+# #     @name = name
+# #   end
+# # end
+
+# # sparky = GoodDog.new("Sparky")  # (no output)
+
+
+# # # Composition
+# # class Engine
+# #   def start
+# #     puts "Engine starting..."
+# #   end
+# # end
+
+# # class Car
+# #   def initialize
+# #     @engine = Engine.new  # Engine instance is created when Car is created 
+# #   end
+
+# #   def start
+# #     @engine.start
+# #   end
+# # end
+
+# # my_car = Car.new
+# # my_car.start      # Engine starting...
+
+
+# # # Aggregation
+# # class Passenger
+# # end
+
+# # class Car
+# #   def initialize(passengers)
+# #     @passengers = passengers
+# #   end
+# # end
+
+# # passengers = [Passenger.new, Passenger.new]
+# # p passengers # [#<Passenger:0x00000001056c5380>, #<Passenger:0x00000001056c5308>]
+
+# # my_car = Car.new(passengers)    # (no output)
+
+# # p my_car   
+# # #<Car:0x0000000109e253d8 @passengers=[#<Passenger:0x0000000109e254f0>, 
+# # #<Passenger:0x0000000109e25478>]>
+
+
+
+# # ## Instance Methods
+# # class GoodDog
+# #   def initialize(name)
+# #     @name = name
+# #   end
+
+# #   def speak
+# #     "Arf!"
+# #   end
+# # end
+
+# # sparky = GoodDog.new("Sparky")
+# # p sparky.speak       # "Arf!"
+# # puts sparky.speak    # Arf!
+
+# # fido = GoodDog.new("Fido")
+# # puts fido.speak      # Arf!
+
+
+
+# class GoodDog
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def speak
+#     "#{@name} says arf!"
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky")  # The class is instantiated
+# puts sparky.speak      # Sparky says arf!
+
+# fido = GoodDog.new("Fido")
+# puts fido.speak        # Sparky says arf!
+
+# puts sparky.name
+# # undefined method `name' for #<GoodDog:0x0000000105955050 @name="Sparky"> (NoMethodError)
+
+# =begin
+# `sparky` and `fio` are objects or an instance of `GoodDog` class.
+
+# - "Sparky" is an argument to `GoodDog.new`
+# - Ruby calls `initialize("Sparky")` behind the scenes
+# - Inside `initialize`, the parameter `name` refers to `"Sparky"`
+# - `@name = name` assigns `"Sparky"` to the instance variable
+#   `@name` for that particular object.
+
+# Same instance variable name `@name`.
+# Different instance variable values `"Sparky"` for `sparky`, 
+# `"Fido"` for `fido`.
+# =end
+
+
+
+## Accessor Methods
 class GoodDog
   def initialize(name)
     @name = name
   end
-end
 
-sparky = GoodDog.new("Sparky")  # (no output)
-
-
-# Composition
-class Engine
-  def start
-    puts "Engine starting..."
-  end
-end
-
-class Car
-  def initialize
-    @engine = Engine.new  # Engine instance is created when Car is created 
-  end
-
-  def start
-    @engine.start
-  end
-end
-
-my_car = Car.new
-my_car.start      # Engine starting...
-
-
-# Aggregation
-class Passenger
-end
-
-class Car
-  def initialize(passengers)
-    @passengers = passengers
-  end
-end
-
-passengers = [Passenger.new, Passenger.new]
-p passengers # [#<Passenger:0x00000001056c5380>, #<Passenger:0x00000001056c5308>]
-
-my_car = Car.new(passengers)    # (no output)
-
-p my_car   
-#<Car:0x0000000109e253d8 @passengers=[#<Passenger:0x0000000109e254f0>, 
-#<Passenger:0x0000000109e25478>]>
-
-
-
-## Instance Methods
-class GoodDog
-  def initialize(name)
-    @name = name
-  end
-
-  def speak
-    "Arf!"
-  end
-end
-
-sparky = GoodDog.new("Sparky")
-p sparky.speak       # "Arf!"
-puts sparky.speak    # Arf!
-
-fido = GoodDog.new("Fido")
-puts fido.speak      # Arf!
-
-
-
-class GoodDog
-  def initialize(name)
-    @name = name
+  def get_name   # getter method
+    @name
   end
 
   def speak
@@ -91,22 +131,6 @@ class GoodDog
   end
 end
 
-sparky = GoodDog.new("Sparky")  # The class is instantiated
+sparky = GoodDog.new("Sparky")
 puts sparky.speak      # Sparky says arf!
-
-fido = GoodDog.new("Fido")
-puts fido.speak        # Sparky says arf!
-
-=begin
-`sparky` and `fio` are objects or an instance of `GoodDog` class.
-
-- "Sparky" is an argument to `GoodDog.new`
-- Ruby calls `initialize("Sparky")` behind the scenes
-- Inside `initialize`, the parameter `name` refers to `"Sparky"`
-- `@name = name` assigns `"Sparky"` to the instance variable
-  `@name` for that particular object.
-
-Same instance variable name `@name`.
-Different instance variable values `"Sparky"` for `sparky`, 
-`"Fido"` for `fido`.
-=end
+puts sparky.get_name   # Sparky
