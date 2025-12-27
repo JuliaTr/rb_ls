afe83aaa4713b4c77778e14b7c2c23ae63c23e33
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-26 18:29:32 -0600

    Add exercises with instance methods

--- rb_120/oop_book/classes_objects_part1/practice_book.rb
+++ rb_120/oop_book/classes_objects_part1/practice_book.rb
@@ -1,4 +1,4 @@
-## Initialize objects:
+## Initialize Objects:
 class GoodDog
   def initialize
     puts "This object was initialized!"
@@ -9,7 +9,7 @@ sparky = GoodDog.new   # This object was initialized!
 
 
 
-## Instance variables
+## Instance Variables
 class GoodDog
   def initialize(name)
     @name = name
@@ -51,6 +51,62 @@ class Car
 end
 
 passengers = [Passenger.new, Passenger.new]
+p passengers # [#<Passenger:0x00000001056c5380>, #<Passenger:0x00000001056c5308>]
+
 my_car = Car.new(passengers)    # (no output)
 
+p my_car   
+#<Car:0x0000000109e253d8 @passengers=[#<Passenger:0x0000000109e254f0>, 
+#<Passenger:0x0000000109e25478>]>
 
+
+
+## Instance Methods
+class GoodDog
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "Arf!"
+  end
+end
+
+sparky = GoodDog.new("Sparky")
+p sparky.speak       # "Arf!"
+puts sparky.speak    # Arf!
+
+fido = GoodDog.new("Fido")
+puts fido.speak      # Arf!
+
+
+
+class GoodDog
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} says arf!"
+  end
+end
+
+sparky = GoodDog.new("Sparky")  # The class is instantiated
+puts sparky.speak      # Sparky says arf!
+
+fido = GoodDog.new("Fido")
+puts fido.speak        # Sparky says arf!
+
+=begin
+`sparky` and `fio` are objects or an instance of `GoodDog` class.
+
+- "Sparky" is an argument to `GoodDog.new`
+- Ruby calls `initialize("Sparky")` behind the scenes
+- Inside `initialize`, the parameter `name` refers to `"Sparky"`
+- `@name = name` assigns `"Sparky"` to the instance variable
+  `@name` for that particular object.
+
+Same instance variable name `@name`.
+Different instance variable values `"Sparky"` for `sparky`, 
+`"Fido"` for `fido`.
+=end
