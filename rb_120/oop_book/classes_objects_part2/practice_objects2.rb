146c9ff8f567b56366884e853239c05b580a8a8e
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-28 15:29:50 -0600

    Add exercise with class variables

--- rb_120/oop_book/classes_objects_part2/practice_objects2.rb
+++ rb_120/oop_book/classes_objects_part2/practice_objects2.rb
@@ -1,8 +1,30 @@
-## Class Methods
+# ## Class Methods
+# class GoodDog
+#   def self.what_am_I     # Class method definition
+#     "I'm a GoodDog class!"
+#   end
+# end
+
+# p GoodDog.what_am_I  # "I'm a GoodDog class!"
+
+
+
+## Class Variables
 class GoodDog
-  def self.what_am_I     # Class method definition
-    "I'm a GoodDog class!"
+  @@number_of_dogs = 0
+
+  def initialize
+    @@number_of_dogs += 1
+  end
+
+  def self.total_number_of_dogs
+    @@number_of_dogs
   end
 end
 
-p GoodDog.what_am_I  # "I'm a GoodDog class!"
+puts GoodDog.total_number_of_dogs    # 0
+
+dog1 = GoodDog.new
+dog2 = GoodDog.new
+
+puts GoodDog.total_number_of_dogs    # 2
