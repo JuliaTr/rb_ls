b025450bc14b2ab3daa7fccf5f05a0e8f84213a7
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2025-12-28 15:42:39 -0600

    Add exercise with to_s method

--- rb_120/oop_book/classes_objects_part2/practice_objects2.rb
+++ rb_120/oop_book/classes_objects_part2/practice_objects2.rb
@@ -31,20 +31,58 @@
 
 
 
-## Constants
+# ## Constants
+# class GoodDog
+#   DOG_YEARS = 7
+
+#   attr_accessor :name, :age
+
+#   def initialize(n, a)
+#     self.name = n
+#     self.age = a * DOG_YEARS
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky", 4)
+# puts sparky.age       # 28
+# puts sparky           # #<GoodDog:0x0000000107045508>
+
+
+# to_s
 class GoodDog
   DOG_YEARS = 7
 
   attr_accessor :name, :age
+
   def initialize(n, a)
-    self.name = n
-    self.age = a * DOG_YEARS
+    @name = n
+    @age = a * DOG_YEARS
+  end
+
+  def to_s
+    "This dog's name is #{name} and it is #{age} in dog years."
   end
 end
 
 sparky = GoodDog.new("Sparky", 4)
-puts sparky.age       # 28
-puts sparky           # #<GoodDog:0x0000000107045508>
+puts sparky
+# This dog's name is Sparky and it is 28 in dog years.
+p sparky
+# #<GoodDog:0x00000001042651d8 @name="Sparky", @age=28>
 
 
-# to_s
+=begin
+irb(main):001:0> arr = [1, 2, 3]
+=> [1, 2, 3]
+irb(main):002:0> x = 5
+=> 5
+irb(main):003:0> "The #{arr} array doesn't include #{x}."
+=> "The [1, 2, 3] array doesn't include 5."
+
+irb(main):004:0> name = 'Sparky'
+=> "Sparky"
+irb(main):005:0> age = 28
+=> 28
+irb(main):006:0> "This dog's name is #{name} and it is #{age} in dog years."
+=> "This dog's name is Sparky and it is 28 in dog years."
+=end
