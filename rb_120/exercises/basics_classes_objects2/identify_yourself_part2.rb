788b1e226aef734b25c29d294a73beb69e69825d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-02 15:30:13 -0600

    Add identify_yourself_part2.rb

---
+++ rb_120/exercises/basics_classes_objects2/identify_yourself_part2.rb
@@ -0,0 +1,33 @@
+=begin
+Update the following code so that it prints `I'm Sophie!` when it 
+invokes `puts kitty`.
+=end
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty     #<Cat:0x00000001093d5ab8>
+
+
+
+## Solution
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty                  # I'm Sophie!
