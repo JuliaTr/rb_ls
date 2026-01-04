991557056b8e20385020169bfec139d31635ba79
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-04 15:30:30 -0600

    Add prefix_name.rb

---
+++ rb_120/exercises/basics_accessor_methods/prefix_name.rb
@@ -0,0 +1,33 @@
+=begin
+Using the following code, add the appropriate accessor 
+methods so that `@name` is returned with the added prefix 
+`'Mr.'`.
+=end
+
+class Person
+  attr_writer :name
+
+  def name              # getter
+    "Mr. #{@name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name       # Mr. James
+
+
+
+## Experiments
+class Person
+  attr_writer :name
+
+  def name
+    @name
+    "Mr. #{@name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name        # Mr. James
