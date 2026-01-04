ddf0ad766b01e6e6202ef5531152930ac5819f8b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-04 12:42:34 -0600

    Add choose_right_method.rb

---
+++ rb_120/exercises/basics_accessor_methods/choose_right_method.rb
@@ -0,0 +1,13 @@
+# Add the appropriate accessor methods to the following code.
+
+## Solution
+class Person
+  attr_accessor :name
+  attr_writer :phone_number
+end
+
+person1 = Person.new
+person1.name = 'Jessica'              # setter
+person1.phone_number = '0123456789'   # setter
+puts person1.name                     # getter
+# Jessica
