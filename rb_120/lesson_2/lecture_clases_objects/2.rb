985c052973d6ab39e05c315511db6bc95e5aa5aa
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-05 10:26:19 -0600

    Add 2.rb

---
+++ rb_120/lesson_2/lecture_clases_objects/2.rb
@@ -0,0 +1,53 @@
+=begin
+Modify the class definition from above to facilitate the 
+following methods. Note that there is no `name=` setter method 
+now.
+=end
+
+## Solution
+class Person
+  attr_reader :name, :first_name
+  attr_accessor :last_name
+
+  def initialize(name)
+    @name = name
+    @first_name = @name
+    @last_name = ''
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+end
+
+bob = Person.new('Robert')
+p bob.name                   # "Robert"
+p bob.first_name             # "Robert"
+p bob.last_name              # ""
+bob.last_name = 'Smith'
+p bob.name                   # "Robert Smith"
+
+
+
+
+## Possible solution
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
+
+bob = Person.new('Robert')
+p bob.name                   # "Robert "
+p bob.first_name             # "Robert"
+p bob.last_name              # ""
+bob.last_name = 'Smith'
+p bob.name                   # "Robert Smith"
