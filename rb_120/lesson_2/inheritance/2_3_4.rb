df38da76509d8afd04e124606db303687414ef1c
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-05 14:52:41 -0600

    Add 2_3_4.rb

---
+++ rb_120/lesson_2/inheritance/2_3_4.rb
@@ -0,0 +1,60 @@
+=begin
+Create a new class called `Cat`, which can do everything a dog 
+can, except swim or fetch. Assume the methods do the exact 
+same thing. Hint: don't just copy and paste all methods in `Dog` 
+into `Cat`; try to come up with some class hierarchy.
+=end
+
+## Possible solution
+class Pet
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+end
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim" 
+  end
+end
+
+class Cat < Pet
+  def speak
+    'meow!'
+  end
+end
+
+pete = Pet.new
+kitty = Cat.new
+dave = Dog.new
+bud = Bulldog.new
+
+p pete.run      # "running!"
+p pete.speak    #  undefined method `speak' for #<Pet:0x0000000107124348> (NoMethodError)
+
+p kitty.run     # "running!"
+p kitty.speak   # "meow!"
+p kitty.fetch   # undefined method `fetch' for #<Cat:0x0000000106854358> (NoMethodError)
+
+p dave.speak    # "bark!"
+
+p bud.run       # "running!"
+p bud.swim      # "can't swim"
