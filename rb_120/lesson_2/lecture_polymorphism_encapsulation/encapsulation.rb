56656b1bfedaae364d01d348ef4f25970364a289
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-06 12:51:41 -0600

    Add encapsulation.rb

---
+++ rb_120/lesson_2/lecture_polymorphism_encapsulation/encapsulation.rb
@@ -0,0 +1,23 @@
+## Encapsulation
+class Dog
+  attr_reader :nickname
+
+  def initialize(n)
+    @nickname = n
+  end
+
+  def change_nickname(n)
+    self.nickname = n
+  end
+
+  def greeting
+    "#{nickname.capitalize} says Woof Woof!"
+  end
+
+  private
+  attr_writer :nickname
+end
+
+dog = Dog.new("rex")
+dog.change_nickname("barny")
+puts dog.greeting             # Barny says Woof Woof!
