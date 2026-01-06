d9a18ff754922ae95862e55da3eb0db17bc41220
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-06 11:17:20 -0600

    Add polymorphism.rb

--- rb_120/lesson_2/lecture_polymorphism_encapsulation/polymorphism.rb
+++ rb_120/lesson_2/lecture_polymorphism_encapsulation/polymorphism.rb
@@ -1,132 +1,133 @@
-# ## Polymorphism
-# # Polymorphism through inheritance
-# class Animal
-#   def move
-#     puts "doesn't move"
-#   end
-# end
+## Polymorphism
+# Polymorphism through inheritance
+class Animal
+  def move
+    puts "doesn't move"
+  end
+end
 
-# class Fish < Animal
-#   def move
-#     puts "swim"
-#   end
-# end
+class Fish < Animal
+  def move
+    puts "swim"
+  end
+end
 
-# class Cat < Animal
-#   def move
-#     puts "walk"
-#   end
-# end
+class Cat < Animal
+  def move
+    puts "walk"
+  end
+end
 
-# # Sponges and corals don't move. So, they don't have a separate 
-# # move method
-# class Sponge < Animal; end
-# class Coral < Animal; end
+# Sponges and corals don't move. So, they don't have a separate 
+# move method
+class Sponge < Animal; end
+class Coral < Animal; end
 
-# animals = [Fish.new, Cat.new, Sponge.new, Coral.new]
-# animals.each { |animal| animal.move }
-# =begin
-# swim
-# walk
-# doesn't move
-# doesn't move
-# =end
-
-
-
-# module Floatable
-#   def float
-#     puts "float"
-#   end
-# end
-
-# class Animal
-#   def move
-#     puts "doesn't move"
-#   end
-# end
-
-# # Fish can float
-# class Fish < Animal
-#   include Floatable
-
-#   def move
-#     puts "swim"
-#   end
-# end
-
-# class Cat < Animal
-#   def move
-#     puts "walk"
-#   end
-# end
-
-# # Sponges can't float, but corals can float
-# class Sponge < Animal; end
-
-# class Coral < Animal
-#   include Floatable
-# end
-
-# floating_animals = [Fish.new, Coral.new]
-# floating_animals.each { |animal| animal.float }
-# =begin
-# float
-# float
-# =end
+animals = [Fish.new, Cat.new, Sponge.new, Coral.new]
+animals.each { |animal| animal.move }
+=begin
+swim
+walk
+doesn't move
+doesn't move
+=end
 
 
 
-# module Floatable
-#   def float
-#     puts "float"
-#   end
-# end
+module Floatable
+  def float
+    puts "float"
+  end
+end
 
-# class Animal
-#   def move
-#     puts "doesn't move"
-#   end
-# end
+class Animal
+  def move
+    puts "doesn't move"
+  end
+end
 
-# class Fish < Animal
-#   include Floatable
+# Fish can float
+class Fish < Animal
+  include Floatable
 
-#   def move
-#     puts "swim"
-#   end
-# end
+  def move
+    puts "swim"
+  end
+end
 
-# class Cat < Animal
-#   def move
-#     puts "walk"
-#   end
-# end
+class Cat < Animal
+  def move
+    puts "walk"
+  end
+end
 
-# class Sponge < Animal; end
+# Sponges can't float, but corals can float
+class Sponge < Animal; end
 
-# class Coral < Animal
-#   include Floatable
-# end
+class Coral < Animal
+  include Floatable
+end
 
-# # A boat is not a type of animal, but it can float
-# class Boat
-#   include Floatable
-# end
+floating_animals = [Fish.new, Coral.new]
+floating_animals.each { |animal| animal.float }
+=begin
+float
+float
+=end
 
-# floating_things = [Fish.new, Coral.new, Boat.new]
-# floating_things.each { |thing| thing.float }
-# =begin
-# float
-# float
-# float
-# =end
+
+
+module Floatable
+  def float
+    puts "float"
+  end
+end
+
+class Animal
+  def move
+    puts "doesn't move"
+  end
+end
+
+class Fish < Animal
+  include Floatable
+
+  def move
+    puts "swim"
+  end
+end
+
+class Cat < Animal
+  def move
+    puts "walk"
+  end
+end
+
+class Sponge < Animal; end
+
+class Coral < Animal
+  include Floatable
+end
+
+# A boat is not a type of animal, but it can float
+class Boat
+  include Floatable
+end
+
+floating_things = [Fish.new, Coral.new, Boat.new]
+floating_things.each { |thing| thing.float }
+=begin
+float
+float
+float
+=end
 
 
 
 # Polymorphism through duck typing
+# Refactored example below
 class Wedding
-  attr_reader :guests, :flowers, :songes
+  attr_reader :guests, :flowers, :songs
 
   def prepare(preparers)
     preparers.each do |preparer|
@@ -201,3 +202,16 @@ class Musician
     #implementation
   end
 end
+
+
+# Not a real polymorphism, because `Circle#draw` and `Blinds3draw` do
+# totally different conceptual things.
+class Circle
+  def draw; end
+end
+
+class Blinds
+  def draw; end
+end
+
+[Circle.new, Blinds.new].each { |obj| obj.draw }
