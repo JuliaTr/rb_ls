5a597cd312f159f3d05c59dcef9cd773f36fed13
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-06 13:32:47 -0600

    Add collaborator_objects.rb

--- rb_120/lesson_2/lecture_collaborator_objects/collaborator_objects.rb
+++ rb_120/lesson_2/lecture_collaborator_objects/collaborator_objects.rb
@@ -16,3 +16,25 @@ p joe.name               # "Joe"
 A `Person` object's `name` attribute can be saved into a `@name`
 instance variable as a string.
 =end
+
+
+
+class Person
+  def initialize
+    @heroes = ['superman', 'Spiderman', 'Batman']
+    @cash = { 'ones' => 12, 'fives' => 2, 'tens' => 0, 'twenties' => 2, 'hundreds' => 0 }
+  end
+
+  def cash_on_hand
+    # this method will use @cash to calculate total cash value
+    # we'll skip the implementation
+  end
+
+  def heroes
+    @heroes.join(', ')
+  end
+end
+
+joe = Person.new
+joe.cash_on_hand      # => "$62.00"
+joe.heroes            # => "Superman, Spiderman, Batman"
