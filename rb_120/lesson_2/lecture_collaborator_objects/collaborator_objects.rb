159efc18208b3f2c6281ce1c0a9fdd3ae3355433
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-06 14:16:48 -0600

    Add collaborator_objects.rb

--- rb_120/lesson_2/lecture_collaborator_objects/collaborator_objects.rb
+++ rb_120/lesson_2/lecture_collaborator_objects/collaborator_objects.rb
@@ -1,40 +1,80 @@
-## Collaborator Objects
+# ## Collaborator Objects
+# class Person
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+# end
+
+# joe = Person.new("Joe")
+# p joe.name               # "Joe"
+
+# =begin
+# A `Person` object's `name` attribute can be saved into a `@name`
+# instance variable as a string.
+# =end
+
+
+
+# class Person
+#   def initialize
+#     @heroes = ['superman', 'Spiderman', 'Batman']
+#     @cash = { 'ones' => 12, 'fives' => 2, 'tens' => 0, 'twenties' => 2, 'hundreds' => 0 }
+#   end
+
+#   def cash_on_hand
+#     # this method will use @cash to calculate total cash value
+#     # we'll skip the implementation
+#   end
+
+#   def heroes
+#     @heroes.join(', ')
+#   end
+# end
+
+# joe = Person.new
+# joe.cash_on_hand      # => "$62.00"
+# joe.heroes            # => "Superman, Spiderman, Batman"
+
+
+
+
 class Person
+  attr_accessor :name, :pet
+
   def initialize(name)
-    @name = name
-  end
-
-  def name
-    @name
+    @name = name   # technically a collaborator object
   end
 end
 
-joe = Person.new("Joe")
-p joe.name               # "Joe"
-
-=begin
-A `Person` object's `name` attribute can be saved into a `@name`
-instance variable as a string.
-=end
-
-
-
-class Person
-  def initialize
-    @heroes = ['superman', 'Spiderman', 'Batman']
-    @cash = { 'ones' => 12, 'fives' => 2, 'tens' => 0, 'twenties' => 2, 'hundreds' => 0 }
+class Dog
+  def speak
+    'bark!'
   end
 
-  def cash_on_hand
-    # this method will use @cash to calculate total cash value
-    # we'll skip the implementation
+  def fetch
+    'fetching!'
   end
 
-  def heroes
-    @heroes.join(', ')
+  def swim
+    'swimming!'
   end
 end
 
-joe = Person.new
-joe.cash_on_hand      # => "$62.00"
-joe.heroes            # => "Superman, Spiderman, Batman"
+class Bulldog < Dog
+end
+
+bob = Person.new("Robert")
+bud = Bulldog.new
+
+# Collaborator object
+bob.pet = bud
+
+p bob.pet              #<Bulldog:0x00000001065f5148>
+p bob.pet.class        # Bulldog
+
+p bob.pet.speak        # "bark!"
+p bob.pet.fetch        # "fetching!"
