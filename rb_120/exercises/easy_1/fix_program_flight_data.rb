8a691dc79e51dda2f5d07816135c2d3f93b38f87
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-08 14:20:18 -0600

    Add fix_program_flight_data.rb

---
+++ rb_120/exercises/easy_1/fix_program_flight_data.rb
@@ -0,0 +1,24 @@
+=begin
+There is nothing technically incorrect about this class, but 
+the definition may lead to problems in the future. How can 
+this class be fixed to be resistant to future problems?
+=end
+
+class Flight
+  attr_accessor :database_handle
+
+  def initialize(flight_number)
+    @database_handle = Database.init
+    @flight_number = flight_number
+  end
+end
+
+
+## Possible fix
+# We shouldn't provide direct access to `@database_handle`
+class Flight
+  def initialize(flight_number)
+    @database_handle = Database.init
+    @flight_number = flight_number
+  end
+end
