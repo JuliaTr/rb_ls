8d7d92bd1419119844702f8132da8dba929ab197
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-14 17:08:52 -0600

    Add exercise

--- rb_120/lesson_3/equivalence.rb
+++ rb_120/lesson_3/equivalence.rb
@@ -4,7 +4,11 @@
 # p str1 == str2      # true
 
 
-int1 = 1
-int2 = 1
-p int1 == int2        # true
+# int1 = 1
+# int2 = 1
+# p int1 == int2      # true
 
+
+sym1 = :something
+sym2 = :something
+p sym1 == sym2        # true
