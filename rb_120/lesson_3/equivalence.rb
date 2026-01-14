f84a489a5e4fa50f85bb2dc61f946181fdfc533a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-14 17:32:47 -0600

    Add exercise

--- rb_120/lesson_3/equivalence.rb
+++ rb_120/lesson_3/equivalence.rb
@@ -1,70 +1,71 @@
-# ## The `==` method
-# str1 = "something"
-# str2 = "something"
-# p str1 == str2      # true
+## The `==` method
+str1 = "something"
+str2 = "something"
+p str1 == str2      # true
 
 
 
-# int1 = 1
-# int2 = 1
-# p int1 == int2      # true
+int1 = 1
+int2 = 1
+p int1 == int2      # true
 
 
 
-# sym1 = :something
-# sym2 = :something
-# p sym1 == sym2        # true
+sym1 = :something
+sym2 = :something
+p sym1 == sym2        # true
 
 
 
-# str1 = "something"
-# str2 = "something"
+str1 = "something"
+str2 = "something"
 
-# p str1.class         # String
-# p str2.class         # String
-
-
-
-# str1 = "something"
-# str2 = "something"
-
-# str1 = str1 + " else"
-# p str1                  # "something else"
-
-# p str1 == str2          # false
+p str1.class         # String
+p str2.class         # String
 
 
 
-# str1 = "something"
-# str2 = "something"
-# str1_copy = str1
+str1 = "something"
+str2 = "something"
 
-# # compare values
-# p str1 == str2             # true
-# p str1 == str1_copy        # true
-# p str2 == str1_copy        # true
+str1 = str1 + " else"
+p str1                  # "something else"
 
-# # compare objects
-# p str1.equal? str2         # false
-# p str1.equal? str1_copy    # true
-# p str2.equal? str1_copy    # false
+p str1 == str2          # false
 
 
 
-# class Person
-#   attr_accessor :name
-# end
+str1 = "something"
+str2 = "something"
+str1_copy = str1
 
-# bob = Person.new
-# bob.name = "bob"
+# compare values
+p str1 == str2             # true
+p str1 == str1_copy        # true
+p str2 == str1_copy        # true
 
-# bob2 = Person.new
-# bob2.name = "bob"
+# compare objects
+p str1.equal? str2         # false
+p str1.equal? str1_copy    # true
+p str2.equal? str1_copy    # false
 
-# p bob == bob2          # false
 
-# bob_copy = bob
-# p bob == bob_copy      # true
+
+
+class Person
+  attr_accessor :name
+end
+
+bob = Person.new
+bob.name = "bob"
+
+bob2 = Person.new
+bob2.name = "bob"
+
+p bob == bob2          # false
+
+bob_copy = bob
+p bob == bob_copy      # true
 
 
 
@@ -84,3 +85,9 @@ bob2 = Person.new
 bob2.name = 'bob'
 
 p bob == bob2         # true
+
+
+
+
+p 45 == 45.00           # true
+p 45.00 == 45           # true
