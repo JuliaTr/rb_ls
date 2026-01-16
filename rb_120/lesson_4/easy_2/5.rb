ef69485480ab9741b856b76da6119cfc86f9af3f
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 14:25:53 -0600

    Add 5.rb

---
+++ rb_120/lesson_4/easy_2/5.rb
@@ -0,0 +1,15 @@
+=begin
+There are a number of variables listed below. What are the 
+different types and how do you know which is which?
+=end
+
+excited_dog = "excited_dog"    # local variable
+@excited_dog = "excited_dog"   # instance variable
+@@excited_dog = "excited_dog"  # class variable
+
+=begin
+On line 6, `excited_dog` is a local variable. It doesn't have
+any symbol prepended. On line 7, `@excited_dog` is an instance variable,
+which has `@` prepended. On line 8, `@@ecited_dog` is a class variable,
+which has `@@` prepended.
+=end
