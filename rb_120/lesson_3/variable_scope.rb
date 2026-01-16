d167d6f500b1f8154607044e77c2c37e9caac01d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 17:54:27 -0600

    Add exercise

--- rb_120/lesson_3/variable_scope.rb
+++ rb_120/lesson_3/variable_scope.rb
@@ -1,13 +1,28 @@
-## Instance Variable Scope
+# ## Instance Variable Scope
+
+# class Person
+#   def initialize(n)
+#     @name = n
+#   end
+# end
+
+# bob = Person.new('bob')
+# joe = Person.new('joe')
+
+# puts bob.inspect   # #<Person:0x0000000109d35ab8 @name="bob">
+# puts joe.inspect   # #<Person:0x0000000109d35a18 @name="joe">
+
+
 
 class Person
   def initialize(n)
     @name = n
   end
+
+  def get_name
+    @name
+  end
 end
 
 bob = Person.new('bob')
-joe = Person.new('joe')
-
-puts bob.inspect   # #<Person:0x0000000109d35ab8 @name="bob">
-puts joe.inspect   # #<Person:0x0000000109d35a18 @name="joe">
+p bob.get_name    # "bob"
