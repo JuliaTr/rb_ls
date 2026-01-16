1e1e4fc83de20a313158e2f5da292d10593d3a14
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 14:07:44 -0600

    Add 4.rb

---
+++ rb_120/lesson_4/easy_2/4.rb
@@ -0,0 +1,60 @@
+=begin
+What could you add to this class to simplify it and remove two 
+methods from the class definition while still maintaining the 
+same functionality?
+=end
+
+class BeesWax
+  def initialize(type)
+    @type = type
+  end
+
+  def type
+    @type
+  end
+
+  def type=(t)
+    @type = t
+  end
+
+  def describe_type
+    puts "I am a #{@type} of Bees Wax"
+  end
+end
+
+wax1 = BeesWax.new('yellow')
+p wax1.type
+p wax1.type = 'white'
+p wax1.type
+
+=begin
+"yellow"
+"white"
+"white"
+=end
+
+
+
+## Solution:
+class BeesWax
+  attr_accessor :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def describe_type
+    puts "I am a #{type} of Bees Wax"
+  end
+end
+
+wax1 = BeesWax.new('yellow')
+p wax1.type
+p wax1.type = 'white'
+p wax1.type
+
+=begin
+"yellow"
+"white"
+"white"
+=end
