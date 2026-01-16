ab06fb19697b7350dde926952a7fd6642768ad4d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 17:51:48 -0600

    Add variable_scope.rb

---
+++ rb_120/lesson_3/variable_scope.rb
@@ -0,0 +1,13 @@
+## Instance Variable Scope
+
+class Person
+  def initialize(n)
+    @name = n
+  end
+end
+
+bob = Person.new('bob')
+joe = Person.new('joe')
+
+puts bob.inspect   # #<Person:0x0000000109d35ab8 @name="bob">
+puts joe.inspect   # #<Person:0x0000000109d35a18 @name="joe">
