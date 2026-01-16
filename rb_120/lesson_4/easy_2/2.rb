ca41c289237c9bbb152dd2b00d9f4ba08f59447d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 12:26:34 -0600

    Add experiment

--- rb_120/lesson_4/easy_2/2.rb
+++ rb_120/lesson_4/easy_2/2.rb
@@ -38,3 +38,34 @@ Ruby finds in `Oracle` class, which `RoadTrip` inherits. On line 5,
 Ruby looks for `choices` method definition and first it finds in `RoadTrip`,
 which overrides `choices` method definition in `Oracle` class.
 =end
+
+
+
+## Experiments:
+class Oracle
+  def predict_the_future
+    "You will " + choices.sample
+  end
+
+  def choices
+    ["eat a nice lunch", "take a nap soon", "stay at work late"]
+  end
+end
+
+class RoadTrip < Oracle
+  def choose  # name changed intentionally
+    ["visit Vegas", "fly to Fiji", "romp in Rome"]
+  end
+end
+
+trip = RoadTrip.new
+p trip.predict_the_future
+
+=begin
+$ ruby 2.rb
+"You will stay at work late"
+$ ruby 2.rb
+"You will take a nap soon"
+$ ruby 2.rb
+"You will eat a nice lunch"
+=end
