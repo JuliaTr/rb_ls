6d321c02f9255389021cace2f6493a512e38f6fe
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 16:21:26 -0600

    Add examples

--- rb_120/lesson_3/equivalence.rb
+++ rb_120/lesson_3/equivalence.rb
@@ -1,96 +1,96 @@
-# ## The `==` method
-# str1 = "something"
-# str2 = "something"
-# p str1 == str2      # true
+## The `==` method
+str1 = "something"
+str2 = "something"
+p str1 == str2      # true
 
 
 
-# int1 = 1
-# int2 = 1
-# p int1 == int2      # true
+int1 = 1
+int2 = 1
+p int1 == int2      # true
 
 
 
-# sym1 = :something
-# sym2 = :something
-# p sym1 == sym2        # true
+sym1 = :something
+sym2 = :something
+p sym1 == sym2        # true
 
 
 
-# str1 = "something"
-# str2 = "something"
+str1 = "something"
+str2 = "something"
 
-# p str1.class         # String
-# p str2.class         # String
-
-
-
-# str1 = "something"
-# str2 = "something"
-
-# str1 = str1 + " else"
-# p str1                  # "something else"
-
-# p str1 == str2          # false
+p str1.class         # String
+p str2.class         # String
 
 
 
-# str1 = "something"
-# str2 = "something"
-# str1_copy = str1
+str1 = "something"
+str2 = "something"
 
-# # compare values
-# p str1 == str2             # true
-# p str1 == str1_copy        # true
-# p str2 == str1_copy        # true
+str1 = str1 + " else"
+p str1                  # "something else"
 
-# # compare objects
-# p str1.equal? str2         # false
-# p str1.equal? str1_copy    # true
-# p str2.equal? str1_copy    # false
+p str1 == str2          # false
 
 
 
+str1 = "something"
+str2 = "something"
+str1_copy = str1
 
-# class Person
-#   attr_accessor :name
-# end
+# compare values
+p str1 == str2             # true
+p str1 == str1_copy        # true
+p str2 == str1_copy        # true
 
-# bob = Person.new
-# bob.name = "bob"
-
-# bob2 = Person.new
-# bob2.name = "bob"
-
-# p bob == bob2          # false
-
-# bob_copy = bob
-# p bob == bob_copy      # true
+# compare objects
+p str1.equal? str2         # false
+p str1.equal? str1_copy    # true
+p str2.equal? str1_copy    # false
 
 
 
 
-# class Person
-#   attr_accessor :name
+class Person
+  attr_accessor :name
+end
 
-#   def ==(other)
-#     name == other.name    # rely on `String#==`
-#   end
-# end
+bob = Person.new
+bob.name = "bob"
 
-# bob = Person.new
-# bob.name = 'bob'
+bob2 = Person.new
+bob2.name = "bob"
 
-# bob2 = Person.new
-# bob2.name = 'bob'
+p bob == bob2          # false
 
-# p bob == bob2         # true
-
+bob_copy = bob
+p bob == bob_copy      # true
 
 
 
-# p 45 == 45.00           # true
-# p 45.00 == 45           # true
+
+class Person
+  attr_accessor :name
+
+  def ==(other)
+    name == other.name    # rely on `String#==`
+  end
+end
+
+bob = Person.new
+bob.name = 'bob'
+
+bob2 = Person.new
+bob2.name = 'bob'
+
+p bob == bob2         # true
+
+
+
+
+p 45 == 45.00           # true
+p 45.00 == 45           # true
 
 
 
@@ -101,3 +101,18 @@ str2 = "something"
 
 p str1.object_id        # 60
 p str2.object_id        # 80
+
+
+arr1 = [1, 2, 3]
+arr2 = [1, 2, 3]
+p arr1.object_id == arr2.object_id   # false
+
+
+sym1 = :something
+sym2 = :something
+p sym1.object_id == sym2.object_id     # true
+
+
+int1 = 5
+int2 = 5
+p int1.object_id == int2.object_id     # true
