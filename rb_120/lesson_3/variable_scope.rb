b27e1b41a09907a56d9d2aaf056439f42fd0b627
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-16 18:02:39 -0600

    Add exercise

--- rb_120/lesson_3/variable_scope.rb
+++ rb_120/lesson_3/variable_scope.rb
@@ -1,31 +1,31 @@
-# ## Instance Variable Scope
+## Instance Variable Scope
 
-# class Person
-#   def initialize(n)
-#     @name = n
-#   end
-# end
+class Person
+  def initialize(n)
+    @name = n
+  end
+end
 
-# bob = Person.new('bob')
-# joe = Person.new('joe')
+bob = Person.new('bob')
+joe = Person.new('joe')
 
-# puts bob.inspect   # #<Person:0x0000000109d35ab8 @name="bob">
-# puts joe.inspect   # #<Person:0x0000000109d35a18 @name="joe">
+puts bob.inspect   # #<Person:0x0000000109d35ab8 @name="bob">
+puts joe.inspect   # #<Person:0x0000000109d35a18 @name="joe">
 
 
 
-# class Person
-#   def initialize(n)
-#     @name = n   # initialized
-#   end
+class Person
+  def initialize(n)
+    @name = n   # initialized
+  end
 
-#   def get_name
-#     @name       # accessible
-#   end
-# end
+  def get_name
+    @name       # accessible
+  end
+end
 
-# bob = Person.new('bob')
-# p bob.get_name    # "bob"
+bob = Person.new('bob')
+p bob.get_name    # "bob"
 
 
 
@@ -37,3 +37,16 @@ end
 
 bob = Person.new
 p bob.get_name    # nil
+
+
+
+class Person
+  @name = "bob"   # class level initialization
+
+  def get_name
+    @name
+  end
+end
+
+bob = Person.new
+p bob.get_name     # nil
