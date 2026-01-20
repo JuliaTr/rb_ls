c6278c581be7d2c44175ccd741ed4b688c860030
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-20 14:47:27 -0600

    Add 6.rb

---
+++ rb_120/lesson_4/easy_3/6.rb
@@ -0,0 +1,40 @@
+=begin
+In the `make_one_year_older` method we have used `self`. What is 
+another way we could write this method so we don't have to use 
+the `self` prefix?
+=end
+
+class Cat
+  attr_accessor :type, :age
+
+  def initialize(type)
+    @type = type
+    @age = 0
+  end
+
+  def make_one_year_older
+    self.age += 1   # call setter method inside instance method
+  end
+end
+
+cat1 = Cat.new('tabby')
+p cat1.make_one_year_older    # 1
+
+
+
+## Possible solution
+class Cat
+  attr_accessor :type, :age
+
+  def initialize(type)
+    @type = type
+    @age = 0
+  end
+  
+  def make_one_year_older
+    @age += 1
+  end
+end
+
+cat1 = Cat.new('tabby')
+p cat1.make_one_year_older   # 1
