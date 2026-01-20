cdc683b8a48f4e01f94471e3567628da3179794c
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-20 15:27:41 -0600

    Add experiments

--- rb_120/lesson_4/easy_3/6.rb
+++ rb_120/lesson_4/easy_3/6.rb
@@ -1,48 +1,50 @@
-# =begin
-# In the `make_one_year_older` method we have used `self`. What is 
-# another way we could write this method so we don't have to use 
-# the `self` prefix?
-# =end
+=begin
+In the `make_one_year_older` method we have used `self`. What is 
+another way we could write this method so we don't have to use 
+the `self` prefix?
+=end
 
-# class Cat
-#   attr_accessor :type, :age
+class Cat
+  attr_accessor :type, :age
 
-#   def initialize(type)
-#     @type = type
-#     @age = 0
-#   end
+  def initialize(type)
+    @type = type
+    @age = 0
+  end
 
-#   def make_one_year_older
-#     self.age += 1   # call setter method inside instance method
-#   end
-# end
+  def make_one_year_older
+    self.age += 1   # call setter method inside instance method
+  end
+end
 
-# cat1 = Cat.new('tabby')
-# p cat1.make_one_year_older    # 1
+cat1 = Cat.new('tabby')
+p cat1.make_one_year_older    # 1
+p cat1.age                    # 1
 
 
 
-# ## Possible solution
-# class Cat
-#   attr_accessor :type, :age
+## Possible solution
+class Cat
+  attr_accessor :type, :age
 
-#   def initialize(type)
-#     @type = type
-#     @age = 0
-#   end
+  def initialize(type)
+    @type = type
+    @age = 0
+  end
   
-#   def make_one_year_older
-#     @age += 1
-#   end
-# end
+  def make_one_year_older
+    @age += 1
+  end
+end
 
-# cat1 = Cat.new('tabby')
-# p cat1.make_one_year_older   # 1
+cat1 = Cat.new('tabby')
+p cat1.make_one_year_older   # 1
+p cat1.age                   # 1
 
 
 
 ## Experiments
-# with `self`
+# with `self`:
 class Cat
   attr_accessor :name
 
@@ -58,3 +60,21 @@ end
 cat1 = Cat.new('Sophie')
 cat1.rename('Chloe')
 p cat1.name             # "Chloe"
+
+
+# without `self`:
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def rename(other_name)
+    @name = other_name
+  end
+end
+
+cat1 = Cat.new('Sophie')
+cat1.rename('Chloe')
+p cat1.name               # "Chloe"
