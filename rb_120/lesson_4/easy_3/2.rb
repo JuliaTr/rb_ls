755984a133658ffbbfaa62119e5e751f72f62b8d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-20 11:29:10 -0600

    Add 2.rb

---
+++ rb_120/lesson_4/easy_3/2.rb
@@ -0,0 +1,43 @@
+# If we call `Hello.hi` we get an error message. How would you fix this?
+
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+
+
+## Possible solution
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def self.hi
+    greeting = Greeting.new
+    greeting.greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+Hello.hi    # Hello
