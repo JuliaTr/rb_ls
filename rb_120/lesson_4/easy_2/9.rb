bc3c637e66675c9971180b2d02afe24149461e7b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-20 10:27:03 -0600

    Add experiments

--- rb_120/lesson_4/easy_2/9.rb
+++ rb_120/lesson_4/easy_2/9.rb
@@ -35,3 +35,25 @@ end
 
 game_of_bingo = Bingo.new
 p game_of_bingo.play  # "Start the game in Bingo class"
+
+
+
+## Eperiments
+class Game
+  def play
+    "Start the game!"
+  end
+end
+
+class Bingo < Game
+  def rules_of_play
+  end
+
+  def play
+    "Start the game in #{self} class"
+  end
+end
+
+game_of_bingo = Bingo.new
+p game_of_bingo.play
+# "Start the game in #<Bingo:0x000000010dff5038> class"
