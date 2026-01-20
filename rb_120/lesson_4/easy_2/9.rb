9eaf2e7769d920a3d76c1d22d349712df48d5467
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-20 10:23:42 -0600

    Add 9.rb

---
+++ rb_120/lesson_4/easy_2/9.rb
@@ -0,0 +1,37 @@
+=begin
+What would happen if we added a play method to the Bingo class, 
+keeping in mind that there is already a method of this name in 
+the Game class that the Bingo class inherits from.
+=end
+
+class Game
+  def play
+    "Start the game!"
+  end
+end
+
+class Bingo < Game
+  def rules_of_play
+  end
+end
+
+
+
+## Solution
+class Game
+  def play
+    "Start the game!"
+  end
+end
+
+class Bingo < Game
+  def rules_of_play
+  end
+
+  def play
+    "Start the game in #{self.class} class"
+  end
+end
+
+game_of_bingo = Bingo.new
+p game_of_bingo.play  # "Start the game in Bingo class"
