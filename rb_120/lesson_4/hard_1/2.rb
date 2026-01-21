c59e94a51546ad4ab7eb72b0c18ed92adcffc260
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-21 12:00:34 -0600

    Add 2.rb

---
+++ rb_120/lesson_4/hard_1/2.rb
@@ -0,0 +1,95 @@
+=begin
+Building on the prior vehicles question, we now must also track a 
+basic motorboat. A motorboat has a single propeller and hull, but 
+otherwise behaves similar to a catamaran. Therefore, creators of 
+Motorboat instances don't need to specify number of hulls or 
+propellers. How would you modify the vehicles code to incorporate 
+a new Motorboat class?
+=end
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    super([30, 30, 32, 32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    super([20, 20], 80, 8.0)
+  end
+end
+
+## Possible solution:
+class Seacraft
+  include Moveable
+
+  attr_reader :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    self.fuel_efficiency = fuel_efficiency
+    self.fuel_capacity = fuel_capacity
+  end
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Catamaran < Seacraft
+end
+
+
+
+## Solution:
+class Catamaran
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+    # ..... code omitted
+  end
+end
+
+class Motorboat
+  include Moveable
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+end
