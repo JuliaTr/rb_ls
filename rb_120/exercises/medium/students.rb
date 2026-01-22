f428e8e5a910ed264287bf632cd377533baff021
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-22 17:39:34 -0600

    Add students.rb

---
+++ rb_120/exercises/medium/students.rb
@@ -0,0 +1,62 @@
+=begin
+Below we have 3 classes: `Student`, `Graduate`, and `Undergraduate`. 
+The implementation details for the `#initialize` methods in 
+`Graduate` and `Undergraduate` are missing. Fill in those missing 
+details so that the following requirements are fulfilled:
+
+Graduate students have the option to use on-campus parking, while 
+Undergraduate students do not.
+
+Graduate and Undergraduate students both have a name and year 
+associated with them.
+
+Note, you can do this by adding or altering no more than 5 lines 
+of code.
+=end
+
+class Student
+  def initialize(name, year)
+    @name = name
+    @year = year
+  end
+end
+
+class Graduate
+  def initalize(name, year, parking)
+  end
+end
+
+class Undergraduate
+  def initalize(name, year)
+  end
+end
+
+
+
+## Solution
+class Student
+  def initialize(name, year)
+    @name = name
+    @year = year
+  end
+end
+
+class Graduate < Student
+  def initalize(name, year, parking)
+    super(name, year)
+    @parking = parking 
+  end
+end
+
+class Undergraduate < Student
+  def initialize(name, year)
+    super
+  end
+end
+
+
+
+## Alternative solution:
+# without `Undergraduate#initialize`:
+
+
