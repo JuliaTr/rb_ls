bcec4390f958e74d8d1931e0c364f4420144b038
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-22 15:59:41 -0600

    Add experiments

--- rb_120/exercises/medium/fixed_array.rb
+++ rb_120/exercises/medium/fixed_array.rb
@@ -10,17 +10,29 @@ class FixedArray
     @array = Array.new(max_size)
   end
 
+  # getter method
   def [](index)
+    # raise `IndexError` when the index is out range by using `@array.fetch(index)`
     @array.fetch(index)
   end
 
+  # setter method
   def []=(index, value)
-    self[index]         # raise error if index is invalid
+    # raise `IndexError` when the index is out range
+    # calls `FixedArray#[]` method (above)
+    # `self[index]` is the same as "run the same index validation I already
+    #  wrote in `#[]` before assigning"
+    # Getther method `[](index)` runs.
+    self[index]                  # the same as `self.[](index)`
+    
+    # # Without `self` Ruby sees it as **creating a new `Array` literal**,
+    # # no method call, no error 
+    # [index]
     @array[index] = value
   end
 
   def to_a
-    @array.clone
+    @array.clone   # `clone` doesn't allow us to change number of elements
   end
 
   def to_s
@@ -54,9 +66,9 @@ puts fixed_array[-4] == 'c'
 
 begin
   fixed_array[6]
-  puts false
+  puts false  # if exception doesn't occur, the code displays `false`
 rescue IndexError
-  puts true
+  puts true   # if exception occurs, it displays `true`
 end
 
 begin
@@ -74,3 +86,32 @@ rescue IndexError
 end
 # The above code should output `true` 16 times.
 # All test cases return `true`.
+
+
+
+## Experiments
+p [nil] * 5    # [nil, nil, nil, nil, nil]
+
+
+arr = Array.new(5)
+p arr          # [nil, nil, nil, nil, nil]
+
+
+=begin
+irb(main):002:0> a = [nil] * 5
+=> [nil, nil, nil, nil, nil]
+irb(main):003:0> a.fetch(6)
+(irb):3:in `fetch': index 6 outside of array bounds: -5...5 (IndexError)
+        from (irb):3:in `<main>'                   
+        from /usr/local/Cellar/ruby/3.2.2_1/lib/ruby/gems/3.2.0/gems/irb-1.6.2/exe/irb:11:in `<top (required)>'                    
+        from /usr/local/opt/ruby/bin/irb:25:in `load'
+        from /usr/local/opt/ruby/bin/irb:25:in `<main>'
+irb(main):004:0> a.fetch(1)
+=> nil
+irb(main):005:0> a[1] = 'a'
+=> "a"
+irb(main):006:0> a.fetch(1)
+=> "a"
+=end
+
+
