27316f8af1224f57bc882ddf55f3c51330d1798f
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-23 16:46:51 -0600

    Add option 2

--- rb_120/exercises/debugging/files.rb
+++ rb_120/exercises/debugging/files.rb
@@ -60,3 +60,31 @@ puts blog_post  # after: Adventures_in_OOP_Land.md
 # Before:
 # uninitialized constant File::FORMAT (NameError)
 
+
+
+# Option 2
+class File
+  # code omitted
+
+  def to_s
+    "#{name}.#{format}"
+  end
+end
+
+class MarkdownFile < File
+  def format
+    :md
+  end
+end
+
+class VectorGraphicsFile < File
+  def format
+    :svg
+  end
+end
+
+class MP3File < File
+  def format
+    :mp3
+  end
+end
