73e48bb556931ee268047139a813ec0af67de402
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-26 17:56:26 -0600

    Add refactored solution

--- study_sessions/third_word_second_letter.rb
+++ study_sessions/third_word_second_letter.rb
@@ -6,6 +6,40 @@ of the string with every second character in every third word
 converted to uppercase. Other characters should remain the same.
 =end
 
+## Refactored:
+def to_weird_case(str)
+  words = str.split
+
+  2.step(by: 3, to: words.size - 1) do |index_word|
+    word = words[index_word]
+
+    1.step(by: 2, to: word.size - 1) do |index_char|
+      word[index_char] = word[index_char].upcase
+    end
+  end
+
+  words.join(' ')
+end
+
+original = 'Lorem Ipsum is simply dummy text of the printing world'
+expected = 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG world'
+p to_weird_case(original) == expected
+
+original = 'It is a long established fact that a reader will be distracted'
+expected = 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
+p to_weird_case(original) == expected
+
+p to_weird_case('aaA bB c') == 'aaA bB c'
+
+original = "Mary Poppins' favorite word is supercalifragilisticexpialidocious"
+expected = "Mary Poppins' fAvOrItE word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS"
+p to_weird_case(original) == expected
+# All test cases return `true`.
+
+
+
+
+## Solution:
 def to_weird_case(original)
   arr = original.split
 
