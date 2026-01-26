2ab6ed2487e41164983ce9e53693e6a1155b58f4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-26 17:44:21 -0600

    Add third_word_second_letter.rb

---
+++ study_sessions/third_word_second_letter.rb
@@ -0,0 +1,40 @@
+# to_review
+
+=begin
+Create a method that takes a string argument and returns a copy 
+of the string with every second character in every third word 
+converted to uppercase. Other characters should remain the same.
+=end
+
+def to_weird_case(original)
+  arr = original.split
+
+  word_index = 2
+  until arr[word_index].nil?
+
+    letter_index = 1
+    until arr[word_index][letter_index].nil?
+      arr[word_index][letter_index] = arr[word_index][letter_index].upcase
+      letter_index += 2
+    end
+
+    word_index += 3
+  end
+
+  arr.join(' ')
+end
+
+original = 'Lorem Ipsum is simply dummy text of the printing world'
+expected = 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG world'
+p to_weird_case(original) == expected
+
+original = 'It is a long established fact that a reader will be distracted'
+expected = 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
+p to_weird_case(original) == expected
+
+p to_weird_case('aaA bB c') == 'aaA bB c'
+
+original = "Mary Poppins' favorite word is supercalifragilisticexpialidocious"
+expected = "Mary Poppins' fAvOrItE word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS"
+p to_weird_case(original) == expected
+# All test cases return `true`.
