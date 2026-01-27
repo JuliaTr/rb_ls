9413234deb8573b117aa85f9125a06bc56483812
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-27 11:47:53 -0600

    Add stringy_strings.rb

--- rb_101_rb_119_small_problems/easy_1_pass3/stringy_strings.rb
+++ rb_101_rb_119_small_problems/easy_1_pass3/stringy_strings.rb
@@ -50,3 +50,24 @@ puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
 # All test cases return `true`.
+
+
+
+
+## Possile solution:
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+
+  numbers.join
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+# All test cases return `true`.
