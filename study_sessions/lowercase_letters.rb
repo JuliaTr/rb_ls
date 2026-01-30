ac72c2917f34b6c00e720624b3cdef056458452b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-01-30 17:45:48 -0600

    Add solution for idea2

--- study_sessions/lowercase_letters.rb
+++ study_sessions/lowercase_letters.rb
@@ -36,6 +36,7 @@ Algo:
 
 =end
 
+# Idea1:
 ALPHA = ('a'..'z').to_a
 
 def count_letters(str)
@@ -55,3 +56,34 @@ p count_letters('W. E. B. Du Bois') == expected
 p count_letters('x') == {'x' => 1}
 p count_letters('') == {}
 p count_letters('!!!') == {}
+# All test cases return `true`.
+
+
+
+
+# Idea2:
+ALPHA = ('a'..'z').to_a
+
+def count_letters(str)
+  hash = {}
+
+  str.each_char do |char|
+    hash[char] = str.count(char) if ALPHA.include?(char)
+  end
+
+  hash
+end
+
+expected = {'w' => 1, 'o' => 2, 'e' => 3, 'b' => 1, 'g' => 1, 'n' => 1}
+p count_letters('woebegone') == expected
+
+expected = {'l' => 1, 'o' => 1, 'w' => 1, 'e' => 4, 'r' => 2,'c' => 2, 'a' => 2, 's' => 2, 'u' => 1, 'p' => 2}
+p count_letters('lowercase/uppercase') == expected
+
+expected = {'u' => 1, 'o' => 1, 'i' => 1, 's' => 1}
+p count_letters('W. E. B. Du Bois') == expected
+
+p count_letters('x') == {'x' => 1}
+p count_letters('') == {}
+p count_letters('!!!') == {}
+# All test cases return `true`.
