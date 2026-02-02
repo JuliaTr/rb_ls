b4f2999029398bdf6e6fe8388065669e5d0448b9
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-02 11:49:35 -0600

    Add more idiomatic solution

--- rb_101_rb_119_small_problems/easy_1_pass3/stringy_strings.rb
+++ rb_101_rb_119_small_problems/easy_1_pass3/stringy_strings.rb
@@ -28,6 +28,25 @@ stringy(6) == '101010'
 '10'
 =end
 
+## More idiomatic:
+def stringy(size)
+  str = ''
+
+  size.times do |index|
+    str << (index.even? ? '1' : '0')
+  end
+
+  str
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+# All test cases return `true`.
+
+
+
 ## Refactored:
 def stringy(number)
   str = ''
@@ -48,46 +67,46 @@ puts stringy(7) == '1010101'
 
 
 
-# ## Solution:
-# def stringy(number)
-#   str = ''
+## Solution:
+def stringy(number)
+  str = ''
 
-#   number.times do
-#     if str.empty? || str[-1] == '0'
-#       str << '1'
-#     else
-#       str << '0'
-#     end
-#   end
+  number.times do
+    if str.empty? || str[-1] == '0'
+      str << '1'
+    else
+      str << '0'
+    end
+  end
 
-#   str
-# end
+  str
+end
 
-# p stringy(3) # "101"
+p stringy(3) # "101"
 
-# puts stringy(6) == '101010'
-# puts stringy(9) == '101010101'
-# puts stringy(4) == '1010'
-# puts stringy(7) == '1010101'
-# # All test cases return `true`.
-
-
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+# All test cases return `true`.
 
 
-# ## Possile solution:
-# def stringy(size)
-#   numbers = []
 
-#   size.times do |index|
-#     number = index.even? ? 1 : 0
-#     numbers << number
-#   end
 
-#   numbers.join
-# end
+## Possile solution:
+def stringy(size)
+  numbers = []
 
-# puts stringy(6) == '101010'
-# puts stringy(9) == '101010101'
-# puts stringy(4) == '1010'
-# puts stringy(7) == '1010101'
-# # All test cases return `true`.
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+
+  numbers.join
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+# All test cases return `true`.
