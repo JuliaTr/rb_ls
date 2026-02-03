515c690853716b5402840868499ed83f9ad39392
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-03 14:38:56 -0600

    Add step 3

--- rb_120/code_spike.rb
+++ rb_120/code_spike.rb
@@ -23,7 +23,7 @@ and cafeteria workers all have the ability to eat lunch.
 Brainstorming:
 Nouns (classes): preschool, children, teacher, class assistant,
       principal, janitor, cafeteria worker, kid
-Inheritance relashiopship:
+Inheritance relationship:
 `Assistant` is a subclass of `Teacher`.
 
 ## Step 2:
@@ -40,6 +40,83 @@ Nouns (classes): preschool, children, teacher, class assistant,
 Verbs: help, watch, teach, learn, play, supervise, expel, clean
        serve, eat
 =end
+
+## Step 3 - Collaborationship relationship:
+module Supervise
+  def supervise; end
+end
+
+class Preschool
+  def initialize
+    @attendees = []
+  end
+
+  def <<(attendees)
+    @attendees << attendees
+  end
+end
+
+=begin
+`Preschool` has its attribute of attendees. `Preschool` holds
+its attendees. 
+
+`Preschool` has some people in it and we need to do smth 
+with these people. (collaboration)
+
+It's `Preschool`'s job to get these collaborators. 
+We don't have to define it anywheere in the collaborator.
+As soon as I create you, I move you to `Preschool` as part of your code.
+=end
+
+class Person
+  def initialize(preschool)
+    # Every time we create a `Person`, that `Person` automatically
+    # registers itself with the `preschool`collection passed in.
+    # Newly created `Person` object gets added to `preschool`
+    # right when it's initialized.
+    # `self` is passed as the element to be added to `preschool`.
+    # `Person` object add itself to some external collection on
+    # initialization.
+    preschool << self
+  end
+
+  def eat_lunch; end
+end
+
+=begin
+preschool = []
+alice = Person.new(preschool)
+preschool << alice
+=end
+
+class CareTaker < Person
+  def help_school_work; end
+  def watch_palyground; end
+end
+
+class Teacher < CareTaker
+  include Supervise
+
+  def teach; end
+end
+
+class Assistant < CareTaker
+  def help_kids; end
+end
+
+class Principal < Person
+  include Supervise
+end
+
+class Janitor < Person; end
+
+class CafeteriaWorker < Person; end
+
+class Kid < Person; end
+
+
+
+
 
 ## Step 2:
 module Supervise
@@ -86,6 +163,7 @@ class Kid < Person; end
 Teacher  Assistant
 
 =end
+
 
 
 
