fd5c014600e1f141c9d1503d9ada04f625daea04
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 11:23:24 -0600

    Add 9.rb

---
+++ rb_120/lesson_1/quiz/9.rb
@@ -0,0 +1,12 @@
+# Examine the code below. Identify all the superclasses.
+
+class Thing
+end
+
+class AnotherThing < Thing
+end
+
+class SomethingElse < AnotherThing
+end
+
+# Superclasses: `Thing`, `AnotherThing`, `Object`.
