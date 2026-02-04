08ded4a1768f3c04357638e60df9c9007fe11b45
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 11:30:56 -0600

    Add 10.rb

---
+++ rb_120/lesson_1/quiz/10.rb
@@ -0,0 +1,33 @@
+=begin
+Given the following code, select the method lookup path that Ruby 
+will use as a result of the call to the `fly` method?
+=end
+
+module Flight
+  def fly; end
+end
+
+module Aquatic
+  def swim; end
+end
+
+module Migratory
+  def migrate; end
+end
+
+class Animal
+end
+
+class Bird < Animal
+end
+
+class Penguin < Bird
+  include Aquatic
+  include Migratory
+end
+
+pingu = Penguin.new
+pingu.fly
+
+# Penguin > Migratory > Aquatic > Bird > Animal > Object > Kernel 
+#         > BasicObject
