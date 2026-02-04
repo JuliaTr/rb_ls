7c022d725f51c5d175bb4c6ba2b06555801869db
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 11:55:17 -0600

    Add option 3

--- rb_120/lesson_1/quiz/14.rb
+++ rb_120/lesson_1/quiz/14.rb
@@ -1,29 +1,45 @@
-=begin
-Which of the following class definitions will let us run this 
-code and get the same result?
-=end
+# =begin
+# Which of the following class definitions will let us run this 
+# code and get the same result?
+# =end
 
-## Works as expected:
+# ## Works as expected:
 
-# Option 1:
+# # Option 1:
+# class Person
+#   attr_accessor :name
+# end
+
+# kate = Person.new
+# kate.name = 'Kate'
+# p kate.name   # "Kate"
+
+
+# # Option 2:
+# class Person
+#   attr_writer :name
+
+#   def name
+#     @name
+#   end
+# end
+
+# kate = Person.new
+# kate.name = 'Kate'
+# p kate.name # "Kate"
+
+
+# Option 3:
 class Person
-  attr_accessor :name
-end
+  attr_reader :name
 
-kate = Person.new
-kate.name = 'Kate'
-p kate.name   # "Kate"
-
-
-# Option 2:
-class Person
-  attr_writer :name
-
-  def name
-    @name
+  def name=(name)
+    @name = name
   end
 end
 
 kate = Person.new
 kate.name = 'Kate'
-p kate.name # "Kate"
+p kate.name  # "Kate"
+
+
