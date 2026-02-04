822b49286ebdf034acf8a35d03b9d3d14f3aefc4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 14:39:04 -0600

    Add exercise

--- rb_120/lesson_1/quiz/16.rb
+++ rb_120/lesson_1/quiz/16.rb
@@ -1,67 +1,69 @@
-# =begin
-# Examine the following Ruby code.
-# The last line in the above code should return "A". Which of the 
-# following methods can we add to the Student class, so the code 
-# works as expected.
-# =end
+=begin
+Examine the following Ruby code.
+The last line in the above code should return "A". Which of the 
+following methods can we add to the Student class, so the code 
+works as expected.
+=end
 
-# class Student
-#   attr_accessor :name, :grade
+class Student
+  attr_accessor :name, :grade
 
-#   def initialize(name)
-#     @name = name
-#     @grade = nil
-#   end
-# end
+  def initialize(name)
+    @name = name
+    @grade = nil
+  end
+end
 
-# priya = Student.new("Priya")
-# priya.change_grade('A')
-# priya.grade
-
-
-
-# ## Solution
-# # Option 1:
-# class Student
-#   attr_accessor :name, :grade
-
-#   def initialize(name)
-#     @name = name
-#     @grade = nil
-#   end
-
-#   def change_grade(new_grade)
-#     @grade = new_grade
-#   end
-# end
-
-# priya = Student.new("Priya")
-# priya.change_grade('A')
-# p priya.grade  # "A"
+priya = Student.new("Priya")
+priya.change_grade('A')
+priya.grade
 
 
 
-# # Option
-# class Student
-#   attr_accessor :name, :grade
+## Solution
+# Option 1:
+class Student
+  attr_accessor :name, :grade
 
-#   def initialize(name)
-#     @name = name
-#     @grade = nil
-#   end
+  def initialize(name)
+    @name = name
+    @grade = nil
+  end
 
-#   def change_grade(new_grade)
-#     self.grade = new_grade
-#   end
-# end
+  def change_grade(new_grade)
+    @grade = new_grade
+  end
+end
 
-# priya = Student.new("Priya")
-# priya.change_grade('A')
-# p priya.grade  # "A"
+priya = Student.new("Priya")
+priya.change_grade('A')
+p priya.grade  # "A"
 
 
 
-# Won't work
+# Option 2:
+class Student
+  attr_accessor :name, :grade
+
+  def initialize(name)
+    @name = name
+    @grade = nil
+  end
+
+  def change_grade(new_grade)
+    self.grade = new_grade
+  end
+end
+
+priya = Student.new("Priya")
+priya.change_grade('A')
+p priya.grade  # "A"
+
+
+
+
+
+# Won't work as expected:
 class Student
   attr_accessor :name, :grade
 
@@ -81,4 +83,29 @@ priya.grade
 =begin
 undefined method `change_grade' for #<Student:0x000000010ad65078 
 @name="Priya", @grade=nil> (NoMethodError)
+=end
+
+
+
+class Student
+  attr_accessor :name, :grade
+
+  def initialize(name)
+    @name = name
+    @grade = nil
+  end
+
+  def change_grade(new_grade)
+    grade = new_grade
+  end
+end
+
+priya = Student.new("Priya")
+priya.change_grade('A')
+p priya.grade  # nil
+
+=begin
+`grade` is a local variable. Assigning the value of `new_grade` to
+it doesn't change the value of the `@grade` instance variable, nor 
+does it call the `grade` setter method.
 =end
