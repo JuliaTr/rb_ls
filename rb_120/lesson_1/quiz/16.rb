2af3c1a3fc946efbe70bc7862d0ecb968cc437f8
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 14:16:22 -0600

    Add exercise

--- rb_120/lesson_1/quiz/16.rb
+++ rb_120/lesson_1/quiz/16.rb
@@ -20,24 +20,24 @@
 
 
 
-## Solution
-# Option 1:
-class Student
-  attr_accessor :name, :grade
+# ## Solution
+# # Option 1:
+# class Student
+#   attr_accessor :name, :grade
 
-  def initialize(name)
-    @name = name
-    @grade = nil
-  end
+#   def initialize(name)
+#     @name = name
+#     @grade = nil
+#   end
 
-  def change_grade(new_grade)
-    @grade = new_grade
-  end
-end
+#   def change_grade(new_grade)
+#     @grade = new_grade
+#   end
+# end
 
-priya = Student.new("Priya")
-priya.change_grade('A')
-p priya.grade  # "A"
+# priya = Student.new("Priya")
+# priya.change_grade('A')
+# p priya.grade  # "A"
 
 
 
@@ -58,3 +58,27 @@ p priya.grade  # "A"
 # priya = Student.new("Priya")
 # priya.change_grade('A')
 # p priya.grade  # "A"
+
+
+
+# Won't work
+class Student
+  attr_accessor :name, :grade
+
+  def initialize(name)
+    @name = name
+    @grade = nil
+  end
+
+  def self.change_grade(new_grade)
+    grade = new_grade
+  end
+end
+
+priya = Student.new("Priya")
+priya.change_grade('A')
+priya.grade
+=begin
+undefined method `change_grade' for #<Student:0x000000010ad65078 
+@name="Priya", @grade=nil> (NoMethodError)
+=end
