f32abe1175bb042dbae8c610ead03dcd235c7175
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-04 16:58:02 -0600

    Add attributes.rb

---
+++ rb_120/lesson_1/attributes.rb
@@ -0,0 +1,19 @@
+class Laptop
+  def initialize(memory)
+    @memory = memory
+  end
+
+  def memory
+    @memory
+  end
+
+  def memory=(memory)
+    @memory = memory
+  end
+end
+
+laptop = Laptop.new('8GB')
+p laptop.memory   # "8GB"
+
+laptop.memory = '16GB'
+p laptop.memory   # "16GB"
