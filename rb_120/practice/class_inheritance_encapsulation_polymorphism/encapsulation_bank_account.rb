fef572991b2e3d7a2856a4491da84c67875a46af
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-05 15:26:50 -0600

    Add example of breaking encapsulation

--- rb_120/practice/class_inheritance_encapsulation_polymorphism/encapsulation_bank_account.rb
+++ rb_120/practice/class_inheritance_encapsulation_polymorphism/encapsulation_bank_account.rb
@@ -23,22 +23,58 @@ class BankAccount
   end
 
   def display_balance
-    @balance
+    "Your balance is: $#{@balance}."
   end
 end
 
 account = BankAccount.new
 account.deposit(100)
-p account.display_balance  # 100
+p account.display_balance  # "Your balance is: $100."
 
 account.withdraw(30)
-p account.display_balance  # 70
+p account.display_balance  # "Your balance is: $70."
 
 p account.balance
 =begin
 undefined method `balance' for #<BankAccount:0x000000010bd75620 
 @balance=70> (NoMethodError)
+
+Object cannot see balance directly, but can deposit and 
+withdraw money, display balance.
 =end
 
 
 
+
+# "Broken" encapsulation
+# with `attr_reader :balance`
+class BankAccount
+  attr_reader :balance   # "breaks" incapsulation
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(number)
+    @balance += number
+  end
+
+  def withdraw(number)
+    @balance -= number
+  end
+
+  def display_balance
+    @balance
+  end
+end
+
+account = BankAccount.new
+p account.balance # 0
+p account.balance = 999999
+
+=begin
+undefined method `balance=' for #<BankAccount:0x0000000106b65290 
+@balance=0> (NoMethodError)
+
+An object doesn't have access to change it account balance directly.
+=end
