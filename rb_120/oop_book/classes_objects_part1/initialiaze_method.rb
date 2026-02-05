06ab09fdb2aee36847f446346f124910977f1163
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-05 11:40:35 -0600

    Add exercise

--- rb_120/oop_book/classes_objects_part1/initialiaze_method.rb
+++ rb_120/oop_book/classes_objects_part1/initialiaze_method.rb
@@ -1,9 +1,28 @@
-## Use `initialize` with parameters
+# # (not from the book)
+
+# ## Use `initialize` with parameters
+# class GoodDog
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky") 
+# # "Sparky" is passed to the initialize method
+
+# p sparky # #<GoodDog:0x000000010ac25ac8 @name="Sparky">
+
+
+
+
+## Use `initialize` without parameters
+
+# To perform action on creation
 class GoodDog
-  def initialize(name)
-    @name = name
+  def initialize
+    puts "This object was initialized!"
   end
 end
 
-sparky = GoodDog.new("Sparky") 
-# "Sparky" is passed to the initialize method
+sparky = GoodDog.new
+# This object was initialized!
