3b94ae114281d46aedde50762aafd1702d77cd74
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-06 18:20:07 -0600

    Add alternative solution

--- rb_101_rb_119_small_problems/easy_3_pass3/uppercase_check.rb
+++ rb_101_rb_119_small_problems/easy_3_pass3/uppercase_check.rb
@@ -57,3 +57,18 @@ p uppercase?('FOUR SCORE') == true
 p uppercase?('4SCORE!') == true
 p uppercase?('') == true
 # All test cases return `true`.
+
+
+
+## Possible solution
+def uppercase?(str)
+  str == str.upcase
+end
+
+p uppercase?('t') == false
+p uppercase?('T') == true
+p uppercase?('Four Score') == false
+p uppercase?('FOUR SCORE') == true
+p uppercase?('4SCORE!') == true
+p uppercase?('') == true
+# All test cases return `true`
