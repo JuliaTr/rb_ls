c7e72164610b63e99c0e35031e6a32af3e54f0ce
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-09 16:07:56 -0600

    Add ascii_string_value.rb

---
+++ rb_101_rb_119_small_problems/easy_5_pass2/ascii_string_value.rb
@@ -0,0 +1,23 @@
+=begin
+Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. The ASCII string value is the sum of the ASCII values of every character in the string. (You may use String#ord to determine the ASCII value of a character.)
+
+High-level
+Loop over a str. Add each ASCII value. Keep track of count to return it.
+
+Algo:
+- Create `count` = 0
+- Iterate over each CHAR
+  - `count` += char.ord
+- Return `count`
+=end
+
+def ascii_value(str)
+  count = 0
+  str.each_char { |char| count += char.ord }
+  count
+end
+
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
