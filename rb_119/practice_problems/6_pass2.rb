a699507fd6e3b6000417bcec51f8e6f406be2074
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-09 16:31:00 -0600

    Add option 3

--- rb_119/practice_problems/6_pass2.rb
+++ rb_119/practice_problems/6_pass2.rb
@@ -9,30 +9,31 @@ High-level:
 - Tally
 =end
 
-# ## Solution:
-# # Option 1:
-# ALPHA = ('a'..'z').to_a
+## Solution:
+# Option 1:
+ALPHA = ('a'..'z').to_a
 
-# def count_letters(str)
-#   str.chars
-#      .select { |char| ALPHA.include?(char) }
-#      .tally
-# end
+def count_letters(str)
+  str.chars
+     .select { |char| ALPHA.include?(char) }
+     .tally
+end
 
-# expected = {'w' => 1, 'o' => 2, 'e' => 3, 'b' => 1, 'g' => 1, 'n' => 1}
-# p count_letters('woebegone') == expected
+expected = {'w' => 1, 'o' => 2, 'e' => 3, 'b' => 1, 'g' => 1, 'n' => 1}
+p count_letters('woebegone') == expected
 
-# expected = {'l' => 1, 'o' => 1, 'w' => 1, 'e' => 4, 'r' => 2,
-#             'c' => 2, 'a' => 2, 's' => 2, 'u' => 1, 'p' => 2}
-# p count_letters('lowercase/uppercase') == expected
+expected = {'l' => 1, 'o' => 1, 'w' => 1, 'e' => 4, 'r' => 2,
+            'c' => 2, 'a' => 2, 's' => 2, 'u' => 1, 'p' => 2}
+p count_letters('lowercase/uppercase') == expected
 
-# expected = {'u' => 1, 'o' => 1, 'i' => 1, 's' => 1}
-# p count_letters('W. E. B. Du Bois') == expected
+expected = {'u' => 1, 'o' => 1, 'i' => 1, 's' => 1}
+p count_letters('W. E. B. Du Bois') == expected
 
-# p count_letters('x') == {'x' => 1}
-# p count_letters('') == {}
-# p count_letters('!!!') == {}
-# # All test cases return `true`.
+p count_letters('x') == {'x' => 1}
+p count_letters('') == {}
+p count_letters('!!!') == {}
+# All test cases return `true`.
+
 
 
 
@@ -40,6 +41,31 @@ High-level:
 def count_letters(str)
   str.chars.each_with_object({}) do |char, hash|
     hash[char] = str.count(char) if char.match?(/[a-z]/)
+  end
+end
+
+expected = {'w' => 1, 'o' => 2, 'e' => 3, 'b' => 1, 'g' => 1, 'n' => 1}
+p count_letters('woebegone') == expected
+
+expected = {'l' => 1, 'o' => 1, 'w' => 1, 'e' => 4, 'r' => 2,
+            'c' => 2, 'a' => 2, 's' => 2, 'u' => 1, 'p' => 2}
+p count_letters('lowercase/uppercase') == expected
+
+expected = {'u' => 1, 'o' => 1, 'i' => 1, 's' => 1}
+p count_letters('W. E. B. Du Bois') == expected
+
+p count_letters('x') == {'x' => 1}
+p count_letters('') == {}
+p count_letters('!!!') == {}
+# All test cases return `true`.
+
+
+
+
+# Option 3:
+def count_letters(str)
+  str.chars.each_with_object(Hash.new(0)) do |char, hash|
+    hash[char] += 1 if char.match?(/[a-z]/)
   end
 end
 
