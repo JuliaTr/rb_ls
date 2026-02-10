6b39b86c158465a935b05eaf1018c4b3ace888a4
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-10 16:21:36 -0600

    Add number_guesser_part1.rb

--- rb_120/exercises/medium/number_guesser_part1.rb
+++ rb_120/exercises/medium/number_guesser_part1.rb
@@ -6,6 +6,92 @@ Note that a game object should start a new game with a new number
 to guess with each call to `#play`.
 =end
 
+## Possible solution
+class GuessingGame
+  MAX_GUESSES = 7
+  RANGE = 1..100
+
+  RESULT_OF_GUESS_MESSAGE = {
+    high:  "Your number is too high.",
+    low:   "Your number is too low.",
+    match: "That's the number!"
+  }.freeze
+
+  WIN_OR_LOSE = {
+    high:  :lose,
+    low:   :lose,
+    match: :win 
+  }.freeze
+
+  RESULT_OF_GAME_MESSAGE = {
+    win:  "You won!",
+    lose: "You have no more guesses. You lost!"
+  }.freeze
+
+  def initialize
+    @secret_number = nil
+  end
+
+  def play
+    reset
+    game_result = play_game
+    display_game_end_message(game_result)
+  end
+
+  private
+
+  def reset
+    @secret_number = rand(RANGE)
+  end
+
+  def play_game
+    result = nil
+
+    MAX_GUESSES.downto(1) do |remaining_guesses|
+      display_guesses_remaining(remaining_guesses)
+      result = check_guess(obtain_one_guess)
+      puts RESULT_OF_GUESS_MESSAGE[result]
+      break if result == :match
+    end
+
+    WIN_OR_LOSE[result]
+  end
+
+  def display_guesses_remaining(remaining)
+    puts
+    if remaining == 1
+      puts 'You have 1 guess remaining.'
+    else
+      puts "You have #{remaining} guesses remaining."
+    end
+  end
+
+  def obtain_one_guess
+    loop do
+      print "Enter a number between #{RANGE.first} and #{RANGE.last}: "
+      guess = gets.chomp.to_i
+      return guess if RANGE.cover?(guess)
+      print "Invalid guess. "
+    end
+  end
+
+  def check_guess(guess_value)
+    return :match if guess_value == @secret_number
+    return :low if guess_value < @secret_number
+    :high
+  end
+
+  def display_game_end_message(result)
+    puts "", RESULT_OF_GAME_MESSAGE[result]
+  end
+end
+
+game = GuessingGame.new
+game.play
+# Works as expected
+
+
+
 ## Refactored:
 class GuessingGame
   def decrease_guesses
@@ -61,174 +147,175 @@ game.play
 # Works as expected
 
 
-# ## Solution:
-# class GuessingGame
-#   def initialize
-#     @guesses = 7
-#     @rand_number = rand(1..100)
-#   end
 
-#   def decrease_guesses
-#     @guesses -= 1
-#   end
+## Solution:
+class GuessingGame
+  def initialize
+    @guesses = 7
+    @rand_number = rand(1..100)
+  end
 
-#   def play
-#     @rand_number = rand(1..100)
-#     p @rand_number
+  def decrease_guesses
+    @guesses -= 1
+  end
 
-#     loop do
-#       puts "You have #{@guesses} guesses remaining."
-#       choice = nil
+  def play
+    @rand_number = rand(1..100)
+    p @rand_number
 
-#       loop do
-#         print "Enter a number between 1 and 100: "
-#         choice = gets.chomp.to_i
+    loop do
+      puts "You have #{@guesses} guesses remaining."
+      choice = nil
 
-#         if choice > 100 || choice < 1
-#           print "Invalid guess. "
-#         elsif choice > @rand_number
-#           puts "Your guess is too high."
-#           puts
-#           break
-#         elsif choice < @rand_number 
-#           puts "Your guess is too low."
-#           puts
-#           break
-#         elsif choice == @rand_number
-#           puts "That's the number!"
-#           puts
-#           break
-#         end
-#       end
+      loop do
+        print "Enter a number between 1 and 100: "
+        choice = gets.chomp.to_i
 
-#       decrease_guesses
+        if choice > 100 || choice < 1
+          print "Invalid guess. "
+        elsif choice > @rand_number
+          puts "Your guess is too high."
+          puts
+          break
+        elsif choice < @rand_number 
+          puts "Your guess is too low."
+          puts
+          break
+        elsif choice == @rand_number
+          puts "That's the number!"
+          puts
+          break
+        end
+      end
 
-#       if @guesses == 0
-#         puts "You have no more guesses. You lost!"
-#         puts
-#         break
-#       elsif choice == @rand_number
-#         puts "You won!"
-#         puts
-#         break
-#       end
-#     end
+      decrease_guesses
 
-#     @guesses = 7
-#   end
-# end
+      if @guesses == 0
+        puts "You have no more guesses. You lost!"
+        puts
+        break
+      elsif choice == @rand_number
+        puts "You won!"
+        puts
+        break
+      end
+    end
 
-# game = GuessingGame.new
-# game.play
+    @guesses = 7
+  end
+end
 
-# # Actual output (meets the pattern):
-# =begin
-# 72
-# You have 7 guesses remaining.
-# Enter a number between 1 and 100: 104
-# Invalid guess. Enter a number between 1 and 100: 50
-# Your guess is too low.
+game = GuessingGame.new
+game.play
 
-# You have 6 guesses remaining.
-# Enter a number between 1 and 100: 0
-# Invalid guess. Enter a number between 1 and 100: 23
-# Your guess is too low.
+# Actual output (meets the pattern):
+=begin
+72
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 104
+Invalid guess. Enter a number between 1 and 100: 50
+Your guess is too low.
 
-# You have 5 guesses remaining.
-# Enter a number between 1 and 100: 72
-# That's the number!
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 0
+Invalid guess. Enter a number between 1 and 100: 23
+Your guess is too low.
 
-# You won!
-# =end
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 72
+That's the number!
 
-# # The game should play like this:
-# =begin
-# You have 7 guesses remaining.
-# Enter a number between 1 and 100: 104
-# Invalid guess. Enter a number between 1 and 100: 50
-# Your guess is too low.
+You won!
+=end
 
-# You have 6 guesses remaining.
-# Enter a number between 1 and 100: 75
-# Your guess is too low.
+# The game should play like this:
+=begin
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 104
+Invalid guess. Enter a number between 1 and 100: 50
+Your guess is too low.
 
-# You have 5 guesses remaining.
-# Enter a number between 1 and 100: 85
-# Your guess is too high.
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 75
+Your guess is too low.
 
-# You have 4 guesses remaining.
-# Enter a number between 1 and 100: 0
-# Invalid guess. Enter a number between 1 and 100: 80
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 85
+Your guess is too high.
 
-# You have 3 guesses remaining.
-# Enter a number between 1 and 100: 81
-# That's the number!
-# =end
+You have 4 guesses remaining.
+Enter a number between 1 and 100: 0
+Invalid guess. Enter a number between 1 and 100: 80
 
-# game.play
-# # Actual output (meets the pattern):
-# =begin
-# 27
-# You have 7 guesses remaining.
-# Enter a number between 1 and 100: 23
-# Your guess is too low.
+You have 3 guesses remaining.
+Enter a number between 1 and 100: 81
+That's the number!
+=end
 
-# You have 6 guesses remaining.
-# Enter a number between 1 and 100: 87
-# Your guess is too high.
+game.play
+# Actual output (meets the pattern):
+=begin
+27
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 23
+Your guess is too low.
 
-# You have 5 guesses remaining.
-# Enter a number between 1 and 100: 65
-# Your guess is too high.
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 87
+Your guess is too high.
 
-# You have 4 guesses remaining.
-# Enter a number between 1 and 100: 11
-# Your guess is too low.
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 65
+Your guess is too high.
 
-# You have 3 guesses remaining.
-# Enter a number between 1 and 100: 34
-# Your guess is too high.
+You have 4 guesses remaining.
+Enter a number between 1 and 100: 11
+Your guess is too low.
 
-# You have 2 guesses remaining.
-# Enter a number between 1 and 100: 4
-# Your guess is too low.
+You have 3 guesses remaining.
+Enter a number between 1 and 100: 34
+Your guess is too high.
 
-# You have 1 guesses remaining.
-# Enter a number between 1 and 100: 2
-# Your guess is too low.
+You have 2 guesses remaining.
+Enter a number between 1 and 100: 4
+Your guess is too low.
 
-# You have no more guesses. You lost!
-# =end
+You have 1 guesses remaining.
+Enter a number between 1 and 100: 2
+Your guess is too low.
 
-# # The game should play like this:
-# =begin
-# You have 7 guesses remaining.
-# Enter a number between 1 and 100: 50
-# Your guess is too high.
+You have no more guesses. You lost!
+=end
 
-# You have 6 guesses remaining.
-# Enter a number between 1 and 100: 25
-# Your guess is too low.
+# The game should play like this:
+=begin
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 50
+Your guess is too high.
 
-# You have 5 guesses remaining.
-# Enter a number between 1 and 100: 37
-# Your guess is too high.
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 25
+Your guess is too low.
 
-# You have 4 guesses remaining.
-# Enter a number between 1 and 100: 31
-# Your guess is too low.
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 37
+Your guess is too high.
 
-# You have 3 guesses remaining.
-# Enter a number between 1 and 100: 34
-# Your guess is too high.
+You have 4 guesses remaining.
+Enter a number between 1 and 100: 31
+Your guess is too low.
 
-# You have 2 guesses remaining.
-# Enter a number between 1 and 100: 32
-# Your guess is too low.
+You have 3 guesses remaining.
+Enter a number between 1 and 100: 34
+Your guess is too high.
 
-# You have 1 guess remaining.
-# Enter a number between 1 and 100: 32
-# Your guess is too low.
+You have 2 guesses remaining.
+Enter a number between 1 and 100: 32
+Your guess is too low.
 
-# You have no more guesses. You lost!
-# =end
+You have 1 guess remaining.
+Enter a number between 1 and 100: 32
+Your guess is too low.
+
+You have no more guesses. You lost!
+=end
