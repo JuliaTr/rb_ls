6fece8b64483d73427b244c2e500ee2246831a9a
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-12 14:22:04 -0600

    Add example

--- rb_120/practice/class_inheritance_encapsulation_polymorphism/4.rb
+++ rb_120/practice/class_inheritance_encapsulation_polymorphism/4.rb
@@ -1,38 +1,44 @@
-# Polymorphism with Modules
+# # Polymorphism with Modules
 
-module Taggable
-  def add_tag(tag)
-    @tags ||= []
-    @tags << tag
-  end
+# module Taggable
+#   def add_tag(tag)
+#     @tags ||= []
+#     @tags << tag
+#   end
 
-  def tags
-    @tags || []
-  end
-end
+#   def tags
+#     @tags || []
+#   end
+# end
 
-class Photo
-  include Taggable
-end
+# class Photo
+#   include Taggable
+# end
 
-class BlogPost
-  include Taggable
-end
+# class BlogPost
+#   include Taggable
+# end
 
-photo = Photo.new
-photo.add_tag('sunset')
-photo.add_tag('beach')
+# photo = Photo.new
+# photo.add_tag('sunset')
+# photo.add_tag('beach')
 
-post = BlogPost.new
-post.add_tag('ruby')
-post.add_tag('oop')
+# post = BlogPost.new
+# post.add_tag('ruby')
+# post.add_tag('oop')
 
-puts "Photo tags: #{photo.tags.join(', ')}"
-puts "Blog post tags: #{post.tags.join(', ')}"
+# puts "Photo tags: #{photo.tags.join(', ')}"
+# puts "Blog post tags: #{post.tags.join(', ')}"
 
 
 
-## Experiments:
-name = 'Yuliia'
-name ||= "Guest"
-puts name # Yuliia
+
+# ## Experiments:
+# name = 'Yuliia'
+# name ||= "Guest" # short-circuits
+# puts name  # Yuliia
+
+
+email = nil
+email ||= "default@example.com" # full evaluation
+puts email  # default@example.com
