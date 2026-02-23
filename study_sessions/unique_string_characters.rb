e5beb769b7215679a799e151f5c5af15b40ecdf6
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-02-23 17:24:26 -0600

    Add alternative solution

--- study_sessions/unique_string_characters.rb
+++ study_sessions/unique_string_characters.rb
@@ -8,8 +8,10 @@ in the two strings.
 
 "xyab","xzca"
 
-"xyab"
-"xzca"
+x, y, a, b, x, z, c, a
+y, b  (unique from str1)
+z, c  (unique from str2)
+
 
 'ybzc'
 
@@ -40,9 +42,32 @@ Idea2:
 
 =end
 
+## Alternative:
+def unique_string_characters(str1, str2)
+  str1_chars = str1.chars.uniq
+  str2_chars = str2.chars.uniq
+
+  # Characters in str1 but not in str2
+  unique_to_str1 = str1_chars - str2_chars  # ["y", "b"]
+
+  # Characters in str2 but not in str1
+  unique_to_str2 = str2.chars - str1_chars  # ["z", "c"]
+
+  (unique_to_str1 + unique_to_str2).join
+end
+
+puts unique_string_characters("xyab", "xzca") == "ybzc" # true
+puts unique_string_characters("a", "z") == "az" # true
+puts unique_string_characters("abcd", "de") == "abce" # true
+puts unique_string_characters("abc", "abba") == "c" # true
+puts unique_string_characters("xyz", "zxy") == "" # 
+# All test cases print `true`
 
 
-# Solution for Idea2:
+
+
+
+## Solution for Idea2:
 def unique_string_characters(str1, str2)
   unique_arr = (str1 + str2).chars.uniq
   result = ''
