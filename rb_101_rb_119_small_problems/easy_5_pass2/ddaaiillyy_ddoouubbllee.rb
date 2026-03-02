34eea9bc97f0330c02930f825cc5fab32d90432d
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-02 17:09:52 -0600

    Add ddaaiillyy_ddoouubbllee.rb

---
+++ rb_101_rb_119_small_problems/easy_5_pass2/ddaaiillyy_ddoouubbllee.rb
@@ -0,0 +1,44 @@
+=begin
+ddaaiillyy ddoouubbllee
+
+Write a method that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character. You may not use String#squeeze or String#squeeze!.
+
+
+Idea1:
+- Build new str
+- Loop over input
+- Check if last char is already in a new str.
+- If not, add
+- If yes, next
+
+- new_str = ''
+- arr = split str
+- Iterate over each substring from arr
+  - word = ''
+  - Iterate over each char in substring
+    - word << char if word [-1] in NOT the same char
+  - new_str << word
+- new_str
+
+  'ddaaiillyy ddoouubbllee'
+=end
+
+def crunch(str)
+  return '' if str.empty?
+
+  result = str.split.map do |substr|
+    word = ''
+    substr.each_char do |char|
+      word << char unless word[-1] == char
+    end
+    word
+  end
+
+  result.join(' ')
+end
+
+p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
