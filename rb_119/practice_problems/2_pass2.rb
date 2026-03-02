5a882bf194ebdaf30e17450bbdd86fe4663c2736
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-02 13:18:38 -0600

    Add refactored option 2

--- rb_119/practice_problems/2_pass2.rb
+++ rb_119/practice_problems/2_pass2.rb
@@ -30,22 +30,28 @@ Idea1:
 
 =end
 
-## Solution
+## Refactored
+# Option 2:
 def substrings(arr)
   substrings = []
+
   (0...arr.size).each do |idx|
-    substrings << arr.slice(idx, 5)
+    substring = arr.slice(idx, 5)
+
+    # Push only substring with size of 5 elements
+    substrings << substring if substring.size == 5
   end
+
   substrings
 end
 
 def minimum_sum(arr)
   return nil if arr.size < 5
 
+  # Nested array with substrings with size of 5
   substrings = substrings(arr)
-  selected = substrings.select { |subarrays| subarrays.size == 5 }
 
-  selected.map(&:sum).sort[0]
+  substrings.map(&:sum).min
 end
 
 p minimum_sum([1, 2, 3, 4]) == nil
@@ -54,3 +60,31 @@ p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
 p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
 p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
 # All test cases return `true`
+
+
+
+
+# ## Solution
+# def substrings(arr)
+#   substrings = []
+#   (0...arr.size).each do |idx|
+#     substrings << arr.slice(idx, 5)
+#   end
+#   substrings
+# end
+
+# def minimum_sum(arr)
+#   return nil if arr.size < 5
+
+#   substrings = substrings(arr)
+#   selected = substrings.select { |subarrays| subarrays.size == 5 }
+
+#   selected.map(&:sum).sort[0]
+# end
+
+# p minimum_sum([1, 2, 3, 4]) == nil
+# p minimum_sum([1, 2, 3, 4, 5, -5]) == 9
+# p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
+# p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
+# p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+# # All test cases return `true`
