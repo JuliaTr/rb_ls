7573934c127141fbc3311a7041f0f4398fcfb3dc
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-04 15:10:18 -0600

    Add 6.rb

---
+++ rb_120/practice/class_inheritance_encapsulation_polymorphism/6.rb
@@ -0,0 +1,28 @@
+=begin
+Description​​: Create an Animal class with a constructor that 
+takes a name. It should have an eat method that returns a 
+string "{name} is eating." Create a Fish class that inherits 
+from Animal.[3:16 PM]The Fish class should also have a swim 
+method that returns "{name} is swimming."
+=end
+
+class Animal
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    "#{@name} is eating."
+  end
+end
+
+class Fish < Animal
+  def swim
+    "#{@name} is swimming."
+  end
+end
+
+nemo = Fish.new('Nemo')
+p nemo.eat == 'Nemo is eating.'
+p nemo.swim == 'Nemo is swimming.'
+# All test cases return `true`
