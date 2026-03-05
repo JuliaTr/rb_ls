e949defd33edcefbdee1c19e423d4e5f9930d1c5
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-05 11:29:18 -0600

    Add experiment

--- rb_120/practice/classes_objects/5.rb
+++ rb_120/practice/classes_objects/5.rb
@@ -1,36 +1,4 @@
-# Predict output
-
-class Person
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def to_s
-    "My name is #{name}."
-  end
-end
-
-bob = Person.new("Robert Smith")
-puts bob  # My name is Robert Smith.
-p bob.to_s # "My name is Robert Smith."
-
-
-
-## Experiments:
-class Person
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
-
-bob = Person.new("Robert Smith")
-puts "My name is #{bob.name}."
-
-
+# # Predict output
 
 # class Person
 #   attr_accessor :name
@@ -41,13 +9,52 @@ puts "My name is #{bob.name}."
 
 #   def to_s
 #     "My name is #{name}."
-#     42
 #   end
 # end
 
 # bob = Person.new("Robert Smith")
+# puts bob  # My name is Robert Smith.
+# p bob.to_s # "My name is Robert Smith." 
 
-# p bob # puts bob.inspect
-# puts bob # bob.to_s
+# =begin
+# `to_s` defined inside `Person` class overrides deefault `to_s`
+# method.
+# =end
 
-# # "My name is Robert Smith."
+
+
+# ## Experiments:
+# class Person
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# bob = Person.new("Robert Smith")
+# puts "My name is #{bob.name}."
+# # My name is Robert Smith.
+
+
+
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "My name is #{name}."
+    42
+  end
+end
+
+bob = Person.new("Robert Smith")
+
+p bob   # puts bob.inspect
+#<Person:0x0000000103a351c0 @name="Robert Smith">
+
+puts bob # bob.to_s
+#<Person:0x00000001093c51e0>
