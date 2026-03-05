0fb06dedc23470649867ad5937798d42e6d5db86
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-05 11:43:45 -0600

    Add experiments

--- rb_120/practice/classes_objects/5.rb
+++ rb_120/practice/classes_objects/5.rb
@@ -23,23 +23,24 @@
 
 
 
-# ## Experiments:
-# class Person
-#   attr_accessor :name
+## Experiments:
+class Person
+  attr_reader :name
 
-#   def initialize(name)
-#     @name = name
-#   end
-# end
+  def initialize(name)
+    @name = name
+  end
+end
 
-# bob = Person.new("Robert Smith")
-# puts "My name is #{bob.name}."
-# # My name is Robert Smith.
+bob = Person.new("Robert Smith")
+puts "My name is #{bob.name}." # My name is Robert Smith.
+puts "My name is #{bob}."      # My name is #<Person:0x000000010aa14fe0>.
+
 
 
 
 class Person
-  attr_accessor :name
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -55,6 +56,29 @@ bob = Person.new("Robert Smith")
 
 p bob   # puts bob.inspect
 #<Person:0x0000000103a351c0 @name="Robert Smith">
+puts bob.inspect
+#<Person:0x0000000103a351c0 @name="Robert Smith">
 
 puts bob # bob.to_s
 #<Person:0x00000001093c51e0>
+
+
+
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "My name is #{@name}."
+    "42"
+  end
+end
+
+bob = Person.new("Robert Smith")
+
+p bob             #<Person:0x0000000107ba5038 @name="Robert Smith">
+puts bob.inspect  # #<Person:0x0000000106925188 @name="Robert Smith">
+
+puts bob # 42
