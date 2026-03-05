4aeff18dc4d5c0905d2069128580ac01498422fb
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-05 11:01:00 -0600

    Add 5.rb

---
+++ rb_120/practice/classes_objects/5.rb
@@ -0,0 +1,42 @@
+# Predict output
+
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "My name is #{name}."
+  end
+end
+
+bob = Person.new("Robert Smith")
+puts bob  # My name is Robert Smith.
+
+
+
+## Experiments:
+
+
+
+# class Person
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def to_s
+#     "My name is #{name}."
+#     42
+#   end
+# end
+
+# bob = Person.new("Robert Smith")
+
+# p bob # puts bob.inspect
+# puts bob # bob.to_s
+
+# # "My name is Robert Smith."
