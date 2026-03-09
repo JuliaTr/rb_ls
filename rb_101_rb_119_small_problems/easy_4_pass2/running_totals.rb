138b7a799fe00ab6ad6e3b77500a5ab2a7068c12
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-09 15:45:52 -0500

    Add another example for further exploration

--- rb_101_rb_119_small_problems/easy_4_pass2/running_totals.rb
+++ rb_101_rb_119_small_problems/easy_4_pass2/running_totals.rb
@@ -10,42 +10,56 @@ looking at index 2 of the array `[14, 11, 7, 15, 20]` is
 47 (14 + 11 + 7 + 15).
 =end
 
-# ## Solution:
-# def running_total(arr)
-#   arr.map.with_index { |_, index| arr[0..index].sum }
-# end
+## Solution:
+def running_total(arr)
+  arr.map.with_index { |_, index| arr[0..index].sum }
+end
 
-# p running_total([2, 5, 13]) == [2, 7, 20]
-# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-# p running_total([3]) == [3]
-# p running_total([]) == []
-# # All test cases return `true`.
-
-
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
+# All test cases return `true`.
 
 
-# ## Possible solution:
-# def running_total(array)
-#   sum = 0
-#   array.map { |value| sum += value }
-# end
 
-# p running_total([2, 5, 13]) == [2, 7, 20]
-# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-# p running_total([3]) == [3]
-# p running_total([]) == []
-# # All test cases return `true`.
+
+## Possible solution:
+def running_total(array)
+  sum = 0
+  array.map { |value| sum += value }
+end
+
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
+# All test cases return `true`.
 
 
 
 
 ## Further exploration:
-# `Enumerable#each_with_object`
+# `Enumerable#each_with_object` (idiomatic)
 def running_total(arr)
   sum = 0
   arr.each_with_object([]) do |value, result|
     result << sum += value
   end
+end
+
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
+# All test cases return `true`.
+
+
+
+
+# `Enumerable#reduce`:
+def running_total(arr)
+  arr.map.with_index { |_, idx| arr[0..idx].reduce(:+) }
 end
 
 p running_total([2, 5, 13]) == [2, 7, 20]
