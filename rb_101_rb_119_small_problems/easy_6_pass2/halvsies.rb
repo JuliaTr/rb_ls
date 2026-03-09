daf91e8a8c1ede2a26704f547f2ddb3b8a62f219
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-09 13:43:54 -0500

    Add halvsies.rb

---
+++ rb_101_rb_119_small_problems/easy_6_pass2/halvsies.rb
@@ -0,0 +1,38 @@
+=begin
+Write a method that takes an Array as an argument, and returns 
+two Arrays (as a pair of nested Arrays) that contain the first 
+half and second half of the original Array, respectively. If 
+the original array contains an odd number of elements, the 
+middle element should be placed in the first half Array.
+=end
+
+## Solution
+def halvsies(arr)
+  middle = (arr.size / 2.0).ceil
+  left = arr[0...middle]
+  right = arr[middle..-1]
+
+  [left, right]
+end
+
+p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]] # true
+p halvsies([1, 2, 3, 4, 1]) == [[1, 2, 3], [4, 1]] # true
+p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]] # true
+p halvsies([5]) == [[5], []] # true
+p halvsies([]) == [[], []] # true
+
+
+
+## Possible solution
+def halvsies(array)
+  middle = (array.size / 2.0).ceil
+  first_half = array.slice(0, middle)
+  second_half = array.slice(middle, array.size - middle)
+  [first_half, second_half]
+end
+
+p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]] # true
+p halvsies([1, 2, 3, 4, 1]) == [[1, 2, 3], [4, 1]] # true
+p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]] # true
+p halvsies([5]) == [[5], []] # true
+p halvsies([]) == [[], []] # true
