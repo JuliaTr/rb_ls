e376a49d2554f584e9aaa8612ce894187e3d962b
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-09 13:14:30 -0500

    Add combining_arrays.rb

---
+++ rb_101_rb_119_small_problems/easy_6_pass2/combining_arrays.rb
@@ -0,0 +1,23 @@
+=begin
+Write a method that takes two Arrays as arguments, and returns 
+an Array that contains all of the values from the argument 
+Arrays. There should be no duplication of values in the 
+returned Array, even if there are duplicates in the original 
+Arrays.
+=end
+
+## Solution:
+def merge(arr1, arr2)
+  (arr1 + arr2).uniq
+end
+
+p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]  # true
+
+
+
+## Possible solution:
+def merge(arr1, arr2)
+  arr1 | arr2
+end
+
+p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]  # true
