b4be6ae92f74cbba6bd5b8efe5a8e3c474be1aba
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-10 15:11:47 -0500

    Add example

--- rb_101_rb_119_small_problems/delete_method_strings_arrays.rb
+++ rb_101_rb_119_small_problems/delete_method_strings_arrays.rb
@@ -1,20 +1,33 @@
-# ## `String#delete` (non-destructive):
-# p 'abcabc'.delete('a') # "bcbc"
+## `String#delete` (non-destructive):
+p 'abcabc'.delete('a') # "bcbc"
 
-# str = 'abcabc'
-# p str.delete('a')  # "bcbc"
-# p str              # "abcabc"
+str = 'abcabc'
+p str.delete('a')  # "bcbc"  (returns a new obj with deleted chars)
+p str              # "abcabc"
 
-# # `str` is reassigned:
-# str = str.delete('a') # "bcbc"
-# p str
+# `str` is reassigned:
+str = str.delete('a') # "bcbc"
+p str
 
 
 
-# `Array#delete` (destructive)
+## `Array#delete` (destructive)
 arr = [1, 2, 3, 2]
 p arr.delete(2)  # 2
 p arr            # [1, 3]
 
 
 
+
+## `Array#delete_at` (destructive)
+arr = [10, 20, 30]
+p arr.delete_at(1) # 20
+p arr              # [10, 30]
+
+
+
+
+## `Array#delete_if` (destructive)
+arr = [1, 2, 3, 4]
+p arr.delete_if { |num| num.even? } # [1, 3]
+p arr                               # [1, 3]
