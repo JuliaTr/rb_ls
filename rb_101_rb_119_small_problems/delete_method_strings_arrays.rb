f49ee73b8bdcf487e49bba42e9e1dd54402c9336
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-10 15:05:35 -0500

    Add example

--- rb_101_rb_119_small_problems/delete_method_strings_arrays.rb
+++ rb_101_rb_119_small_problems/delete_method_strings_arrays.rb
@@ -1,10 +1,20 @@
-## `String#delete` (non-destructive):
-p 'abcabc'.delete('a') # "bcbc"
+# ## `String#delete` (non-destructive):
+# p 'abcabc'.delete('a') # "bcbc"
 
-str = 'abcabc'
-p str.delete('a')  # "bcbc"
-p str              # "abcabc"
+# str = 'abcabc'
+# p str.delete('a')  # "bcbc"
+# p str              # "abcabc"
 
-# `str` is reassigned:
-str = str.delete('a') # "bcbc"
-p str
+# # `str` is reassigned:
+# str = str.delete('a') # "bcbc"
+# p str
+
+
+
+# `Array#delete` (destructive)
+arr = [1, 2, 3, 2]
+p arr.delete(2)  # 2
+p arr            # [1, 3]
+
+
+
