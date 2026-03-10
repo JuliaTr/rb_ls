04a89ff3ad5e311a1b3054401c27ea3fe12872b6
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-10 13:06:07 -0500

    Add option 2

--- rb_101_rb_119_small_problems/easy_5_pass2/alphabetical_numbers.rb
+++ rb_101_rb_119_small_problems/easy_5_pass2/alphabetical_numbers.rb
@@ -13,14 +13,12 @@ seventeen, eighteen, nineteen
 NUMBER_WORDS = { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three',
                 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven',
                 8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven',
-                12 => 'twelve', 13 => 'thirteen', 14 => 'fourteem',
+                12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen',
                 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen',
                 18 => 'eighteen', 19 => 'nineteen' }
 
 def alphabetic_number_sort(arr)
-  arr.sort_by do |element|
-    NUMBER_WORDS[element]
-  end
+  arr.sort_by { |element| NUMBER_WORDS[element] }
 end
 
 p alphabetic_number_sort((0..19).to_a) == [
@@ -29,3 +27,47 @@ p alphabetic_number_sort((0..19).to_a) == [
 ] # true
 
 
+
+
+# Option 2:
+# Genrate key-value pairs:
+WORDS = %w(zero one two three four five six seven 
+          eight nine ten eleven twelve thirteen fourteen 
+          fifteen sixteen seventeen eighteen nineteen)
+
+def build_hash(arr)
+  arr.each_with_object({}).with_index do |(number, hash), idx|
+    hash[number] = WORDS[idx]
+  end
+end
+
+def alphabetic_number_sort(arr)
+  # Generate hash with numbers (keys), words (vlues)
+  hash = build_hash(arr)
+
+  # Sort
+  arr.sort_by { |element| hash[element] }
+end
+
+numbers = (0..19).to_a
+p alphabetic_number_sort(numbers) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+] # true
+
+
+
+
+## Possible solution:
+NUMBER_WORDS = %w(zero one two three four five six seven 
+          eight nine ten eleven twelve thirteen fourteen 
+          fifteen sixteen seventeen eighteen nineteen)
+
+def alphabetic_number_sort(arr)
+  numbers.sort_by { |number| NUMBER_WORDS[number] }
+end
+
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+] # true
