108f6d56130f9dd53ebc6b6ba4138b230f898c68
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-11 13:48:30 -0500

    Add alternative solution

--- rb_101_rb_119_small_problems/advanced_pass2/transpose_3_3.rb
+++ rb_101_rb_119_small_problems/advanced_pass2/transpose_3_3.rb
@@ -12,16 +12,43 @@ produce a new matrix and leave the original matrix
 unchanged.
 =end
 
-## Without tracking indexes manually:
-def create_columns(matrix)
-  new_matrix = []
-  matrix[0].size.times { new_matrix << [] }
-  new_matrix
+## Alternative to the solution below:
+# new_matrix = [
+#   [matrix[0][0], matrix[1][0], matrix[2][0]],
+#   [matrix[0][1], matrix[1][1], matrix[2][1]], 
+#   [matrix[0][2], matrix[1][2], matrix[2][2]]
+# ]
+def transpose(matrix)
+  rows_count = matrix.size     # 3
+  cols_count = matrix[0].size  # 3
+
+  Array.new(cols_count) do |col_idx|
+    Array.new(rows_count) do |row_idx|
+      matrix[row_idx][col_idx]
+    end
+  end
 end
 
-def transpose(matrix)
-  new_matrix = create_columns(matrix)
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
 
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
+
+
+
+## More refactored:
+def transpose(matrix)
+  # Compact version to create a nested array; just a shorter way
+  new_matrix = Array.new(matrix[0].size) { [] }  # empty
+
+  # Without tracking indexes manually
   new_matrix.each_with_index do |column, idx|
     matrix.each do |row|
       column << row[idx]
@@ -46,125 +73,157 @@ p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
 
 
 
-# ## More flexible:
-# def create_columns(matrix)
-#   new_matrix = []
-#   matrix[0].size.times { new_matrix << [] }
-#   new_matrix
-# end
+## More flexible:
+def create_columns(matrix)
+  new_matrix = []
+  matrix[0].size.times { new_matrix << [] }
+  new_matrix
+end
 
-# def transpose(matrix)
-#   # Create columns
-#   new_matrix = create_columns(matrix)
+# assumes a non-empty matrix with rows of equal length
+def transpose(matrix)
+  # Create columns
+  new_matrix = create_columns(matrix)
 
-#   # Put item in each column
-#   #       .
-#   # [[], [], []]
-#   idx = 0
-#   new_matrix.each do |column|
-#     matrix.each do |row|
-#       column << row[idx]
-#     end
-#     idx += 1
-#   end
+  # Put item in each column
+  #       .
+  # [[], [], []]
+  idx = 0
+  new_matrix.each do |column|
+    matrix.each do |row|
+      column << row[idx]
+    end
+    idx += 1
+  end
 
-#   new_matrix
-# end
+  new_matrix
+end
 
-# matrix = [
-#   [1, 5, 8],
-#   [4, 7, 2],
-#   [3, 9, 6]
-# ]
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
 
-# new_matrix = transpose(matrix)
+new_matrix = transpose(matrix)
 
-# p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-# p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
-# # All test cases return `true`.
-
-
-
-
-# ## Refactored Option 1:
-# def transpose(matrix)
-#   col1 = []
-#   col2 = []
-#   col3 = []
-
-#   matrix.each do |row|
-#     col1 << row[0]
-#     col2 << row[1]
-#     col3 << row[2]
-#   end
-
-#   [col1, col2, col3]
-# end
-
-# matrix = [
-#   [1, 5, 8],
-#   [4, 7, 2],
-#   [3, 9, 6]
-# ]
-
-# new_matrix = transpose(matrix)
-
-# p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-# p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
-# # All test cases return `true`.
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
 
 
 
 
-# ## Solution
-# # Option 1:
-# def transpose(matrix)
-#   col1 = []
-#   col2 = []
-#   col3 = []
-#   new_matrix = [col1, col2, col3]
+## Refactored Option 1:
+def transpose(matrix)
+  col1 = []
+  col2 = []
+  col3 = []
 
-#   matrix.each do |row| 
-#     col1 << row[0]
-#     col2 << row[1]
-#     col3 << row[2]
-#   end
+  matrix.each do |row|
+    col1 << row[0]
+    col2 << row[1]
+    col3 << row[2]
+  end
 
-#   new_matrix
-# end
+  [col1, col2, col3]
+end
 
-# matrix = [
-#   [1, 5, 8],
-#   [4, 7, 2],
-#   [3, 9, 6]
-# ]
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
 
-# new_matrix = transpose(matrix)
+new_matrix = transpose(matrix)
 
-# p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-# p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
-# # All test cases return `true`.
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
 
 
 
 
-# # Option 2:
-# def transpose(matrix)
+## Solution
+# Option 1:
+def transpose(matrix)
+  col1 = []
+  col2 = []
+  col3 = []
+  new_matrix = [col1, col2, col3]
+
+  matrix.each do |row| 
+    col1 << row[0]
+    col2 << row[1]
+    col3 << row[2]
+  end
+
+  new_matrix
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
+
+
+
+
+# Option 2:
+def transpose(matrix)
+  new_matrix = [
+    [matrix[0][0], matrix[1][0], matrix[2][0]],
+    [matrix[0][1], matrix[1][1], matrix[2][1]], 
+    [matrix[0][2], matrix[1][2], matrix[2][2]]
+  ]
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
+
+
+
+
+## Possible solution
 #   new_matrix = [
 #     [matrix[0][0], matrix[1][0], matrix[2][0]],
 #     [matrix[0][1], matrix[1][1], matrix[2][1]], 
 #     [matrix[0][2], matrix[1][2], matrix[2][2]]
 #   ]
-# end
+def transpose(matrix)
+  result = []
+  (0..2).each do |column_index|
+    # `row_index` increments for each `column_index` 
+    new_row = (0..2).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+  result
+end
 
-# matrix = [
-#   [1, 5, 8],
-#   [4, 7, 2],
-#   [3, 9, 6]
-# ]
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
 
-# new_matrix = transpose(matrix)
+new_matrix = transpose(matrix)
 
-# p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-# p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
-# # All test cases return `true`.
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
