8485475d3a703101c500a3f98ff011fb84b06348
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-12 13:59:55 -0500

    Add improved solution

--- rb_101_rb_119_small_problems/easy_6_pass2/reversed_arrays_part1.rb
+++ rb_101_rb_119_small_problems/easy_6_pass2/reversed_arrays_part1.rb
@@ -6,39 +6,102 @@ this method. The return value should be the same Array object.
 You may not use `Array#reverse` or `Array#reverse!`.
 =end
 
-## Solution:
+## Improved solution
 def reverse!(list)
-  counter = -2
-  first_to_move = list[counter]
+  if list.empty? || list.size == 1 ||
+      (list.first == list.last && list.size == 3) ||
+      list.all? { |element| element == list.first }
+    return list
+  end
 
-  until first_to_move == list[1]
+  counter = list.size - 2
+  list.size.times do
     list << list.slice!(counter)
     counter -= 1
   end
-
   list
 end
 
+list = [1, 2]
+result = reverse!(list)
+p result == [2, 1]
+p list == [2, 1]
+p list.object_id == result.object_id
+
+list = [1, 1, 2]
+result = reverse!(list)
+p result == [2, 1, 1]
+p list == [2, 1, 1]
+p list.object_id == result.object_id
+
+list = ['a', 'b', 'a']
+result = reverse!(list)
+p result == ['a', 'b', 'a']
+p list == ['a', 'b', 'a']
+p list.object_id == result.object_id
+
+
 list = [1, 2, 3, 4]
 result = reverse!(list)
-p result == [4, 3, 2, 1] # true
-p list == [4, 3, 2, 1] # true
-p list.object_id == result.object_id # true
+p result == [4, 3, 2, 1]
+p list == [4, 3, 2, 1]
+p list.object_id == result.object_id
 
 list = [1, 2, 3, 4, 1]
 result = reverse!(list)
-p result == [1, 4, 3, 2, 1] # true
-p list == [1, 4, 3, 2, 1] # true
-p list.object_id == result.object_id # true
+p result == [1, 4, 3, 2, 1]
+p list == [1, 4, 3, 2, 1]
+p list.object_id == result.object_id
 
 list = %w(a b e d c)
-p reverse!(list) == ["c", "d", "e", "b", "a"] # true
-p list == ["c", "d", "e", "b", "a"] # true
+p reverse!(list) == ["c", "d", "e", "b", "a"]
+p list == ["c", "d", "e", "b", "a"]
 
 list = ['abc']
-p reverse!(list) == ["abc"] # true
-p list == ["abc"] # true
+p reverse!(list) == ["abc"]
+p list == ["abc"]
 
 list = []
-p reverse!(list) == [] # true
-p list == [] # true
+p reverse!(list) == []
+p list == []
+# All test cases print `true`.
+
+
+
+
+# ## Solution:
+# def reverse!(list)
+#   counter = -2
+#   first_to_move = list[counter]
+
+#   until first_to_move == list[1]
+#     list << list.slice!(counter)
+#     counter -= 1
+#   end
+
+#   list
+# end
+
+# list = [1, 2, 3, 4]
+# result = reverse!(list)
+# p result == [4, 3, 2, 1] # true
+# p list == [4, 3, 2, 1] # true
+# p list.object_id == result.object_id # true
+
+# list = [1, 2, 3, 4, 1]
+# result = reverse!(list)
+# p result == [1, 4, 3, 2, 1] # true
+# p list == [1, 4, 3, 2, 1] # true
+# p list.object_id == result.object_id # true
+
+# list = %w(a b e d c)
+# p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+# p list == ["c", "d", "e", "b", "a"] # true
+
+# list = ['abc']
+# p reverse!(list) == ["abc"] # true
+# p list == ["abc"] # true
+
+# list = []
+# p reverse!(list) == [] # true
+# p list == [] # true
