ff6cb56bc57902f2f630cfca3178b7ab1ee0cdab
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-13 11:00:49 -0500

    Add more refactored solution

--- rb_101_rb_119_small_problems/easy_7_pass2/staggered_caps_part1.rb
+++ rb_101_rb_119_small_problems/easy_7_pass2/staggered_caps_part1.rb
@@ -28,6 +28,26 @@ a
 staggered_case('ALL_CAPS') == 'AlL_CaPs'
 =end
 
+## More refactored
+# Without checking is char is a letter:
+def staggered_case(str)
+  new_str = ''
+
+  str.each_char.with_index do |char, idx|
+    new_str << (idx.even? ? char.upcase : char.downcase)
+  end
+
+  new_str
+end
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+# All test cases return `true`.
+
+
+
+
 ## Refactored
 def staggered_case(str)
   new_str = ''
@@ -52,24 +72,49 @@ p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
 
 
 
-# ## Solution
-# def staggered_case(str)
-#   new_str = ''
+## Solution
+def staggered_case(str)
+  new_str = ''
 
-#   str.each_char.with_index do |char, idx|
-#     if char.match?(/[a-zA-Z]/) && idx.even?
-#       new_str << char.upcase
-#     elsif char.match?(/[a-zA-Z]/) && idx.odd?
-#       new_str << char.downcase
-#     elsif char.match?(/[^a-zA-Z]/)
-#       new_str << char
-#     end
-#   end
+  str.each_char.with_index do |char, idx|
+    if char.match?(/[a-zA-Z]/) && idx.even?
+      new_str << char.upcase
+    elsif char.match?(/[a-zA-Z]/) && idx.odd?
+      new_str << char.downcase
+    elsif char.match?(/[^a-zA-Z]/)
+      new_str << char
+    end
+  end
 
-#   new_str
-# end
+  new_str
+end
 
-# p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-# p staggered_case('ALL_CAPS') == 'AlL_CaPs'
-# p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
-# # All test cases return `true`.
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+# All test cases return `true`.
+
+
+
+## Possible solution:
+def staggered_case(string)
+  result = ''
+  need_upper = true
+
+  string.chars.each do |char|
+    if need_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+
+    need_upper = !need_upper
+  end
+
+  result
+end
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+# All test cases return `true`.
