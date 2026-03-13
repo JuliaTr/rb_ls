15573bcfd485f3511ab49e45bf6b4c707cee75fb
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-13 15:58:37 -0500

    Add further exploration with zip method

--- rb_101_rb_119_small_problems/easy_7_pass2/multiply_lists.rb
+++ rb_101_rb_119_small_problems/easy_7_pass2/multiply_lists.rb
@@ -1,30 +1,41 @@
-=begin
-Write a method that takes two Array arguments in which each 
-Array contains a list of numbers, and returns a new Array that 
-contains the product of each pair of numbers from the arguments 
-that have the same index. You may assume that the arguments 
-contain the same number of elements.
-=end
+# =begin
+# Write a method that takes two Array arguments in which each 
+# Array contains a list of numbers, and returns a new Array that 
+# contains the product of each pair of numbers from the arguments 
+# that have the same index. You may assume that the arguments 
+# contain the same number of elements.
+# =end
 
-## Solution
-def multiply_list(arr1, arr2)
-  arr1.map.with_index { |val, idx| val * arr2[idx] }
-end
+# ## Solution
+# def multiply_list(arr1, arr2)
+#   arr1.map.with_index { |val, idx| val * arr2[idx] }
+# end
 
-p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]  # true
-
-
+# p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]  # true
 
 
-## Possible solution
+
+
+# ## Possible solution
+# def multiply_list(list_1, list_2)
+#   products = []
+
+#   list_1.each_with_index do |item, index|
+#     products << item * list_2[index]
+#   end
+
+#   products
+# end
+
+# p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]  # true
+
+
+
+
+## Further exploration
+# `Array#zip`
 def multiply_list(list_1, list_2)
-  products = []
-
-  list_1.each_with_index do |item, index|
-    products << item * list_2[index]
-  end
-
-  products
+  list_1.zip(list_2).map { |num1, num2| num1 * num2 }
 end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]  # true
