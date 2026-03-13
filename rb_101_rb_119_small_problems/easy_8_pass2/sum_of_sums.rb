a62a7100e593ca6b667e1ec9d3a15f3a8d469bd6
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-13 16:34:37 -0500

    Add clearer solution with sum method

--- rb_101_rb_119_small_problems/easy_8_pass2/sum_of_sums.rb
+++ rb_101_rb_119_small_problems/easy_8_pass2/sum_of_sums.rb
@@ -4,6 +4,20 @@ the sum of the sums of each leading subsequence for that Array.
 You may assume that the Array always contains at least one number.
 =end
 
+## Clearer (without `#inject`)
+def sum_of_sums(arr)
+  subsequences = arr.map.with_index { |_, idx| arr[0..idx] }
+  subsequences.sum { |subarr| subarr.sum }
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
+# All test cases return `true`.
+
+
+
 ## Solution
 def sum_of_sums(arr)
   subsequences = arr.map.with_index { |_, idx| arr[0..idx] }
