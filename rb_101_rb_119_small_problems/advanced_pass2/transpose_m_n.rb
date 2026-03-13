de54a61e94cd965ff2e1c54ac04697fe5e3e9ac2
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-13 15:54:58 -0500

    Add concise solution with zip method

--- rb_101_rb_119_small_problems/advanced_pass2/transpose_m_n.rb
+++ rb_101_rb_119_small_problems/advanced_pass2/transpose_m_n.rb
@@ -12,6 +12,22 @@ I:
 - Populate nested arrays
 =end
 
+## Concise
+def transpose(matrix)
+  first_row = matrix.first
+  first_row.zip(*matrix[1..])  # with any number of rows
+end
+
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+  [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
+# All test cases return `true`.
+
+
+
+
 ## Solution
 def transpose(matrix)
   # Create empty nested array with necessary number of subarrays
