75f4003bfd6b2308a2a3f1a8fa5ada6fe0a31bdc
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-16 18:11:27 -0500

    Add another solution

--- rb_101_rb_119_small_problems/easy_8_pass2/array_average.rb
+++ rb_101_rb_119_small_problems/easy_8_pass2/array_average.rb
@@ -34,3 +34,15 @@ puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
 # All test cases print `true`.
+
+
+
+
+def average(numbers)
+  numbers.reduce(:+) / numbers.count
+end
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+# All test cases print `true`.
