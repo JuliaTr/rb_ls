d8f7281be0e5ec6c332ac197162f50eac77f2c33
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-23 16:23:28 -0500

    Add rotation_part1.rb

---
+++ rb_101_rb_119_small_problems/medium1_pass2/rotation_part1.rb
@@ -0,0 +1,25 @@
+=begin
+Write a method that rotates an array by moving the first element 
+to the end of the array. The original array should not be 
+modified.
+
+Do not use the method Array#rotate or Array#rotate! for your 
+implementation.
+=end
+
+## Solution
+
+# Option 2
+def rotate_array(arr)
+  new_arr = arr.map { |element| element }
+  first_el = new_arr.shift
+  new_arr + [first_el]
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+x == [1, 2, 3, 4]                 # => true
