e86c4d4e52e4fca2b2bd071c073444995d39e7a0
Yuliia Tryhubniak <juliatrygubniak@gmail.com>
2026-03-23 17:03:57 -0500

    Add transpose_3_x_3.rb
    z
    c

---
+++ rb_101_rb_119_small_problems/advanced_pass3/transpose_3_x_3.rb
@@ -0,0 +1,67 @@
+=begin
+Write a method that takes a 3 x 3 matrix in Array of 
+Arrays format and returns the transpose of the 
+original matrix. Note that there is a `Array#transpose` 
+method that does this -- you may not use it for this 
+exercise. You also are not allowed to use the `Matrix` 
+class from the standard library. Your task is to do 
+this yourself.
+
+Take care not to modify the original matrix: you must 
+produce a new matrix and leave the original matrix 
+unchanged.
+=end
+
+def transpose(matrix)
+  # Empty matrix
+  new_matrix = Array.new(matrix.first.size) { [] }
+
+  # Populate new matrix
+  new_matrix.each_with_index do |row, row_idx|
+    matrix.each_with_index do |_, column_idx| 
+      row << matrix[column_idx][row_idx]
+    end
+  end
+
+  new_matrix
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
+
+
+
+
+
+## Possible solution:
+def transpose(matrix)
+  result = []
+
+  (0..2).each do |column_index|
+    new_row = (0..2).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+
+  result
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+# All test cases return `true`.
